1ae073d1fe2814f7618a09742438e5b291736160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:53 +0000

    Add package pyz3r/4.1.1/pyz3r-4.1.1.tar.gz

---
+++ packages/pyz3r/pyz3r-4.1.1.tar.gz/pyz3r-4.1.1/pyz3r/__init__.py
@@ -0,0 +1,17 @@
+"""
+ALTTPR API Wrapper
+~~~~~~~~~~~~~~~~~~~
+A basic wrapper for the ALTTPR.com API.
+"""
+
+__title__ = 'pyz3r'
+__author__ = 'Thomas Prescott'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2018-2019 Thomas Prescott'
+__version__ = '4.1.1'
+
+from .alttpr import alttpr
+from .rom import romfile
+
+from .alttpr import alttpr as async_alttpr
+from .rom import romfile as async_romfile
