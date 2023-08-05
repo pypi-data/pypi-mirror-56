4d6e2fede49091c7a9e23a61a17d0bf9dd11418b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:18 +0000

    Add package aws-cdk.cx-api/1.17.1/aws-cdk.cx-api-1.17.1.tar.gz

---
+++ packages/aws-cdk.cx-api/aws-cdk.cx-api-1.17.1.tar.gz/aws-cdk.cx-api-1.17.1/setup.py
@@ -0,0 +1,50 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.cx-api",
+    "version": "1.17.1",
+    "description": "Cloud executable protocol",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.cx_api",
+        "aws_cdk.cx_api._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.cx_api._jsii": [
+            "cx-api@1.17.1.jsii.tgz"
+        ],
+        "aws_cdk.cx_api": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.7",
+        "publication>=0.0.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
