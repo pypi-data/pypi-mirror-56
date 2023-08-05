307ee35c530e86f6a7dee2e0af8384b0ee81c657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:29 +0000

    Add package pangeamt-tea/0.0.68/pangeamt-tea-0.0.68.tar.gz

---
+++ packages/pangeamt-tea/pangeamt-tea-0.0.68.tar.gz/pangeamt-tea-0.0.68/pangeamt_tea/console_scripts/tea_test.py
@@ -0,0 +1,35 @@
+import os
+import asyncclick as click
+import multiprocessing
+import os
+import asyncio
+
+from pangeamt_tea.project.workflow.stage.clean_stage import MultiprocessingCleaner
+
+@click.group(invoke_without_command=True)
+async def tea_test():
+    pass
+
+# New Project
+@tea_test.command()
+async def clean():
+    os.environ['PYTHONASYNCIODEBUG'] = "1"
+    data_generator_len = 100
+    data_generator = ((i, f'Hello {i}', f'Hola {i}') for i in range(data_generator_len))
+    max_workers = 2
+
+    multiprocessing_manager = multiprocessing.Manager()
+
+    multiprocessing_cleaner = MultiprocessingCleaner.new(
+        data_generator,
+        data_generator_len,
+        max_workers,
+        multiprocessing_manager)
+
+    await multiprocessing_cleaner.clean()
+
+
+    click.echo("xxxxxxxxx")
+
+def main():
+    tea_test(_anyio_backend="asyncio")  # or asyncio, or curio
