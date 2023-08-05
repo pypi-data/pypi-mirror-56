560060513a06328dd0ecc0f2457f20b7e39bbeed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:52 +0000

    Add package django-microframework/0.1.0/django_microframework-0.1.0-py3-none-any.whl

---
+++ packages/django-microframework/django_microframework-0.1.0-py3-none-any.whl/microframework/handler.py
@@ -0,0 +1,100 @@
+import json
+import logging
+from dateutil.parser import parse
+from django.contrib.contenttypes.models import ContentType
+from django.db import transaction
+from nameko.events import event_handler
+from .utils import create_model_name_list
+from .models import SyncData
+
+
+log = logging.getLogger(__name__)
+
+
+class HandlerException(Exception):
+    pass
+
+
+class DjangoObjectHandler:
+    synced_save_models = []
+
+    def get_field_by_name(self, name, model_meta_fields):
+        for field in model_meta_fields:
+            if field.name == name:
+                return field
+        return None
+
+    def verify_sync(self, sync_data):
+        sender_models_list = sync_data['models_list']
+        listener_models_list = create_model_name_list(self.synced_save_models)
+        if not set(sender_models_list) == set(listener_models_list):
+            log.warning(f"Sender and listener models list are not the same. "
+                        f"Sender:{str(sender_models_list)} Listener: {str(listener_models_list)}")
+
+    def object_saved_handler(self, payload, model):
+        data = json.loads(payload)
+        self.verify_sync(data["sync_data"])
+        fields = data["object_data"]["fields"]
+        fields_trimmed = {}
+
+        for field_name, field_value in fields.items():
+            field = self.get_field_by_name(field_name, model._meta.fields)
+            if field:
+                field_type = field.get_internal_type()
+                if field_type in ['ForeignKey', 'TreeForeignKey']:
+                    fields_trimmed[f'{field_name}_id'] = field_value
+                else:
+                    fields_trimmed[field_name] = field_value
+        with transaction.atomic():
+            sync_data, created = SyncData.objects.get_or_create(object_id=data["object_data"]["pk"],
+                                                                content_type=ContentType.objects.get_for_model(model),
+                                                                defaults={'date_modified': data["sync_data"]["date_modified"]})
+            if not created:
+                if sync_data.date_modified > parse(data["sync_data"]["date_modified"]):
+                    log.warning("Sync data mismatch. Incoming object is older than current object in database")
+                    return False
+            model.objects.update_or_create(id=data["object_data"]["pk"],
+                                           defaults=fields_trimmed)
+            sync_data.date_modified = data["sync_data"]["date_modified"]
+            sync_data.save()
+
+    def object_deleted_handler(self, payload, model):
+        data = json.loads(payload)
+        self.verify_sync(data["sync_data"])
+        obj = model.objects.get(id=data["object_data"]["pk"])
+        obj.delete()
+
+
+class NamekoHandlerMeta(type):
+    def __new__(mcs, name, bases, dct):
+        x = super().__new__(mcs, name, bases, dct)
+        try:
+            sender_name = dct['sender_name']
+        except KeyError:
+            raise HandlerException('Service class must define a `sender_name` attribute')
+        try:
+            synced_save_models = dct['synced_save_models']
+        except KeyError:
+            raise HandlerException('Service class must define a `synced_save_models` attribute')
+        for synced_save_model in synced_save_models:
+            saved_method_name = f'{synced_save_model.__name__}_saved'
+            deleted_method_name = f'{synced_save_model.__name__}_deleted'
+            setattr(x, saved_method_name, mcs.create_saved_nameko_handler(sender_name, synced_save_model))
+            setattr(x, deleted_method_name, mcs.create_deleted_nameko_handler(sender_name, synced_save_model))
+        return x
+
+    @classmethod
+    def create_saved_nameko_handler(mcs, sender_name, model):
+        @event_handler(sender_name, f'{model.__name__}_saved')
+        def handler(self, payload):
+            self.object_saved_handler(payload, model)
+            log.info(f'{model.__name__}_saved')
+        return handler
+
+    @classmethod
+    def create_deleted_nameko_handler(mcs, sender_name, model):
+        @event_handler(sender_name, f'{model.__name__}_deleted')
+        def handler(self, payload):
+            self.object_deleted_handler(payload, model)
+            log.info(f'{model.__name__}_deleted')
+        return handler
---
+++ packages/django-microframework/django_microframework-0.1.0-py3-none-any.whl/microframework/sender.py
@@ -0,0 +1,73 @@
+from decimal import Decimal
+from nameko.standalone.events import event_dispatcher
+from django.db.models.signals import post_save, post_delete
+import pytz
+from django.core import serializers
+import json
+import datetime
+import logging
+from django.conf import settings
+from .utils import create_model_name_list
+
+log = logging.getLogger(__name__)
+
+if not hasattr(settings, 'MICROFRAMEWORK_AMQP_URI'):
+    raise Exception('You need to define MICROFRAMEWORK_AMQP_URI in django settings')
+
+AMQP_CONFIG = {
+    'AMQP_URI': settings.MICROFRAMEWORK_AMQP_URI
+}
+
+dispatch = event_dispatcher(AMQP_CONFIG)
+
+
+class DateTimeDecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (datetime.datetime, datetime.date, datetime.time)):
+            return obj.isoformat()
+        elif isinstance(obj, datetime.timedelta):
+            return (datetime.datetime.min + obj).time().isoformat()
+        elif isinstance(obj, Decimal):
+            return str(obj)
+        return super(DateTimeDecimalEncoder, self).default(obj)
+
+
+encoder = DateTimeDecimalEncoder()
+
+
+def create_save_signal_handler(synced_save_model, sender_name, model_name_list):
+    def handler(sender, instance, created, **kwargs):
+        object_data = serializers.serialize("python", [instance, ])[0]
+        payload = {
+            'sync_data': {
+                'models_list': model_name_list,
+                'date_modified': datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+            },
+            'object_data': object_data
+        }
+        payload = encoder.encode(payload)
+        dispatch(sender_name, f'{synced_save_model.__name__}_saved', payload)
+    return handler
+
+
+def create_delete_signal_handler(synced_save_model, sender_name, model_name_list):
+    def handler(sender, instance, **kwargs):
+        object_data = serializers.serialize("python", [instance, ])[0]
+        payload = {
+            'sync_data': {
+                'models_list': model_name_list
+            },
+            'object_data': object_data
+        }
+        payload = encoder.encode(payload)
+        dispatch(sender_name, f'{synced_save_model.__name__}_deleted', payload)
+    return handler
+
+
+def connect_signals(models, sender_name):
+    model_name_list = create_model_name_list(models)
+    for synced_save_model in models:
+        save_signal_handler = create_save_signal_handler(synced_save_model, sender_name, model_name_list)
+        delete_signal_handler = create_delete_signal_handler(synced_save_model, sender_name, model_name_list)
+        post_save.connect(save_signal_handler, sender=synced_save_model, weak=False)
+        post_delete.connect(delete_signal_handler, sender=synced_save_model, weak=False)
