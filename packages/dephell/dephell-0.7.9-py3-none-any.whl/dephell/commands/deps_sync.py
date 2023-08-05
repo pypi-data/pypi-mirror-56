756c8b4f047c9e978b21ee1b4c01ed81051b12fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:17 +0000

    Add package dephell/0.7.9/dephell-0.7.9-py3-none-any.whl

---
+++ packages/dephell/dephell-0.7.9-py3-none-any.whl/dephell/commands/deps_sync.py
@@ -0,0 +1,26 @@
+# built-in
+from argparse import ArgumentParser
+
+# app
+from ..config import builders
+from .deps_install import DepsInstallCommand
+
+
+class DepsSyncCommand(DepsInstallCommand):
+    """Install project dependencies with removing all other packages from venv.
+    """
+    # DepsInstallCommand contains all logic for sync.
+    # There we just set `sync` flag and change some minor metainfo.
+    sync = True
+
+    @classmethod
+    def get_parser(cls) -> ArgumentParser:
+        parser = cls._get_default_parser()
+        builders.build_config(parser)
+        builders.build_to(parser)
+        builders.build_resolver(parser)
+        builders.build_api(parser)
+        builders.build_venv(parser)
+        builders.build_output(parser)
+        builders.build_other(parser)
+        return parser
---
+++ packages/dephell/dephell-0.7.9-py3-none-any.whl/dephell/converters/pyproject.py
@@ -0,0 +1,45 @@
+# built-in
+from pathlib import Path
+
+# external
+from dephell_discover import Root as PackageRoot
+from packaging.requirements import Requirement
+from tomlkit import document, dumps, parse
+
+# app
+from ..controllers import DependencyMaker
+from ..models import RootDependency
+from .base import BaseConverter
+
+
+class PyProjectConverter(BaseConverter):
+    lock = False
+
+    def loads(self, content: str) -> RootDependency:
+        doc = parse(content)
+        deps = []
+        root = RootDependency(
+            package=PackageRoot(path=self.project_path or Path()),
+        )
+        for req in doc['build-system']['requires']:
+            req = Requirement(req)
+            deps.extend(DependencyMaker.from_requirement(source=root, req=req))
+        root.attach_dependencies(deps)
+        return root
+
+    def dumps(self, reqs, project: RootDependency, content=None) -> str:
+        doc = document()
+        deps = []
+        for req in reqs:
+            deps.append(self._format_req(req=req))
+        doc['build-system']['requires'] = deps
+        return dumps(doc)
+
+    def _format_req(self, req):
+        line = req.raw_name
+        if req.extras:
+            line += '[{extras}]'.format(extras=','.join(req.extras))
+        line += req.version
+        if req.markers:
+            line += '; ' + req.markers
+        return line
