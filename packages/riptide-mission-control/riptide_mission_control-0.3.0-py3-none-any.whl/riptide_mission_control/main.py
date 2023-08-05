618b4e0192d46ea8ead3e4f373162d39ef5c8a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:51 +0000

    Add package riptide-mission-control/0.3.0/riptide_mission_control-0.3.0-py3-none-any.whl

---
+++ packages/riptide-mission-control/riptide_mission_control-0.3.0-py3-none-any.whl/riptide_mission_control/main.py
@@ -0,0 +1,12 @@
+import click
+
+
+@click.command()
+def main():
+    """
+    GraphQL API server for Riptide Projects.
+
+    See the documentation at https://riptide-docs.readthedocs.io/
+    """
+
+    raise Exception("This is currently just a place-holder package.")
