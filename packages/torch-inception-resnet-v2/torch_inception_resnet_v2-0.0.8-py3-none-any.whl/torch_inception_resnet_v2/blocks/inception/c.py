534453cac0f48c9697775c52b5e172ba2226d0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:23 +0000

    Add package torch-inception-resnet-v2/0.0.8/torch_inception_resnet_v2-0.0.8-py3-none-any.whl

---
+++ packages/torch-inception-resnet-v2/torch_inception_resnet_v2-0.0.8-py3-none-any.whl/torch_inception_resnet_v2/blocks/inception/c.py
@@ -0,0 +1,25 @@
+"""
+Responsible for implementing inception block C
+"""
+
+from torch import nn
+from torch_inception_resnet_v2.blocks.inception.inception_resnet import InceptionResNetBlock
+from torch_inception_resnet_v2.utils.branch import Branch
+from torch_inception_resnet_v2.utils import ConvolutionConfig as Convolution
+
+
+IN_CHANNELS = 1888
+SCALE = 0.17
+
+
+class InceptionC(InceptionResNetBlock):
+    def __init__(self):
+        left = Branch(IN_CHANNELS, Convolution(192, 1))
+        right = Branch(IN_CHANNELS, Convolution(192, 1), Convolution(224, (1, 3)), Convolution(256, (3, 1)))
+        join = nn.Conv2d(448, 2018, 1, bias=True)
+        super().__init__(1, join, left, right)
+
+
+if __name__ == '__main__':
+    block = InceptionC()
+    print(block)
