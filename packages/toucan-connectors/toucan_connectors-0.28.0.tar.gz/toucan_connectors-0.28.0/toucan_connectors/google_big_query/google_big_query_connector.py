9525d56c84ee99b7c89e566447ca381697e964be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:14 +0000

    Add package toucan-connectors/0.28.0/toucan_connectors-0.28.0.tar.gz

---
+++ packages/toucan-connectors/toucan_connectors-0.28.0.tar.gz/toucan_connectors-0.28.0/toucan_connectors/google_big_query/google_big_query_connector.py
@@ -0,0 +1,42 @@
+from enum import Enum
+from typing import List
+
+import pandas as pd
+import pandas_gbq
+
+from toucan_connectors.google_credentials import GoogleCredentials, get_google_oauth2_credentials
+from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
+
+
+class Dialect(str, Enum):
+    legacy = 'legacy'
+    standard = 'standard'
+
+
+class GoogleBigQueryDataSource(ToucanDataSource):
+    query: str
+
+
+class GoogleBigQueryConnector(ToucanConnector):
+    data_source_model: GoogleBigQueryDataSource
+
+    credentials: GoogleCredentials
+    dialect: Dialect = Dialect.legacy
+    scopes: List[str] = ["https://www.googleapis.com/auth/bigquery"]
+
+    def _retrieve_data(self, data_source: GoogleBigQueryDataSource) -> pd.DataFrame:
+        """
+        Uses Pandas read_gbq method to extract data from Big Query into a dataframe
+        See: http://pandas.pydata.org/pandas-docs/stable/generated/pandas.io.gbq.read_gbq.html
+        Note:
+            The parameter reauth is set to True to force Google BigQuery to reauthenticate the user
+            for each query. This is necessary when extracting multiple data to avoid the error:
+            [Errno 54] Connection reset by peer
+        """
+        credentials = get_google_oauth2_credentials(self.credentials).with_scopes(self.scopes)
+        return pandas_gbq.read_gbq(
+            query=data_source.query,
+            project_id=self.credentials.project_id,
+            credentials=credentials,
+            dialect=self.dialect,
+        )
