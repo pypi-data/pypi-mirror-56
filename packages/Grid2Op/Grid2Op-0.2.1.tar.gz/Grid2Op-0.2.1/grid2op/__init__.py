ee7e2aa60d6010f73f08ab5537fdc48b86f7d9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:38 +0000

    Add package Grid2Op/0.2.1/Grid2Op-0.2.1.tar.gz

---
+++ packages/Grid2Op/Grid2Op-0.2.1.tar.gz/Grid2Op-0.2.1/grid2op/__init__.py
@@ -0,0 +1,19 @@
+"""
+Grid2Op
+Document will be made later on.
+
+"""
+import os
+import pkg_resources
+
+__version__ = "0.2.1"
+
+__all__ = ['Action', "BackendPandaPower", "Agent", "Backend", "ChronicsHandler", "Environment", "Exceptions",
+           "Observation", "Parameters", "GameRules", "Reward", "Runner", "main"]
+
+from .MakeEnv import make
+from .MakeEnv import CASE_14_FILE, CHRONICS_FODLER, CHRONICS_MLUTIEPISODE, NAMES_CHRONICS_TO_BACKEND
+
+
+# TODO integrate Balthazar's code in the data generation process, to have a better environment.
+# At least for the case 14
---
+++ packages/Grid2Op/Grid2Op-0.2.1.tar.gz/Grid2Op-0.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+extras = {
+   'with_pygame': ['pygame']
+}
+
+setup(name='Grid2Op',
+      version='0.2.1',
+      description='An environment that allows to perform powergrid optimization.',
+      long_description='Built with modularity in mind, this package allows to perform the same operations independantly of the software used to compute powerflow or method to generate grid states or forecasts.',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+      ],
+      keywords='ML powergrid optmization RL',
+      author='Benjamin DONNOT',
+      author_email='benjamin.donnot@rte-france.com',
+      url="https://github.com/rte-france/Grid2Op",
+      license='MPL',
+      packages=['grid2op'],
+      include_package_data=False,
+      package_data={"": ["./data/chronics/*", "./data/test_multi_chronics/1/*", "./data/test_multi_chronics/2/*",
+                         "./data/test_multi_chronics/chronics/*", "./data/test_PandaPower/*"]},
+      install_requires=["numpy", "pandas", "pandapower"],
+      extras_require=extras,
+      zip_safe=False,
+      entry_points={'console_scripts': ['Grid2Op=grid2op.command_line:main']})
