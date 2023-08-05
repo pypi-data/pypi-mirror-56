f541cee9897cdf98151d67d8883bb2bc722e6be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:40 +0000

    Add package sendy/0.3.1/sendy-0.3.1.tar.gz

---
+++ packages/sendy/sendy-0.3.1.tar.gz/sendy-0.3.1/sendyapi/__init__.py
@@ -0,0 +1,4 @@
+from .api import Sendy, SendyException, SendyAlreadySubscribed, SendyNotSubscribed, SendyServerError
+
+
+__all__ = ['Sendy', 'SendyException', 'SendyAlreadySubscribed', 'SendyNotSubscribed', 'SendyServerError']
