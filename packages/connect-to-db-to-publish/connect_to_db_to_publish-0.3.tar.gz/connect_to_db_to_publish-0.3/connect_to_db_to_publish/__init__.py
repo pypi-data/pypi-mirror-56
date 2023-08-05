e298272dd8f82bd415649b9c701406fd8631a6ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:00 +0000

    Add package connect-to-db-to-publish/0.3/connect_to_db_to_publish-0.3.tar.gz

---
+++ packages/connect-to-db-to-publish/connect_to_db_to_publish-0.3.tar.gz/connect_to_db_to_publish-0.3/connect_to_db_to_publish/__init__.py
@@ -0,0 +1,12 @@
+from sshtunnel import SSHTunnelForwarder
+import pymongo
+import os.path
+from pymongo import MongoClient
+
+
+def connectToDB(connectionType):
+    
+    print(connectionType)
+
+def dispose():
+    print("Closing connection to DB")
