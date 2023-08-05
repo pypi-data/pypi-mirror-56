6c5450f84089fbabd8c894af5899c61bfa9d02e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:56 +0000

    Add package once-upon-a-time/0.0.1.dev2/once_upon_a_time-0.0.1.dev2-py2-none-any.whl

---
+++ packages/once-upon-a-time/once_upon_a_time-0.0.1.dev2-py2-none-any.whl/wide_wild_west/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__name__ = 'once_upon_a_time'
+__version__ = '0.0.1.dev2'
+__author__ = 'Xavier ROY'
+__author_email__ = 'xavier.roy@telecomnnancy.net'
+
+__import__('pkg_resources').declare_namespace(__name__)
