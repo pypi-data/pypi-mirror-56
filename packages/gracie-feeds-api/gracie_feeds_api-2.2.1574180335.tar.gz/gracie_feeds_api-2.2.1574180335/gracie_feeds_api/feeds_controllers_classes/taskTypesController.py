e3275b0fc3251fa096b29df99f52610d07f9d3f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:36 +0000

    Add package gracie-feeds-api/2.2.1574180335/gracie_feeds_api-2.2.1574180335.tar.gz

---
+++ packages/gracie-feeds-api/gracie_feeds_api-2.2.1574180335.tar.gz/gracie_feeds_api-2.2.1574180335/gracie_feeds_api/feeds_controllers_classes/taskTypesController.py
@@ -0,0 +1,34 @@
+from gracie_feeds_api import GracieBaseAPI
+
+
+class taskTypesController(GracieBaseAPI):
+    """Task Types Controller"""
+
+    _controller_name = "taskTypesController"
+
+    def list(self):
+        """"""
+
+        all_api_parameters = {}
+        parameters_names_map = {}
+        api = '/task-types/list'
+        actions = ['post']
+        params, data = self._format_params_for_api(locals(), all_api_parameters, parameters_names_map)
+        return self._process_api(self._controller_name, api, actions, params, data)
+
+    def retrieve(self, id):
+        """
+
+        Args:
+            id: (string): id
+
+        Returns:
+            application/json;charset=UTF-8
+        """
+
+        all_api_parameters = {'id': {'name': 'id', 'required': True, 'in': 'query'}}
+        parameters_names_map = {}
+        api = '/task-types/retrieve'
+        actions = ['post']
+        params, data = self._format_params_for_api(locals(), all_api_parameters, parameters_names_map)
+        return self._process_api(self._controller_name, api, actions, params, data)
