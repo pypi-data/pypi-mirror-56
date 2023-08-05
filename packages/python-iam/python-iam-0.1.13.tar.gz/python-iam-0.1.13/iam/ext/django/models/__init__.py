74dea66ec5213ed98d4b23d446189ac328457c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:21 +0000

    Add package python-iam/0.1.13/python-iam-0.1.13.tar.gz

---
+++ packages/python-iam/python-iam-0.1.13.tar.gz/python-iam-0.1.13/iam/ext/django/models/__init__.py
@@ -0,0 +1,4 @@
+from .principalgroups import PrincipalGroup
+from .resourceauthorizations import ResourceAuthorization
+from .subjects import Subject
+from .usersubjects import User
---
+++ packages/python-iam/python-iam-0.1.13.tar.gz/python-iam-0.1.13/iam/ext/django/models/subjectmanager.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import BaseUserManager
+from django.db import models
+
+
+class SubjectManager(BaseUserManager):
+
+    class queryset_class(models.QuerySet):
+
+        def disable(self):
+            """Disables all :class:`Subject` instances in the
+            :class:`~django.db.models.QuerySet`.
+            """
+            return self.update(disabled=True)
+
+        def enable(self):
+            """Enables all :class:`Subject` instances in the
+            :class:`~django.db.models.QuerySet`.
+            """
+            return self.update(disabled=False)
+
+    def get_queryset(self):
+        return self.queryset_class(self.model, using=self._db)
+
+    def disable(self, **params):
+        """Disables all :class:`Subject` entities filtered
+        by the given parameters.
+        """
+        return self.filter(**params).disable()
+
+    def enable(self, **params):
+        """Enable all :class:`Subject` entities filtered
+        by the given parameters.
+        """
+        return self.filter(**params).enable()
