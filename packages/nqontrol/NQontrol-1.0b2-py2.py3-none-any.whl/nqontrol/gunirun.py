e6e1998697cb5c691ba87cb0ac517b1c4deddbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:48 +0000

    Add package nqontrol/1.0b2/NQontrol-1.0b2-py2.py3-none-any.whl

---
+++ packages/nqontrol/NQontrol-1.0b2-py2.py3-none-any.whl/nqontrol/gunirun.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+
+
+def main():
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(script_path)
+    subprocess.run(['gunicorn', '-c', 'guniconfig.py.ini', 'run:server'])
+
+
+if __name__ == '__main__':
+    main()
