e2aec9f63ccd9cf6452c8f3cbc70e9ec289ff39c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:33 +0000

    Add package wix-media-platform/1.27.0/wix_media_platform-1.27.0.tar.gz

---
+++ packages/wix-media-platform/wix_media_platform-1.27.0.tar.gz/wix_media_platform-1.27.0/media_platform/job/import_file_job.py
@@ -0,0 +1,32 @@
+from media_platform.job.job_type import JobType
+from media_platform.job.specification import Specification
+from media_platform.service.destination import Destination
+from media_platform.job.job import Job
+
+
+class ImportFileSpecification(Specification):
+    def __init__(self, source_url, destination):
+        # type: (str, Destination) -> None
+        super(ImportFileSpecification, self).__init__()
+
+        self.source_url = source_url
+        self.destination = destination
+
+    def serialize(self):
+        # type: () -> dict
+        return {
+            'sourceUrl': self.source_url,
+            'destination': self.destination.serialize()
+        }
+
+    @classmethod
+    def deserialize(cls, data):
+        # type: (dict) -> ImportFileSpecification
+        destination = Destination.deserialize(data['destination'])
+
+        return ImportFileSpecification(data['sourceUrl'], destination)
+
+
+class ImportFileJob(Job):
+    type = JobType.import_file
+    specification_type = ImportFileSpecification
