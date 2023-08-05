40bf5fb1c66a5e2ff1fb41d285c332ba2cb4843a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package pylifttk/0.0.14/pylifttk-0.0.14.tar.gz

---
+++ packages/pylifttk/pylifttk-0.0.14.tar.gz/pylifttk-0.0.14/pylifttk/legacy/runscript_extensions.py
@@ -0,0 +1,73 @@
+
+import datetime as _datetime
+import glob as _glob
+import typing as _typing
+
+import dateutil.parser as _dateutil_parser
+
+import pylifttk
+import pylifttk.legacy
+
+
+# This is the path on the local file system
+LEGACY_EXTENSION_PATH = "/u/{course}/assignments/*/grading/duetime.extensions"
+
+
+def parse_extension_file(filepath):
+    # type: (str) -> dict
+    """
+
+    :param filepath:
+    :return:
+    """
+
+    try:
+        lines = map(str.strip, open(filepath).readlines())
+    except:
+        lines = []
+
+    # janettestu 2019-11-12 23:59
+
+    local_extensions = {}
+    for line in lines:
+        try:
+            (student, extended_due_date) = line.split(" ", 1)
+        except ValueError:
+            continue
+
+        local_extensions[student] = _dateutil_parser.parse(extended_due_date)
+
+    return local_extensions
+
+
+def parse_extensions(course=None):
+    # type: (str) -> _typing.Dict[str, _typing.Dict[str, _datetime.datetime]]
+    """
+
+    :param course:
+    :return:
+    """
+
+    if course is None:
+        try:
+            course = pylifttk.config["course"].get(str).lower()
+        except:
+            raise  # FIXME: ?
+        if course is None:
+            return {}
+
+    glob_path = LEGACY_EXTENSION_PATH.format(course=course)
+    extension_files = _glob.glob(glob_path)
+
+    glob_path_pieces = glob_path.split("/")
+    glob_path_index = glob_path_pieces.index("*")
+
+    extensions = {}
+    for filepath in extension_files:
+        assignment = filepath.split("/")[glob_path_index]
+        assignment_exts = parse_extension_file(filepath)
+
+        if assignment_exts is not None and len(assignment_exts) > 0:
+            extensions[assignment] = assignment_exts
+
+    return extensions
