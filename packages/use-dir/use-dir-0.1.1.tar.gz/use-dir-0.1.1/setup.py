0fbbe986db53f93f7262d82c491a624aee3d2d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:48 +0000

    Add package use-dir/0.1.1/use-dir-0.1.1.tar.gz

---
+++ packages/use-dir/use-dir-0.1.1.tar.gz/use-dir-0.1.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['use_dir']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'use-dir',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Mark Rawls',
+    'author_email': 'markrawls96@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
