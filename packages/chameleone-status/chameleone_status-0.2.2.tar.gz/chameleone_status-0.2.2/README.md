f3ca411bd0e998d53de84925bdb3983764ea7803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:18 +0000

    Add package chameleone-status/0.2.2/chameleone_status-0.2.2.tar.gz

---
+++ packages/chameleone-status/chameleone_status-0.2.2.tar.gz/chameleone_status-0.2.2/README.md
@@ -0,0 +1,2 @@
+This is a cameleon packaget to send mail. You can use
+call function message_send() to activate
---
+++ packages/chameleone-status/chameleone_status-0.2.2.tar.gz/chameleone_status-0.2.2/setup.py
@@ -0,0 +1,21 @@
+
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='chameleone_status',
+    version='0.2.2',
+    scripts=['chameleone_status.py'],
+    author="avinash",
+    author_email="avinashn686@gmail.com",
+    description="chameleone status sender",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+)
