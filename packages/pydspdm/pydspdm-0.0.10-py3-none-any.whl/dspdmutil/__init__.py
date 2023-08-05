8a4e777fd47487da504d719696de059566aaed82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:34 +0000

    Add package pydspdm/0.0.10/pydspdm-0.0.10-py3-none-any.whl

---
+++ packages/pydspdm/pydspdm-0.0.10-py3-none-any.whl/dspdmutil/__init__.py
@@ -0,0 +1,12 @@
+
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
+
+def filterWellEqual(well,filter,condition):
+    output_well = [x for x in well if (filter in x and x[filter] == condition )]
+    # count all well retrieved from API
+    print("total well count:" , len(well))
+    # count well which is actived
+    print("active well count:" , len(output_well))
+    return output_well
