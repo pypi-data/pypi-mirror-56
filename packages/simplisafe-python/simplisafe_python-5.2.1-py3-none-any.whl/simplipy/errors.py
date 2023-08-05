03f214d844c6f285eb77850bb6f0b4d339c96a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:51 +0000

    Add package simplisafe-python/5.2.1/simplisafe_python-5.2.1-py3-none-any.whl

---
+++ packages/simplisafe-python/simplisafe_python-5.2.1-py3-none-any.whl/simplipy/errors.py
@@ -0,0 +1,25 @@
+"""Define package errors."""
+
+
+class SimplipyError(Exception):
+    """Define a base error."""
+
+    pass
+
+
+class InvalidCredentialsError(SimplipyError):
+    """Define an error related to invalid requests."""
+
+    pass
+
+
+class PinError(SimplipyError):
+    """Define an error related to invalid PINs or PIN operations."""
+
+    pass
+
+
+class RequestError(SimplipyError):
+    """Define an error related to invalid requests."""
+
+    pass
