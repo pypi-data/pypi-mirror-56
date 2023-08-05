dfac7dd4058a31eb402570fe9fc124efb8c604d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:26 +0000

    Add package dcggraph/0.0.4/dcggraph-0.0.4.tar.gz

---
+++ packages/dcggraph/dcggraph-0.0.4.tar.gz/dcggraph-0.0.4/dcggraph/__init__.py
@@ -0,0 +1,137 @@
+"""
+This file contains the base classes for the dcggraph package.
+"""
+
+from typing import List, Text
+
+class DCGnode:
+    """A node in the DCG"""
+    def __init__(self, name):
+        self._name = name
+        self._edges_out = {}
+        self._edges_in = {}
+
+    def __repr__(self):
+        return f"DCGnode({self.name})"
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    @property
+    def name(self)->str:
+        """returns the node name"""
+        return self._name
+
+    @property
+    def root(self) -> bool:
+        """if this node has no edges-in"""
+        return len(self._edges_in) == 0
+
+    @property
+    def edges_in(self)->List["DCGedge"]:
+        """returns the list of edges pointing in to this node"""
+        return self._edges_in.values()
+
+    @property
+    def edges_out(self)->List["DCGedge"]:
+        """returns the list of edges pointing out of this node"""
+        return self._edges_out.values()
+
+    def add_edge_in(self, edge: "DCGedge") -> None:
+        """adds an inward-pointing edge"""
+        if (edge.name in self._edges_out) or (edge.name in self._edges_in):
+            raise Exception(f"{edge} is already connected within {self}")
+        if edge.to_node != self:
+            raise Exception(f"{edge} is not connected to {self}")
+        self._edges_in[edge.name] = edge
+
+    def add_edge_out(self, edge: "DCGedge") -> None:
+        """adds an outward-pointing edge"""
+        if (edge.name in self._edges_out) or (edge.name in self._edges_in):
+            raise Exception(f"{edge} is already connected to {self}")
+        if edge.from_node != self:
+            raise Exception(f"{edge} is not connected from {self}")
+        self._edges_out[edge.name] = edge
+
+class DCGedge:
+    """An non-weighted, directed edge in the DCG"""
+    def __init__(self, name: str, from_node: DCGnode,
+                 to_node: DCGnode) -> None:
+        self._name = name
+        self._from_node = from_node
+        self._to_node = to_node
+
+    def __repr__(self):
+        return f"DCGedge({self.from_node},{self.to_node})"
+
+    def __eq__(self, other):
+        return (self.from_node.name == other.from_node.name) and \
+               (self.to_node.name == other.to_node.name)
+
+    @property
+    def name(self)->Text:
+        """returns the name of this edge"""
+        return self._name
+
+    @property
+    def from_node(self)->DCGnode:
+        """returns the source node"""
+        return self._from_node
+
+    @property
+    def to_node(self)->DCGnode:
+        """returns the dest node"""
+        return self._to_node
+
+class DCGgraph:
+    """
+    A minimal-feature Directed-Cyclic-Graph used mainly for doing
+    predicated searches for all reachable nodes from a given node
+    """
+    def __init__(self):
+        self._nodes = {}
+        self._edges = {}
+
+    @property
+    def nodes(self)->List[DCGnode]:
+        """list of nodes in DCG"""
+        return self._nodes.values()
+
+    def get_node(self, name: str) -> DCGnode:
+        """return node by name"""
+        return self._nodes[name]
+
+    def create_node(self, name: str) -> DCGnode:
+        """create node from name, and returns it"""
+        if name in self._nodes:
+            raise Exception(f"node {self._nodes[name]} exists")
+        self._nodes[name] = DCGnode(name)
+        return self._nodes[name]
+
+    def add_node(self, node: DCGnode) -> None:
+        """adds an already-existing node to the graph"""
+        if node.name in self._nodes:
+            raise Exception(f"node {node} exists as {self._nodes[node.name]}")
+        self._nodes[node.name] = node
+
+    @property
+    def edges(self)->List[DCGedge]:
+        """returns list of edges in DCG"""
+        return self._edges.values()
+
+    def get_edge(self, name: str) -> DCGedge:
+        """return edge by name"""
+        return self._edges[name]
+
+    def create_edge(self, from_node: DCGnode, to_node: DCGnode) -> DCGedge:
+        """create edge from a node to a node"""
+        # pylint: disable=W0212
+        edges_out = from_node.edges_out
+        if len(list(filter(lambda x: x.to_node == to_node, edges_out))) > 0:
+            raise Exception(f"edge from {from_node} to {to_node} exists")
+        edge_name = f"edge_{len(self._edges.keys())}"
+        edge = DCGedge(name=edge_name, from_node=from_node, to_node=to_node)
+        self._edges[edge_name] = edge
+        from_node.add_edge_out(edge)
+        to_node.add_edge_in(edge)
+        return self._edges[edge_name]
---
+++ packages/dcggraph/dcggraph-0.0.4.tar.gz/dcggraph-0.0.4/test/test_compress.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python
+
+import unittest
+import re
+
+from dcggraph import DCGnode, DCGedge, DCGgraph
+from dcggraph.search import PredicateResult, DCGsearch
+
+class TestCompressNonCyclic(unittest.TestCase):
+
+    def setUp(self):
+        g = DCGgraph()
+        self.g = g
+
+        self.n0 = g.create_node("n0")
+        self.n1 = g.create_node("n1")
+        self.n2 = g.create_node("n2")
+        self.n3 = g.create_node("n3")
+        self.n4 = g.create_node("n4")
+        g.create_edge(self.n0, self.n1)
+        g.create_edge(self.n0, self.n2)
+        g.create_edge(self.n1, self.n3)
+        g.create_edge(self.n1, self.n4)
+
+        self.m0 = g.create_node("m0")
+        self.m1 = g.create_node("m1")
+        self.m2 = g.create_node("m2")
+        self.m3 = g.create_node("m3")
+        self.m4 = g.create_node("m4")
+        g.create_edge(self.m0, self.m1)
+        g.create_edge(self.m0, self.m2)
+        g.create_edge(self.m1, self.m3)
+        g.create_edge(self.m1, self.m4)
+
+    def test_no_predicate(self):
+        s = DCGsearch(self.g).compress()
+
+        # no compression
+        self.assertEqual(len(s.graph.nodes), 10)
+        self.assertEqual(len(s.graph.edges), 8)
+
+        self.assertEqual(len(s.search(self.n0)), 5)
+        self.assertEqual(len(s.search(self.n1)), 3)
+        self.assertEqual(len(s.search(self.n2)), 1)
+        self.assertEqual(len(s.search(self.n3)), 1)
+        self.assertEqual(len(s.search(self.n4)), 1)
+
+        self.assertEqual(len(s.search(self.m0)), 5)
+        self.assertEqual(len(s.search(self.m1)), 3)
+        self.assertEqual(len(s.search(self.m2)), 1)
+        self.assertEqual(len(s.search(self.m3)), 1)
+        self.assertEqual(len(s.search(self.m4)), 1)
+
+    def test_even_predicate(self):
+        """predicate always returns PASS/FAIL (never WAIT)"""
+        really_run = True
+        def even(node, visited, passed, failed):
+            if not really_run:
+                return PredicateResult.FAIL
+            elif re.match("[a-z][02468]", node.name):
+                return PredicateResult.PASS
+            else:
+                return PredicateResult.FAIL
+
+        s1 = DCGsearch(self.g, even).compress()
+
+        # did compression
+        self.assertEqual(len(s1.graph.nodes), 6)
+        self.assertEqual(len(s1.graph.edges), 4)
+
+        self.assertEqual(len(s1.search(self.n0)), 3)
+        self.assertEqual(len(s1.search(self.n2)), 1)
+        self.assertEqual(len(s1.search(self.n4)), 1)
+
+    def test_even_memo(self):
+        """show that memo is caching results as expected for PASS/FAIL"""
+        really_run = True
+        def odd(node, visited, passed, failed):
+            if not really_run:
+                return PredicateResult.FAIL
+            elif re.match("[a-z][13579]", node.name):
+                return PredicateResult.PASS
+            else:
+                return PredicateResult.FAIL
+
+        s1 = DCGsearch(self.g, odd).compress()
+
+        # did compression
+        self.assertEqual(len(s1.graph.nodes), 4)
+        self.assertEqual(len(s1.graph.edges), 2)
+
+        self.assertEqual(len(s1.search(self.n1)), 2)
+        self.assertEqual(len(s1.search(self.n3)), 1)
+
+        # now, show the memo cached results in the original 
+        really_run = False
+        s2 = DCGsearch(self.g, odd).compress()
+
+        # did compression
+        self.assertEqual(len(s2.graph.nodes), 0)
+        self.assertEqual(len(s2.graph.edges), 0)
+
+        self.assertEqual(len(s1.search(self.n1)), 2)
+        self.assertEqual(len(s1.search(self.n3)), 1)
+
+        really_run = True
+        self.assertEqual(len(s1.search(self.n1)), 2)
+        self.assertEqual(len(s1.search(self.n3)), 1)
+
+class TestCyclic(unittest.TestCase):
+
+    def setUp(self):
+        g = DCGgraph()
+        self.g = g
+
+        self.n0 = g.create_node("n0")
+        self.n1 = g.create_node("n1")
+        self.n2 = g.create_node("n2")
+        self.n3 = g.create_node("n3")
+        self.n4 = g.create_node("n4")
+        g.create_edge(self.n0, self.n1)
+        g.create_edge(self.n0, self.n2)
+        g.create_edge(self.n1, self.n3)
+        g.create_edge(self.n1, self.n4)
+
+        self.m0 = g.create_node("m0")
+        self.m1 = g.create_node("m1")
+        self.m2 = g.create_node("m2")
+        self.m3 = g.create_node("m3")
+        self.m4 = g.create_node("m4")
+        g.create_edge(self.m0, self.m1)
+        g.create_edge(self.m0, self.m2)
+        g.create_edge(self.m1, self.m3)
+        g.create_edge(self.m1, self.m4)
+
+        self.p0 = g.create_node("p0")
+
+        # connect two graphs
+        g.create_edge(self.n0, self.m0)
+        g.create_edge(self.m0, self.p0)
+        g.create_edge(self.p0, self.n0)
+
+    def test_no_predicate(self):
+        s = DCGsearch(self.g).compress()
+
+        # no compression
+        self.assertEqual(len(s.graph.nodes), 11)
+        self.assertEqual(len(s.graph.edges), 11)
+
+        self.assertEqual(len(s.search(self.n0)), 11)
+        self.assertEqual(len(s.search(self.n1)), 3)
+        self.assertEqual(len(s.search(self.n2)), 1)
+        self.assertEqual(len(s.search(self.n3)), 1)
+        self.assertEqual(len(s.search(self.n4)), 1)
+
+        self.assertEqual(len(s.search(self.m0)), 11)
+        self.assertEqual(len(s.search(self.m1)), 3)
+        self.assertEqual(len(s.search(self.m2)), 1)
+        self.assertEqual(len(s.search(self.m3)), 1)
+        self.assertEqual(len(s.search(self.m4)), 1)
+
+        self.assertEqual(len(s.search(self.p0)), 11)
+
+    def test_even_predicate(self):
+        """predicate always returns PASS/FAIL (never WAIT)"""
+        def even(node, visited, passed, failed):
+            if re.match("[a-z][02468]", node.name):
+                return PredicateResult.PASS
+            else:
+                return PredicateResult.FAIL
+
+        s1 = DCGsearch(self.g, even).compress()
+
+        # did compression
+        self.assertEqual(len(s1.graph.nodes), 7)
+        self.assertEqual(len(s1.graph.edges), 7)
+
+        self.assertEqual(len(s1.search(self.n0)), 7)
+        self.assertEqual(len(s1.search(self.n2)), 1)
+        self.assertEqual(len(s1.search(self.n4)), 1)
+
+        self.assertEqual(len(s1.search(self.m0)), 7)
+        self.assertEqual(len(s1.search(self.m2)), 1)
+        self.assertEqual(len(s1.search(self.m4)), 1)
+
+        self.assertEqual(len(s1.search(self.p0)), 7)
+
+    def test_wait_even_predicate(self):
+        """
+        even predicate returns WAIT UNTIL all subnodes resolved
+        """
+        def even(node, visited, passed, failed):
+            for edge in node.edges_out:
+                if edge.to_node.name not in visited:
+                    return PredicateResult.WAIT
+            if re.match("[a-z][02468]", node.name):
+                return PredicateResult.PASS
+            else:
+                return PredicateResult.FAIL
+
+        s1 = DCGsearch(self.g, even).compress()
+
+        # did compression
+        self.assertEqual(len(s1.graph.nodes), 7)
+        self.assertEqual(len(s1.graph.edges), 7)
+
+        self.assertEqual(len(s1.search(self.n0)), 7)
+        self.assertEqual(len(s1.search(self.n2)), 1)
+        self.assertEqual(len(s1.search(self.n4)), 1)
+
+        self.assertEqual(len(s1.search(self.m0)), 7)
+        self.assertEqual(len(s1.search(self.m2)), 1)
+        self.assertEqual(len(s1.search(self.m4)), 1)
+
+        self.assertEqual(len(s1.search(self.p0)), 7)
+
+    def test_unresolved_even_predicate(self):
+        """
+        even predicate returns WAIT if all subnodes PASS/FAIL. so
+        n0, m0, p0 remain in WAIT until no more to visit, and end up in FAIL
+        """ 
+        def even(node, visited, passed, failed):
+            for edge in node.edges_out:
+                to_name = edge.to_node.name
+                if (to_name not in passed) and (to_name not in failed):
+                    return PredicateResult.WAIT
+            if re.match("[a-z][02468]", node.name):
+                return PredicateResult.PASS
+            else:
+                return PredicateResult.FAIL
+
+        s1 = DCGsearch(self.g, even).compress()
+
+        # did compression
+        self.assertEqual(len(s1.graph.nodes), 4)
+        self.assertEqual(len(s1.graph.edges), 0)
+
+        self.assertEqual(len(s1.search(self.n2)), 1)
+        self.assertEqual(len(s1.search(self.n4)), 1)
+
+        self.assertEqual(len(s1.search(self.m2)), 1)
+        self.assertEqual(len(s1.search(self.m4)), 1)
+
+if __name__ == "__main__":
+    unittest.main()
+
