40d0ca925d6db2795849367be187b94e63577dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:07 +0000

    Add package dwys/0.0.2/dwys-0.0.2.tar.gz

---
+++ packages/dwys/dwys-0.0.2.tar.gz/dwys-0.0.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['dwys']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'dwys',
+    'version': '0.0.2',
+    'description': 'A language agnostic doctesting library',
+    'long_description': None,
+    'author': 'Vince Knight',
+    'author_email': 'vincent.knight@gmail.com',
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
