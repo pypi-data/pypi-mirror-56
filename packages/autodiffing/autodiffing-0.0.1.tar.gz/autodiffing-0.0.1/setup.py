16ae10375c34ab47867426eec37e6f5655e83425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:09 +0000

    Add package autodiffing/0.0.1/autodiffing-0.0.1.tar.gz

---
+++ packages/autodiffing/autodiffing-0.0.1.tar.gz/autodiffing-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autodiffing",
+    version="0.0.1",
+    author="Shuvom, Sijie, Silin, Edwin",
+    author_email="ssadhuka@college.harvard.edu, sijiesun@g.harvard.edu, szou@g.harvard.edu, chng_weimingedwin@g.harvard.edu",
+    description="A package for Automatic Differentiation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AutoDiffingFall2019/cs207-FinalProject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
