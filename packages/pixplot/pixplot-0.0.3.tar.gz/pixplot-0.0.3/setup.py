0080e557a07e8d93f2d7ec6aea2288eef33b0fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:27 +0000

    Add package pixplot/0.0.3/pixplot-0.0.3.tar.gz

---
+++ packages/pixplot/pixplot-0.0.3.tar.gz/pixplot-0.0.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+from os.path import join
+from pathlib import Path
+from glob2 import glob
+
+web = glob(join('pixplot', 'web', '**'))
+web = [join(*list(Path(i).parts[1:])) for i in web]
+
+setup (
+  name='pixplot',
+  version='0.0.3',
+  packages=['pixplot'],
+  package_data={
+    'pixplot': web,
+  },
+  keywords = ['computer-vision', 'webgl', 'three.js', 'tensorflow', 'machine-learning'],
+  description='Visualize large image collections with WebGL',
+  url='https://github.com/yaledhlab/pix-plot',
+  author='Douglas Duhaime',
+  author_email='douglas.duhaime@gmail.com',
+  license='MIT',
+  install_requires=[
+    'glob2>=0.6',
+    'Keras>=2.3.0',
+    'Pillow>=6.1.0',
+    'numpy>=1.16.0',
+    'scikit-image>=0.15.0',
+    'scikit-learn>=0.21.3',
+    'tensorflow>=1.14.0,<=2.0.0',
+    'umap-learn>=0.3.10',
+    'yale-dhlab-rasterfairy>=1.0.3',
+  ],
+  entry_points={
+    'console_scripts': [
+      'pixplot=pixplot:parse',
+    ],
+  },
+)
