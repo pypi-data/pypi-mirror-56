6718b9d766c3d14471095a7160c41191a80af735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:52 +0000

    Add package haxball.py/1.0.3/haxball.py-1.0.3.tar.gz

---
+++ packages/haxball.py/haxball.py-1.0.3.tar.gz/haxball.py-1.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='haxball.py',
+    author='Point#4171',
+    url='https://github.com/Pointsz/haxball.py',
+    version='1.0.3',
+    packages=find_packages(),
+    install_requires=['pyppeteer'],
+    description='Haxball web module.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    keywords=['haxball', 'haxball.py'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
