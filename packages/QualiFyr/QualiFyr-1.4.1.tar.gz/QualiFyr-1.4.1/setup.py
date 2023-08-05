1ab5ff9b1ae3dd7a93626f1230cb9f9bab35b7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:43 +0000

    Add package QualiFyr/1.4.1/QualiFyr-1.4.1.tar.gz

---
+++ packages/QualiFyr/QualiFyr-1.4.1.tar.gz/QualiFyr-1.4.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name='QualiFyr',
+    version='1.4.1',
+    description='Package to read in output files relating to quality and assess overall pass/fail.',
+    long_description='See https://gitlab.com/cgps/qualifyr for more details',
+    author='Anthony Underwood',
+    author_email='au3@sanger.ac.uk',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'qualifyr = qualifyr.run_qualifyr:main'
+        ]
+    },
+    install_requires=['pyyaml', 'colorlog', 'jinja2'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    classifiers=[ 
+        'Development Status :: 3 - Alpha', 
+        'Intended Audience :: Science/Research', 
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
