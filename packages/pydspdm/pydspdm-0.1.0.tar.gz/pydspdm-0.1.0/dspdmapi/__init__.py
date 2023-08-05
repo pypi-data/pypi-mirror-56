79eeaf245cec85b5c275cfd3c132b93ac60aba04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:37 +0000

    Add package pydspdm/0.1.0/pydspdm-0.1.0.tar.gz

---
+++ packages/pydspdm/pydspdm-0.1.0.tar.gz/pydspdm-0.1.0/dspdmapi/__init__.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
+
+def getData(boname,headers,data):
+    url = "http://10.134.192.116:8081/dspdm/rest/common/"
+    response = requests.request("POST", url,headers= headers, json=data)
+    return json.loads(response.text)["data"][boname]["list"]
