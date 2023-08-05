ea8b74b82c488eda2219cca9af1d580f782f7572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:02 +0000

    Add package modmailtranslation/1.1.0/modmailtranslation-1.1.0.tar.gz

---
+++ packages/modmailtranslation/modmailtranslation-1.1.0.tar.gz/modmailtranslation-1.1.0/modmailtranslation/__init__.py
@@ -0,0 +1,9 @@
+from .Translator import Translator
+from .errors.KeyNotFoundError import KeyNotFoundError
+
+__version__ = "v1.1.0"
+__author__ = "Piyush Bhangale"
+__github__ = "https://github.com/officialpiyush/modmailtranslation"
+__license__ = "GNU General Public License v3 (GPLv3)"
+__title__ = "modmailtranslation"
+__name__ = __title__
