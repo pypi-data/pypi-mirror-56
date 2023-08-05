29339ea3f5004d59d6ae8c27402a7e92d9b43967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:40 +0000

    Add package nrdash/0.2.1/nrdash-0.2.1-py3-none-any.whl

---
+++ packages/nrdash/nrdash-0.2.1-py3-none-any.whl/nrdash/main.py
@@ -0,0 +1,40 @@
+"""Main entry point for New Relic dashboard builder CLI tool."""
+import click
+
+from nrdash import new_relic_api, parsing
+
+
+@click.group()
+def main():
+    """Build New Relic dashboards."""
+
+
+@main.command()
+@click.argument("config-file", type=str, required=True)
+@click.option("--api-key", type=str, required=True, help="New Relic admin API key")
+@click.option("--account-id", type=int, required=True, help="New Relic account id")
+def build(config_file, api_key, account_id):
+    """Build New Relic dashboards based on YAML configuration."""
+    dashboards = parsing.parse_file(config_file)
+    client = new_relic_api.NewRelicApiClient(api_key, account_id)
+
+    for dashboard in dashboards.values():
+        dashboard_id = client.get_dashboard_id_by_title(dashboard.title)
+        if dashboard_id:
+            print(f"Updating {dashboard.name}")
+            client.update_dashboard(dashboard_id, dashboard)
+        else:
+            print(f"Creating {dashboard.name}")
+            client.create_dashboard(dashboard)
+
+
+@main.command()
+@click.argument("config-file", type=str, required=True)
+def lint(config_file):
+    """Lint New Relic dashboard YAML configuration."""
+    parsing.parse_file(config_file)
+    print(f"{config_file} is valid")
+
+
+if __name__ == "__main__":
+    main()
