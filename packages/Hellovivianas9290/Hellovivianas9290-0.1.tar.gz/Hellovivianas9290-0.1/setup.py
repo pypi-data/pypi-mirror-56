aeaf634b41397b2fd1ecfa61a6e5c57a5363229c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:02 +0000

    Add package Hellovivianas9290/0.1/Hellovivianas9290-0.1.tar.gz

---
+++ packages/Hellovivianas9290/Hellovivianas9290-0.1.tar.gz/Hellovivianas9290-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = "Hellovivianas9290",
+    version ="0.1",
+    scripts = ['cluster.py'],
+
+    data_files = [(',',['data1.csv'])],
+
+
+    install_requires = ['matplotlib','pandas'],
+
+    author = "Me",
+    description = "This is my project",
+    keywords = "clusters example",
+
+    classifiers=[
+        'License :: OSI Approved :: Python Software Foundation License'
+    ]
+)
