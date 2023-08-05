2a7b6edd277c5f9370dc4df5626ddab5ac41a9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:17 +0000

    Add package gch747/0.0.3/gch747-0.0.3.tar.gz

---
+++ packages/gch747/gch747-0.0.3.tar.gz/gch747-0.0.3/gch747/__init__.py
@@ -0,0 +1,6 @@
+from gch747.fct_normplot import *
+from gch747.fct_regress import *
+from gch747.fct_xlsread import *
+from gch747.fct_PCA_NIPALS import *
+from gch747.fct_PCA_SVD import *
+from gch747.fct_flip import *
