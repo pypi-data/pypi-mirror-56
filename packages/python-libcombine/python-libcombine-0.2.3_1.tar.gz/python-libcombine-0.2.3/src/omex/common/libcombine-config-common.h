b718eef773e04d7044b00dcdf0d4a594a84fa4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:30 +0000

    Add package python-libcombine/0.2.3/python-libcombine-0.2.3_1.tar.gz

---
+++ packages/python-libcombine/python-libcombine-0.2.3_1.tar.gz/python-libcombine-0.2.3/src/omex/common/libcombine-config-common.h
@@ -0,0 +1,39 @@
+/**
+ * @file    libcombine-config-common.h.cmake
+ * @brief   Configuration variables 
+ */
+
+/* Define to 1 if you have the <check.h> header file. */
+/* #undef HAVE_CHECK_H */
+
+/* Define to 1 if you have the <expat.h> header file. */
+/* #undef HAVE_EXPAT_H */
+
+/* Define to 1 to use the Expat XML library */
+/* #undef USE_EXPAT */
+
+
+/* Define to 1 if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1 
+
+/* Define to 1 if you have the <ieeefp.h> header file. */
+#define HAVE_IEEEFP_H 1
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if you have the <math.h> header file. */
+#define HAVE_MATH_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the `m' library (-lm). */
+/* #undef HAVE_LIBM */
+
+/* Define to 1 to enable primitive memory tracing. */
+/* #undef TRACE_MEMORY */
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
