44d1af2675e9236a28696125f1e093753794851e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:55 +0000

    Add package MCRLLM/0.0.1/MCRLLM-0.0.1.tar.gz

---
+++ packages/MCRLLM/MCRLLM-0.0.1.tar.gz/MCRLLM-0.0.1/MCRLLM/__init__.py
@@ -0,0 +1,6 @@
+from MCRLLM.fct_normplot import *
+from MCRLLM.fct_regress import *
+from MCRLLM.fct_xlsread import *
+from MCRLLM.fct_PCA_NIPALS import *
+
+# import bocode.fct_pls as pls
---
+++ packages/MCRLLM/MCRLLM-0.0.1.tar.gz/MCRLLM-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="MCRLLM",
+    version="0.0.1",
+    author="Ryan Gosselin",
+    author_email="ryan.gosselin@usherbrooke.ca",
+    packages=["MCRLLM"],
+    description="misc stuff",
+    long_description="misc stuff",
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
