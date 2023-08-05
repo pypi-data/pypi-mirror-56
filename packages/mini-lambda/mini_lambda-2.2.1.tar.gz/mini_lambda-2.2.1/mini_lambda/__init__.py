c9d3d01536d5cd79d7142f1cc96c1bb704c75d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:38 +0000

    Add package mini-lambda/2.2.1/mini_lambda-2.2.1.tar.gz

---
+++ packages/mini-lambda/mini_lambda-2.2.1.tar.gz/mini_lambda-2.2.1/mini_lambda/__init__.py
@@ -0,0 +1,29 @@
+from mini_lambda.base import FunctionDefinitionError, evaluate, get_repr
+
+# this one only exports one private class, no need
+# from mini_lambda.generated_magic import *
+
+from mini_lambda.main import _, L, F, C, Not, And, Or, Format, Get, In, Slice, InputVar, Constant, \
+    make_lambda_friendly, make_lambda_friendly_method, make_lambda_friendly_class, as_function, is_mini_lambda_expr
+
+__all__ = [
+    # submodules
+    'base', 'generated_magic_replacements', 'main', 'symbols', 'vars',  # generated_magic
+    # symbols
+    'FunctionDefinitionError', 'evaluate', 'get_repr',
+    '_', 'L', 'F', 'C', 'InputVar', 'Constant', 'make_lambda_friendly', 'make_lambda_friendly_method',
+    'make_lambda_friendly_class', 'as_function', 'is_mini_lambda_expr',
+    'Not', 'And', 'Or', 'Format', 'Get', 'In', 'Slice',
+]
+
+# for these two ones we can, there is a `__all__` inside
+from mini_lambda.vars import *
+from mini_lambda.vars import __all__ as vall
+
+from mini_lambda.generated_magic_replacements import *
+from mini_lambda.generated_magic_replacements import __all__ as gall
+
+from mini_lambda.symbols import *
+from mini_lambda.symbols import __all__ as sall
+
+__all__ = __all__ + vall + gall + sall
