c5f6f0ecc299ebef4bde15a4c724679a7fa0c604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:50 +0000

    Add package shirtctl/0.0/shirtctl-0.0.tar.gz

---
+++ packages/shirtctl/shirtctl-0.0.tar.gz/shirtctl-0.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'shirtctl',         
+  packages = ['shirtctl'],   
+  version = '0.0',      
+  license='MIT',        
+  description = 'The shirtctl Python SDK', 
+  author = 'Blair Hudson',                
+  author_email = 'blair@shirtctl.com',     
+  url = 'https://github.com/shirtctl/shirtctl-python-sdk',   
+  # download_url = '',    
+  keywords = ['shirtctl', 'Python', 'SDK'],   
+  install_requires=[],
+  classifiers=[]
+)
