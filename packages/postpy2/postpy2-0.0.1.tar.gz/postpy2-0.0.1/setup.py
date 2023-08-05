63e10a18e2d344adb85abfeeb7869f56a7256570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:27 +0000

    Add package postpy2/0.0.1/postpy2-0.0.1.tar.gz

---
+++ packages/postpy2/postpy2-0.0.1.tar.gz/postpy2-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='postpy2',
+    packages=['postpy2'],
+    version='0.0.1',
+    author='Martin Kapinos',
+    author_email='matkapi19@gmail.com',
+    description='A library to use postman collection V2 in python. Inspired by https://github.com/k3rn3l-p4n1c/postpython Bardia Heydari nejad',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/matkapi/postpy2',
+    download_url='https://codeload.github.com/matkapi/postpy2/zip/master',
+    keywords=['postman', 'rest', 'api'],  # arbitrary keywords
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
