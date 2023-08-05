849cc9a3d6b91b5ed3c2023477aaf221ed73ae4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:24 +0000

    Add package graph-distributions/0.1/graph_distributions-0.1.tar.gz

---
+++ packages/graph-distributions/graph_distributions-0.1.tar.gz/graph_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='graph_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['graph_distributions'],
+      author='Sreekanth Pulakkatt',
+      author_email='email@gmail.com',
+      zip_safe=False)
