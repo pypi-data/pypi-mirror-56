c8dcb5f9f4ca6212ff19bc8a44f2f1c6ab6cd998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:10 +0000

    Add package nonocaptcha-fork/2.1.0/nonocaptcha-fork-2.1.0.tar.gz

---
+++ packages/nonocaptcha-fork/nonocaptcha-fork-2.1.0.tar.gz/nonocaptcha-fork-2.1.0/nonocaptcha_fork/__init__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os.path
+import sys
+
+version_info = (2, 1, 0)
+__version__ = "{}.{}.{}".format(*version_info)
+
+
+authors = (
+    ("Michael Mooney", "mikeyy@mikeyy.com"),
+    ("Franc[e]sco", "lolisamurai@tfwno.gf")
+)
+
+authors_email = ", ".join("{}".format(email) for _, email in authors)
+
+__license__ = "GPL-3.0"
+__author__ = ", ".join(
+    "{} <{}>".format(name, email) for name, email in authors
+)
+
+package_info = (
+    "An asynchronized Python library to automate solving ReCAPTCHA v2 by audio"
+)
+__maintainer__ = __author__
+
+__all__ = (
+    "__author__",
+    "__author__",
+    "__license__",
+    "__maintainer__",
+    "__version__",
+    "version_info",
+    "package_dir",
+    "package_info",
+)
+
+sys.path.append(os.getcwd())
+package_dir = os.path.dirname(os.path.abspath(__file__))
