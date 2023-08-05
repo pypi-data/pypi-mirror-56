85ccb4f583fcd255f1077244459520e631ba1b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:18 +0000

    Add package pymagnum/1.1a3/pymagnum-1.1a3.tar.gz

---
+++ packages/pymagnum/pymagnum-1.1a3.tar.gz/pymagnum-1.1a3/README.rst
@@ -0,0 +1,20 @@
+pyMagnum
+========
+
+pyMagnum is a Python 3 implementation of a read only interface to the
+Magnum Energy network.
+
+`The Python package site <https://pypi.org/project/pymagnum/>`_
+
+This software is not endorsed or supported by Magnum Energy, a product
+of Sensata Technologies https://www.magnum-dimensions.com/
+
+Please read the documentation before installing this package.
+
+g`Documentation <https://pymagnum.readthedocs.io/>`_
+
+`Changes <https://github.com/CharlesGodwin/pymagnum/blob/master/CHANGES.rst>`_
+
+Copyright (c) 2018-2019 Charles Godwin magnum@godwin.ca
+
+SPDX-License-Identifier: BSD-3-Clause
---
+++ packages/pymagnum/pymagnum-1.1a3.tar.gz/pymagnum-1.1a3/setup.py
@@ -0,0 +1,33 @@
+#
+# Copyright (c) 2018-2019 Charles Godwin <magnum@godwin.ca>
+#
+# SPDX-License-Identifier:    BSD-3-Clause
+#
+# tutorial https://packaging.python.org/tutorials/packaging-projects/
+#
+from distutils.core import setup
+import setuptools
+import magnum
+
+with open("README.rst", "r") as file:
+    long_description = file.read()
+
+setup(name='pymagnum',
+      version=magnum.__version__,
+      description='Magnum Energy Network Interface (read-only)',
+      author='Charles Godwin',
+      author_email='magnum@godwin.ca',
+      long_description=long_description,
+      long_description_content_type="text/x-rst",
+      license="BSD",
+      url='https://github.com/CharlesGodwin/pymagnum',
+      packages=setuptools.find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3 :: Only",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent"
+      ],
+      install_requires=['pyserial'],
+      python_requires='>=3.5',
+      keywords='Magnum Energy Renewable Solar Network RS485 IoT'
+      )
