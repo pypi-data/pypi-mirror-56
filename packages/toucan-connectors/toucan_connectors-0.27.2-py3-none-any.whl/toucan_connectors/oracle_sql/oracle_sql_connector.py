7d5de86b29870604bef9000c5ac920906491fa09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:58 +0000

    Add package toucan-connectors/0.27.2/toucan_connectors-0.27.2-py3-none-any.whl

---
+++ packages/toucan-connectors/toucan_connectors-0.27.2-py3-none-any.whl/toucan_connectors/oracle_sql/oracle_sql_connector.py
@@ -0,0 +1,37 @@
+import cx_Oracle
+import pandas as pd
+from pydantic import DSN
+
+from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
+
+
+class OracleSQLDataSource(ToucanDataSource):
+    query: str
+
+
+class OracleSQLConnector(ToucanConnector):
+    data_source_model: OracleSQLDataSource
+
+    dsn: DSN
+    user: str = None
+    password: str = None
+    encoding: str = None
+
+    def get_connection_params(self):
+        con_params = {
+            'user': self.user,
+            'password': self.password,
+            'dsn': self.dsn,
+            'encoding': self.encoding,
+        }
+        return {k: v for k, v in con_params.items() if v is not None}
+
+    def _retrieve_data(self, data_source: OracleSQLDataSource) -> pd.DataFrame:
+        connection = cx_Oracle.connect(**self.get_connection_params())
+
+        query = data_source.query[:-1] if data_source.query.endswith(';') else data_source.query
+        df = pd.read_sql(query, con=connection)
+
+        connection.close()
+
+        return df
