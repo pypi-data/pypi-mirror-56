51ddaae5d0dfb9c1b0fd38c391073261efd65bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:46 +0000

    Add package addok-getbyid/0.0.1/addok_getbyid-0.0.1-py3-none-any.whl

---
+++ packages/addok-getbyid/addok_getbyid-0.0.1-py3-none-any.whl/addok_getbyid/__init__.py
@@ -0,0 +1,13 @@
+from addok import core
+
+class GetByIdView:
+
+    def on_get(self, req, resp, _id):
+        result = core.Result.from_id(_id)
+        print(result.format())
+        resp.status = 200
+        resp.body = json.dumps(result.format())
+        resp.content_type = 'application/json; charset=utf-8'
+
+def register_http_endpoint(api):
+  api.add_route('/id/{id}', GetByIdView())
