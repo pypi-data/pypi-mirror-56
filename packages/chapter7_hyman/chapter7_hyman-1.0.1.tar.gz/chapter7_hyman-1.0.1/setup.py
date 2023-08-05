da804d38294a8041f0771f7668f017c6d272f748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:13 +0000

    Add package chapter7_hyman/1.0.1/chapter7_hyman-1.0.1.tar.gz

---
+++ packages/chapter7_hyman/chapter7_hyman-1.0.1.tar.gz/chapter7_hyman-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name = 'chapter7_hyman',
+        version = '1.0.1',
+        py_modules = ['chapter7'],
+        author = 'hyman',
+        author_email = '578046099@qq.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'athlete web 开发',
+
+    )
