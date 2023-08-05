9f4cfcd3eb7821d61b74e845a11fcddeb2d336e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:58 +0000

    Add package toucan-connectors/0.27.2/toucan_connectors-0.27.2.tar.gz

---
+++ packages/toucan-connectors/toucan_connectors-0.27.2.tar.gz/toucan_connectors-0.27.2/toucan_connectors/snowflake/snowflake_connector.py
@@ -0,0 +1,57 @@
+from os import path
+
+import pandas as pd
+import snowflake.connector
+from pydantic.types import constr
+
+from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
+
+
+class Path(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not path.exists(v):
+            raise ValueError(f'path does not exists: v')
+        return v
+
+
+class SnowflakeDataSource(ToucanDataSource):
+    database: str = None
+    warehouse: str = None
+    query: constr(min_length=1)
+
+
+class SnowflakeConnector(ToucanConnector):
+    """
+    Import data from Snowflake data warehouse.
+    """
+
+    data_source_model: SnowflakeDataSource
+
+    user: str
+    password: str
+    account: str
+    ocsp_response_cache_filename: Path = None
+
+    def _retrieve_data(self, data_source: SnowflakeDataSource) -> pd.DataFrame:
+        connection = snowflake.connector.connect(
+            user=self.user,
+            password=self.password,
+            account=self.account,
+            database=data_source.database,
+            warehouse=data_source.warehouse,
+            ocsp_response_cache_filename=self.ocsp_response_cache_filename,
+        )
+
+        # https://docs.snowflake.net/manuals/sql-reference/sql/use-warehouse.html
+        connection.cursor().execute(f'USE WAREHOUSE {data_source.warehouse}')
+
+        df = pd.read_sql(data_source.query, con=connection)
+
+        connection.close()
+
+        return df
