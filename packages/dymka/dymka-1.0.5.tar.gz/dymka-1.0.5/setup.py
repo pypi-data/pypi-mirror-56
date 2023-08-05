2f615d12f793199f09edcc79ec896a01865c8d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:31 +0000

    Add package dymka/1.0.5/dymka-1.0.5.tar.gz

---
+++ packages/dymka/dymka-1.0.5.tar.gz/dymka-1.0.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('readme.md', 'r') as fh:
+    long_description = fh.read()
+
+    setuptools.setup(
+        name='dymka',
+        version='1.0.5',
+        author='Denis Glotov',
+        description='Swiss-knife cli for Ethereum-based blockchains',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/denisglotov/dymka',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'web3',
+        ],
+        scripts=['dymka'],
+        classifiers=[
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+        ],
+        python_requires='>=3.6',
+    )
