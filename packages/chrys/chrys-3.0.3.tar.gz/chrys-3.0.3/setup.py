420584eb387d45244999884284b448fd0fc135c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:49 +0000

    Add package chrys/3.0.3/chrys-3.0.3.tar.gz

---
+++ packages/chrys/chrys-3.0.3.tar.gz/chrys-3.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chrys",
+    version="3.0.3",
+    author="Hein Bekker",
+    author_email="hein@netbek.co.za",
+    description="A collection of color palettes for mapping and visualisation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='BSD-3-Clause',
+    url="https://github.com/netbek/chrys",
+    install_requires=[
+        'matplotlib >= 2.2.4',
+        'numpy >= 1.7.1',
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
