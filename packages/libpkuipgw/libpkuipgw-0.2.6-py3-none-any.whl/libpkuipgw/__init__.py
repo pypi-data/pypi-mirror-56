4cf559a539db9607aec406730a543c3296fe293a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:17 +0000

    Add package libpkuipgw/0.2.6/libpkuipgw-0.2.6-py3-none-any.whl

---
+++ packages/libpkuipgw/libpkuipgw-0.2.6-py3-none-any.whl/libpkuipgw/__init__.py
@@ -0,0 +1,6 @@
+name="libpkuipgw"
+# TODO: Understand how the hell Python packaging really works. I think it works like shit.
+from .libpkuipgw import IPGWClient
+from .libpkuipgw import IPGWError
+from .libpkuipgw import IPGWStatus
+
