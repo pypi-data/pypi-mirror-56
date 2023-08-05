279bff483f35bbe272274d92e8f8aab8bcef0a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:02 +0000

    Add package netsweeper/0.1.2/netsweeper-0.1.2.tar.gz

---
+++ packages/netsweeper/netsweeper-0.1.2.tar.gz/netsweeper-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='netsweeper',
+    version='0.1.2',
+    description='Python class to allow programmers to create easily your own net sweeper scripts.',
+    author='Alexsandro Camargo',
+    py_modules=['netsweeper'],
+    author_email='alexx.files@gmail.com',
+    download_url='https://github.com/alexx-files/netsweeper/archive/0.1.2.tar.gz',
+    packages=find_packages(include=['exampleproject', 'exampleproject.*']),
+    install_requires=[
+        'ping3'
+    ],
+    python_requires='>=3'
+)
+
