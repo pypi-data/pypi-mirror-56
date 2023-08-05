f8e90315991ba6175a0074c17d9fad0fcf7f6bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:15 +0000

    Add package toucan-connectors/0.27.1/toucan_connectors-0.27.1-py3-none-any.whl

---
+++ packages/toucan-connectors/toucan_connectors-0.27.1-py3-none-any.whl/tests/test_datasource.py
@@ -0,0 +1,112 @@
+from typing import Any
+
+import pytest
+from pydantic import ValidationError
+
+from toucan_connectors.toucan_connector import ToucanDataSource
+
+
+class DataSource(ToucanDataSource):
+    collection: str  # required, validated against type
+    query: Any  # required, not validated
+    comment: str = None  # not required, no default, validated against type when present
+    test_default: int = 101  # not required because it has a default, validated
+
+
+def test_instantiation():
+    # no errors with required args at the right type
+    data_source = {
+        'domain': 'my_domain',
+        'name': 'my_name',
+        'collection': 'my_collection',
+        'query': {},
+    }
+    mds = DataSource(**data_source)
+    assert mds.name == data_source['name']
+    assert mds.test_default == 101
+
+
+def test_required_arg():
+    # error with missing required arg
+    data_source = {'name': 'my_name', 'collection': 'my_collection', 'query': {}}
+    with pytest.raises(ValidationError) as e:
+        DataSource(**data_source)
+    assert 'domain' in e.value.errors()[0]['loc']  # Are we testing pydantic here ?
+    assert e.value.errors()[0]['msg'] == 'field required'
+
+
+def test_required_arg_wrong_type():
+    # error with required arg of wrong type
+    data_source = {'domain': [], 'name': 'my_name', 'collection': 'my_collection', 'query': {}}
+    with pytest.raises(ValidationError) as e:
+        DataSource(**data_source)
+    assert 'domain' in e.value.errors()[0]['loc']
+    assert e.value.errors()[0]['msg'] == 'str type expected'
+
+
+def test_not_required():
+    data_source = {
+        'domain': 'my_domain',
+        'name': 'my_name',
+        'collection': 'my_collection',
+        'query': {},
+        'comment': 'test',
+    }
+    mds = DataSource(**data_source)
+    assert mds.comment == 'test'
+
+
+def test_default_override():
+    data_source = {
+        'domain': 'my_domain',
+        'name': 'my_name',
+        'collection': 'my_collection',
+        'query': {},
+        'test_default': 102,
+    }
+    mds = DataSource(**data_source)
+    assert mds.test_default == 102
+
+
+def test_default_override_validated():
+    data_source = {
+        'domain': 'my_domain',
+        'name': 'my_name',
+        'collection': 'my_collection',
+        'query': {},
+        'test_default': {},
+    }
+    with pytest.raises(ValidationError):
+        DataSource(**data_source)
+
+
+def test_unknown_arg():
+    data_source = {
+        'domain': 'my_domain',
+        'name': 'my_name',
+        'collection': 'my_collection',
+        'query': {},
+        'unk': '@',
+    }
+    with pytest.raises(ValidationError) as e:
+        DataSource(**data_source)
+    assert 'unk' in e.value.errors()[0]['loc']
+    assert e.value.errors()[0]['msg'] == 'extra fields not permitted'
+
+
+def test_get_form():
+    default_form = ToucanDataSource.get_form(None, {})
+    assert default_form == {
+        'title': 'ToucanDataSource',
+        'type': 'object',
+        'properties': {
+            'domain': {'title': 'Domain', 'type': 'string'},
+            'name': {'title': 'Name', 'type': 'string'},
+            'type': {'title': 'Type', 'type': 'string'},
+            'load': {'title': 'Load', 'type': 'boolean', 'default': True},
+            'live_data': {'title': 'Live_Data', 'type': 'boolean', 'default': False},
+            'validation': {'title': 'Validation', 'type': 'array', 'items': {}},
+            'parameters': {'title': 'Parameters', 'type': 'object'},
+        },
+        'required': ['domain', 'name'],
+    }
---
+++ packages/toucan-connectors/toucan_connectors-0.27.1-py3-none-any.whl/toucan_connectors/google_spreadsheet/google_spreadsheet_connector.py
@@ -0,0 +1,45 @@
+from typing import List
+
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+from toucan_connectors.google_credentials import GoogleCredentials
+from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
+
+
+class GoogleSpreadsheetDataSource(ToucanDataSource):
+    spreadsheet_id: str
+    sheetname: str = None
+    skip_rows: int = 0
+
+
+class GoogleSpreadsheetConnector(ToucanConnector):
+    """
+    For authentication, download an authentication file from console.developper.com
+    and use the values here. This is an oauth2 credential file. For more information
+    see this: http://gspread.readthedocs.io/en/latest/oauth2.html
+    """
+
+    data_source_model: GoogleSpreadsheetDataSource
+
+    credentials: GoogleCredentials
+    scope: List[str] = [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://spreadsheets.google.com/feeds',
+    ]
+
+    def _retrieve_data(self, data_source):
+        credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+            self.credentials.dict(), self.scope
+        )
+        gc = gspread.authorize(credentials)
+
+        sheets = gc.open_by_key(data_source.spreadsheet_id)
+        sheetname = data_source.sheetname
+        sheet = sheets.sheet1 if sheetname is None else sheets.worksheet(sheetname)
+        starting_row = 1 + data_source.skip_rows
+        records = sheet.get_all_records(head=starting_row)
+        df = pd.DataFrame(records)
+        return df
