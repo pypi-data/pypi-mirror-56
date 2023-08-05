0a454d0724a6097b4d84225e2763b05952d46fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:28 +0000

    Add package tengu/0.1.12/tengu-0.1.12-py2.py3-none-any.whl

---
+++ packages/tengu/tengu-0.1.12-py2.py3-none-any.whl/tengu/views.py
@@ -0,0 +1,36 @@
+import json
+import socket
+from flask import Blueprint, request
+import boto3
+from .__init__ import app
+
+
+
+#obx = Blueprint('obx', __name__)
+
+def page_view_log_count():
+    client = boto3.client(
+            'cloudwatch',
+            aws_access_key_id=app.config['access_key'],
+            aws_secret_access_key=app.config['secret_key'],
+            aws_region=app.config['aws_region'],
+            )
+
+    response = client.put_metric_data(
+        Namespace='Application',
+        MetricData=[
+            {
+                'MetricName': 'ViewCount',
+                'Value': 1.0,
+                'Unit': 'Count',
+            },
+        ]
+    )
+
+    return response
+
+
+@app.route('/')
+def root_view():
+    page_view_log_count()
+    return 'Hello World ' + socket.gethostname()
