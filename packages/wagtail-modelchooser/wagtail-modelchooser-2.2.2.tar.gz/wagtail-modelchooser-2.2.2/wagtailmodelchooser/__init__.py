df44a6fdb6f9f9dccea35539caa7559ff942dcd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:00 +0000

    Add package wagtail-modelchooser/2.2.2/wagtail-modelchooser-2.2.2.tar.gz

---
+++ packages/wagtail-modelchooser/wagtail-modelchooser-2.2.2.tar.gz/wagtail-modelchooser-2.2.2/wagtailmodelchooser/__init__.py
@@ -0,0 +1,56 @@
+from .utils import kwarg_decorator, last_arg_decorator
+from .version import version as __version__
+from .version import version_info
+
+__all__ = [
+    '__version__', 'version_info', 'registry', 'register_model_chooser',
+    'register_simple_model_chooser', 'register_filter',
+]
+
+
+class Registry(object):
+    def __init__(self):
+        self.choosers = {}
+        self.filters = {}
+
+    def register_chooser(self, chooser, **kwargs):
+        """Adds a model chooser definition to the registry."""
+        if not issubclass(chooser, Chooser):
+            return self.register_simple_chooser(chooser, **kwargs)
+
+        self.choosers[chooser.model] = chooser(**kwargs)
+        return chooser
+
+    def register_simple_chooser(self, model, **kwargs):
+        """
+        Generates a model chooser definition from a model, and adds it to the
+        registry.
+        """
+        name = '{}Chooser'.format(model._meta.object_name)
+        attrs = {'model': model}
+        attrs.update(kwargs)
+
+        chooser = type(name, (Chooser,), attrs)
+        self.register_chooser(chooser)
+
+        return model
+
+    def register_filter(self, model, name, filter):
+        assert model in self.choosers
+        self.filters[(model, name)] = filter
+        return filter
+
+
+class Chooser(object):
+    model = None
+    icon = 'placeholder'
+
+    def get_queryset(self, request):
+        return self.model._default_manager.all()
+
+
+registry = Registry()
+
+register_model_chooser = kwarg_decorator(registry.register_chooser)
+register_simple_model_chooser = kwarg_decorator(registry.register_simple_chooser)
+register_filter = last_arg_decorator(registry.register_filter)
