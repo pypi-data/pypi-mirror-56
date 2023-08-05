f35869a972d9f5d820ee73b8692ea09dbd91fc97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:01 +0000

    Add package yapg/0.0.8/yapg-0.0.8.tar.gz

---
+++ packages/yapg/yapg-0.0.8.tar.gz/yapg-0.0.8/README.md
@@ -0,0 +1,14 @@
+# Yet Another Password Generator
+
+This is a password generator which aims to generate passwords that are:
+ * Readable
+ * Pronounceable
+ * Secure
+ * Long
+
+It uses [zxcvbn](https://github.com/dropbox/zxcvbn) to estimate the complexity of the passwords.
+
+This package is provided under the [GNU Affero General Public License](https://www.gnu.org/licenses/agpl-3.0.en.html).
+
+It is provided free of charge, and if you like it, feel free to buy me a beer.
+Or, as I'm not currently disclosing my contact informations, feel free to *Wish that I stumble upon a beer*.
---
+++ packages/yapg/yapg-0.0.8.tar.gz/yapg-0.0.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yapg",
+    version="0.0.8",
+    author="Alexandre Janvrin",
+    author_email="alexandre.janvrin@reseau.eseo.fr",
+    description="Yet Another Password Generator - A simple password generator checking password complexity with zxcvbn",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/yapg/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: POSIX :: Linux",
+    ],
+    install_requires=[
+        'zxcvbn',
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+)
