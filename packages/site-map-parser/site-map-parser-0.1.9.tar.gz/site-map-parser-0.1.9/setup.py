6245d42568bca86aa5c2a59c49fd6984881de5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:43 +0000

    Add package site-map-parser/0.1.9/site-map-parser-0.1.9.tar.gz

---
+++ packages/site-map-parser/site-map-parser-0.1.9.tar.gz/site-map-parser-0.1.9/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+from os import sys
+import pypandoc
+
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+long_description = pypandoc.convert_file('README.md', 'rst')
+long_description = long_description.replace("\r", "")
+
+setuptools.setup(
+    name="site-map-parser",
+    version="0.1.9",
+    author="Dave O'Connor",
+    author_email="github@dead-pixels.org",
+    description="Script/Library to read and parse sitemap.xml data",
+    long_description=long_description,
+    url="https://github.com/daveoconnor/site-map-parser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'smapper = smapper:main',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        '': ['*.ini'],
+    },
+    python_requires='>=3.5',
+    tests_require=["pytest"],
+    setup_requires=[] + pytest_runner,
+
+)
