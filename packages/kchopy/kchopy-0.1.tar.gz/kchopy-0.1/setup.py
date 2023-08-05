271fece5541046b0111f110c6e3eda8239129171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:51 +0000

    Add package kchopy/0.1/kchopy-0.1.tar.gz

---
+++ packages/kchopy/kchopy-0.1.tar.gz/kchopy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kchopy',
+      version='0.1',
+      description='Tools for neuroimaging pipelines by kcho',
+      url='http://github.com/kcho/kchopy',
+      author='Kevin Kang Ik Cho',
+      author_email='kevincho@bwh.harvard.edu',
+      license='MIT',
+      packages=['kchopy'],
+      zip_safe=False)
