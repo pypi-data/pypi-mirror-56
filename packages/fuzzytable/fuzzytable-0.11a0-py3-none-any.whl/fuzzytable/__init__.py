e6b3363061eaaa65da087c591e90f5724b4bab05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:40 +0000

    Add package fuzzytable/0.11a0/fuzzytable-0.11a0-py3-none-any.whl

---
+++ packages/fuzzytable/fuzzytable-0.11a0-py3-none-any.whl/fuzzytable/__init__.py
@@ -0,0 +1,8 @@
+"""
+Read tables from messy spreadsheets.
+"""
+from fuzzytable.main.fuzzytable import FuzzyTable
+from fuzzytable.patterns.fieldpattern import FieldPattern
+
+
+__version__ = "0.11-alpha"
