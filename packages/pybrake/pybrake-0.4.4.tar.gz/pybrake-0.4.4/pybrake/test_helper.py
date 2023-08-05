04c588cfcff669d3a07717ee3a35e0d64ffced88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:42 +0000

    Add package pybrake/0.4.4/pybrake-0.4.4.tar.gz

---
+++ packages/pybrake/pybrake-0.4.4.tar.gz/pybrake-0.4.4/pybrake/test_helper.py
@@ -0,0 +1,53 @@
+import logging
+
+
+def get_exception():
+    try:
+        raise ValueError("hello")
+    except ValueError as err:
+        return err
+
+
+def build_notice_from_str(notifier, s):
+    return notifier.build_notice(s)
+
+
+def build_logging_record_error(*args, **kwargs):
+    logger, dh = logger_dummy_handler()
+    logger.error(*args, **kwargs)
+    return dh.record
+
+
+def build_logging_record_exception():
+    logger, dh = logger_dummy_handler()
+    try:
+        raise ValueError("hello")
+    except ValueError as err:
+        logger.exception(err)
+        return dh.record
+
+
+def logger_dummy_handler():
+    class DummyHandler(logging.Handler):
+        def __init__(self):
+            super().__init__()
+            self.record = None
+
+        def emit(self, record):
+            self.record = record
+
+    logger = logging.getLogger("test")
+    dh = DummyHandler()
+    logger.addHandler(dh)
+
+    return logger, dh
+
+
+def get_nested_exception():
+    try:
+        raise get_exception()
+    except ValueError as err:
+        try:
+            raise ValueError("world") from err
+        except ValueError as err:
+            return err
