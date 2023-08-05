7346a7d6c862a956a382655fffb282d04d50ce53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:23 +0000

    Add package odoo10-addon-pos-product-template/10.0.2.1.0/odoo10_addon_pos_product_template-10.0.2.1.0-py2-none-any.whl

---
+++ packages/odoo10-addon-pos-product-template/odoo10_addon_pos_product_template-10.0.2.1.0-py2-none-any.whl/odoo/addons/pos_product_template/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'POS - Product Template',
+    'version': '10.0.2.1.0',
+    'category': 'Point Of Sale',
+    'author': "Akretion,Odoo Community Association (OCA)",
+    'summary': 'Manage Product Template in Front End Point Of Sale',
+    'website': 'http://www.akretion.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'point_of_sale',
+        'pos_fix_search_limit',
+    ],
+    'data': [
+        'view/view.xml',
+    ],
+    'qweb': [
+        'static/src/xml/ppt.xml',
+    ],
+    'demo': [
+        'demo/product_attribute_value.yml',
+        'demo/product_product.yml',
+        'demo/res_groups.yml',
+    ],
+    'images': [
+        'static/src/img/screenshots/pos_product_template.png',
+    ],
+    'installable': True,
+}
