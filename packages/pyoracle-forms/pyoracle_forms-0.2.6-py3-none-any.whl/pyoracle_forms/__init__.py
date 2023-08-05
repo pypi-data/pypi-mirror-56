faa2fec9ace293a7364edd068d47cb6dfc410521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:17 +0000

    Add package pyoracle-forms/0.2.6/pyoracle_forms-0.2.6-py3-none-any.whl

---
+++ packages/pyoracle-forms/pyoracle_forms-0.2.6-py3-none-any.whl/pyoracle_forms/__init__.py
@@ -0,0 +1,34 @@
+from .context import context
+from .error_handling import FormsException
+from .forms_api import read_api_objects
+from .forms_objects import Alert
+from .forms_objects import AttachedLibrary
+from .forms_objects import Canvas
+from .forms_objects import DataBlock
+from .forms_objects import FormParameter
+from .forms_objects import GenericObject
+from .forms_objects import Graphic
+from .forms_objects import Item
+from .forms_objects import Module
+from .forms_objects import Point
+from .forms_objects import ProgramUnit
+from .forms_objects import PropertyClass
+from .forms_objects import RadioButton
+from .forms_objects import Relation
+from .forms_objects import TabPage
+from .forms_objects import Trigger
+from .forms_objects import VisualAttribute
+from .forms_objects import Window
+from .misc import add_properties
+from .misc import registered_objects
+
+__version__ = "0.2.6"
+
+
+def initialize_context(version: str = "12c", encoding: str = "utf-8") -> None:
+    context.init(version=version, encoding=encoding)
+
+    api_objects = read_api_objects(version=version)
+
+    for forms_object in registered_objects.values():
+        add_properties(forms_object, api_objects)
