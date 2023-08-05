342a8884453194f94b834e5ecfb2c414e1e2d9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:43 +0000

    Add package site-map-parser/0.1.9/site_map_parser-0.1.9-py3-none-any.whl

---
+++ packages/site-map-parser/site_map_parser-0.1.9-py3-none-any.whl/sitemapparser/sitemap.py
@@ -0,0 +1,12 @@
+class Sitemap:
+    def __init__(self, loc, lastmod=None):
+        """
+        Representation of the <sitemap> element
+        :param loc: String of the url
+        :param lastmod: DateTime, The date of last modification of the file.
+        """
+        self.loc = loc
+        self.lastmod = lastmod
+
+    def __str__(self):
+        return self.loc
