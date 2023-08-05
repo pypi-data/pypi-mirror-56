ff2d242a88b3f2db408004e09ad646ba8fbb1459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:15 +0000

    Add package autoplot/0.3.4/autoplot-0.3.4.tar.gz

---
+++ packages/autoplot/autoplot-0.3.4.tar.gz/autoplot-0.3.4/autoplot/__init__.py
@@ -0,0 +1,6 @@
+# Allow "from autoplot import javaaddpath"
+from autoplot.autoplot import javaaddpath
+from autoplot.autoplot import applot
+from autoplot.autoplot import to_qdataset
+from autoplot.autoplot import to_ndarray
+
