ab98afc8ff4a9e83cee65e7a8e7a2dc7ec741d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:03 +0000

    Add package ocds-babel/0.2.1/ocds-babel-0.2.1.tar.gz

---
+++ packages/ocds-babel/ocds-babel-0.2.1.tar.gz/ocds-babel-0.2.1/README.rst
@@ -0,0 +1,14 @@
+|PyPI Version| |Build Status| |Coverage Status| |Python Version|
+
+This Python package provides `Babel extractors <http://babel.pocoo.org/en/latest/messages.html>`__ and translation methods for standards that use JSON Schema and CSV codelists with `custom JSON Schema properties <https://os4d.opendataservices.coop/development/schema/#extended-json-schema>`__, like the Open Contracting Data Standard (OCDS) or Beneficial Ownership Data Standard (BODS).
+
+If you are viewing this on GitHub or PyPi, open the `full documentation <https://ocds-babel.readthedocs.io/>`__ for additional details.
+
+.. |PyPI Version| image:: https://img.shields.io/pypi/v/ocds-babel.svg
+   :target: https://pypi.org/project/ocds-babel/
+.. |Build Status| image:: https://secure.travis-ci.org/open-contracting/ocds-babel.png
+   :target: https://travis-ci.org/open-contracting/ocds-babel
+.. |Coverage Status| image:: https://coveralls.io/repos/github/open-contracting/ocds-babel/badge.png?branch=master
+   :target: https://coveralls.io/github/open-contracting/ocds-babel?branch=master
+.. |Python Version| image:: https://img.shields.io/pypi/pyversions/ocds-babel.svg
+   :target: https://pypi.org/project/ocds-babel/
---
+++ packages/ocds-babel/ocds-babel-0.2.1.tar.gz/ocds-babel-0.2.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='ocds-babel',
+    version='0.2.1',
+    author='Open Contracting Partnership',
+    author_email='data@open-contracting.org',
+    url='https://github.com/open-contracting/ocds-babel',
+    description='Provides Babel extractors and translation methods for standards like OCDS or BODS',
+    license='BSD',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    long_description=long_description,
+    extras_require={
+        'markdown': [
+            'docutils',
+            # See docs/api/translate.rst
+            # 'recommonmark',
+            'Sphinx>=1.6',
+        ],
+        'test': [
+            'coveralls',
+            'pytest',
+            'pytest-cov',
+        ],
+        'docs': [
+            'Sphinx',
+            'sphinx-autobuild',
+            'sphinx_rtd_theme',
+        ],
+    },
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points={
+        'babel.extractors': [
+            'ocds_codelist = ocds_babel.extract:extract_codelist',
+            'ocds_schema = ocds_babel.extract:extract_schema',
+        ],
+    },
+)
