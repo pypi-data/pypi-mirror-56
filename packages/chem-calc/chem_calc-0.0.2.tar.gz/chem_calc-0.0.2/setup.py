f9fca151fe7dcf84c7e5b285470e4792f46383d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:07 +0000

    Add package chem-calc/0.0.2/chem_calc-0.0.2.tar.gz

---
+++ packages/chem-calc/chem_calc-0.0.2.tar.gz/chem_calc-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding = 'utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chem_calc", # Replace with your own username
+    version="0.0.2",
+    author="Joy Garnett",
+    author_email="garne041@gmail.com",
+    description="A package to calculate various stoichometric features of compounds.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/garne041/chem_calc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.2',
+)
