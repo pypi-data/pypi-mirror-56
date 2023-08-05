5f7d7aaa87b5aad6fe02f2a4aab07459f2106548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:08 +0000

    Add package besc/2.0.1/besc-2.0.1.tar.gz

---
+++ packages/besc/besc-2.0.1.tar.gz/besc-2.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='besc',
+    version='2.0.1',
+    packages=find_packages(),
+    license='MIT License',
+    description='Send data to the ESS API BESC',
+    download_url='https://github.com/rock98rock/BESC-API/archive/1.0.tar.gz',
+    install_requires=['query_string'],
+    author='Clive Lim'
+)
