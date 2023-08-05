736f2c5b8a740a6a1a4f59f30c78a9b9a22ee80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:02 +0000

    Add package freecall/0.2.0/freecall-0.2.0.tar.gz

---
+++ packages/freecall/freecall-0.2.0.tar.gz/freecall-0.2.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name="freecall",
+        version="0.2.0",
+        author="Zenith00",
+        author_email="Zenith00dev@gmail.com",
+        description="Function memoization",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/Zenith00/freecall",
+        packages=setuptools.find_packages(),
+        classifiers=[
+                "Programming Language :: Python :: 3",
+        ],
+        install_requires=["dill"],
+)
