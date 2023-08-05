d3fdb276344c41bfe80caada3d1e90536ed203ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:35 +0000

    Add package pyobs-core/0.8/pyobs-core-0.8.tar.gz

---
+++ packages/pyobs-core/pyobs-core-0.8.tar.gz/pyobs-core-0.8/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name='pyobs-core',
+    version='0.8',
+    description='robotic telescope software',
+    author='Tim-Oliver Husser',
+    author_email='thusser@uni-goettingen.de',
+    packages=find_packages(include=['pyobs', 'pyobs.*']),
+    scripts=[
+        'bin/pyobs',
+        'bin/pyobsd'
+    ],
+    install_requires=[
+        'sep',
+        'photutils',
+        'scipy',
+        'pyinotify',
+        'paramiko',
+        'pandas',
+        'matplotlib',
+        'pytz',
+        'astropy',
+        'astroplan',
+        'Pillow',
+        'PyYAML',
+        'numpy',
+        'lmfit',
+        'aplpy',
+        'tornado',
+        'sleekxmpp',
+        'py_expression_eval',
+        'colour',
+        'requests',
+        'python-daemon;platform_system=="Linux"'
+    ]
+)
