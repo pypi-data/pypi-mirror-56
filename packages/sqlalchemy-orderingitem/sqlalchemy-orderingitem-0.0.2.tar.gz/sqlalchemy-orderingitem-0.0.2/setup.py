9a7aaee17a2c7dc77f37c1c9d42726735dc66d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:01 +0000

    Add package sqlalchemy-orderingitem/0.0.2/sqlalchemy-orderingitem-0.0.2.tar.gz

---
+++ packages/sqlalchemy-orderingitem/sqlalchemy-orderingitem-0.0.2.tar.gz/sqlalchemy-orderingitem-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sqlalchemy-orderingitem",
+    version="0.0.2",
+    author="Dillon Bowen",
+    author_email="dsbowen@wharton.upenn.edu",
+    description="SQLAlchemy-OrderingItem provides an OrderingItem base for children of orderinglist relationships.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dsbowen/sqlalchemy-orderingitem",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
