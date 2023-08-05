1c6c5f4bb2b99b89bf0125906ea4da6cb7a690ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:05 +0000

    Add package ipyhc/0.0.0a4/ipyhc-0.0.0a4.tar.gz

---
+++ packages/ipyhc/ipyhc-0.0.0a4.tar.gz/ipyhc-0.0.0a4/README.md
@@ -0,0 +1,13 @@
+# ipyhc
+
+Plugin to use Highcharts in Jupyter notebooks.
+
+### Documentation
+
+Documentation is available on the website [https://ipyhc.gitlab.io/](https://ipyhc.gitlab.io/).
+
+### Credits
+
+This plugin is the result of a collaboration between
+[Louis Raison](https://gitlab.com/DGothrek),
+[Pierre Tholoniat](https://gitlab.com/tholoz) and [oscar6echo](https://gitlab.com/oscar6echo).
---
+++ packages/ipyhc/ipyhc-0.0.0a4.tar.gz/ipyhc-0.0.0a4/ipyhc/_config.py
@@ -0,0 +1,15 @@
+import json
+
+SAMPLES_DIR = 'samples'
+
+DF_ONE_IDX_SEVERAL_COL = 'df_one_idx_several_col.csv'
+DF_ONE_IDX_SEVERAL_COL_2 = 'df_one_idx_several_col_2.csv'
+DF_ONE_IDX_ONE_COL = 'df_one_idx_one_col.csv'
+DF_ONE_IDX_TWO_COL = 'df_one_idx_two_col.csv'
+DF_TWO_IDX_ONE_COL = 'df_two_idx_one_col.csv'
+DF_SCATTER = 'df_scatter.csv'
+DF_BUBBLE = 'df_bubble.csv'
+DF_HEATMAP = 'df_heatmap.csv'
+DF_SEVERAL_IDX_ONE_COL = 'df_several_idx_one_col.csv'
+DF_SEVERAL_IDX_ONE_COL_2 = 'df_several_idx_one_col_2.csv'
+DF_TWO_IDX_SEVERAL_COL = 'df_two_idx_several_col.csv'
