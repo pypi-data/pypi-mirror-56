e76fb9ce5c37559cb93618dfbc2146a49af6b9d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:57 +0000

    Add package papyru/0.0.1.dev33/papyru-0.0.1.dev33.tar.gz

---
+++ packages/papyru/papyru-0.0.1.dev33.tar.gz/papyru-0.0.1.dev33/papyru/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from tests.test_logging import *  # noqa
+from tests.test_middleware import *  # noqa
+from tests.test_stack_context import *  # noqa
+from tests.test_xml_helper import *  # noqa
+
+unittest.main()
