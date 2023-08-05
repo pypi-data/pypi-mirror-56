e949aa441cfe0521b3556a0ca346ad15d6487da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:43 +0000

    Add package tnmk-ensae/1.0/tnmk_ensae-1.0.tar.gz

---
+++ packages/tnmk-ensae/tnmk_ensae-1.0.tar.gz/tnmk_ensae-1.0/README.md
@@ -0,0 +1,6 @@
+﻿# README
+
+
+### Presentation du package
+
+Le package tnmk_ensae est un module graphique de visualisation (animee ou non) utilisant principalement une surcouche du package matplotlib, a savoir cartopy. Il contient deux fonctions qui visualisent l evolution temporelle d une donnee numerique geolocalisee, de deux facons distinctes.
---
+++ packages/tnmk-ensae/tnmk_ensae-1.0.tar.gz/tnmk_ensae-1.0/setup.py
@@ -0,0 +1,44 @@
+﻿# --  preambule ------------------------------------------------------------
+import os
+from distutils.core import setup
+from setuptools import find_packages	
+	
+# -- settings & data -------------------------------------------------------
+
+project_var_name = "tnmk_ensae"
+readme = 'README.md'	
+
+KEYWORDS = project_var_name + ', TABSOBA_NGARBAYE_MBIA_KONDO'
+DESCRIPTION = """Un module graphique pour la visualisation"""
+CLASSIFIERS = ['Programming Language :: Python :: 3',
+	       'Topic :: Scientific/Engineering :: Visualization',
+	       'Topic :: Software Development :: Libraries :: Python Modules',
+	       'Topic :: Education',
+	       'License :: OSI Approved :: MIT License'
+			   ]
+
+here = os.path.dirname(__file__)
+packages = find_packages()	
+package_dir = {k: os.path.join(here, k.replace(".", "/")) for k in packages}	   
+
+
+# -- common part ------------------------------------------------------------
+
+with open(readme, "r") as fh:
+    descriptif_long = fh.read()
+
+setup(
+    name=project_var_name,
+    version="1.0",
+    author="TABSOBA_NGARBAYE_MBIA_KONDO",
+    author_email='ngarbaye.54@gmail.com',
+    license="MIT",
+    description=DESCRIPTION,
+    long_description=descriptif_long,
+    long_description_content_type="text/markdown",
+    packages=packages,
+    package_dir=package_dir,
+    classifiers=CLASSIFIERS,
+    python_requires='>=3.7')
+
+# -- end file ---------------------------------------------------------------
