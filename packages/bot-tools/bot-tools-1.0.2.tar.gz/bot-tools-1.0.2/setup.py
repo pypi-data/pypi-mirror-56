e5b0cd4c6ea1877b33574c109c1ae2ce1e3faa6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:09 +0000

    Add package bot-tools/1.0.2/bot-tools-1.0.2.tar.gz

---
+++ packages/bot-tools/bot-tools-1.0.2.tar.gz/bot-tools-1.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from bot_tools import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bot-tools",
+    version=__version__,
+    author="Thor",
+    author_email="theonlythor@protonmail.com",
+    description="Common tools for python bots",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/onlythor/bot_tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ],
+    python_requires='>=3.6'
+)
