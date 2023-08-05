8f6958cc41308ef37c87351a44e8ef4743d65d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:30 +0000

    Add package manta-maastricht/1.2.1.2/manta_maastricht-1.2.1.2.zip

---
+++ packages/manta-maastricht/manta_maastricht-1.2.1.2.zip/manta_maastricht-1.2.1.2/setup.py
@@ -0,0 +1,31 @@
+import sys
+import os
+from setuptools import setup, find_packages
+
+setup(
+    # Module name (lowercase)
+    name='manta_maastricht',
+    version='1.2.1.2',
+    description='manta_maastricht',
+    url='http://www.jordiheijman.net/',
+
+    # Packages to exclude
+    packages=find_packages(),
+    zip_safe=False,
+
+    # List of dependencies
+    install_requires=[
+        'myokit',
+        #'PyQt5',
+    ],
+
+    # Register as a shell script
+    entry_points = {
+        'console_scripts': [
+            'manta = manta_maastricht.__main__:main',
+        ],
+    },
+
+    # Include non-python files (via MANIFEST.in)
+    include_package_data=True,
+)
