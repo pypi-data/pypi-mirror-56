3d70efa50d468936e81c727d2a4c29f0a735df8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:23 +0000

    Add package DDmaker/0.0.1/DDmaker-0.0.1-py3-none-any.whl

---
+++ packages/DDmaker/DDmaker-0.0.1-py3-none-any.whl/DDmaker/client.py
@@ -0,0 +1,9 @@
+class Client(object):
+    def left(self,step):
+        print("向左转")
+    def right(self,step):
+        print("向右转")
+    def forward(self,step):
+        print("齐步走")
+    def backward(self,step):
+        print("向后退")
