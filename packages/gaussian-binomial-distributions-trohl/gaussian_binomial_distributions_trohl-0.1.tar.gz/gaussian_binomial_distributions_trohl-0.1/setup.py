25bc53d4a8ce1219ae6aa23cf6fb70b8a5a903f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:32 +0000

    Add package gaussian-binomial-distributions-trohl/0.1/gaussian_binomial_distributions_trohl-0.1.tar.gz

---
+++ packages/gaussian-binomial-distributions-trohl/gaussian_binomial_distributions_trohl-0.1.tar.gz/gaussian_binomial_distributions_trohl-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_binomial_distributions_trohl',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['gaussian_binomial_distributions_trohl'],
+      author = 'Thorsten Rohling',
+      author_email = 'thorstenrohling@aol.com',
+      zip_safe=False)
