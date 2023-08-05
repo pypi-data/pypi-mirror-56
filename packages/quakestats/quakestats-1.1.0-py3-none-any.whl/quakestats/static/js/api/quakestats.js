d268a9ed8960e6629abb9e2827d686630980ceba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:20 +0000

    Add package quakestats/1.1.0/quakestats-1.1.0-py3-none-any.whl

---
+++ packages/quakestats/quakestats-1.1.0-py3-none-any.whl/quakestats/static/js/api/quakestats.js
@@ -0,0 +1,113 @@
+class QuakeStatsApi {
+    constructor() {
+        this.endpoint = '/api/v2'
+    }
+
+    get(path) {
+        var req = new Request(`${this.endpoint}/${path}`)
+        return fetch(req).then(function(response) {
+            return response.json().then(function(json) {
+                return json
+            })
+        })
+    }
+
+    getMatches() {
+        return this.get('match')
+    }
+
+    getMatch(matchId) {
+        return this.get(`match/${matchId}/metadata`)
+    }
+
+    getMatchPlayers(matchId) {
+        return this.get(`match/${matchId}/players`)
+    }
+
+    getMatchScores(matchId) {
+        return this.get(`match/${matchId}/score`)
+    }
+
+    getMatchTeams(matchId) {
+        return this.get(`match/${matchId}/teams`)
+    }
+
+    getMatchSpecial(matchId) {
+        return this.get(`match/${matchId}/special`)
+    }
+
+    getMatchKills(matchId) {
+        return this.get(`match/${matchId}/kill`)
+    }
+
+    getMatchBadges(matchId) {
+        return this.get(`match/${matchId}/badge`)
+    }
+
+    getMatchPlayerStats(matchId) {
+        return this.get(`match/${matchId}/player_stats`)
+    }
+
+    getBoardBadges(lastNMatches) {
+        if (lastNMatches) {
+            return this.get(`board/badges?latest=${lastNMatches}`)
+        } else {
+            return this.get(`board/badges`)
+        }
+    }
+
+    getAllPlayers() {
+        return this.get(`players`)
+    }
+
+    getTotalStats(lastNMatches) {
+        if (lastNMatches) {
+            return this.get(`board/total?latest=${lastNMatches}`)
+        } else {
+            return this.get(`board/total`)
+        }
+    }
+
+    getMaps() {
+        return this.get(`maps`)
+    }
+
+    setMapInfo(map_name, info) {
+        var headers = new Headers()
+        headers.set('Content-type', 'application/json')
+        var req = new Request(
+            `${this.endpoint}/map/size`, {
+                method: 'POST',
+                credentials: 'include',
+                headers: headers,
+                body: JSON.stringify({
+                    map_name: map_name,
+                    size: info.size,
+                    rate: info.rate
+                })
+            })
+        fetch(req)
+    }
+
+    getPlayerKills(playerId) {
+        return this.get(`player/${playerId}/kills`)
+    }
+
+    getPlayerDeaths(playerId) {
+        return this.get(`player/${playerId}/deaths`)
+    }
+    getPlayerDeaths(playerId) {
+        return this.get(`player/${playerId}/deaths`)
+    }
+    getPlayerBadges(playerId) {
+        return this.get(`player/${playerId}/badges`)
+    }
+
+    getPlayer(playerId) {
+        return this.get(`player/${playerId}`)
+    }
+
+    getPresenceList(matchCount) {
+        return this.get(`presence/${matchCount}`)
+    }
+}
