bacdcdf7d133c582ef52329af5bf858dec16147f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:57 +0000

    Add package stagesepx/0.8.4/stagesepx-0.8.4.tar.gz

---
+++ packages/stagesepx/stagesepx-0.8.4.tar.gz/stagesepx-0.8.4/stagesepx/classifier/base.py
@@ -0,0 +1,252 @@
+import pathlib
+import typing
+import cv2
+import numpy as np
+from loguru import logger
+
+from stagesepx.cutter import VideoCutRange
+from stagesepx import toolbox
+from stagesepx import constants
+from stagesepx.hook import BaseHook, GreyHook, CompressHook
+from stagesepx.video import VideoObject, VideoFrame
+
+
+class SingleClassifierResult(object):
+    def __init__(self, video_path: str, frame_id: int, timestamp: float, stage: str):
+        self.video_path = video_path
+        self.frame_id = frame_id
+        self.timestamp = timestamp
+        self.stage = stage
+
+    def to_dict(self) -> typing.Dict:
+        return self.__dict__
+
+    def __str__(self):
+        return f"<ClassifierResult stage={self.stage} frame_id={self.frame_id} timestamp={self.timestamp}>"
+
+    __repr__ = __str__
+
+
+class ClassifierResult(object):
+    def __init__(self, data: typing.List[SingleClassifierResult]):
+        self.video_path = data[0].video_path
+        self.data = data
+
+    def get_timestamp_list(self) -> typing.List[float]:
+        return [each.timestamp for each in self.data]
+
+    def get_stage_list(self) -> typing.List[str]:
+        return [each.stage for each in self.data]
+
+    def get_stage_set(self) -> typing.Set[str]:
+        return set(self.get_stage_list())
+
+    def get_offset(self) -> float:
+        # timestamp offset between frames
+        return self.data[1].timestamp - self.data[0].timestamp
+
+    def get_specific_stage(
+        self, stage_name: str
+    ) -> typing.List[SingleClassifierResult]:
+        """ get specific stage range by stage name """
+        return sorted(
+            [i for i in self.data if i.stage == stage_name], key=lambda x: x.frame_id
+        )
+
+    def get_length(self) -> int:
+        return len(self.data)
+
+    def calc_changing_cost(
+        self
+    ) -> typing.Dict[str, typing.Tuple[SingleClassifierResult, SingleClassifierResult]]:
+        """ calc time cost between stages """
+        # add changing cost
+        cost_dict: typing.Dict[
+            str, typing.Tuple[SingleClassifierResult, SingleClassifierResult]
+        ] = {}
+        i = 0
+        while i < len(self.data) - 1:
+            cur = self.data[i]
+            next_one = self.data[i + 1]
+
+            # next one is changing
+            if next_one.stage == constants.UNSTABLE_FLAG:
+                for j in range(i + 1, len(self.data)):
+                    i = j
+                    next_one = self.data[j]
+                    if next_one.stage != constants.UNSTABLE_FLAG:
+                        break
+
+                changing_name = f"{cur.stage} - {next_one.stage}"
+                cost_dict[changing_name] = (cur, next_one)
+            else:
+                i += 1
+        return cost_dict
+
+
+class BaseClassifier(object):
+    def __init__(
+        self,
+        compress_rate: float = None,
+        target_size: typing.Tuple[int, int] = None,
+        *args,
+        **kwargs,
+    ):
+        # default compress rate is 0.2
+        if (not compress_rate) and (not target_size):
+            logger.debug(
+                f"no compress rate or target size received. set compress rate to 0.2"
+            )
+            compress_rate = 0.2
+
+        self.compress_rate = compress_rate
+        self.target_size = target_size
+        logger.debug(f"compress rate: {self.compress_rate}")
+        logger.debug(f"target size: {self.target_size}")
+
+        self._data: typing.Dict[str, typing.Union[typing.List[pathlib.Path]]] = dict()
+
+        # init inner hooks
+        self._hook_list: typing.List[BaseHook] = list()
+        compress_hook = CompressHook(
+            overwrite=True, compress_rate=compress_rate, target_size=target_size
+        )
+        grey_hook = GreyHook(overwrite=True)
+        self.add_hook(compress_hook)
+        self.add_hook(grey_hook)
+
+    def add_hook(self, new_hook: BaseHook):
+        """
+        add a hook
+
+        :param new_hook:
+        :return:
+        """
+        self._hook_list.append(new_hook)
+        logger.debug(f"add hook: {new_hook.__class__.__name__}")
+
+    def load(
+        self, data: typing.Union[str, typing.List[VideoCutRange], None], *args, **kwargs
+    ):
+        """
+        at most of time, you MUST load data (from cutter) before classification
+        otherwise you need a trained model
+
+        :param data: path to your cutter's result (mainly from pick_and_save)
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        if isinstance(data, str):
+            return self.load_from_dir(data, *args, **kwargs)
+        if isinstance(data, list):
+            return self.load_from_list(data, *args, **kwargs)
+        raise TypeError(f"data type error, should be str or typing.List[VideoCutRange]")
+
+    def load_from_list(
+        self, data: typing.List[VideoCutRange], frame_count: int = None, *_, **__
+    ):
+        for stage_name, stage_data in enumerate(data):
+            target_frame_list = stage_data.pick(frame_count)
+            self._data[str(stage_name)] = target_frame_list
+
+    def load_from_dir(self, dir_path: str, *_, **__):
+        p = pathlib.Path(dir_path)
+        stage_dir_list = p.iterdir()
+        for each in stage_dir_list:
+            # load dir only
+            if each.is_file():
+                continue
+            stage_name = each.name
+            stage_pic_list = [i.absolute() for i in each.iterdir()]
+            self._data[stage_name] = stage_pic_list
+            logger.debug(
+                f"stage [{stage_name}] found, and got {len(stage_pic_list)} pics"
+            )
+
+    def read(self, *args, **kwargs):
+        for stage_name, stage_data in self._data.items():
+            if isinstance(stage_data[0], pathlib.Path):
+                yield stage_name, self.read_from_path(stage_data, *args, **kwargs)
+            else:
+                raise TypeError(
+                    f"data type error, should be str or typing.List[VideoCutRange]"
+                )
+
+    @staticmethod
+    def read_from_path(data: typing.List[pathlib.Path], *_, **__):
+        return (toolbox.imread(each.as_posix()) for each in data)
+
+    def read_from_list(
+        self, data: typing.List[int], video_cap: cv2.VideoCapture = None, *_, **__
+    ):
+        raise DeprecationWarning("this function already deprecated")
+
+    def _classify_frame(self, frame: VideoFrame, *args, **kwargs) -> str:
+        """ must be implemented by sub class """
+
+    def _apply_hook(
+        self, frame_id: int, frame: np.ndarray, *args, **kwargs
+    ) -> np.ndarray:
+        for each_hook in self._hook_list:
+            frame = each_hook.do(frame_id, frame, *args, **kwargs)
+        return frame
+
+    def classify(
+        self,
+        video: typing.Union[str, VideoObject],
+        limit_range: typing.List[VideoCutRange] = None,
+        step: int = None,
+        *args,
+        **kwargs,
+    ) -> ClassifierResult:
+        """
+        start classification
+
+        :param video: path to target video or VideoObject
+        :param limit_range: frames in these range will be ignored
+        :param step: step between frames, default to 1
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        logger.debug(f"classify with {self.__class__.__name__}")
+
+        if not step:
+            step = 1
+
+        final_result: typing.List[SingleClassifierResult] = list()
+        if isinstance(video, str):
+            video = VideoObject(video)
+
+        operator = video.get_operator()
+        frame = operator.get_frame_by_id(1)
+        while frame is not None:
+            if limit_range:
+                if not any([each.contain(frame.frame_id) for each in limit_range]):
+                    logger.debug(
+                        f"frame {frame.frame_id} ({frame.timestamp}) not in target range, skip"
+                    )
+                    final_result.append(
+                        SingleClassifierResult(
+                            video.path, frame.frame_id, frame.timestamp, "-1"
+                        )
+                    )
+                    frame = operator.get_frame_by_id(frame.frame_id + step)
+                    continue
+
+            # hook
+            frame.data = self._apply_hook(frame.frame_id, frame.data, *args, **kwargs)
+
+            result = self._classify_frame(frame, *args, **kwargs)
+            logger.debug(
+                f"frame {frame.frame_id} ({frame.timestamp}) belongs to {result}"
+            )
+
+            final_result.append(
+                SingleClassifierResult(
+                    video.path, frame.frame_id, frame.timestamp, result
+                )
+            )
+            frame = operator.get_frame_by_id(frame.frame_id + step)
+        return ClassifierResult(final_result)
---
+++ packages/stagesepx/stagesepx-0.8.4.tar.gz/stagesepx-0.8.4/stagesepx/reporter.py
@@ -0,0 +1,252 @@
+import os
+import typing
+import json
+import numpy as np
+from jinja2 import Markup, Template
+from pyecharts.charts import Line, Bar, Page
+from pyecharts import options as opts
+from loguru import logger
+
+from stagesepx.classifier import ClassifierResult, SingleClassifierResult
+from stagesepx import toolbox
+from stagesepx import constants
+from stagesepx.cutter import VideoCutResult
+from stagesepx import __VERSION__
+
+# load template
+template_dir_path = os.path.join(os.path.dirname(__file__), "template")
+template_path = os.path.join(template_dir_path, "report.html")
+template_zh_path = os.path.join(template_dir_path, "report_zh.html")
+
+
+def get_template(lang: str) -> str:
+    lang_dict = {"en": template_path, "zh": template_zh_path}
+    assert lang in lang_dict, f"template {lang} not found"
+    with open(lang_dict[lang], encoding=constants.CHARSET) as t:
+        template = t.read()
+    return template
+
+
+class Reporter(object):
+    def __init__(self):
+        self.dir_link_list: typing.List[str] = list()
+        self.thumbnail_list: typing.List[typing.Tuple[str, str]] = list()
+        self.extra_dict: typing.Dict[str, str] = dict()
+
+    def add_dir_link(self, data_path: str):
+        """
+        add relative dir (or file) link to your report
+
+        :param data_path:
+        :return:
+        """
+        self.dir_link_list.append(data_path)
+
+    def add_thumbnail(self, name: str, pic_object: np.ndarray):
+        """
+        add picture object (cv2) to your report
+
+        :param name:
+        :param pic_object:
+        :return:
+        """
+        b64_str = toolbox.np2b64str(pic_object)
+        self.thumbnail_list.append((name, b64_str))
+
+    def add_extra(self, name: str, value: str):
+        """
+        add some extra info ( key-value part) to your report
+
+        :param name:
+        :param value:
+        :return:
+        """
+        self.extra_dict[name] = value
+
+    @staticmethod
+    def _draw_line(result: ClassifierResult) -> Line:
+        # draw line chart
+        x_axis = [str(i) for i in result.get_timestamp_list()]
+        y_axis = result.get_stage_list()
+
+        line = Line(init_opts=opts.InitOpts(bg_color=constants.BACKGROUND_COLOR))
+        line.add_xaxis(x_axis)
+        line.add_yaxis("stage", y_axis, is_step=False, is_symbol_show=True)
+        line.set_global_opts(
+            title_opts=opts.TitleOpts(
+                title="Trend", subtitle="describe how these stages switching"
+            ),
+            toolbox_opts=opts.ToolboxOpts(is_show=True),
+            tooltip_opts=opts.TooltipOpts(
+                is_show=True, trigger="axis", axis_pointer_type="cross"
+            ),
+            brush_opts=opts.BrushOpts(x_axis_index="all", tool_box=["lineX"]),
+        )
+        return line
+
+    @staticmethod
+    def _draw_sim(data: VideoCutResult) -> Line:
+        x_axis = [str(i.start) for i in data.range_list]
+        ssim_axis = [i.ssim for i in data.range_list]
+        mse_axis = [i.mse for i in data.range_list]
+        psnr_axis = [i.psnr for i in data.range_list]
+
+        line = Line(init_opts=opts.InitOpts(bg_color=constants.BACKGROUND_COLOR))
+        line.add_xaxis(x_axis)
+        line.add_yaxis("ssim", ssim_axis)
+        line.add_yaxis("mse", mse_axis)
+        line.add_yaxis("psnr", psnr_axis)
+        line.set_global_opts(
+            title_opts=opts.TitleOpts(title="SIM"),
+            toolbox_opts=opts.ToolboxOpts(is_show=True),
+            tooltip_opts=opts.TooltipOpts(
+                is_show=True, trigger="axis", axis_pointer_type="cross"
+            ),
+        )
+        line.set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+        return line
+
+    @staticmethod
+    def _draw_bar(result: ClassifierResult) -> Bar:
+        # draw bar chart
+        bar = Bar(init_opts=opts.InitOpts(bg_color=constants.BACKGROUND_COLOR))
+        x_axis = sorted(list(result.get_stage_set()))
+        y_axis = list()
+        offset = result.get_offset()
+        for each_stage_name in x_axis:
+            each_stage = result.get_specific_stage(each_stage_name)
+            # last frame - first frame
+            time_cost = each_stage[-1].timestamp - each_stage[0].timestamp + offset
+            y_axis.append(time_cost)
+
+        bar.add_xaxis(x_axis)
+        bar.add_yaxis("time cost", y_axis)
+        bar.set_global_opts(
+            title_opts=opts.TitleOpts(title="Time Cost", subtitle="... of each stages"),
+            toolbox_opts=opts.ToolboxOpts(is_show=True),
+        )
+        logger.debug(f"time cost: {dict(zip(x_axis, y_axis))}")
+        return bar
+
+    @staticmethod
+    def get_stable_stage_sample(
+        result: ClassifierResult, *args, **kwargs
+    ) -> np.ndarray:
+        last = result.data[0]
+        picked: typing.List[SingleClassifierResult] = [last]
+        for each in result.data:
+            # ignore unstable stage
+            if each.stage == constants.UNSTABLE_FLAG:
+                continue
+            if last.stage != each.stage:
+                last = each
+                picked.append(each)
+
+        def get_split_line(f):
+            return np.zeros((f.shape[0], 5))
+
+        with toolbox.video_capture(last.video_path) as cap:
+            frame_list: typing.List[np.ndarray] = []
+            for each in picked:
+                frame = toolbox.get_frame(cap, each.frame_id)
+                frame = toolbox.compress_frame(frame, *args, **kwargs)
+                split_line = get_split_line(frame)
+                frame_list.append(frame)
+                frame_list.append(split_line)
+        return np.hstack(frame_list)
+
+    @classmethod
+    def save(cls, to_file: str, result: ClassifierResult):
+        assert not os.path.isfile(to_file), f"file {to_file} already existed"
+        data = [i.to_dict() for i in result.data]
+        with open(to_file, "w", encoding=constants.CHARSET) as f:
+            json.dump(data, f)
+
+    @classmethod
+    def load(cls, from_file: str) -> ClassifierResult:
+        assert os.path.isfile(from_file), f"file {from_file} not existed"
+        with open(from_file, encoding=constants.CHARSET) as f:
+            content = json.load(f)
+        return ClassifierResult([SingleClassifierResult(**each) for each in content])
+
+    def draw(
+        self,
+        classifier_result: ClassifierResult,
+        report_path: str = None,
+        cut_result: VideoCutResult = None,
+        language: str = None,
+        *args,
+        **kwargs,
+    ):
+        """
+        draw report file
+
+        :param classifier_result: classifierResult, output of classifier
+        :param report_path: your report will be there
+        :param cut_result: more charts would be built
+        :param language: 'en' or 'zh'
+        :return:
+        """
+
+        # draw
+        line = self._draw_line(classifier_result)
+        bar = self._draw_bar(classifier_result)
+
+        # merge charts
+        page = Page()
+        page.add(line)
+        page.add(bar)
+
+        # calc time cost
+        cost_dict = classifier_result.calc_changing_cost()
+
+        # insert pictures
+        if cut_result:
+            # sim chart
+            sim_line = self._draw_sim(cut_result)
+            page.add(sim_line)
+
+            _, unstable = cut_result.get_range(*args, **kwargs)
+            # insert thumbnail
+            if not self.thumbnail_list:
+                logger.debug("auto insert thumbnail ...")
+
+                for each in unstable:
+                    self.add_thumbnail(
+                        f"{each.start}({each.start_time}) - {each.end}({each.end_time}), "
+                        f"duration: {each.end_time - each.start_time}",
+                        cut_result.thumbnail(each, *args, **kwargs),
+                    )
+
+        # insert stable frames
+        stable_stage_sample = self.get_stable_stage_sample(
+            classifier_result, compress_rate=0.2
+        )
+        stable_stage_sample = toolbox.np2b64str(stable_stage_sample)
+
+        # time stamp
+        timestamp = toolbox.get_timestamp_str()
+
+        # insert extras
+        # default: zh_cn report
+        if not language:
+            language = "zh"
+        template = Template(get_template(language))
+        template_content = template.render(
+            chart=Markup(page.render_embed()),
+            dir_link_list=self.dir_link_list,
+            thumbnail_list=self.thumbnail_list,
+            stable_sample=stable_stage_sample,
+            extras=self.extra_dict,
+            background_color=constants.BACKGROUND_COLOR,
+            cost_dict=cost_dict,
+            timestamp=timestamp,
+            version_code=__VERSION__,
+        )
+
+        # save to file
+        if not report_path:
+            report_path = f"{timestamp}.html"
+        with open(report_path, "w", encoding=constants.CHARSET) as fh:
+            fh.write(template_content)
+        logger.info(f"save report to {report_path}")
