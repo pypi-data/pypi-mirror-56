a11427d512835e15e7f108a5867f78023ce35ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:58 +0000

    Add package iteration-utilities/0.10.0/iteration_utilities-0.10.0-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/iteration-utilities/iteration_utilities-0.10.0-cp37-cp37m-manylinux1_x86_64.whl/iteration_utilities/__init__.py
@@ -0,0 +1,11 @@
+# Licensed under Apache License Version 2.0 - see LICENSE
+
+"""Utilities based on Pythons iterators and generators."""
+
+from ._iteration_utilities import *
+
+from ._recipes import *
+from ._additional_recipes import *
+from ._classes import *
+
+__version__ = '0.10.0'
