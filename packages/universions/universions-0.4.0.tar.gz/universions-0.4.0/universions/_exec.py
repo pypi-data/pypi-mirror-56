ed83d05a27133a8ba155d68176126a316ece423f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:06 +0000

    Add package universions/0.4.0/universions-0.4.0.tar.gz

---
+++ packages/universions/universions-0.4.0.tar.gz/universions-0.4.0/universions/_exec.py
@@ -0,0 +1,35 @@
+"""Module to execute a command line and clean it."""
+
+import subprocess
+from pathlib import Path
+from subprocess import PIPE, Popen
+from typing import List, Union
+
+from .error import NotFoundError
+
+
+def exec_command(
+    parts: List[Union[str, Path]], clean: bool = True, use_stderr: bool = False
+) -> str:
+    """Exec the command and clean the result.
+
+    Args:
+        parts: Part of the command to execute.
+        clean: Whether to clean the result.
+        use_stderr: Whether to use the stderr
+    Returns:
+        The result of the command.
+
+    """
+    #  cmd = " ".join([str(part) for part in parts])
+    cmd = [str(part) for part in parts]
+    stderr = subprocess.STDOUT if use_stderr else PIPE
+    try:
+        result_string, _ = Popen(cmd, stdout=PIPE, stderr=stderr).communicate()
+    except FileNotFoundError:
+        raise NotFoundError(f"\"{' '.join(cmd)}\" is not a valid command.")
+    if isinstance(result_string, bytes):
+        result_string = str(result_string, encoding="utf-8")
+    if clean:
+        result_string = result_string.strip()
+    return result_string
