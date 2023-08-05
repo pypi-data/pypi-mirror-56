83f449f95bf68757555137558865fde92d8553d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:26 +0000

    Add package po-excel-translate/3.0/po-excel-translate-3.0.tar.gz

---
+++ packages/po-excel-translate/po-excel-translate-3.0.tar.gz/po-excel-translate-3.0/CHANGES.md
@@ -0,0 +1,53 @@
+Changelog
+=========
+
+1.4.2 - May 11, 2017
+-------------------------
+
+- Fix another problem with message context handling in po-to-xls.
+
+
+1.4.1 - May 11, 2017
+-------------------------
+
+- Fix po-to-xls handling of messages with a context.
+
+
+1.4.0 - December 23, 2016
+-------------------------
+
+- Fix compatibility with current OpenPyxl releases.
+
+- Fix Python 3 compatibility.
+
+
+1.3.0 - July 6, 2015
+--------------------
+
+- Fix another ReST syntax error in package description.
+
+- Correcty handle rows with a missing translation.
+
+
+1.2.0 - June 12, 2015
+---------------------
+
+- Fix ReST syntax error in package description.
+
+- Skip rows without a message id.
+
+
+1.1.0 - 25 March 2015
+---------------------
+
+- Use [openpyxl](http://openpyxl.readthedocs.org/) instead of xlrd/xlwt. This
+  fixes warnings about cell type conversions when opening generated xlsx files
+  in Apple Numbers (and possibly others).
+
+
+1.0.0 - 15 March 2015
+---------------------
+
+- Split po-excel conversion tools out from [lingua](https://github.com/wichert/lingua)
+
+- Simplify CLI interfaces.
---
+++ packages/po-excel-translate/po-excel-translate-3.0.tar.gz/po-excel-translate-3.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import sys
+
+import po_excel_translate
+
+setup(
+    name="po-excel-translate",
+    version=po_excel_translate.__version__,
+    description="Convert between Excel and PO files",
+    long_description=open("README.md").read() + "\n" + open("CHANGES.md").read(),
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: DFSG approved",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+    ],
+    keywords="translation po gettext Babel lingua excel portable object",
+    author="Daniel Butum",
+    author_email="daniel@notyet.eu",
+    url="https://github.com/NotYetGames/po-excel-translate",
+    license="BSD",
+    # packages=find_packages(),
+    py_modules=["version", "po_excel_translate", "po2xls", "xls2po"],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=["click", "polib", "openpyxl"],
+    entry_points={"console_scripts": ["po2xls=po2xls:main", "xls2po=xls2po:main"]},
+    python_requires=">=3.2",
+)
