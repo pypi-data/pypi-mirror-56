11ac7f846d93d73af1a72486cd176e4a9d066286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:38 +0000

    Add package snap-micro/1.0.2/snap_micro-1.0.2-py3-none-any.whl

---
+++ packages/snap-micro/snap_micro-1.0.2-py3-none-any.whl/tests/test_application.py
@@ -0,0 +1,140 @@
+import unittest
+import os, sys
+import datetime
+import time
+import sh
+import json
+import requests
+import yaml
+from context import snap
+from snap import core
+from snap import metaobjects as m
+
+
+def tearDownModule():
+    pass
+    #app.process.terminate()
+
+
+class HTTPServiceTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pycmd = sh.Command('python')
+        cls.app = pycmd('test_app.py', '--configfile=data/good_sample_config.yaml', _bg=True)
+        time.sleep(1)
+        if not cls.app.process.is_alive()[0]: # this method returns a tuple
+            raise Exception('Error starting testbed application:\n%s' % cls.app.process.stdout)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.app.process.terminate()
+        
+        
+    def setUp(self):
+        self.project_home = os.getenv('SNAP_TEST_HOME')
+        if not self.project_home:
+            raise Exception('the environment var SNAP_TEST_HOME has not been set.')
+        config_file_path = os.path.join(self.project_home,
+                                        'data/good_sample_config.yaml')
+        with open(config_file_path) as f:
+            self.app_config = yaml.safe_load(f)
+
+
+    def test_should_listen_on_port_specified_in_config(self):
+        port = self.app_config['globals']['port']
+        r = requests.get('http://localhost:%s/ping' % port)
+        self.assertEqual(r.status_code, 200)
+
+
+    def test_should_reject_transform_requests_not_compliant_with_datashape(self):
+        port = self.app_config['globals']['port']
+        payload = {'foo': 'bar'}
+        r = requests.post('http://localhost:%s/posttest' % port, data=payload)
+        self.assertEqual(r.status_code, 400)
+        print(r.json, file=sys.stderr)
+
+
+    '''
+    def test_should_accept_get_transform_requests_compliant_with_datashape(self):
+        port = self.app_config['globals']['port']
+        payload = {'placeholder': 'any_value'}
+
+        #datashapes = m.load_shapes_from_yaml_config(self.app_config)
+        
+        r = requests.get('http://localhost:5000/test', data=payload)
+        self.assertEqual(r.status_code, 200)
+    '''
+
+    def test_should_log_to_target_specified_in_config(self):
+        logfile_name = self.app_config['globals']['logfile']
+        logfile_path = os.path.join(self.project_home, logfile_name)
+        statinfo = os.stat(logfile_path)
+        initial_logfile_size = statinfo.st_size
+        port = self.app_config['globals']['port']
+        r = requests.get('http://localhost:%s/ping' % port)
+        self.assertEqual(r.status_code, 200)
+
+        statinfo = os.stat(logfile_path)
+        final_logfile_size = statinfo.st_size
+        self.assertGreater(final_logfile_size, initial_logfile_size)
+
+
+    def test_endpoint_should_not_validate_missing_optional_fields(self):
+        port = self.app_config['globals']['port']
+        headers = {'Content-Type': 'application/json'}
+        payload = {'placeholder': 'testing'}
+        r = requests.post('http://localhost:%s/postvalidator' % port,
+                          data=json.dumps(payload),
+                          headers=headers)
+        
+        self.assertEqual(r.status_code, 200)
+        
+    '''
+    def test_default_content_protocol_can_decode_standard_content_types(self):
+        protocol = core.default_content_protocol
+        self.assertTrue(False)
+    '''
+    
+    def test_custom_content_protocol_is_triggered_by_specified_content_type(self):
+        port = self.app_config['globals']['port']
+        headers = {'Content-Type': 'application/json'}
+        payload = {'placeholder': datetime.datetime.now().isoformat()}
+        r = requests.post('http://localhost:%s/posttest' % port,
+                          data=json.dumps(payload),
+                          headers=headers)
+        
+        self.assertEqual(r.status_code, 200)
+        response = r.json()
+        self.assertIn('test_decoder_called', response)
+
+    def test_custom_field_validator_is_triggered_by_specified_request_field_datatype(self):
+        port = self.app_config['globals']['port']
+
+        json_field_data = {
+            'foo': 'bar',            
+            'counter': 1
+        }
+
+        headers = {'Content-Type': 'application/json'}
+        payload = {
+            'placeholder': datetime.datetime.now().isoformat(),
+            'data': json_field_data
+        }
+
+        r = requests.post('http://localhost:%s/customvalidator' % port,
+                          data=json.dumps(payload),
+                          headers=headers)
+        
+        self.assertEqual(r.status_code, 200)
+        #response = r.json()
+        
+
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/snap-micro/snap_micro-1.0.2-py3-none-any.whl/tests/testbed_transforms.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+ 
+from snap import snap
+from snap import core
+import json
+from snap.loggers import transform_logger as log
+
+
+def ping_func(input_data, service_objects, **kwargs):
+    return core.TransformStatus(json.dumps({'message': 'pong'}))
+
+def post_target_func(input_data, service_objects, **kwargs):
+    return core.TransformStatus(json.dumps(input_data))
+
+def post_validator_func(input_data, service_objects, **kwargs):
+    return core.TransformStatus(json.dumps({'message': 'called test validator function'}))
+
+def custom_validator_func(input_data, service_objects, **kwargs):
+    return core.TransformStatus(json.dumps(input_data))
+def test_func(input_data, service_objects, **kwargs):
+    raise snap.TransformNotImplementedException('test_func')
