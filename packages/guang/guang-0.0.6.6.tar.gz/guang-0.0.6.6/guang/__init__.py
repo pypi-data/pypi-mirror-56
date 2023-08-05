c4190f649220d7c823649f1dc6699187adfa5dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:14 +0000

    Add package guang/0.0.6.6/guang-0.0.6.6.tar.gz

---
+++ packages/guang/guang-0.0.6.6.tar.gz/guang-0.0.6.6/guang/__init__.py
@@ -0,0 +1,11 @@
+'''
+This is a universal function library.
+'''
+from . import *
+
+__version__ = '0.0.6.06'
+__author__ = 'K.y'
+__copyright = 'Copyright 2019 K.y'
+
+__all__ = ["ML","Utils","DL", "Voice"]
+
