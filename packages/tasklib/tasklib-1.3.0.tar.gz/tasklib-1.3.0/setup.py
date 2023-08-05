7b807bfae02882b1c66a1afff8bac0e069ed83b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:26 +0000

    Add package tasklib/1.3.0/tasklib-1.3.0.tar.gz

---
+++ packages/tasklib/tasklib-1.3.0.tar.gz/tasklib-1.3.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+install_requirements = ['six>=1.4', 'pytz', 'tzlocal']
+
+version = '1.3.0'
+
+try:
+    import importlib
+except ImportError:
+    install_requirements.append('importlib')
+
+setup(
+    name='tasklib',
+    version=version,
+    description='Python Task Warrior library',
+    long_description=open('README.rst').read(),
+    author='Rob Golding',
+    author_email='rob@robgolding.com',
+    license='BSD',
+    url='https://github.com/robgolding63/tasklib',
+    download_url='https://github.com/robgolding63/tasklib/downloads',
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='tasklib.tests',
+    install_requires=install_requirements,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
