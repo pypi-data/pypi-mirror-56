c63310b022d85f9c0997c9b7aa6be676741acc56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:46 +0000

    Add package pyfoobar/0.0.2/pyfoobar-0.0.2.tar.gz

---
+++ packages/pyfoobar/pyfoobar-0.0.2.tar.gz/pyfoobar-0.0.2/pyfoobar/__init__.py
@@ -0,0 +1,13 @@
+from .__about__ import __author__, __email__, __license__, __status__, __version__
+from .cli import show
+from .main import solve
+
+__all__ = [
+    "__author__",
+    "__email__",
+    "__license__",
+    "__version__",
+    "__status__",
+    "solve",
+    "show",
+]
