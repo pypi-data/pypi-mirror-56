4770bfd6a96033f240bbf4321750336aa97bf08b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:22 +0000

    Add package isharp/2.63/isharp-2.63.tar.gz

---
+++ packages/isharp/isharp-2.63.tar.gz/isharp-2.63/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='isharp',
+    packages=find_packages(),
+    version='2.63',
+    description='Algorithmic workbench',
+    author='jeremycward',
+    author_email='jeremycward@yahoo.co.uk',
+    url='https://github.com/jeremycward/isharp-core',
+    download_url='https://github.com/jeremycward/isharp-core/tarball/1.00',
+    keywords=[],
+    classifiers=[],
+    entry_points={
+        'console_scripts':[
+            'datahub=isharp.datahub.broker_service.datahub_main:main',
+            'web=isharp.datahub.broker_service.datahub_webserver_main:main'
+        ]
+    },
+)
