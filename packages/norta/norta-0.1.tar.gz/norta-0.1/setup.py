74c02384db1134e8e7c5eec9ea6c4caffb41b512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:07 +0000

    Add package norta/0.1/norta-0.1.tar.gz

---
+++ packages/norta/norta-0.1.tar.gz/norta-0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="norta",
+    version="0.1",
+    description="Python Distribution Utilities",
+    author="Daniel Duque-Villarreal",
+    author_email="d.duque25@gmail.com",
+    url="https://github.com/dukduque/NortaPy",
+    packages=["norta"],
+)
