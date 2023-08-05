f98817c6a4fc3aa5a744087024d267bfe37cba49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:38 +0000

    Add package aegis-tools/0.0.1/aegis-tools-0.0.1.tar.gz

---
+++ packages/aegis-tools/aegis-tools-0.0.1.tar.gz/aegis-tools-0.0.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+Minimal packaging setup to use command line tools to get started
+virtualenv --python=`which python3` virtualenv
+. ./virtualenv/bin/activate
+python setup.py develop
+"""
+
+import os
+import setuptools
+
+setuptools.setup (
+    name = 'aegis-tools',
+    version = '0.0.1',
+    description = 'Aegis is a set of battle-tested tools and tricks to help everyone make better software',
+    author = "Michael D'Agosta",
+    author_email = 'mdagosta@codebug.com',
+    url = 'https://bitbucket.org/mdagosta/aegis',
+    python_requires='>=3.6',
+    packages = ['aegis'],
+    entry_points={
+        'console_scripts': [
+            "aegis = aegis.aegis:main",
+        ],
+    }
+)
