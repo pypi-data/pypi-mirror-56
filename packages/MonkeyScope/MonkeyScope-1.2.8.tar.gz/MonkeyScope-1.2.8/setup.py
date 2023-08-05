2f5d6f39eb9b3499e5baace7e3cfb2494178b0b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:08 +0000

    Add package MonkeyScope/1.2.8/MonkeyScope-1.2.8.tar.gz

---
+++ packages/MonkeyScope/MonkeyScope-1.2.8.tar.gz/MonkeyScope-1.2.8/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+
+setup(
+    name="MonkeyScope",
+    ext_modules=cythonize(
+        Extension(
+            name="MonkeyScope",
+            sources=["MonkeyScope.pyx"],
+            language=["c++"],
+            extra_compile_args=["-std=gnu++17"],
+        ),
+        compiler_directives={
+            'embedsignature': True,
+            'language_level': 3,
+        },
+    ),
+    author="Robert Sharp",
+    author_email="webmaster@sharpdesigndigital.com",
+    requires=["Cython"],
+    install_requires=['numpy'],
+    version="1.2.8",
+    description="Distribution Stats & Timer for Testing Non-deterministic Value Generators",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="Free for non-commercial use",
+    platforms=["Darwin", "Linux"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Cython",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords=[
+        "MonkeyScope", "distribution tests", "function timer", "performance testing", "statistical analysis",
+    ],
+    python_requires='>=3.6',
+)
