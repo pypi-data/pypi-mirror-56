9c72c0a4ef1f5dc59e125a5c0d1cb8d089c420a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:23 +0000

    Add package MoneyCraft/1.0.3/MoneyCraft-1.0.3.tar.gz

---
+++ packages/MoneyCraft/MoneyCraft-1.0.3.tar.gz/MoneyCraft-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'MoneyCraft',
+    packages = ['moneycraft'],
+    scripts = [],
+    version = '1.0.3',
+    description = 'A python quant platform for financial use.',
+    author = 'austin362667',
+    author_email = 'austin362667@gmail.com',
+    url = 'https://github.com/austin362667/MonyCraft',
+    download_url = 'https://github.com/austin362667/MonyCraft.git',
+    keywords = ['finance', 'backtest', 'quant'],
+    classifiers = []
+)
