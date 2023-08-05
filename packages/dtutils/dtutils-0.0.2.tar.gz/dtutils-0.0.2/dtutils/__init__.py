d94cdc70c876a31f594b90fcefbf0fda74048064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:09 +0000

    Add package dtutils/0.0.2/dtutils-0.0.2.tar.gz

---
+++ packages/dtutils/dtutils-0.0.2.tar.gz/dtutils-0.0.2/dtutils/__init__.py
@@ -0,0 +1,1 @@
+from dtutils.dtutils import *
---
+++ packages/dtutils/dtutils-0.0.2.tar.gz/dtutils-0.0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='dtutils', # 应用名
+    version='0.0.2', # 版本号
+    packages=['dtutils'], # 包括在安装包内的 Python 包
+)
