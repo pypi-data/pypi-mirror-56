d3d33c64cc2af164d9cbbba3b5e14c7d29157ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:57 +0000

    Add package mkstuff/1.2.0/mkstuff-1.2.0.tar.gz

---
+++ packages/mkstuff/mkstuff-1.2.0.tar.gz/mkstuff-1.2.0/mkstuff/__init__.py
@@ -0,0 +1,12 @@
+"""
+.. module :: mkstuff
+
+.. moduleauthor :: Martin Kilbinger
+
+"""
+
+#__all__ = ['athena', 'mkplot', 'mkstuff', 'populationWL']  # List of submodules
+__all__ = []
+
+from . import *
+from .info import __version__, __about__
