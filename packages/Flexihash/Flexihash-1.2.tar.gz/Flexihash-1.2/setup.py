d209266af5ba7a9fab1d42d5471794ac4e4cbb63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:35 +0000

    Add package Flexihash/1.2/Flexihash-1.2.tar.gz

---
+++ packages/Flexihash/Flexihash-1.2.tar.gz/Flexihash-1.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+setup(name='Flexihash',
+      version='1.2',
+      description='Python port of PHP Flexihash',
+      author='Shish',
+      author_email='webmaster@shishnet.org',
+      url='http://github.com/shish/python-flexihash',
+      packages=['flexihash'],
+     )
