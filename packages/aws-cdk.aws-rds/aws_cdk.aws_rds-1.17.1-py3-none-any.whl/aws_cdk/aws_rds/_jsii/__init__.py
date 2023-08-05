3d66dfa4bcade33f92e6a7e402d38396a27e4441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:00 +0000

    Add package aws-cdk.aws-rds/1.17.1/aws_cdk.aws_rds-1.17.1-py3-none-any.whl

---
+++ packages/aws-cdk.aws-rds/aws_cdk.aws_rds-1.17.1-py3-none-any.whl/aws_cdk/aws_rds/_jsii/__init__.py
@@ -0,0 +1,24 @@
+import abc
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+from jsii.python import classproperty
+
+import aws_cdk.aws_cloudwatch
+import aws_cdk.aws_ec2
+import aws_cdk.aws_events
+import aws_cdk.aws_iam
+import aws_cdk.aws_kms
+import aws_cdk.aws_lambda
+import aws_cdk.aws_logs
+import aws_cdk.aws_sam
+import aws_cdk.aws_secretsmanager
+import aws_cdk.core
+__all__ = []
+
+publication.publish()
