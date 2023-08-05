a8175b75f476283ae462ec43b9617276ff92c1a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:37 +0000

    Add package delete-chrome-history-py/0.1.7/delete-chrome-history-py-0.1.7.tar.gz

---
+++ packages/delete-chrome-history-py/delete-chrome-history-py-0.1.7.tar.gz/delete-chrome-history-py-0.1.7/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="delete-chrome-history-py",
+    version="0.1.7",
+    author="Bhavesh Bhatt",
+    author_email="bhattbhavesh91@gmail.com",
+    description="Delete specific websites from Google Chrome using Python",
+    py_modules=["chrome_delete"],
+    url="https://github.com/bhattbhavesh91/delete-chrome-history",
+    install_requires=[],
+)
