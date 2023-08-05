754be27e47f3d3d110376720626a729f55de3c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:15 +0000

    Add package iotile-support-lib-controller-4/4.0.0.dev8/iotile_support_lib_controller_4-4.0.0.dev8.tar.gz

---
+++ packages/iotile-support-lib-controller-4/iotile_support_lib_controller_4-4.0.0.dev8.tar.gz/iotile_support_lib_controller_4-4.0.0.dev8/iotile_support_lib_controller_4/lib_controller_types/fw_configvariable.py
@@ -0,0 +1,54 @@
+import struct
+from iotile.core.exceptions import *
+from . import fw_tileselector
+
+
+class ConfigDescriptor:
+    ConfigMagic = 0xCACA
+
+    def __init__(self, arg):
+        if isinstance(arg, bytearray):
+            if len(arg) != size():
+                raise ValidationError("Invalid size for buffer containing a TileDescriptor", expected=size(), actual=len(arg))
+
+            self.raw_data = arg
+            self._extract_info()
+        else:
+            raise ValidationError("You can only create a ConfigDescriptor from binary data")
+
+    def _extract_info(self):
+        magic, offset, length, match_info, valid, _ = struct.unpack("<HHH8sBB", self.raw_data)
+
+        if magic != ConfigDescriptor.ConfigMagic:
+            raise ValidationError("Invalid magic number in config variable", expected=ConfigDescriptor.ConfigMagic, actual=magic)
+
+        self.data_offset = offset
+        self.data_length = length
+        self.target = fw_tileselector.convert_binary(bytearray(match_info))
+        self.valid = bool(valid == 0xFF)
+
+    def __str__(self):
+        out = ""
+
+        out += "Target: %s\n" % str(self.target)
+        out += "Data Offset: %d\n" % self.data_offset
+        out += "Data Length: %d\n" % self.data_length
+        out += "Valid: %s" % str(self.valid)
+
+        return out
+
+def size():
+    return 16
+
+def convert(arg):
+    if isinstance(arg, ConfigDescriptor):
+        return arg
+
+    return ConfigDescriptor(arg)
+
+def convert_binary(arg, **kwargs):
+    return ConfigDescriptor(arg)
+
+#Formatting Functions
+def default_formatter(arg, **kwargs):
+    return str(arg)
