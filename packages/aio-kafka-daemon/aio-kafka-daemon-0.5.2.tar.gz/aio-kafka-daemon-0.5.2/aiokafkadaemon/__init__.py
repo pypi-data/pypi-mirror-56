9922895bf1f22c6baf1d1bfd16ceb11e5a4b3f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:24 +0000

    Add package aio-kafka-daemon/0.5.2/aio-kafka-daemon-0.5.2.tar.gz

---
+++ packages/aio-kafka-daemon/aio-kafka-daemon-0.5.2.tar.gz/aio-kafka-daemon-0.5.2/aiokafkadaemon/__init__.py
@@ -0,0 +1,7 @@
+"""aiokafkadaemon: framework for daemonized apps using kafka with AsyncIO"""
+# pylint: disable=I0011,W0401
+
+from .daemon import Daemon  # noqa: F403
+from .worker import Worker  # noqa: F403
+
+__all__ = ['Daemon', 'Worker',]
