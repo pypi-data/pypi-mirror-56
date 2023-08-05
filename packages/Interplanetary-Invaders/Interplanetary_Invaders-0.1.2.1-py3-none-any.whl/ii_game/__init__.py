440e9665b2841648d0467ff46a25a5c8194715d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:15 +0000

    Add package Interplanetary-Invaders/0.1.2.1/Interplanetary_Invaders-0.1.2.1-py3-none-any.whl

---
+++ packages/Interplanetary-Invaders/Interplanetary_Invaders-0.1.2.1-py3-none-any.whl/ii_game/__init__.py
@@ -0,0 +1,6 @@
+__myname__ = "Interplanetary Invaders"
+__title__ = "Interplanetary Invaders"
+__author__ = "NachoMonkey"
+__version__ = "0.1.2.1"
+__minimum_version__ = (3, 7)
+__maximum_version = (3, 9)
