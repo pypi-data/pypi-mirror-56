1374c6a563a03a0f10bc80e476566c4ed3c48571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:26 +0000

    Add package nwae.utils/1.3.9/nwae.utils-1.3.9-py3-none-any.whl

---
+++ packages/nwae.utils/nwae.utils-1.3.9-py3-none-any.whl/nwae/utils/Profiling.py
@@ -0,0 +1,45 @@
+
+import datetime
+import time
+
+
+class Profiling:
+
+    def __init__(self):
+        return
+
+    @staticmethod
+    def start():
+        return datetime.datetime.now()
+
+    @staticmethod
+    def stop():
+        return datetime.datetime.now()
+
+    @staticmethod
+    def get_time_dif(
+            start,
+            stop,
+            decimals=4
+    ):
+        diftime = (stop - start)
+        diftime = round(diftime.seconds + diftime.microseconds / 1000000, decimals)
+        return diftime
+
+    @staticmethod
+    def get_time_dif_str(
+            start,
+            stop,
+            decimals=4
+    ):
+        return (str(Profiling.get_time_dif(start, stop, decimals)) + ' secs')
+
+
+if __name__ == '__main__':
+
+    a = Profiling.start()
+    time.sleep(2.59384)
+    b = Profiling.stop()
+
+    print(Profiling.get_time_dif(a, b))
+    print(Profiling.get_time_dif_str(a, b))
