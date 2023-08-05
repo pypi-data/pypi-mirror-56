3783ae98bd34fa924716c9eafef905e307b290bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:21 +0000

    Add package cloudformation-cli/0.1/cloudformation-cli-0.1.tar.gz

---
+++ packages/cloudformation-cli/cloudformation-cli-0.1.tar.gz/cloudformation-cli-0.1/src/main/java/com/my/resource/type/CallbackContext.java
@@ -0,0 +1,12 @@
+package com.my.resource.type;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+public class CallbackContext {
+
+}
