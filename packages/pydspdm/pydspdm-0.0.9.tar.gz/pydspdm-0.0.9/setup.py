f4769524b0dfafb84323f727207044de39cb0e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:33 +0000

    Add package pydspdm/0.0.9/pydspdm-0.0.9.tar.gz

---
+++ packages/pydspdm/pydspdm-0.0.9.tar.gz/pydspdm-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import distutils
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    distutils.dir_util.remove_tree
+
+setuptools.setup(
+    name="pydspdm",
+    version="0.0.9",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
