78d0742f0cddfc7d72efd3ed16805ed6590622e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:30 +0000

    Add package distributions-aleksa/0.2/distributions_aleksa-0.2.tar.gz

---
+++ packages/distributions-aleksa/distributions_aleksa-0.2.tar.gz/distributions_aleksa-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_aleksa',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_aleksa'],
+      author_email='mihajlovic.aleksa@gmail.com',
+      zip_safe=False)
