312dbc07bc0417efd4c0385c25d4de3c55142f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:26 +0000

    Add package dataset-loader/1.2/dataset-loader-1.2.tar.gz

---
+++ packages/dataset-loader/dataset-loader-1.2.tar.gz/dataset-loader-1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+#
+# with open("LICENSE", "r") as fh:
+#     license = fh.read()
+
+
+setup(
+    name='dataset-loader',
+    version='1.2',
+    packages=find_packages(),
+    license=license,
+    long_description_content_type="text/markdown",
+    # long_description=long_description,
+    author="André Claudino",
+    url="https://github.com/andreclaudino/dataset_loader",
+    description="Load partitioned data from multilevel folder structure",
+    setup_requires=['wheel', 'twine'],
+    install_requires=['pandas']
+)
