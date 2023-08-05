c49c69e44cea1acbab8ef667ac3d7a42c97f845c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:51 +0000

    Add package kitconc/1.4.1/kitconc-1.4.1.tar.gz

---
+++ packages/kitconc/kitconc-1.4.1.tar.gz/kitconc-1.4.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Author: jlopes@usp.br
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='kitconc',
+    version='1.4.1',
+    author='José Lopes Moreira Filho',
+    author_email='jlopes@usp.br',
+    packages=['kitconc'],
+    package_data={'kitconc': ['data/*.*','data/images/*.*','data/scripts/*.*']},
+    scripts=['bin/kit.py'],
+    url='http://pypi.python.org/pypi/Kitconc/',
+    license='LICENSE.txt',
+    description='A toolkit for Corpus Linguistics Analysis',
+    long_description=open('README.txt').read(),
+    long_description_content_type='text/x-rst',
+    install_requires=[
+        "nltk >= 3.2.5",
+        "numpy >= 1.14.0",
+        "pandas >= 0.22.0",
+        "matplotlib >= 2.1.2",
+        "xlsxwriter >= 1.0.2"
+    ],
+)
