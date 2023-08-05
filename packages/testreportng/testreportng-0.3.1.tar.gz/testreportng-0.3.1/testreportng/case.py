d789df3d246ae13eeecfcbcb7feb94bd7353536a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:54 +0000

    Add package testreportng/0.3.1/testreportng-0.3.1.tar.gz

---
+++ packages/testreportng/testreportng-0.3.1.tar.gz/testreportng-0.3.1/testreportng/case.py
@@ -0,0 +1,70 @@
+import unittest
+
+from testreportng.result import NGCaseDetail, NGResult
+from testreportng.constants import Label
+from testreportng.utils import get_timestamp
+
+
+class NGCase(unittest.TestCase):
+    # default cls
+    ng_detail_kls = NGCaseDetail
+    ng_result_kls = NGResult
+
+    # result inst
+    ng_result: NGResult
+
+    # ng hook
+    def _start_hook(self, name: str):
+        mapping = {
+            Label.LABEL_STATUS_PASS: "when_pass",
+            Label.LABEL_STATUS_FAIL: "when_fail",
+            Label.LABEL_STATUS_ERROR: "when_error",
+            Label.LABEL_STATUS_SKIP: "when_skip",
+        }
+        assert name in mapping, f"hook [ {name} ] not found"
+        target = mapping[name]
+        assert hasattr(self, target)
+        return getattr(self, target)()
+
+    def when_pass(self):
+        """ will execute when case passed """
+        pass
+
+    def when_fail(self):
+        """ will execute when case failed """
+
+    def when_error(self):
+        """ will execute when case error """
+
+    def when_skip(self):
+        """ will execute when case skipped """
+        pass
+
+    def when_always(self):
+        """ will execute after each cases """
+        pass
+
+    # origin hook
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.ng_result: NGResult = cls.ng_result_kls(cls.__name__)
+
+    def setUp(self) -> None:
+        # init this case
+        cur = self.ng_detail_kls(self._testMethodName)
+        # time record
+        cur.start_time = get_timestamp()
+        self.ng_result.set(cur)
+
+    def tearDown(self) -> None:
+        # update case's result
+        cur = self.ng_result.get(self._testMethodName)
+        # time record
+        cur.end_time = get_timestamp()
+        cur.duration = cur.end_time - cur.start_time
+        cur.outcome = getattr(self, "_outcome")
+        self.ng_result.set(cur)
+
+        # ng hook
+        self._start_hook(cur.status)
+        self.when_always()
---
+++ packages/testreportng/testreportng-0.3.1.tar.gz/testreportng-0.3.1/testreportng/ext/html.py
@@ -0,0 +1,131 @@
+"""
+this is a offline html report builder, with no third-party js package requirement.
+"""
+from jinja2 import Template
+import typing
+from collections import OrderedDict
+
+from testreportng.result import NGResult, NGResultOperator
+from testreportng.constants import Label
+
+COLOR_DICT = {
+    Label.LABEL_STATUS_PASS: "#dff0d8",
+    Label.LABEL_STATUS_FAIL: "#fcf8e3",
+    Label.LABEL_STATUS_ERROR: "#f2dede",
+    Label.LABEL_STATUS_SKIP: "#ffffff",
+    Label.LABEL_STATUS_INIT: "#ffffff",
+}
+
+TEMPLATE = r"""
+<!DOCTYPE html>
+<html>
+<head>
+<title>{{ test_name }}</title>
+
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 5px;
+}
+th {
+  text-align: left;
+}
+</style>
+</head>
+
+<body>
+<h1>{{ test_name }}</h1>
+
+<h2>Summary</h2>
+<table style="width:90%">
+  <tr>
+    {% for title, count in summary.items() %}
+      <th>{{ title }}</th>
+    {% endfor %}
+  </tr>
+
+  <tr">
+    {% for title, count in summary.items() %}
+      <td>{{ count }}</td>
+    {% endfor %}
+  </tr>
+  
+</table>
+
+<h2>Result</h2>
+<table style="width:90%">
+  <tr>
+    <th>suite</th>
+    <th>case</th>
+    <th>status</th>
+    <th>traceback</th>
+  </tr>
+
+  {% for suite_name, each in test_result.items() %}
+    {% for each_case_name, each_case in each.items() %}
+        <tr style="background-color: {{ color_dict[each_case.status] }}">
+          <td>{{ suite_name }}</td>
+          <td>{{ each_case_name }}</td>
+          <td>{{ each_case.status }}</td>
+          <td>{{ each_case.traceback }}</td>
+        </tr>
+    {% endfor %}
+  {% endfor %}
+</table>
+
+</body>
+</html>
+"""
+
+
+class HtmlReporter(object):
+    _template = TEMPLATE
+    _default_test_name = "default"
+
+    @classmethod
+    def render(
+        cls,
+        test_name: str,
+        result: typing.Union[typing.Dict[str, NGResult], NGResult],
+        sorted_by_status: bool = None,
+    ) -> str:
+        html_template = Template(TEMPLATE)
+        # re-format
+        if isinstance(result, NGResult):
+            result = {test_name: result}
+
+        result_operator = NGResultOperator()
+        result_operator.load(result)
+        summary = result_operator.summary()
+
+        # sorted by status, eg: error -> fail -> pass -> skip
+        if sorted_by_status:
+            sorted_result = OrderedDict()
+            for each_label in (
+                Label.LABEL_STATUS_ERROR,
+                Label.LABEL_STATUS_FAIL,
+                Label.LABEL_STATUS_PASS,
+                Label.LABEL_STATUS_SKIP,
+            ):
+                cur = result_operator.get_data_by_status(each_label)
+                for case_name, data in cur.items():
+                    sorted_result[case_name] = data
+            # cover
+            result = {test_name: sorted_result}
+        else:
+            # wrap to dict
+            new_result = dict()
+            for k, v in result.items():
+                new_result[k] = v.to_dict(safe_repr=True)
+            result = new_result
+
+        return html_template.render(
+            test_name=test_name,
+            summary=summary,
+            test_result=result,
+            color_dict=COLOR_DICT,
+            sorted_by_status=sorted_by_status,
+        )
