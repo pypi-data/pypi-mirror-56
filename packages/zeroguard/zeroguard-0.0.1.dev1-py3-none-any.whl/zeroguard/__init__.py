3e0226321300f6af952195d95865c14492bb9543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:00 +0000

    Add package zeroguard/0.0.1.dev1/zeroguard-0.0.1.dev1-py3-none-any.whl

---
+++ packages/zeroguard/zeroguard-0.0.1.dev1-py3-none-any.whl/zeroguard/__init__.py
@@ -0,0 +1,7 @@
+"""."""
+# flake8: noqa
+# pylama:ignore=W0611:
+from .__version__ import (
+    __title__, __description__, __version__, __docs_url__, __source_url__,
+    __home_url__, __author__, __author_email__, __copyright__, __license__
+)
---
+++ packages/zeroguard/zeroguard-0.0.1.dev1-py3-none-any.whl/zeroguard/__version__.py
@@ -0,0 +1,14 @@
+"""Information about ZeroGuard Python OSS tools package."""
+
+__title__ = 'zeroguard'
+__description__ = 'ZeroGuard Python OSS tools'
+__version__ = '0.0.1.dev1'
+
+__docs_url__ = 'https://github.com/zeroguard/zeroguard/tree/master/python'
+__source_url__ = 'https://github.com/zeroguard/zeroguard/tree/master/python'
+__home_url__ = 'https://zeroguard.com'
+
+__author__ = 'ZeroGuard'
+__author_email__ = 'oss@zeroguard.com'
+__copyright__ = '2019 ZeroGuard Ltd'
+__license__ = 'GNU AGPLv3'
