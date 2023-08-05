4811cf70ee7fc6eefb4768e3111570081ae74e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:13 +0000

    Add package aws-cdk.aws-codepipeline/1.17.1/aws-cdk.aws-codepipeline-1.17.1.tar.gz

---
+++ packages/aws-cdk.aws-codepipeline/aws-cdk.aws-codepipeline-1.17.1.tar.gz/aws-cdk.aws-codepipeline-1.17.1/setup.py
@@ -0,0 +1,55 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-codepipeline",
+    "version": "1.17.1",
+    "description": "Better interface to AWS Code Pipeline",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_codepipeline",
+        "aws_cdk.aws_codepipeline._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_codepipeline._jsii": [
+            "aws-codepipeline@1.17.1.jsii.tgz"
+        ],
+        "aws_cdk.aws_codepipeline": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.7",
+        "publication>=0.0.3",
+        "aws-cdk.aws-events~=1.17,>=1.17.1",
+        "aws-cdk.aws-iam~=1.17,>=1.17.1",
+        "aws-cdk.aws-kms~=1.17,>=1.17.1",
+        "aws-cdk.aws-s3~=1.17,>=1.17.1",
+        "aws-cdk.core~=1.17,>=1.17.1"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
