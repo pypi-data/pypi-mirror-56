c3d80bef37115c7aabf38eda1ed65434c50b5d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:13 +0000

    Add package pynamodb-rest/0.0.2/pynamodb_rest-0.0.2-py2.py3-none-any.whl

---
+++ packages/pynamodb-rest/pynamodb_rest-0.0.2-py2.py3-none-any.whl/pynamodb_rest/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Pynamodb Rest."""
+
+__author__ = """Nathan Danielsen"""
+__email__ = 'nathan.danielsen@gmail.com'
+__version__ = '0.0.2'
+
+
+from pynamodb_rest import PynamodbRestMixin
