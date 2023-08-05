6c3946ac600a42afcafdacf69c33586df96e4b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:56 +0000

    Add package polychemprint3/3.0.1a17/polychemprint3-3.0.1a17.tar.gz

---
+++ packages/polychemprint3/polychemprint3-3.0.1a17.tar.gz/polychemprint3-3.0.1a17/polychemprint3/__init__.py
@@ -0,0 +1,17 @@
+def main():
+    import sys, os
+
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+    from polychemprint3 import *
+
+    for p in sys.path:
+        print(p)
+
+    from polychemprint3 import mainMethod
+    mainMethod.main()
+    print("h")
+
+
+if __name__ == "__main__":
+    main()
