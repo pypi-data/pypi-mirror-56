b7ca74fd7a0875721c10f523f6e9c94aac9c7a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:32 +0000

    Add package adslconnector/0.0.3/adslconnector-0.0.3.tar.gz

---
+++ packages/adslconnector/adslconnector-0.0.3.tar.gz/adslconnector-0.0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="adslconnector", # Replace with your own username
+    version="0.0.3",
+    author="wengo",
+    author_email="398711943@qq.com",
+    description="Adsl Connector",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
