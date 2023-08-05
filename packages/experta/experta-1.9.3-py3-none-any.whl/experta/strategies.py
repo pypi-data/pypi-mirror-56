3333751cfa5e1f0b4d1b6bd710193860a67dc8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:35 +0000

    Add package experta/1.9.3/experta-1.9.3-py3-none-any.whl

---
+++ packages/experta/experta-1.9.3-py3-none-any.whl/experta/strategies.py
@@ -0,0 +1,27 @@
+from functools import lru_cache
+import bisect
+
+from experta.abstract import Strategy
+
+
+class DepthStrategy(Strategy):
+    @lru_cache()
+    def get_key(self, activation):
+        salience = activation.rule.salience
+        facts = sorted((f['__factid__'] for f in activation.facts),
+                       reverse=True)
+        return (salience, facts)
+
+    def _update_agenda(self, agenda, added, removed):
+        for act in removed:
+            act.key = self.get_key(act)
+            try:
+                idx = bisect.bisect_left(agenda.activations, act)
+                if agenda.activations[idx] == act:
+                    del agenda.activations[idx]
+            except IndexError:
+                pass
+
+        for act in added:
+            act.key = self.get_key(act)
+            bisect.insort(agenda.activations, act)
