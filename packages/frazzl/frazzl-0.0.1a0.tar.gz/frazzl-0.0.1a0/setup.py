d0b3ce4559c0aaaf81a04133b01437c2b064c3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:51 +0000

    Add package frazzl/0.0.1a0/frazzl-0.0.1a0.tar.gz

---
+++ packages/frazzl/frazzl-0.0.1a0.tar.gz/frazzl-0.0.1a0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from frazzl.constants import VERSION
+
+setup(
+    name="frazzl",
+    version=VERSION,
+    author="jimtheplant",
+    author_email="jimtheplant1@gmail.com",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "frazzl=frazzl.__main__:cli"
+        ]
+    },
+    install_requires=[
+        "ariadne==0.7",
+        "uvicorn==0.10.8",
+        "nodeenv==1.3.3",
+        "click==7.0"
+    ]
+)
