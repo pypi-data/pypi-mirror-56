7fcab6c946a50a2779fe4f94e237d703f21250b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:34 +0000

    Add package tripphrase/0.5/tripphrase-0.5.tar.gz

---
+++ packages/tripphrase/tripphrase-0.5.tar.gz/tripphrase-0.5/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'tripphrase',
+  packages = ['tripphrase'],
+  package_data={'tripphrase': ['*.txt']},
+  version = '0.5',
+  description = 'An implementation of Bret Victor\'s tripphrases',
+  author = 'Ryan "Lanny" Jenkins',
+  author_email = 'lan.rogers.book@gmail.com',
+  url = 'https://github.com/RyanJenkins/tripphrase',
+  download_url = 'https://github.com/RyanJenkins/tripphrase/archive/0.3.tar.gz',
+  keywords = ['tripphrase', 'utility'],
+  classifiers = [],
+)
+
