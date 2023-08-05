b2b37d6d6b5a5a480a3209fc24208b19a0b8c640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:26 +0000

    Add package odoo12-addon-connector-elasticsearch/12.0.1.0.0/odoo12_addon_connector_elasticsearch-12.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo12-addon-connector-elasticsearch/odoo12_addon_connector_elasticsearch-12.0.1.0.0-py3-none-any.whl/odoo/addons/connector_elasticsearch/models/se_backend_elasticsearch.py
@@ -0,0 +1,15 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class SeBackendElasticsearch(models.Model):
+
+    _name = "se.backend.elasticsearch"
+    _inherit = "se.backend.spec.abstract"
+    _description = "Elasticsearch Backend"
+
+    _search_engine_name = "elasticsearch"
+
+    es_server_host = fields.Char(string="ElasticSearch host")
