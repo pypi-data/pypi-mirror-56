31ad77989a1ea183123fa65a9938930e6d7eedf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:27 +0000

    Add package torch-inception-resnet-v2/0.0.14/torch_inception_resnet_v2-0.0.14.tar.gz

---
+++ packages/torch-inception-resnet-v2/torch_inception_resnet_v2-0.0.14.tar.gz/torch_inception_resnet_v2-0.0.14/torch_inception_resnet_v2/blocks/reduction/a.py
@@ -0,0 +1,23 @@
+"""
+Responsible for implementing reduction block A
+"""
+from torch import nn
+from torch_inception_resnet_v2.utils import ConvolutionConfig as Convolution
+from torch_inception_resnet_v2.hparams import K, L, M, N
+from torch_inception_resnet_v2.blocks.reduction.reduction_block import ReductionBlock
+from torch_inception_resnet_v2.utils.branch import Branch
+
+INPUT_SIZE = 384
+
+
+class ReductionA(ReductionBlock):
+    def __init__(self):
+        left = nn.MaxPool2d(3, padding=0, stride=2)
+        center = Branch(INPUT_SIZE, Convolution(N, 3, 2, 'same'))
+        right = Branch(INPUT_SIZE, Convolution(K, 1, 1, 'same'), Convolution(L, 3), Convolution(M, 3, 2, 'valid'))
+        super().__init__(left, center, right)
+
+
+if __name__ == '__main__':
+    reduction = ReductionA()
+    print(reduction)
