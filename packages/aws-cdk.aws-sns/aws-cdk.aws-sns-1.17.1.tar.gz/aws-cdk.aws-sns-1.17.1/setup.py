b7b813e696e5d657973fd0ddfb26f37eb5a6f111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:17 +0000

    Add package aws-cdk.aws-sns/1.17.1/aws-cdk.aws-sns-1.17.1.tar.gz

---
+++ packages/aws-cdk.aws-sns/aws-cdk.aws-sns-1.17.1.tar.gz/aws-cdk.aws-sns-1.17.1/setup.py
@@ -0,0 +1,54 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-sns",
+    "version": "1.17.1",
+    "description": "CDK Constructs for AWS SNS",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_sns",
+        "aws_cdk.aws_sns._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_sns._jsii": [
+            "aws-sns@1.17.1.jsii.tgz"
+        ],
+        "aws_cdk.aws_sns": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.20.7",
+        "publication>=0.0.3",
+        "aws-cdk.aws-cloudwatch~=1.17,>=1.17.1",
+        "aws-cdk.aws-events~=1.17,>=1.17.1",
+        "aws-cdk.aws-iam~=1.17,>=1.17.1",
+        "aws-cdk.core~=1.17,>=1.17.1"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
