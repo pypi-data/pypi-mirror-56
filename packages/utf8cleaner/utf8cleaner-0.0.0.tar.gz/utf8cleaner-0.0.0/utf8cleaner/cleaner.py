0a15084d0ba06b0ce962854ee8eabcea16c09ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:24 +0000

    Add package utf8cleaner/0.0.0/utf8cleaner-0.0.0.tar.gz

---
+++ packages/utf8cleaner/utf8cleaner-0.0.0.tar.gz/utf8cleaner-0.0.0/utf8cleaner/cleaner.py
@@ -0,0 +1,25 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+
+def clean(filename):
+    output_filename = f"{filename}.clean"
+    logger.info(f"Cleaning file {filename} and saving to {output_filename}")
+    i = 0
+    with open(filename, 'rb') as fi:
+        with open(output_filename, 'wb') as fo:
+            while 1:
+                byte_s = fi.read(1)
+                if not byte_s:
+                    break
+                try:
+                    # `unicode()` function is gone from python3 and should be replaced with
+                    # `str()` - see https://stackoverflow.com/a/38860645/3441106
+                    u = str(byte_s, "utf-8")
+                    fo.write(byte_s)
+                except:
+                    logging.error(f"skipped bad byte at offset {i}!")
+
+            i = i + 1
