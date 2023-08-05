ad17855682d1f5fd9dfaf860795dd6ae4fb2e84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:28 +0000

    Add package cpaassdk/1.0.0/cpaassdk-1.0.0.tar.gz

---
+++ packages/cpaassdk/cpaassdk-1.0.0.tar.gz/cpaassdk-1.0.0/tests/test_resources/test_twofactor.py
@@ -0,0 +1,72 @@
+import responses
+
+from cpaassdk.resources.twofactor import Twofactor
+from tests.util import decallmethods, deep_equal
+from tests.mocker import mock
+
+
+@decallmethods(responses.activate)
+class TestTwofactor:
+  def base_url(self, api):
+    return '/cpaas/auth/v1/{}'.format(api.user_id)
+
+  def client(self, api):
+    return Twofactor(api)
+
+  def test_send_code(self, api):
+    client = Twofactor(api)
+    params = {
+      'destination_address': [ '345', '567' ],
+      'method': 'sms',
+      'length': 6,
+      'type': 'numeric',
+      'expiry': 120,
+      'message': 'Hi'
+    }
+
+    url = '{}/codes'.format(self.base_url(api))
+
+    mock(url, 'POST')
+
+    response = client.send_code(params)
+
+    assert response['__for_test__']['url'] == api.config.base_url + url
+
+  def test_verify_code(self, api):
+    client = Twofactor(api)
+    params = {
+      'code_id': 'test-code-id',
+      'verification_code': 'test-verification-code'
+    }
+
+    url = '{}/codes/{}/verify'.format(self.base_url(api), params.get('code_id'))
+    mock(url, 'PUT')
+    response = client.verify_code(params)
+    assert response['__for_test__']['url'] == api.config.base_url + url
+
+  def test_resend_code(self, api):
+    client = Twofactor(api)
+    params = {
+      'destination_address': [ '345', '567' ],
+      'method': 'sms',
+      'length': 6,
+      'type': 'numeric',
+      'expiry': 120,
+      'message': 'Hi'
+    }
+
+    url = '{}/codes'.format(self.base_url(api))
+    mock(url, 'POST')
+    response = client.send_code(params)
+    assert response['__for_test__']['url'] == api.config.base_url + url
+
+  def test_delete_code(self, api):
+    client = Twofactor(api)
+    params = {
+      'code_id': 'test-code_id'
+    }
+
+    url = '{}/codes/{}'.format(self.base_url(api), params.get('code_id'))
+    mock(url, 'PUT')
+    response = client.delete_code(params)
+    assert response['__for_test__']['url'] == api.config.base_url + url
