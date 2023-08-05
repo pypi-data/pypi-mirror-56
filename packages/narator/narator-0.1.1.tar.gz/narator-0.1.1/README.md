c171fa17c49cdc80a6922b1eb89c2abda2b2b60f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:16 +0000

    Add package narator/0.1.1/narator-0.1.1.tar.gz

---
+++ packages/narator/narator-0.1.1.tar.gz/narator-0.1.1/README.md
@@ -0,0 +1,30 @@
+#### Usage
+
+  - virtualenv --python=python3 sandbox
+  - . sandbox/bin/activate
+  - pip install -r requirements.txt
+  - GITHUB_TOKEN=<auth-token-here> `python narator/narator.py <url-to-work-report-issue>`
+
+
+#### Example
+
+Issue description:
+
+```
+Title 1
+
+todo create PR
+done investigate
+done discuss with team#1
+```
+
+Output:
+
+```markdown
+#### Title 1
+  - DONE:
+    - investigate
+    - discuss with team#1
+  - TODO
+    - create PR
+```
---
+++ packages/narator/narator-0.1.1.tar.gz/narator-0.1.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='narator',
+    packages=['narator'],
+    scripts=['narator/narator'],
+    version='0.1.1',
+    description='Generate markdown work-reports from the body of github issues.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='',
+    author='Mihai Dincă',
+    author_email='dincamihai@gmail.com',
+    license='MIT',
+    include_package_data=True,
+    package_data={
+        'narator': ['templates/*']
+    },
+    install_requires=[
+        "jinja2",
+        "requests"
+    ],
+    tests_requires=[
+        'pytest',
+    ],
+    zip_safe=False
+)
