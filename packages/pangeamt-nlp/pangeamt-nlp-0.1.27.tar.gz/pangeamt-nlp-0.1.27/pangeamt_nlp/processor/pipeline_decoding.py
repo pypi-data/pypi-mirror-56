b2f24729d492e5cf554e2bb1a8aaaa49cf2e41f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:05 +0000

    Add package pangeamt-nlp/0.1.27/pangeamt-nlp-0.1.27.tar.gz

---
+++ packages/pangeamt-nlp/pangeamt-nlp-0.1.27.tar.gz/pangeamt-nlp-0.1.27/pangeamt_nlp/processor/pipeline_decoding.py
@@ -0,0 +1,22 @@
+from typing import List
+from pangeamt_nlp.processor.base.normalizer_base import NormalizerBase
+from pangeamt_nlp.seg import Seg
+
+
+class PipelineDecoding:
+    def __init__(self, processors: List[NormalizerBase]):
+        self._processors = processors
+        self._inverted_processors = reversed(processors)
+
+    def process_src(self, seg: Seg):
+        for p in self._processors:
+            p.process_src_decoding(seg)
+
+    def process_tgt(self, seg: Seg):
+        for p in self._inverted_processors:
+            p.process_tgt_decoding(seg)
+
+    @staticmethod
+    def create_from_dict():
+        pass
+        #TODO
