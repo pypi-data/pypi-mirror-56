5cb89c884d82de1ab54c28cde4080730324cac99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:31 +0000

    Add package tengu/0.1.17/tengu-0.1.17-py2.py3-none-any.whl

---
+++ packages/tengu/tengu-0.1.17-py2.py3-none-any.whl/tengu/app.py
@@ -0,0 +1,33 @@
+import os
+from .factory import FlaskFactory
+#from .views import obx
+from .models import db
+
+
+def get_aws_keys():
+    import hvac
+    # client = hvac.Client(
+    #             url=os.environ.get('VAULT_ADDR', 'http://localhost:8200'),
+    #             token=os.environ.get('VAULT_TOKEN', 'null'),
+    #             )
+    # creds = client.read('aws/creds/{}'.format(os.environ.get('VAULT_ROLE', 'default')))
+    # creds.update({'aws_region': 'us-east-1'})
+    creds = {
+        'access_key': os.environ.get('AWS_ACCESS_KEY_ID'),
+        'secret_key': os.environ.get('AWS_SECRET_ACCESS_KEY')
+        }
+    return creds
+
+
+def get_app():
+    factory = FlaskFactory()
+    
+#     blueprint_dicts = [
+#             dict(
+#                 bp=obx,
+#             )
+#         ]
+    
+    app = factory.initiallize_flask(db)  # , blueprint_dicts)
+    app.config.update(get_aws_keys())
+    return app
