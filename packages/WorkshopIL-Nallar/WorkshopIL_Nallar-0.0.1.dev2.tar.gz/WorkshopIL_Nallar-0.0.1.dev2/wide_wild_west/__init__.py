26450f54daeb6f76444b1a8112aaa1eda0f26ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:48 +0000

    Add package WorkshopIL-Nallar/0.0.1.dev2/WorkshopIL_Nallar-0.0.1.dev2.tar.gz

---
+++ packages/WorkshopIL-Nallar/WorkshopIL_Nallar-0.0.1.dev2.tar.gz/WorkshopIL_Nallar-0.0.1.dev2/wide_wild_west/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__name__ = 'WorkshopIL_Nallar'
+__version__ = '0.0.1.dev2'
+__author__ = 'Thomas LC'
+__author_email__ = 'thomas.le-corre@telecomnnancy.net'
+
+__import__('pkg_resources').declare_namespace(__name__)
