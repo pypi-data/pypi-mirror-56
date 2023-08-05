36417bbcd042d411d6ef0645e20318b86790c090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:09 +0000

    Add package gch747/0.0.2/gch747-0.0.2-py3-none-any.whl

---
+++ packages/gch747/gch747-0.0.2-py3-none-any.whl/gch747/__init__.py
@@ -0,0 +1,7 @@
+#from gch747.fct_normplot import *
+#from gch747.fct_regress import *
+#from gch747.fct_xlsread import *
+#from gch747.fct_PCA_NIPALS import *
+
+import gch747.fct_pca_NIPALS as PCA_nipals
+import gch747.fct_pca_SVD as PCA_SVD
