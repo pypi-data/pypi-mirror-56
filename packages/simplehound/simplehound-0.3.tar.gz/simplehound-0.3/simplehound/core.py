0f5083df5e79a148ba62802bb43d59ea02290d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:03 +0000

    Add package simplehound/0.3/simplehound-0.3.tar.gz

---
+++ packages/simplehound/simplehound-0.3.tar.gz/simplehound-0.3/simplehound/core.py
@@ -0,0 +1,127 @@
+"""
+Simplehound core.
+"""
+import base64
+import json
+from typing import Dict, List, Tuple
+
+import requests
+
+## Const
+HTTP_OK = 200
+BAD_API_KEY = 401
+
+## API urls
+URL_DETECTIONS_BASE = "https://{}.sighthoundapi.com/v1/detections"
+ALLOWED_MODES = ["dev", "prod"]
+
+DETECTIONS_PARAMS = (
+    ("type", "all"),
+    ("faceOption", "gender,age"),
+)
+
+
+def bbox_to_tf_style(bbox: Dict, img_width: int, img_height: int) -> Tuple:
+    """
+    Convert Sighthound bounding box to tensorflow box style.
+    
+    In Tensorflow the bounding box is defined by the tuple (y_min, x_min, y_max, x_max)
+    where the coordinates are floats in the range [0.0, 1.0] and
+    relative to the width and height of the image.
+    For example, if an image is 100 x 200 pixels (height x width) and the bounding
+    box is `(0.1, 0.2, 0.5, 0.9)`, the upper-left and bottom-right coordinates of
+    the bounding box will be `(40, 10)` to `(180, 50)` (in (x,y) coordinates).
+    """
+
+    decimals = 5
+    x_min = round(bbox["x"] / img_width, decimals)
+    x_max = round((bbox["x"] + bbox["width"]) / img_width, decimals)
+    y_min = round(bbox["y"] / img_height, decimals)
+    y_max = round((bbox["y"] + bbox["height"]) / img_height, decimals)
+    return (y_min, x_min, y_max, x_max)
+
+
+def encode_image(image: bytes) -> str:
+    """base64 encode an image."""
+    return base64.b64encode(image).decode("ascii")
+
+
+def get_faces(detections: Dict) -> List[Dict]:
+    """
+    Get the list of the faces.
+    """
+    faces = []
+    for obj in detections["objects"]:
+        if not obj["type"] == "face":
+            continue
+        face = {}
+        face["gender"] = obj["attributes"]["gender"]
+        face["age"] = obj["attributes"]["age"]
+        face["boundingBox"] = obj["boundingBox"]
+        faces.append(face)
+    return faces
+
+
+def get_people(detections: Dict) -> List[Dict]:
+    """
+    Get the list of the people.
+    """
+    people = []
+    for obj in detections["objects"]:
+        if not obj["type"] == "person":
+            continue
+        person = {}
+        person["boundingBox"] = obj["boundingBox"]
+        people.append(person)
+    return people
+
+
+def get_metadata(detections: Dict) -> Dict:
+    """
+    Get the detection metadata.
+    """
+    metadata = {}
+    metadata["image_width"] = detections["image"]["width"]
+    metadata["image_height"] = detections["image"]["height"]
+    metadata["requestId"] = detections["requestId"]
+    return metadata
+
+
+def run_detection(
+    image_encoded: str, api_key: str, url_detections: str
+) -> requests.models.Response:
+    """Post an image to Sighthound."""
+    headers = {"Content-type": "application/json", "X-Access-Token": api_key}
+    response = requests.post(
+        url_detections,
+        headers=headers,
+        params=DETECTIONS_PARAMS,
+        data=json.dumps({"image": image_encoded}),
+    )
+    return response
+
+
+class SimplehoundException(Exception):
+    pass
+
+
+class cloud:
+    """Work with Sighthound cloud."""
+
+    def __init__(self, api_key: str, mode: str = "dev"):
+        if not mode in ALLOWED_MODES:
+            raise SimplehoundException(
+                f"Mode {mode} is not allowed, must be dev or prod"
+            )
+        self._api_key = api_key
+        self._url_detections = URL_DETECTIONS_BASE.format(mode)
+
+    def detect(self, image: bytes) -> Dict:
+        """Run detection on an image (bytes)."""
+        response = run_detection(
+            encode_image(image), self._api_key, self._url_detections
+        )
+        if response.status_code == HTTP_OK:
+            return response.json()
+        elif response.status_code == BAD_API_KEY:
+            raise SimplehoundException(f"Bad API key for Sightound")
---
+++ packages/simplehound/simplehound-0.3.tar.gz/simplehound-0.3/tests/test_simplehound.py
@@ -0,0 +1,142 @@
+import pytest
+import requests
+import requests_mock
+
+import simplehound.core as hound
+
+MOCK_API_KEY = "mock_api_key"
+MOCK_BYTES = b"Test"
+B64_ENCODED_MOCK_BYTES = "VGVzdA=="
+URL_DETECTIONS_DEV = hound.URL_DETECTIONS_BASE.format("dev")
+URL_DETECTIONS_PROD = hound.URL_DETECTIONS_BASE.format("prod")
+
+DETECTIONS = {
+    "image": {"width": 960, "height": 480, "orientation": 1},
+    "objects": [
+        {
+            "type": "face",
+            "boundingBox": {"x": 305, "y": 151, "height": 28, "width": 30},
+            "attributes": {
+                "gender": "male",
+                "genderConfidence": 0.9733,
+                "age": 33,
+                "ageConfidence": 0.7801,
+                "frontal": True,
+            },
+        },
+        {
+            "type": "face",
+            "boundingBox": {"x": 855, "y": 147, "height": 29, "width": 24},
+            "attributes": {
+                "gender": "male",
+                "genderConfidence": 0.9834,
+                "age": 37,
+                "ageConfidence": 0.5096,
+                "frontal": False,
+            },
+        },
+        {
+            "type": "person",
+            "boundingBox": {"x": 227, "y": 133, "height": 245, "width": 125},
+        },
+        {
+            "type": "person",
+            "boundingBox": {"x": 833, "y": 137, "height": 268, "width": 93},
+        },
+    ],
+    "requestId": "467f195c4bbf46c69f964b59884dee04",
+}
+
+FACES = [
+    {
+        "boundingBox": {"x": 305, "y": 151, "height": 28, "width": 30},
+        "gender": "male",
+        "age": 33,
+    },
+    {
+        "boundingBox": {"x": 855, "y": 147, "height": 29, "width": 24},
+        "gender": "male",
+        "age": 37,
+    },
+]
+
+PEOPLE = [
+    {"boundingBox": {"x": 227, "y": 133, "height": 245, "width": 125}},
+    {"boundingBox": {"x": 833, "y": 137, "height": 268, "width": 93}},
+]
+
+METADATA = {
+    "image_width": 960,
+    "image_height": 480,
+    "requestId": "467f195c4bbf46c69f964b59884dee04",
+}
+
+
+def test_bbox_to_tf_style():
+    bbox = {"x": 227, "y": 133, "height": 245, "width": 125}
+    img_width = 960
+    img_height = 480
+    assert hound.bbox_to_tf_style(bbox, img_width, img_height) == (
+        0.27708,
+        0.23646,
+        0.7875,
+        0.36667,
+    )
+
+
+def test_encode_image():
+    assert hound.encode_image(MOCK_BYTES) == B64_ENCODED_MOCK_BYTES
+
+
+def test_get_faces():
+    assert hound.get_faces(DETECTIONS) == FACES
+
+
+def test_get_people():
+    assert hound.get_people(DETECTIONS) == PEOPLE
+
+
+def test_get_metadata():
+    assert hound.get_metadata(DETECTIONS) == METADATA
+
+
+def test_good_run_detection():
+    with requests_mock.Mocker() as mock_req:
+        mock_req.post(URL_DETECTIONS_DEV, status_code=hound.HTTP_OK, json=DETECTIONS)
+        response = hound.run_detection(
+            B64_ENCODED_MOCK_BYTES, MOCK_API_KEY, URL_DETECTIONS_DEV
+        )
+        assert response.json() == DETECTIONS
+
+
+def test_cloud_init():
+    """Test that the dev or prod url are being set correctly."""
+    api_dev = hound.cloud(MOCK_API_KEY)
+    assert api_dev._url_detections == URL_DETECTIONS_DEV
+
+    api_prod = hound.cloud(MOCK_API_KEY, mode="prod")
+    assert api_prod._url_detections == URL_DETECTIONS_PROD
+
+    with pytest.raises(hound.SimplehoundException) as exc:
+        hound.cloud(MOCK_API_KEY, mode="bad")
+    assert str(exc.value) == "Mode bad is not allowed, must be dev or prod"
+
+
+def test_cloud_detect_good():
+    with requests_mock.Mocker() as mock_req:
+        mock_req.post(URL_DETECTIONS_DEV, status_code=hound.HTTP_OK, json=DETECTIONS)
+        api = hound.cloud(MOCK_API_KEY)
+        detections = api.detect(MOCK_BYTES)
+        assert detections == DETECTIONS
+
+
+def test_cloud_detect_bad_key():
+    with pytest.raises(
+        hound.SimplehoundException
+    ) as exc, requests_mock.Mocker() as mock_req:
+        mock_req.post(
+            URL_DETECTIONS_DEV, status_code=hound.BAD_API_KEY, json=DETECTIONS
+        )
+        api = hound.cloud(MOCK_API_KEY)
+        detections = api.detect(MOCK_BYTES)
+    assert str(exc.value) == "Bad API key for Sightound"
