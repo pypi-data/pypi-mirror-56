a2bb6070fdf8dc29eb386f4c6ef0ff7c99bdd9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:15 +0000

    Add package padmet/4.0/padmet-4.0.tar.gz

---
+++ packages/padmet/padmet-4.0.tar.gz/padmet-4.0/README.md
@@ -0,0 +1,13 @@
+author: meziane aite (meziane.aite@inria.fr)
+
+## Description:
+
+The PADMet package allows conciliating genomics and metabolic network information used to produce a genome-scale constraint-based metabolic model within a database that traces all the reconstruction process steps. It allows representing the metabolic model in the form of a Wiki containing all the used/traced information. Other standard outputs are made available with the package. 
+
+The main concept underlying PADMet-Package is to provide solutions that ensure the consistency, the internal standardization and the reconciliation of the information used within any workflow that combines several tools involving metabolic networks reconstruction or analysis. The PADMet package is at the core of the AuReMe workflow, dedicated to the primary reconstruction of genome-scale metabolic networks from raw data. It allows the study of organisms for which few experimental data are available. Its main feature is to undergo the reconstruction of the metabolic network by combining several heterogeneous knowledge and data sources, including the information reported by several scaffold metabolic networks for cousin species.
+
+## Documentation:
+
+https://padmet.readthedocs.io/en/latest/
+
+
---
+++ packages/padmet/padmet-4.0.tar.gz/padmet-4.0/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Definition of setup function for setuptools module."""
+
+# Standard imports
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+################################################################################
+
+class PyTest(TestCommand):
+    """Call tests with the custom 'python setup.py test' command."""
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main()
+        sys.exit(errno)
+
+################################################################################
+ 
+setup(
+ 
+    name='padmet',
+    version="4.0",
+ 
+    packages=find_packages(),
+ 
+    author="Meziane AITE",
+    author_email="meziane.aite@inria.fr",
+ 
+    description="Padmet package for metabolic network",
+ 
+    long_description=open('README.md', encoding='utf8').read(),
+ 
+    
+    install_requires= ["docopt==0.6.2",
+                       "python-libsbml==5.18.0",
+                       "cobra==0.14.0",
+                       "biopython==1.74",
+                       "seaborn==0.9.0",
+                       "matplotlib==3.1.1",
+                       "grequests==0.4.0",
+                       'lxml==4.3.4',
+                       'scipy==1.3.0',
+                       ],
+ 
+    include_package_data=True,
+ 
+    url='https://github.com/AuReMe/padmet',
+ 
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved",
+        "Natural Language :: French",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+)
