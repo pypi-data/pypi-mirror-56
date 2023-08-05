44c40057eca57b785459a69a5dd8cee20cc53e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:33 +0000

    Add package verity-sdk/0.0.9/verity-sdk-0.0.9.tar.gz

---
+++ packages/verity-sdk/verity-sdk-0.0.9.tar.gz/verity-sdk-0.0.9/verity_sdk/transports.py
@@ -0,0 +1,9 @@
+import requests
+
+
+def send_message(url: str, message: bytes):
+  try:
+    return requests.post(url, data=message, headers={'Content-Type': 'application/octet-stream'})
+  except requests.RequestException as e:
+    print('ERROR: Failed to POST message to {}'.format(url))
+    raise e
