9a058ee0c831a9d5127aac7a28da0b82c2997308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:55 +0000

    Add package conport/0.0.4/conport-0.0.4.tar.gz

---
+++ packages/conport/conport-0.0.4.tar.gz/conport-0.0.4/conport/config.py
@@ -0,0 +1,14 @@
+import configparser
+import os
+
+
+def parse_config_file(config_file):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    return config._sections
+
+
+def get_default_config():
+    config_file = os.path.join(os.path.abspath(
+        os.path.dirname(__file__)), "cfg.ini")
+    return parse_config_file(config_file)
