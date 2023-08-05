26f2643ad6e7a6fc3c3d084c91708591057d1ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:25 +0000

    Add package et-micc-build/0.2.3/et-micc-build-0.2.3.tar.gz

---
+++ packages/et-micc-build/et-micc-build-0.2.3.tar.gz/et-micc-build-0.2.3/et_micc_build/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package et_micc_build
+=====================
+
+Top-level package for et_micc_build.
+"""
+import et_micc_build.cli_micc_build
+
+__version__ = "0.2.3"
