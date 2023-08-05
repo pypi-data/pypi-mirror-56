9aae920558ca85e240373de650c6af39c185e8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:08 +0000

    Add package aqtinstall/0.5.1/aqtinstall-0.5.1.tar.gz

---
+++ packages/aqtinstall/aqtinstall-0.5.1.tar.gz/aqtinstall-0.5.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import io
+import os
+
+from setuptools import setup
+
+
+def readme():
+    with io.open(os.path.join(os.path.dirname(__file__), 'README.rst'), mode="r", encoding="UTF-8") as readmef:
+        return readmef.read()
+
+
+setup(name='aqtinstall',
+      version='0.5.1',
+      description='Another unofficial Qt installer',
+      url='http://github.com/miurahr/aqtinstall',
+      license='MIT',
+      long_description=readme(),
+      author='Hioshi Miura',
+      author_email='miurahr@linux.com',
+      packages=["aqt"],
+      package_data={'aqt': ['*.yml', "*.json", "*.ini"]},
+      install_requires=['requests', 'six', 'py7zr'],
+      extras_require={'dev': ['pytest', 'pytest-pep8', 'pytest-cov', 'flake8']},
+      scripts=["bin/aqt"],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Environment :: X11 Applications :: Qt',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: C++',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries',
+      ],
+      )
