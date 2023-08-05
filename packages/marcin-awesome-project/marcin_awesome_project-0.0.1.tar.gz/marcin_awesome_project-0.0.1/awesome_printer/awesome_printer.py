b335e9ce42787a2e1663835a5d31cf3b4590704b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:39 +0000

    Add package marcin-awesome-project/0.0.1/marcin_awesome_project-0.0.1.tar.gz

---
+++ packages/marcin-awesome-project/marcin_awesome_project-0.0.1.tar.gz/marcin_awesome_project-0.0.1/awesome_printer/awesome_printer.py
@@ -0,0 +1,2 @@
+def awesome_print():
+    print("This is completely normal print, but awesome")
---
+++ packages/marcin-awesome-project/marcin_awesome_project-0.0.1.tar.gz/marcin_awesome_project-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='marcin_awesome_project',
+    version='0.0.1',
+    author='Marcin',
+    author_email='marcin.sopot@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Some awesome project'
+)
