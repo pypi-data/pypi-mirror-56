01c075d9125a8198c3fdf04d4f08dcdcd8dbf1bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:33 +0000

    Add package guet/2.0.0/guet-2.0.0-py3-none-any.whl

---
+++ packages/guet/guet-2.0.0-py3-none-any.whl/guet/commands/init.py
@@ -0,0 +1,26 @@
+from guet.config.already_initialized import already_initialized
+from guet.config.initialize import initialize
+from .command import Command
+
+
+class InitDataSourceCommand(Command):
+
+    def __init__(self, args):
+        super().__init__(args, args_needed=False)
+
+    def execute_hook(self):
+        extra_arguments_given = len(self.args) > 0
+        if extra_arguments_given:
+            print('Invalid arguments.\n\n   {}'.format(self.help()))
+        else:
+            if not already_initialized():
+                initialize()
+            else:
+                print('Config folder already exists.')
+
+    def help(self):
+        return ''
+
+    @classmethod
+    def help_short(cls) -> str:
+        return 'Initialize guet for use'
---
+++ packages/guet/guet-2.0.0-py3-none-any.whl/guet/config/initialize.py
@@ -0,0 +1,26 @@
+from os import mkdir
+from os.path import join
+
+from guet import constants, __version__
+from guet.config import configuration_directory
+from guet.files.write_lines import write_lines
+
+
+def _prepend_version_number_to_config_file():
+    write_lines(join(configuration_directory, constants.CONFIG), [f'{__version__}\n', '\n'])
+
+
+def initialize():
+    mkdir(configuration_directory)
+    _create_file_with_name(join(configuration_directory, constants.COMMITTER_NAMES))
+    _create_file_with_name(join(configuration_directory, constants.AUTHOR_NAME))
+    _create_file_with_name(join(configuration_directory, constants.AUTHOR_EMAIL))
+    _create_file_with_name(join(configuration_directory, constants.COMMITTERS))
+    _create_file_with_name(join(configuration_directory, constants.COMMITTERS_SET))
+    _create_file_with_name(join(configuration_directory, constants.CONFIG))
+    _create_file_with_name(join(configuration_directory, constants.ERRORS))
+    _prepend_version_number_to_config_file()
+
+
+def _create_file_with_name(file_name: str) -> None:
+    open(file_name, 'w').close()
