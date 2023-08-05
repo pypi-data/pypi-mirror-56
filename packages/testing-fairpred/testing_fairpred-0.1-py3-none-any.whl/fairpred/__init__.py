be1c8d88eab20f5a6ab0f100edb3ae94a90eeba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:22 +0000

    Add package testing-fairpred/0.1/testing_fairpred-0.1-py3-none-any.whl

---
+++ packages/testing-fairpred/testing_fairpred-0.1-py3-none-any.whl/fairpred/__init__.py
@@ -0,0 +1,4 @@
+
+from .fair_predictor import FairPredictor
+
+name = "fairpred"
---
+++ packages/testing-fairpred/testing_fairpred-0.1-py3-none-any.whl/fairpred/fair_predictor.py
@@ -0,0 +1,29 @@
+"""
+	FairPred FairPredict:
+	Gets data and protected column and 
+"""
+
+import os
+import pandas as pandas
+import numpy as np 
+
+
+class FairPredictor:
+	"""
+
+	"""
+
+	def __init__(self, target_column, protected_column, fairness_metric):
+		self.target_column = target_column
+		self.protected_column = protected_column
+		self.fairness_metric = fairness_metric
+
+	def fit(self, input_data):
+		print("yeet")
+		pass
+
+	def predict(self):
+		print("yeet, plus")
+		pass
+
+
