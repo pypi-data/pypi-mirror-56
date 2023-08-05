74ccebba55faac49331ad751a0bd36724d4e9f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:26 +0000

    Add package songkick/0.2.0/songkick-0.2.0.tar.gz

---
+++ packages/songkick/songkick-0.2.0.tar.gz/songkick-0.2.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['songkick']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dateutil>=2.8,<3.0', 'requests>=2,<3']
+
+setup_kwargs = {
+    'name': 'songkick',
+    'version': '0.2.0',
+    'description': 'A Python SDK for the Songkick API.',
+    'long_description': '# Songkick API Python SDK\n\nThis is a Python SDK for the [Songkick API](https://www.songkick.com/developer/).\n',
+    'author': 'Craig Anderson',
+    'author_email': 'craiga@craiga.id.au',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
