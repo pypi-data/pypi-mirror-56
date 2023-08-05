debae2657ea74c779f980858628eeed31844f2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:58 +0000

    Add package bobocep/0.35.0/bobocep-0.35.0.tar.gz

---
+++ packages/bobocep/bobocep-0.35.0.tar.gz/bobocep-0.35.0/bobocep/setup/distributed/bobo_dist_constants.py
@@ -0,0 +1,16 @@
+TRANSITION = "transition"
+CLONE = "clone"
+HALT = "halt"
+FINAL = "final"
+ACTION = "action"
+
+SYNC_REQ = "sync_req"
+SYNC_RES = "sync_res"
+
+NFA_NAME = "nfa_name"
+RUN_ID = "run_id"
+STATE_FROM = "state_from"
+STATE_TO = "state_to"
+EVENT = "event"
+HISTORY = "history"
+HANDLERS = "handlers"
