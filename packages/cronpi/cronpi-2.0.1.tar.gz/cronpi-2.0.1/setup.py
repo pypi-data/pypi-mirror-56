eeb186a45eab3551be7f84bb2e95b529f7792b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:16 +0000

    Add package cronpi/2.0.1/cronpi-2.0.1.tar.gz

---
+++ packages/cronpi/cronpi-2.0.1.tar.gz/cronpi-2.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cronpi",
+    version="2.0.1",
+    author="dakc",
+    author_email="dakc@outlook.jp",
+    description="a small crontab deploying package for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dakc/cronpi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python ",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    License="MIT",
+)
