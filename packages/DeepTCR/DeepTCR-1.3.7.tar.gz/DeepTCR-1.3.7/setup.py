01dc323b438c5471c56c1fd97689019dcd137255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:34 +0000

    Add package DeepTCR/1.3.7/DeepTCR-1.3.7.tar.gz

---
+++ packages/DeepTCR/DeepTCR-1.3.7.tar.gz/DeepTCR-1.3.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import sys
+
+
+if sys.version_info.major != 3:
+    raise RuntimeError("DeepTCR requires Python 3")
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name="DeepTCR",
+    description="Deep Learning Methods for Parsing T-Cell Receptor Sequencing (TCRSeq) Data",
+    version="1.3.7",
+    author="John-William Sidhom",
+    author_email="jsidhom1@jhmi.edu",
+    packages=find_packages(),
+    install_requires = required,
+    dependency_links = ['https://github.com/jacoblevine/PhenoGraph/master#egg=PhenoGraph'],
+    url="https://github.com/sidhomj/DeepTCR",
+    license="LICENSE",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown'
+)
+
