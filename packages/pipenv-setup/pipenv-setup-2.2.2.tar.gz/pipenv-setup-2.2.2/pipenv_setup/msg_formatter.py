0fda4e31fc43639f9b9f03753230c8e3307ec754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:07 +0000

    Add package pipenv-setup/2.2.2/pipenv-setup-2.2.2.tar.gz

---
+++ packages/pipenv-setup/pipenv-setup-2.2.2.tar.gz/pipenv-setup-2.2.2/pipenv_setup/msg_formatter.py
@@ -0,0 +1,63 @@
+"""
+All kinds of messages pipenv-setup prints to console
+"""
+
+from vistir.compat import Path
+from colorama import Fore
+
+
+def colorful_help():
+    string = ""
+    string += "Commands:\n"
+    string += (
+        "  "
+        + Fore.GREEN
+        + "sync"
+        + Fore.RESET
+        + "\t\tsync Pipfile.lock with setup.py\n"
+    )
+    string += (
+        "  "
+        + Fore.BLUE
+        + "check"
+        + Fore.RESET
+        + "\t\tcheck whether Pipfile is consistent with setup.py.\n  \t\tNon-zero exit code"
+        " if there is inconsistency\n  \t\t(package missing; version incompatible)\n"
+    )
+    return string
+
+
+def missing_file(file):  # type: (Path) -> str
+    return "file %s not found" % file.name
+
+
+def setup_not_found():
+    return "setup.py not found under current directory"
+
+
+def no_sync_performed():
+    return "can not perform sync"
+
+
+def checked_no_problem():
+    return "No version conflict or missing packages/dependencies found in setup.py!"
+
+
+def update_success(
+    default_package_count, dev_package_count=0
+):  # type: (int, int) -> str
+    """
+    :param default_package_count: The number of updated default packages
+    :param dev_package_count: The number of updated dev packages
+    """
+    string = (
+        "setup.py successfully updated"
+        + "\n%d default packages from Pipfile.lock synced to setup.py"
+        % default_package_count
+    )
+    if dev_package_count != 0:
+        string += (
+            "\n%d dev packages from Pipfile.lock synced to setup.py"
+            % default_package_count
+        )
+    return string
