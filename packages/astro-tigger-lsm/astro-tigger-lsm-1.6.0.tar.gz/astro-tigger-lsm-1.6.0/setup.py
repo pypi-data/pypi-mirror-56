993a17d64e2c67d3add1a6a189114b27a702facd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:55 +0000

    Add package astro-tigger-lsm/1.6.0/astro-tigger-lsm-1.6.0.tar.gz

---
+++ packages/astro-tigger-lsm/astro-tigger-lsm-1.6.0.tar.gz/astro-tigger-lsm-1.6.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup, find_packages
+
+__version__ = "1.6.0"
+
+requirements = ['astro_kittens', 'numpy', 'scipy', 'astlib', 'astropy', 'future']
+
+scripts = [
+    'Tigger/bin/tigger-convert',
+    'Tigger/bin/tigger-make-brick',
+    'Tigger/bin/tigger-restore',
+    'Tigger/bin/tigger-tag',
+]
+
+package_data = {
+}
+
+extras_require = {
+}
+
+
+setup(
+    name="astro-tigger-lsm",
+    version=__version__,
+    packages=find_packages(),
+    extras_require=extras_require,
+    scripts=scripts,
+    package_data=package_data,
+    description="Python libraries and command-line tools for manipulating Tigger-format LSMs",
+    author="Oleg Smirnov",
+    author_email="osmirnov@gmail.com",
+    url="https://github.com/ska-sa/tigger-lsm",
+    install_requires=requirements,
+)
+
