62c0ad727d84b46a714cee84f8962915eca4f258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:58 +0000

    Add package ceviche/0.0.2/ceviche-0.0.2-py3-none-any.whl

---
+++ packages/ceviche/ceviche-0.0.2-py3-none-any.whl/ceviche/constants.py
@@ -0,0 +1,9 @@
+from numpy import sqrt
+
+""" Funamental Constants """
+
+EPSILON_0 = 8.85418782e-12        # vacuum permittivity
+MU_0 = 1.25663706e-6              # vacuum permeability
+C_0 = 1 / sqrt(EPSILON_0 * MU_0)  # speed of light in vacuum
+ETA_0 = sqrt(MU_0 / EPSILON_0)    # vacuum impedance
+Q_e = 1.602176634e-19             # funamental charge
