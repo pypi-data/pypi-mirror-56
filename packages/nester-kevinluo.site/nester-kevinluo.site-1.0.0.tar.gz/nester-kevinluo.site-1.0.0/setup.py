31f74eda8e74d6e1bd57e4341a7294714147f9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:14 +0000

    Add package nester-kevinluo.site/1.0.0/nester-kevinluo.site-1.0.0.tar.gz

---
+++ packages/nester-kevinluo.site/nester-kevinluo.site-1.0.0.tar.gz/nester-kevinluo.site-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'nester-kevinluo.site',
+    version     = '1.0.0',
+    py_modules  = ['nester'],
+    author      = 'Kevin Luo',
+    author_email= 'kevin@kevin.net',
+    url         = 'https://kevinluo.site',
+    description = 'A simple printer of nested lists'
+)
