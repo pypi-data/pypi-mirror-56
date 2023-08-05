d23b545fbdc0c00dfde244b5b9379c295adcc591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:10 +0000

    Add package lendsmart-api/0.28.0/lendsmart_api-0.28.0.tar.gz

---
+++ packages/lendsmart-api/lendsmart_api-0.28.0.tar.gz/lendsmart_api-0.28.0/lendsmart_api/objects/__init__.py
@@ -0,0 +1,7 @@
+from .base import Base, Property, MappedObject
+from .dbase import DerivedBase
+from .filtering import and_, or_
+from .document import Document
+from .prediction import PredictionChunk, PredictionWorkflow, PredictionSegment, PredictionInference
+from .loan import LoanApplication
+from .signature import AccountSign
