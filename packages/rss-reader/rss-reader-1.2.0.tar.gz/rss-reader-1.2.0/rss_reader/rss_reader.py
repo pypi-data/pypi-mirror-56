d3b30312b6113a3bbe5784f0b4738d36ecf177c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:50 +0000

    Add package rss-reader/1.2.0/rss-reader-1.2.0.tar.gz

---
+++ packages/rss-reader/rss-reader-1.2.0.tar.gz/rss-reader-1.2.0/rss_reader/rss_reader.py
@@ -0,0 +1,49 @@
+"""Main module"""
+import argparse
+import parsing
+import logging as log
+import sys
+
+v = '1.2.0'
+
+
+def parse():
+    """This function gets all the args from command line and return args namespace"""
+    parser = argparse.ArgumentParser(description='Pure Python command-line RSS reader.')
+    parser.add_argument('source',
+                        help='RSS URL',
+                        type=str)
+    parser.add_argument('--version',
+                        help='Print version info',
+                        action='version',
+                        version='RssReader ' + v)
+    parser.add_argument('--json',
+                        help='Print result as JSON in stdout',
+                        action='store_true')
+    parser.add_argument('--verbose',
+                        help='Outputs verbose status messages',
+                        action='store_true')
+    parser.add_argument('--limit',
+                        help='Limit news topics if this parameter provided',
+                        type=int)
+    return parser.parse_args()
+
+
+def main():
+    """Main function that starts everything"""
+    args = parse()
+    if args.verbose:
+        log.basicConfig(stream=sys.stdout, level=log.INFO,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    log.info('Parsed args successfully')
+    log.info('Parsing url ... ')
+    soup = parsing.get_soup(args.source)
+    log.info('Parsed successfully, printing')
+    if args.json:
+        parsing.print_json(args.limit, soup)
+    else:
+        parsing.print_regular(args.limit, soup)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/rss-reader/rss-reader-1.2.0.tar.gz/rss-reader-1.2.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='rss-reader',
+    version='1.2.0',
+    author='Alexey Artishevskiy',
+    author_email='1337kwiz@gmail.com',
+    description='A simple CLI rss reader',
+    url='https://github.com/Kwizchm/PythonHomework',
+    license='MIT',
+    long_description=read('README.md'),
+    packages=find_packages(),
+    python_requires='>=3.6',
+    keywords='cli rss reader',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+    entry_points={
+        'console_scripts':
+            ['rss-reader=rss_reader.rss_reader.main:main'],
+    }
+)
