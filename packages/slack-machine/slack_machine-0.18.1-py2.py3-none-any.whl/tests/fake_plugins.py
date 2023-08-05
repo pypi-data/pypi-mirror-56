00d01635dc11aea2a1b92cc8dc5371e314b4b0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:39 +0000

    Add package slack-machine/0.18.1/slack_machine-0.18.1-py2.py3-none-any.whl

---
+++ packages/slack-machine/slack_machine-0.18.1-py2.py3-none-any.whl/tests/fake_plugins.py
@@ -0,0 +1,26 @@
+from machine.plugins.base import MachineBasePlugin
+from machine.plugins.decorators import respond_to, listen_to, process
+
+
+class FakePlugin(MachineBasePlugin):
+
+    @respond_to(r'hello')
+    def respond_function(self, msg):
+        pass
+
+    @listen_to(r'hi')
+    def listen_function(self, msg):
+        pass
+
+    @process('some_event')
+    def process_function(self, event):
+        pass
+
+
+class FakePlugin2(MachineBasePlugin):
+
+    def init(self):
+        self.x = 42
+
+    def catch_all(self, event):
+        pass
