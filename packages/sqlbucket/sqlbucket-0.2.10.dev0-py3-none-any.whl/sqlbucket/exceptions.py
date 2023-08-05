b971f5ea5e2922079d933809ec65c6c0771a4dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:32 +0000

    Add package sqlbucket/0.2.10.dev0/sqlbucket-0.2.10.dev0-py3-none-any.whl

---
+++ packages/sqlbucket/sqlbucket-0.2.10.dev0-py3-none-any.whl/sqlbucket/exceptions.py
@@ -0,0 +1,18 @@
+class ProjectNotFound(Exception):
+    pass
+
+
+class ConnectionNotFound(Exception):
+    pass
+
+
+class GroupNotFound(KeyError):
+    pass
+
+
+class OrderNotInRightFormat(TypeError):
+    pass
+
+
+class ReservedVariableNameError(Exception):
+    pass
