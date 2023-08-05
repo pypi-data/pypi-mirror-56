9ca74ec82ac73e04070f05e88bbfffc625aac60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:06 +0000

    Add package general-distribution-functions/0.1/general_distribution_functions-0.1.tar.gz

---
+++ packages/general-distribution-functions/general_distribution_functions-0.1.tar.gz/general_distribution_functions-0.1/general_distribution_functions/__init__.py
@@ -0,0 +1,3 @@
+from .Gaussian_Distribution import Gaussian
+from .Bionomial_Distribution import Binomial
+
---
+++ packages/general-distribution-functions/general_distribution_functions-0.1.tar.gz/general_distribution_functions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='general_distribution_functions',
+      version='0.1',
+      description='Gaussian_binomial_ distributions',
+      packages=['general_distribution_functions'],
+      author='Ahmed Safwat',
+      author_Email='safwategy@gmail.com',
+      zip_safe=False)
