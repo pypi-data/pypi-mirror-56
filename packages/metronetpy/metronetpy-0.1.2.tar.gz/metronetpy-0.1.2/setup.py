b26d84cbce1ba95dccbb82a1bcbd589b80f73e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:27 +0000

    Add package metronetpy/0.1.2/metronetpy-0.1.2.tar.gz

---
+++ packages/metronetpy/metronetpy-0.1.2.tar.gz/metronetpy-0.1.2/setup.py
@@ -0,0 +1,31 @@
+import os
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = {}
+with open("metronetpy/__version__.py") as fh:
+    exec(fh.read(), version)
+
+setuptools.setup(
+    name="metronetpy",
+    version=version["__version__"],
+    author="Paolo Tuninetto",
+    author_email="paolo.tuninetto@gmail.com",
+    description="Python Package for programmatically controlling Security and Intrusion systems made by IESS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tulindo/metronetpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+    ],
+    python_requires=">=3.7",
+    install_requires=["hyper"],
+    extras_requires=["sslkeylog"],
+    scripts=["metronet"],
+)
