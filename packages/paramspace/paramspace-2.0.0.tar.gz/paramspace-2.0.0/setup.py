f5369a4c1ffec6908e857fb3cc504fb9f6b096d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:55 +0000

    Add package paramspace/2.0.0/paramspace-2.0.0.tar.gz

---
+++ packages/paramspace/paramspace-2.0.0.tar.gz/paramspace-2.0.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+# Dependency lists
+install_deps = ['numpy>=1.15', 'xarray>=0.10.9', 'ruamel.yaml>=0.15.71']
+test_deps    = ['tox>=3.1', 'pytest>=3.4', 'pytest-cov>=2.6']
+
+
+setup(name='paramspace',
+      version='2.0.0',  # NOTE: also change this in __init__.py
+      description='Multidimensional parameter space with dictionaries at each point.',
+      long_description='Classes that allow easy iteration over a multidimensional parameter space, generating dictionaries at each point in this parameter space.',
+      author='Yunus Sevinchan',
+      author_email='Yunus.Sevinchan@iup.uni-heidelberg.de',
+      url='https://ts-gitlab.iup.uni-heidelberg.de/yunus/paramspace',
+      license='MIT',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities'
+      ],
+      packages=['paramspace'],
+      include_package_data=True,
+      install_requires=install_deps,
+      tests_require=test_deps,
+      test_suite='tox',
+      extras_require=dict(test_deps=test_deps)
+  )
