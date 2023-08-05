84348a7f5c30e709a4197d9f6ea0c01043ccf4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:48 +0000

    Add package flask-api-stats/0.4/flask-api-stats-0.4.tar.gz

---
+++ packages/flask-api-stats/flask-api-stats-0.4.tar.gz/flask-api-stats-0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='flask-api-stats',
+    version='0.4',
+    author="Fernando Bugni",
+    author_email="fernando.bugni@gmail.com",
+    description="A plugin for flask to obtain API statistics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Gaturron/flask-stats",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
