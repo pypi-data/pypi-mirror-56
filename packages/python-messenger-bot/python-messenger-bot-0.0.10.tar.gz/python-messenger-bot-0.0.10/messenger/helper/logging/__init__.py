ee2e7c38958b15e0e05b96cdd6ddf0d42d27a166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:04 +0000

    Add package python-messenger-bot/0.0.10/python-messenger-bot-0.0.10.tar.gz

---
+++ packages/python-messenger-bot/python-messenger-bot-0.0.10.tar.gz/python-messenger-bot-0.0.10/messenger/helper/logging/__init__.py
@@ -0,0 +1,1 @@
+﻿from .log import Log
---
+++ packages/python-messenger-bot/python-messenger-bot-0.0.10.tar.gz/python-messenger-bot-0.0.10/messenger/helper/logging/log.py
@@ -0,0 +1,31 @@
+﻿import datetime
+import logging
+import os
+
+logging_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(format=logging_format, level=logging.INFO)
+
+class Log:
+
+    def __init__(self, name, sender_psid = '0', folder_log = '', **kwargs):
+        self.logger = self.prepare_logger(name, sender_psid, folder_log)
+
+        return super().__init__(**kwargs)
+
+    def prepare_logger(self, name, sender_psid, folder_log):
+        logger = logging.getLogger(name)
+
+        user_log = folder_log + sender_psid + '/log ' + datetime.datetime.now().strftime('%y-%b-%d %H') + '.txt'
+
+        try:
+            fh = logging.FileHandler(user_log, 'a+')
+        except FileNotFoundError:
+            os.makedirs(folder_log + sender_psid, exist_ok=True)
+            fh = logging.FileHandler(user_log, 'a+')
+
+        formatter = logging.Formatter(logging_format)
+        fh.setFormatter(formatter)
+        logger.handlers = []
+        logger.addHandler(fh)
+
+        return logger
