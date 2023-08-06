35d4b09b39534d5866c3a32d533f81acdc5dd305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:36 +0000

    Add package numpysane/0.19/numpysane-0.19.tar.gz

---
+++ packages/numpysane/numpysane-0.19.tar.gz/numpysane-0.19/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+from setuptools import setup
+import re
+import glob
+
+version = None
+with open("numpysane.py", "r") as f:
+    for l in f:
+        m = re.match("__version__ *= *'(.*?)' *$", l)
+        if m:
+            version = m.group(1)
+            break
+
+if version is None:
+    raise Exception("Couldn't find version in 'numpysane.py'")
+
+pywrap_templates = glob.glob('pywrap-templates/*.c')
+
+setup(name         = 'numpysane',
+      version      = version,
+      author       = 'Dima Kogan',
+      author_email = 'dima@secretsauce.net',
+      url          = 'http://github.com/dkogan/numpysane',
+      description  = 'more-reasonable core functionality for numpy',
+
+      long_description = """numpysane is a collection of core routines to provide basic numpy
+functionality in a more reasonable way""",
+
+      license      = 'LGPL-3+',
+      py_modules   = ['numpysane', 'numpysane_pywrap'],
+      data_files = [ ('pywrap-templates', pywrap_templates)],
+
+      # Tell setuputils to not deal with the egg nonsense. numpysane_pywrap.py
+      # assumes this
+      eager_resources = pywrap_templates,
+      test_suite   = 'test_numpysane',
+      install_requires = 'numpy')
