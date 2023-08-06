75bca8587f0d9ed42b8446d89c5fcd4ae7159795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:31 +0000

    Add package wimprates/0.3.1/wimprates-0.3.1.tar.gz

---
+++ packages/wimprates/wimprates-0.3.1.tar.gz/wimprates-0.3.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+readme = open('README.md').read()
+history = open('HISTORY.md').read().replace('.. :changelog:', '')
+requirements = open('requirements.txt').read().splitlines()
+
+setuptools.setup(
+    name='wimprates',
+    version='0.3.1',
+    description='Differential rates of WIMP-nucleus scattering',
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    author='Jelle Aalbers',
+    url='https://github.com/jelleaalbers/wimprates',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    setup_requires=['pytest-runner'],
+    install_requires=requirements,
+    package_dir={'wimprates': 'wimprates'},
+    package_data={'wimprates': [
+        'data/bs/*', 'data/migdal/*', 'data/sd/*', 'data/dme/*']},
+    tests_require=requirements + ['pytest'],
+    keywords='wimp,spin-dependent,spin-independent,bremsstrahlung,migdal',
+    classifiers=['Intended Audience :: Science/Research',
+                 'Development Status :: 3 - Alpha',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 3'],
+    zip_safe=False)
