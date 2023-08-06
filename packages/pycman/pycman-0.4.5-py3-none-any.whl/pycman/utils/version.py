3b8ea15f5bfcd5ef8997b7c2b0d7e659c5028121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:34 +0000

    Add package pycman/0.4.5/pycman-0.4.5-py3-none-any.whl

---
+++ packages/pycman/pycman-0.4.5-py3-none-any.whl/pycman/utils/version.py
@@ -0,0 +1,21 @@
+from pycman.utils import print_logo
+import importlib
+import os
+import sys
+# 动态导入，避免重复冲突
+version = importlib.import_module('pycman').__version__
+
+
+def get_version(self: bool = True) -> str:
+    if self:
+        return version
+    context = os.path.abspath(os.getcwd())
+    sys.path.append(context)
+    package = importlib.import_module('package')
+    return package.package['version']
+
+
+def show_version():
+    print_logo()
+    print("")
+    print(f"version: {version}")
