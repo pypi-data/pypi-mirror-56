1896f64b3d3f091d0cb3b9beb3d1de55fc0f47fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:41 +0000

    Add package kaldi-active-grammar/1.0.3/kaldi_active_grammar-1.0.3-py2.py3-none-win_amd64.whl

---
+++ packages/kaldi-active-grammar/kaldi_active_grammar-1.0.3-py2.py3-none-win_amd64.whl/kaldi_active_grammar/__init__.py
@@ -0,0 +1,24 @@
+#
+# This file is part of kaldi-active-grammar.
+# (c) Copyright 2019 by David Zurow
+# Licensed under the AGPL-3.0, with exceptions; see LICENSE.txt file.
+#
+
+_name = 'kaldi_active_grammar'
+__version__ = '1.0.3'
+REQUIRED_MODEL_VERSION = '0.5.0'
+DEFAULT_MODEL_DIR = 'kaldi_model_zamia'
+DEFAULT_TMP_DIR_SUFFIX = '.tmp'
+FILE_CACHE_FILENAME = 'file_cache.json'
+
+import logging
+_log = logging.getLogger('kaldi')
+
+class KaldiError(Exception):
+    pass
+
+from .compiler import Compiler, KaldiRule
+from .model import Model
+from .wrapper import KaldiAgfNNet3Decoder, KaldiPlainNNet3Decoder
+from .wfst import WFST
+from .plain_dictation import PlainDictationRecognizer
