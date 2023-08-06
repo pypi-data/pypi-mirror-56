a1891114467242bcf98cc0843922e04a497a5e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:33 +0000

    Add package django-mssql-backend/2.4.2/django-mssql-backend-2.4.2.tar.gz

---
+++ packages/django-mssql-backend/django-mssql-backend-2.4.2.tar.gz/django-mssql-backend-2.4.2/testapp/models.py
@@ -0,0 +1,28 @@
+import uuid
+
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    title = models.CharField('title', max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField('text')
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
+
+
+class UUIDModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    other = models.UUIDField(default=uuid.uuid4)
+
+    def __str__(self):
+        return self.pk
