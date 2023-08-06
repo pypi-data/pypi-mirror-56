e0936e7361c6958b94be63e1bf398c773fc50571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:10 +0000

    Add package MCRLLM/0.0.6/mcrllm-0.0.6.tar.gz

---
+++ packages/MCRLLM/mcrllm-0.0.6.tar.gz/mcrllm-0.0.6/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="mcrllm",
+    version="0.0.6",
+    author="Ryan Gosselin",
+    author_email="ryan.gosselin@usherbrooke.ca",
+    packages=["mcrllm"],
+    description="MCRLLM: Multivariate Curve Resolution by Log-Likelihood Maximization",
+    long_description="MCRLLM: Multivariate Curve Resolution by Log-Likelihood Maximization",
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
