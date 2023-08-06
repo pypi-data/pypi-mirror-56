f9ca7840677a9a1cf4c35e2fcca84f689fdf434f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:38 +0000

    Add package mih-sort/0.4.3/mih-sort-0.4.3.tar.gz

---
+++ packages/mih-sort/mih-sort-0.4.3.tar.gz/mih-sort-0.4.3/heap/sequence.py
@@ -0,0 +1,74 @@
+# encoding: utf-8
+
+import math
+import numpy as np
+
+
+class Sequence(object):
+
+    
+    def __init__(self, values=[]):
+
+        self._seq  = np.array(values)
+
+
+
+    def index(self, value):
+
+        return np.where(self._seq == value)[0][0]
+
+
+
+    def tail_index(self):
+
+        return math.floor((len(self._seq)+1)/2) - 1 
+
+
+
+    def tail(self):
+
+        return self._seq[self.tail_index()]
+
+
+
+    def last(self):
+
+        return self._seq[-1]
+
+
+
+    def last_drop(self):
+
+        self._seq = self._seq[:-1]
+
+
+
+    def __getitem__(self, key):
+
+        try:
+            return self._seq[key]
+        except:
+            return None
+
+
+
+    def __setitem__(self, key, value):
+
+        self._seq[key] = value
+
+
+
+    def append(self, value):
+
+        self._seq = np.concatenate((self._seq, [value]))
+
+
+
+    def __repr__(self):
+
+        return '\n'.join([v.__repr__() for v in self._seq])
+
+
+    def __len__(self):
+
+        return len(self._seq)
