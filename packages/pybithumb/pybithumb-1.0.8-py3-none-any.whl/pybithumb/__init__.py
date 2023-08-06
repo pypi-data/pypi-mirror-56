a4e49d84a3d3c51859294608df47f76f958e62c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:56 +0000

    Add package pybithumb/1.0.8/pybithumb-1.0.8-py3-none-any.whl

---
+++ packages/pybithumb/pybithumb-1.0.8-py3-none-any.whl/pybithumb/__init__.py
@@ -0,0 +1,22 @@
+from pybithumb.client import Bithumb
+from .history import *
+
+
+def get_ohlc(order_currency, payment_currency="KRW"):
+    return Bithumb.get_ohlc(order_currency, payment_currency)
+
+
+def get_tickers(payment_currency="KRW"):
+    return Bithumb.get_tickers(payment_currency)
+
+
+def get_market_detail(order_currency, payment_currency="KRW"):
+    return Bithumb.get_market_detail(order_currency, payment_currency)
+
+
+def get_current_price(order_currency, payment_currency="KRW"):
+    return Bithumb.get_current_price(order_currency, payment_currency)
+
+
+def get_orderbook(order_currency, payment_currency="KRW", limit=5):
+    return Bithumb.get_orderbook(order_currency, payment_currency, limit)
