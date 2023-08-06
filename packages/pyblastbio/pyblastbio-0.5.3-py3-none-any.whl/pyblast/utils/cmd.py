b81c7f54937886c94322cbd6785f4ff5cd207f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:38 +0000

    Add package pyblastbio/0.5.3/pyblastbio-0.5.3-py3-none-any.whl

---
+++ packages/pyblastbio/pyblastbio-0.5.3-py3-none-any.whl/pyblast/utils/cmd.py
@@ -0,0 +1,34 @@
+"""cmd.py."""
+import shlex
+from subprocess import CalledProcessError
+from subprocess import check_output
+from subprocess import STDOUT
+
+
+def run_cmd_str(cmd_str):
+    """Runs a command from a string."""
+    args = shlex.split(cmd_str)
+    try:
+        output = check_output(args, stderr=STDOUT)
+    except CalledProcessError as error:
+        error_message = ">>> Error while executing:\n{}\n>>> Returned with error:\n{}".format(
+            cmd_str, str(error.output)
+        )
+        error_message += (
+            "\nParameter info: https://www.ncbi.nlm.nih.gov/books/NBK279684/"
+        )
+        raise Exception(error_message)
+
+    # output = subprocess.Popen(args, shell=False)
+    # output.wait()
+    return output.decode()
+
+
+def run_cmd(cmd, **kwargs):
+    """Run a command using parameters kwargs."""
+    run_cmd_str(dict_to_cmd(cmd, **kwargs))
+
+
+def dict_to_cmd(cmd, **kwargs):
+    """Create a command string for cmd and parameters 'kwargs'."""
+    return cmd + " " + " ".join(["-{} {}".format(k, kwargs[k]) for k in kwargs])
