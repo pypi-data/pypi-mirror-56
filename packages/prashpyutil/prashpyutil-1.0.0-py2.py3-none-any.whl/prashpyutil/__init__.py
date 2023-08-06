e4afcc0025e06e8c10ab68133f993dbccadfa4fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:27 +0000

    Add package prashpyutil/1.0.0/prashpyutil-1.0.0-py2.py3-none-any.whl

---
+++ packages/prashpyutil/prashpyutil-1.0.0-py2.py3-none-any.whl/prashpyutil/__init__.py
@@ -0,0 +1,10 @@
+
+
+"""
+prashpyutil Library is just for the Utility method used for.
+~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2017 by Prashant Farkya.
+:license: MIT, see LICENSE for more details.
+"""
+from .sum import sumoftwo
---
+++ packages/prashpyutil/prashpyutil-1.0.0-py2.py3-none-any.whl/prashpyutil/sum.py
@@ -0,0 +1,6 @@
+def sumoftwo(a, b):
+    """This is an sumoftwo function that can easily used by it
+       sumoftwo(2, 3)
+       will return 5
+    """
+    return a + b
