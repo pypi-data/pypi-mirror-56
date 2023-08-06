b32433fd996245a003bd4c51b5d94648490d2761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:40 +0000

    Add package knora/1.2.3/knora-1.2.3-py3-none-any.whl

---
+++ packages/knora/knora-1.2.3-py3-none-any.whl/knora/__init__.py
@@ -0,0 +1,7 @@
+from .knora import KnoraError
+from .knora import KnoraStandoffXml
+from .knora import Knora
+from .knora import Sipi
+from .knora import BulkImport
+from .knora import IrisLookup
+from .knora import ListsLookup
