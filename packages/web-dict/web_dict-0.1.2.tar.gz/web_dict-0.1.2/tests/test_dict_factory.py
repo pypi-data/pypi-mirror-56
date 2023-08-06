9d41b1bee0e4469e235729733145944dfe233ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:33 +0000

    Add package web-dict/0.1.2/web_dict-0.1.2.tar.gz

---
+++ packages/web-dict/web_dict-0.1.2.tar.gz/web_dict-0.1.2/tests/test_dict_factory.py
@@ -0,0 +1,24 @@
+import json
+import unittest
+
+from web_dict import CollinsDictionary, OxfordDictionary
+
+
+class CollinsTest(unittest.TestCase):
+
+    def test_collins(self):
+        with CollinsDictionary(word='hacer') as c:
+            self._p(c.es2en())
+
+    def rtest_oxford(self):
+        c = OxfordDictionary()
+        self._p(
+            c.es2en('hacer')
+        )
+
+    def _p(self, c):
+        print(json.dumps(c, ensure_ascii=False, indent=4))
+
+
+if __name__ == '__main__':
+    unittest.main()
