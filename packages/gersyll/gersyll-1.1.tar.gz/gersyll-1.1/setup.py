4c543c28e05cedb5ff7d4d0ca0b55604c89d2c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:46 +0000

    Add package gersyll/1.1/gersyll-1.1.tar.gz

---
+++ packages/gersyll/gersyll-1.1.tar.gz/gersyll-1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gersyll",
+    version="1.1",
+    author="gakonorde",
+    author_email="gabrielko@yandex.ru",
+    description="This module counts the number of words of different syllable lenght for all texts in a directory",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gakonorde/gersyll",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
