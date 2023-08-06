bdec8957b35673e1779fab6901b29ecbef78a149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:10 +0000

    Add package choochoo/0.27.1/choochoo-0.27.1.tar.gz

---
+++ packages/choochoo/choochoo-0.27.1.tar.gz/choochoo-0.27.1/ch2/commands/activities.py
@@ -0,0 +1,30 @@
+
+from ..commands.args import PATH, FORCE, FAST, parse_pairs, CONSTANT, KARG, WORKER
+from ..squeal import PipelineType
+from ..stoats.pipeline import run_pipeline
+
+
+def activities(args, db):
+    '''
+## activities
+
+    > ch2 activities PATH [PATH ...]
+
+Read activities data from FIT files.
+
+### Examples
+
+    > ch2 activities -D 'Bike=Cotic Soul' ~/fit/2018-01-01.fit
+
+will load the give file and create an entry for the `Bike` statistic with value `Cotic Soul`
+(this particular variable is used to identify bike-specific parameters for power calculation,
+but arbitrary names and values can be used).
+
+Note: When using bash use `shopt -s globstar` to enable ** globbing.
+    '''
+    run_pipeline(db, PipelineType.ACTIVITY, paths=args[PATH], force=args[FORCE],
+                 worker=args[WORKER] is not None, id=args[WORKER],
+                 constants=args[CONSTANT], **parse_pairs(args[KARG]))
+    if not args[FAST] and args[WORKER] is None:
+        # don't force this - it auto-detects need
+        run_pipeline(db, PipelineType.STATISTIC, **parse_pairs(args[KARG]))
