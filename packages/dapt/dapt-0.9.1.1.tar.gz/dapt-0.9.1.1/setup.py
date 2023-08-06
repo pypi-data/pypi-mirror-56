1ba2df92a0d8604a62d19d2578e013441d15e715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:16 +0000

    Add package dapt/0.9.1.1/dapt-0.9.1.1.tar.gz

---
+++ packages/dapt/dapt-0.9.1.1.tar.gz/dapt-0.9.1.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dapt",
+    version="0.9.1.1",
+    license='BSD 3-clause "New" or "Revised" license',
+    author="Ben S. Duggan",
+    author_email="dugganbens@gmail.com",
+    description="A library to assist with running parameter sets across multiple systems.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BenSDuggan/DAPT",
+    download_url="https://github.com/BenSDuggan/DAPT/archive/v0.9.0.tar.gz",
+    keywords=['paramater', 'testing', 'distributed'],
+    install_requires=[
+        'boxsdk>=2.0.0',
+        'flask>=1.0.2',
+        'gspread>=3.1.0',
+        'oauth2client>=4.1.3'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
