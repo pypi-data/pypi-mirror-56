1a262cd4e0b8a562ed3eea39ce13e9efe76a55d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:06 +0000

    Add package db-prob/0.1/db_prob-0.1.tar.gz

---
+++ packages/db-prob/db_prob-0.1.tar.gz/db_prob-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='db_prob',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['db_prob'],
+      zip_safe=False)
