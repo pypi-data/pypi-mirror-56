da72a3c047b4f332fdea91dd86be7d08ea9d9830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:49 +0000

    Add package bqup/0.0.6/bqup-0.0.6.tar.gz

---
+++ packages/bqup/bqup-0.0.6.tar.gz/bqup-0.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md', encoding='utf8') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='bqup',
+    version='0.0.6',
+    description='BigQuery backup scripts',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Thinking Machines Data Science',
+    author_email='engineering@thinkingmachin.es',
+    url='https://github.com/thinkingmachines/bqup',
+    packages=['bqup'],
+    install_requires=requirements,
+    license='MIT License',
+    entry_points={'console_scripts': ['bqup=bqup.main:main']},
+    keywords=['google bigquery', 'bigquery', 'bq', 'backup', 'bqup']
+)
