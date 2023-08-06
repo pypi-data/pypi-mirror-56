cf69f9750031fee62abbc50e5889bc1d693a0f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:49 +0000

    Add package xiaoyu/1.0/xiaoyu-1.0.tar.gz

---
+++ packages/xiaoyu/xiaoyu-1.0.tar.gz/xiaoyu-1.0/setup.py
@@ -0,0 +1,10 @@
+#coding=gbk
+from distutils.core import setup
+setup(
+name='xiaoyu', # 对外我们模块的名字
+version='1.0', # 版本号
+description='这是第一个对外发布的模块，测试哦', #描述
+author='gaoqi', # 作者
+author_email='gaoqi110@163.com',
+py_modules=['xiaoyu.moudle_A1','xiaoyu.moudle_A2'] # 要发布的模块
+)
