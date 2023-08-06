649c1af2af7ad0eab6c69bfa0fcd80295fbf4d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:44 +0000

    Add package collective.documentgenerator/3.10/collective.documentgenerator-3.10.tar.gz

---
+++ packages/collective.documentgenerator/collective.documentgenerator-3.10.tar.gz/collective.documentgenerator-3.10/src/collective/documentgenerator/DX_renderer.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""Adapters for dexterity fields rendering."""
+
+from collective.documentgenerator.interfaces import IFieldRendererForDocument
+from collective.excelexport.interfaces import IExportable
+from zope.component import getMultiAdapter
+from zope.interface import implements
+
+import datetime
+
+
+class DexterityExportableAdapter(object):
+
+    """Adapter for collective.excelexport exportables."""
+
+    implements(IFieldRendererForDocument)
+
+    def __init__(self, field, context, request):
+        self.field = field
+        self.context = context
+        self.request = request
+        self.exportable = getMultiAdapter(
+            (field, context, request), IExportable)
+
+    def render_value(self):
+        """Just delegate the rendering to the exportable."""
+        return self.exportable.render_value(self.context)
+
+
+class DexterityDateExportableAdapter(DexterityExportableAdapter):
+
+    """Adapter for collective.excelexport date field exportable."""
+
+    implements(IFieldRendererForDocument)
+
+    def render_value(self):
+        """Format the date."""
+        value = self.exportable.render_value(self.context)
+        plone = getMultiAdapter((self.context, self.request), name=u'plone')
+        if type(value) == datetime.date:
+            value = datetime.datetime(value.year, value.month, value.day)
+        return plone.toLocalizedTime(value)
+
+
+class DexterityDatetimeExportableAdapter(DexterityExportableAdapter):
+
+    """Adapter for collective.excelexport datetime field exportable."""
+
+    implements(IFieldRendererForDocument)
+
+    def render_value(self):
+        """Format the date."""
+        value = self.exportable.render_value(self.context)
+        plone = getMultiAdapter((self.context, self.request), name=u'plone')
+        return plone.toLocalizedTime(value, long_format=True)
