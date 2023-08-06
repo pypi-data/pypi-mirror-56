1c60300a5b9ac6a552f24f6ee1516587cdda3941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:03 +0000

    Add package keybasepy/0.1.2/keybasepy-0.1.2.tar.gz

---
+++ packages/keybasepy/keybasepy-0.1.2.tar.gz/keybasepy-0.1.2/keybasepy/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'keybase'
+__author__ = 'dnorhoj'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 dnorhoj'
+__version__ = '0.1'
+
+from .bot import *
+from .models import *
---
+++ packages/keybasepy/keybasepy-0.1.2.tar.gz/keybasepy-0.1.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['keybasepy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pykeybasebot>=0.1.6,<0.2.0']
+
+setup_kwargs = {
+    'name': 'keybasepy',
+    'version': '0.1.2',
+    'description': 'A bot library for keybase',
+    'long_description': '# keybase.py\n\nThis is a keybase bot library for Python 3.7+\nDocs will be released soon.\n\n## Install\n\n```sh\npip install keybasepy\n```\n\n## Todo\n\n* Add documentation.\n* Create a more comprehensive README\n',
+    'author': 'dnorhoj',
+    'author_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
