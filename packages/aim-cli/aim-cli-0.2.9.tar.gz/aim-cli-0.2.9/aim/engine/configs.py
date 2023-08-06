5e009eaaefac1d3685ca01ea492c692f3ceb4dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:32 +0000

    Add package aim-cli/0.2.9/aim-cli-0.2.9.tar.gz

---
+++ packages/aim-cli/aim-cli-0.2.9.tar.gz/aim-cli-0.2.9/aim/engine/configs.py
@@ -0,0 +1,17 @@
+AIM_REPO_NAME = '.aim'
+AIM_OBJECTS_DIR_NAME = 'objects'
+AIM_METRICS_DIR_NAME = 'metrics'
+AIM_ANNOT_DIR_NAME = 'annotations'
+AIM_MEDIA_DIR_NAME = 'media'
+AIM_IMAGES_DIR_NAME = 'images'
+AIM_MODELS_DIR_NAME = 'models'
+AIM_CONFIG_FILE_NAME = 'config.json'
+AIM_DEFAULT_BRANCH_NAME = 'default'
+
+AIM_LOGGING_DIR_NAME = 'logs'
+AIM_CLI_ACTIVITIED_LOG = 'activities.log'
+AIM_SDK_ERROR_LOG = 'errors.log'
+
+AIM_PROFILE_NAME = '.aim_profile'
+AIM_PROFILE_CONFIG_FILE_NAME = 'config.json'
+AIM_PROFILE_SSH_DIR_NAME = '.ssh'
