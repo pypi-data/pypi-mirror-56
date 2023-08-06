333255394e6114444aebb1b264b7da4d9dd66b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:39:08 +0000

    Add package dimcli/0.6.1.2/dimcli-0.6.1.2-py2.py3-none-any.whl

---
+++ packages/dimcli/dimcli-0.6.1.2-py2.py3-none-any.whl/dimcli/shortcuts.py
@@ -0,0 +1,35 @@
+
+from . import *
+from .core.utils import print_json_stats, chunks_of, normalize_key
+# chunks_of/normalize_key imported so that it's in the shortcuts namespace and easier to import
+
+def dslquery(query_string):
+    """shortcut for running a query - meant to be used only within interactive computing environments
+    """
+    from .core.auth import is_logged_in
+    if is_logged_in():
+        dsl = Dsl()
+        res = dsl.query(query_string, verbose=True)
+        return res
+
+
+def dslquery_json(query_string):
+    """shortcut for backward compatibility 
+    Same as above but returns raw JSON instead of Api.Result object
+
+    Pattern: `from dimcli.shortcuts import dslquery_json as dslquery`
+    """
+    from .core.auth import is_logged_in
+    if is_logged_in():
+        dsl = Dsl()
+        return dsl.query(query_string).json
+
+
+def dslqueryall(query_string):
+    """shortcut for running a loop query - meant to be used only within interactive computing environments
+    NOTE: this requires the file-based credentials file set up.
+    """
+    from .core.auth import is_logged_in
+    if is_logged_in():
+        dsl = Dsl()
+        return dsl.query_iterative(query_string)
