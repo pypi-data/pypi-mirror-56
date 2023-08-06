82d7e69ee1bab7b15e529c5108ffdfc0992e1f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:32 +0000

    Add package chellow/2219/chellow-2219-py3-none-any.whl

---
+++ packages/chellow/chellow-2219-py3-none-any.whl/chellow/edi_lib.py
@@ -0,0 +1,35 @@
+from decimal import Decimal
+from datetime import datetime as Datetime
+from werkzeug.exceptions import BadRequest
+from chellow.utils import to_utc
+
+
+class EdiParser():
+    def __init__(self, f):
+        self.f_iterator = iter(f)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.line = next(self.f_iterator).strip()
+        if self.line[-1] != "'":
+            raise BadRequest("This parser expects one segment per line.")
+        self.elements = [
+            element.split(':') for element in self.line[4:-1].split("+")]
+        return self.line[:3]
+
+
+def to_decimal(components):
+    result = Decimal(components[0])
+    if len(components) > 1 and components[-1] == "R":
+        result *= Decimal("-1")
+    return result
+
+
+def to_date(component):
+    return to_utc(Datetime.strptime(component, "%y%m%d"))
+
+
+def to_int(component):
+    return int(component)
