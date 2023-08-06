048419f1f3eb81255f1728df1d0e3e5c01d5f5b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:48 +0000

    Add package biobb-io/2.0.0/biobb_io-2.0.0.tar.gz

---
+++ packages/biobb-io/biobb_io-2.0.0.tar.gz/biobb_io-2.0.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="biobb_io",
+    version="2.0.0",
+    author="Biobb developers",
+    author_email="pau.andrio@bsc.es",
+    description="Biobb_io is the Biobb module collection to fetch data to be consumed by the rest of the Biobb building blocks.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="Bioinformatics Workflows BioExcel Compatibility",
+    url="https://github.com/bioexcel/biobb_io",
+    project_urls={
+        "Documentation": "http://biobb_io.readthedocs.io/en/latest/",
+        "Bioexcel": "https://bioexcel.eu/"
+    },
+    packages=setuptools.find_packages(exclude=['docs',]),
+    install_requires=['biobb_common==2.0.1'],
+    python_requires='==3.6.*',
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+    ),
+)
