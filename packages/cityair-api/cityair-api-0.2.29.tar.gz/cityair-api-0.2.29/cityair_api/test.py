ff6fd27c9e902a7131f779cb99ddb760f66b3740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:16 +0000

    Add package cityair-api/0.2.29/cityair-api-0.2.29.tar.gz

---
+++ packages/cityair-api/cityair-api-0.2.29.tar.gz/cityair-api-0.2.29/cityair_api/test.py
@@ -0,0 +1,44 @@
+from request import CityAirRequest
+import pandas as pd
+TAKE_COUNT = 5
+
+r = CityAirRequest('ek', 'Oracle23')
+
+"""if True:
+    devices_df= r.get_devices(format = 'raw', include_children = True)
+    devices_dicts = r.get_devices(format = 'dicts', include_children = True)
+    print(devices_df.tail(2))
+    print(devices_dicts[:2])
+
+"""
+devices_list = r.get_devices(format = 'list', include_children = True)
+print(devices_list[:2])
+serial_number  = 'CA01PM000105'#devices_list[80]
+print(f"getting data for the {serial_number} device")
+assert isinstance(serial_number, str), f'Serial number should be string got {type(serial_number)} instead'
+
+device_data_df = r.get_device_data(serial_number, take_count=TAKE_COUNT)
+print(f"-----------------device data____________\n{device_data_df}")
+
+df = r.get_device_data(serial_number, take_count=TAKE_COUNT, all_cols=True)
+assert type(df.index) == pd.core.indexes.datetimes.DatetimeIndex, f'the type of df.index is {type(df.index)}'
+print(f"-----------------device data all cols____________\n{df}")
+
+
+
+device_data_dicts = r.get_device_data(serial_number, separate_device_data=True, take_count=TAKE_COUNT)
+
+print(device_data_df.head(2))
+for d in device_data_dicts:
+    df = device_data_dicts[d]
+    print(df.head(2))
+    assert type(df.index) == pd.core.indexes.datetimes.DatetimeIndex, f'the type of df.index is {type(df.index)}'
+
+
+for station_info in r.get_stations(format = 'dicts')[:2]:
+    print(station_info)
+
+station_ids = r.get_stations()
+print(f"first station_ids: {station_ids[:5]}")
+print(f"---------------------packets from station--------------\n{r.get_station_data(station_ids[3]).head(5)}")
+
---
+++ packages/cityair-api/cityair-api-0.2.29.tar.gz/cityair-api-0.2.29/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='cityair-api',
+    version='0.2.29',
+    packages=['cityair_api'],
+    url='https://github.com/cityairltd/cityair_python_api',
+    license='Apache 2.0',
+    author='Egor Korovin',
+    author_email='ek@cityair.ru',
+    description='A python api to access data from cityair.io',
+    classifiers=['Development Status :: 4 - Beta']      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package]
+)
+
+
