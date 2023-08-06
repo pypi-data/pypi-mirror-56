9298cf5ddff7d5d621e9dd841e3282bf4c0f5faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:08 +0000

    Add package rabbit-client/0.2.2/rabbit-client-0.2.2.tar.gz

---
+++ packages/rabbit-client/rabbit-client-0.2.2.tar.gz/rabbit-client-0.2.2/rabbit/client.py
@@ -0,0 +1,104 @@
+import asyncio
+import logging
+import os
+from typing import Dict
+
+import aioamqp
+from aioamqp.channel import Channel
+
+import attr
+
+from rabbit.exceptions import AttributeNotInitialized
+from rabbit.observer import Observer
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+
+@attr.s(slots=True)
+class AioRabbitClient:
+
+    app = attr.ib(default=asyncio.get_event_loop())
+    host = attr.ib(
+        type=str,
+        default=os.getenv('BROKER_HOST', 'localhost'),
+        validator=attr.validators.instance_of(str)
+    )
+    port = attr.ib(
+        type=int,
+        default=int(os.getenv('BROKER_PORT', 5672)),
+        validator=attr.validators.instance_of(int)
+    )
+    _observer = attr.ib(
+        type=Observer,
+        default=Observer(),
+        init=False
+    )
+    _channel = attr.ib(init=False, default=None)
+    _protocol = attr.ib(init=False, default=None)
+    _transport = attr.ib(init=False, default=None)
+
+    @property
+    def channel(self) -> Channel:
+        if not self._channel:
+            raise AttributeNotInitialized(
+                'Do you need connect before.'
+            )
+        return self._channel
+
+    @channel.setter
+    def channel(self, value) -> None:
+        self._channel = value
+        self._observer.notify(self.app)
+
+    @property
+    def protocol(self):
+        return self._protocol
+
+    @protocol.setter
+    def protocol(self, value) -> None:
+        self._protocol = value
+
+    @property
+    def transport(self):
+        return self._transport
+
+    def monitor_connection(self, observer) -> None:
+        logging.debug(
+            f"Object {observer.__class__} "
+            "registered to monitoring channel changes."
+        )
+        self._observer.attach(observer)
+
+    async def connect(self, channel_max: int = 1, **kwargs) -> None:
+        self._transport, self.protocol = await aioamqp.connect(
+            host=self.host,
+            port=self.port,
+            channel_max=channel_max,
+            **kwargs
+        )
+        await self._configure_channel()
+
+    async def persistent_connect(self,
+                                 channel_max: int = 1,
+                                 **kwargs: Dict[str, str]) -> None:
+        while True:
+            try:
+                await asyncio.sleep(5)
+                await self.connect(channel_max, **kwargs)
+                await asyncio.sleep(10)
+                await self.protocol.wait_closed()
+                self._transport.close()
+            except OSError:
+                logging.info(f'Trying connect on {self.host}:{self.port}')
+                await asyncio.sleep(5)
+                await self.persistent_connect()
+            except aioamqp.exceptions.AmqpClosedConnection:
+                logging.info(f'Trying connect on {self.host}:{self.port}')
+                await asyncio.sleep(5)
+                await self.persistent_connect()
+
+    async def _configure_channel(self) -> None:
+        if self.protocol:
+            await asyncio.sleep(5)
+            self.channel = await self.protocol.channel()
---
+++ packages/rabbit-client/rabbit-client-0.2.2.tar.gz/rabbit-client-0.2.2/rabbit/subscribe.py
@@ -0,0 +1,200 @@
+import asyncio
+import json
+import logging
+import os
+from contextlib import suppress
+from typing import List, Optional
+
+from aioamqp.channel import Channel
+from aioamqp.envelope import Envelope
+from aioamqp.exceptions import SynchronizationError
+from aioamqp.properties import Properties
+
+import attr
+
+from rabbit.client import AioRabbitClient
+from rabbit.dlx import DLX
+from rabbit.exceptions import AttributeNotInitialized
+from rabbit.exchange import Exchange
+from rabbit.job import echo_job
+from rabbit.publish import Publish
+from rabbit.queue import Queue
+from rabbit.task import Task
+from rabbit.tlog.db import DB
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+
+@attr.s(slots=True)
+class Subscribe:
+
+    client = attr.ib(
+        type=AioRabbitClient,
+        validator=attr.validators.instance_of(AioRabbitClient)
+    )
+    exchange = attr.ib(
+        type=Exchange,
+        default=Exchange(
+            name=os.getenv('SUBSCRIBE_EXCHANGE', 'default.in.exchange'),
+            exchange_type=os.getenv('SUBSCRIBE_EXCHANGE_TYPE', 'topic'),
+            topic=os.getenv('SUBSCRIBE_TOPIC', '#')
+        ),
+        validator=attr.validators.instance_of(Exchange)
+    )
+    queue = attr.ib(
+        type=Queue,
+        default=Queue(
+            name=os.getenv('SUBSCRIBE_QUEUE', 'default.subscribe.queue')
+        ),
+        validator=attr.validators.instance_of(Queue)
+    )
+    dlx = attr.ib(
+        type=DLX,
+        default=DLX(
+            dlq_queue=Queue(
+                name=os.getenv('SUBSCRIBE_QUEUE', 'default.subscribe.queue'),
+                arguments={
+                    'x-dead-letter-exchange': os.getenv(
+                        'SUBSCRIBE_EXCHANGE', 'default.in.exchange'
+                    ),
+                    'x-dead-letter-routing-key': os.getenv(
+                        'SUBSCRIBE_TOPIC', '#'
+                    )
+                }
+            ),
+            routing_key=os.getenv('SUBSCRIBE_QUEUE', 'default.subscribe.queue')
+        ),
+        validator=attr.validators.instance_of(DLX)
+    )
+    task = attr.ib(
+        type=Task,
+        default=Task(
+            job=echo_job
+        ),
+        validator=attr.validators.instance_of(Task)
+    )
+    task_type = attr.ib(
+        default='standard',
+        validator=attr.validators.and_(
+            attr.validators.in_(['standard', 'process']),
+            attr.validators.instance_of(str)
+        )
+    )
+    _publish = attr.ib(
+        type=Optional[Publish],
+        default=None,
+        validator=attr.validators.optional(
+            validator=attr.validators.instance_of(Publish)
+        )
+    )
+    _db = attr.ib(
+        type=Optional[DB],
+        default=None,
+        validator=attr.validators.optional(
+            validator=attr.validators.instance_of(DB)
+        )
+    )
+
+    def __attrs_post_init__(self) -> None:
+        self.client.monitor_connection(self)
+        self.dlx.client = self.client
+        if self.publish:
+            self.publish.client = self.client
+
+    @property
+    def publish(self) -> Optional[Publish]:
+        return self._publish
+
+    @publish.setter
+    def publish(self, publish: Publish) -> None:
+        logging.info('Registering connection monitoring')
+        attr.validate(publish)
+        self._publish = publish
+        self._publish.client = self.client
+
+    async def configure(self) -> None:
+        with suppress(SynchronizationError):
+            await asyncio.sleep(5)
+            try:
+                await self._configure_exchange()
+                await self._configure_queue()
+                await self.dlx.configure()
+                await self._configure_publish()
+                await self._configure_queue_bind()
+            except AttributeNotInitialized:
+                logging.debug('Waiting client initialization...SUBSCRIBE')
+
+    async def _configure_publish(self) -> None:
+        if self.publish:
+            await self.publish.configure()
+
+    async def _configure_exchange(self) -> None:
+        await self.client.channel.exchange_declare(
+            exchange_name=self.exchange.name,
+            type_name=self.exchange.exchange_type,
+            durable=self.exchange.durable
+        )
+        await asyncio.sleep(5)
+
+    async def _configure_queue(self) -> None:
+        await self.client.channel.queue_declare(
+            queue_name=self.queue.name,
+            durable=self.queue.durable
+        )
+
+    async def _configure_queue_bind(self) -> None:
+        await self.client.channel.queue_bind(
+            exchange_name=self.exchange.name,
+            queue_name=self.queue.name,
+            routing_key=self.exchange.topic
+        )
+        await self.client.channel.basic_consume(
+            callback=self.callback,
+            queue_name=self.queue.name
+        )
+
+    async def callback(self,
+                       channel: Channel,
+                       body: bytes,
+                       envelope: Envelope,
+                       properties: Properties):
+        process_result = [bytes()]
+        try:
+            await self.ack_event(envelope)
+            process_result = await self._execute(body)
+            if self.publish:
+                for result in process_result:
+                    await self.publish.send_event(result)
+            elif self._db:
+                for result in process_result:
+                    created_by = self._get_created_by(body)
+                    await self._db.save(result, created_by)
+            else:
+                return process_result
+        except Exception as cause:
+            await self.dlx.send_event(cause, body, envelope, properties)
+            # await self.reject_event(envelope)
+
+    async def _execute(self, data: bytes) -> List[bytes]:
+        process_result = [bytes()]
+        if self.task_type == 'process':
+            process_result = await self.task.process_executor(data)
+            return process_result
+        process_result = await self.task.std_executor(data)
+        logging.info(f'Event successfully processed.')
+        return process_result
+
+    def _get_created_by(self, payload: bytes) -> str:
+        raw_data = json.loads(payload)
+        return str(raw_data.get('createdBy', 'anonymous'))
+
+    async def reject_event(self, envelope: Envelope, requeue: bool = False) -> None:
+        await self.client.channel.basic_client_nack(
+            delivery_tag=envelope.delivery_tag,
+            requeue=requeue
+        )
+
+    async def ack_event(self, envelope: Envelope) -> None:
+        await self.client.channel.basic_client_ack(
+            delivery_tag=envelope.delivery_tag
+        )
