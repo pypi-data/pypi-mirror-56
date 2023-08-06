54f5f338c82746bcac6e4109ffbe8761dce46f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:39 +0000

    Add package hca/6.5.1/hca-6.5.1-py2.py3-none-any.whl

---
+++ packages/hca/hca-6.5.1-py2.py3-none-any.whl/hca/query/__init__.py
@@ -0,0 +1,11 @@
+"""
+DCP Query Service
+*****************
+"""
+from ..util import SwaggerClient
+
+
+class DCPQueryClient(SwaggerClient):
+    """
+    Client for the DCP Query Service API.
+    """
