b28b665f16ec7c2bd39d9aed9a70a7757310b1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:18 +0000

    Add package pyconrad/0.7.3/pyconrad-0.7.3.tar.gz

---
+++ packages/pyconrad/pyconrad-0.7.3.tar.gz/pyconrad-0.7.3/tests/test_imshow.py
@@ -0,0 +1,28 @@
+import pyconrad.autoinit
+import numpy as np
+import ij
+import pytest
+import os
+
+
+@pytest.mark.skipif("CI" in os.environ and os.environ["CI"] == "true", reason="Skipping this test on Travis CI.")
+def test_imshow():
+    a = np.random.rand(20, 30)
+    luts = ['Fire', 'Spectrum', 'Ice', 'Cyan']
+
+    for lut in luts:
+        pyconrad.imshow(a, lut=lut)
+
+    ij.WindowManager.closeAllWindows()
+
+    actions = ['FFT']
+
+    for action in actions:
+        pyconrad.imshow(a, run=action, lut='Fire')
+
+    ij.WindowManager.closeAllWindows()
+
+    actions = ['FFT']
+
+    for action in actions:
+        pyconrad.imshow(a, "foo", run=action, lut='Fire')
