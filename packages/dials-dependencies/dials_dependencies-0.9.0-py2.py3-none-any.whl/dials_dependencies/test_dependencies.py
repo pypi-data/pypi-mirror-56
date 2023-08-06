9cdd75fa0c8ae7e4a431c136c01ed454c0727ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:39 +0000

    Add package dials-dependencies/0.9.0/dials_dependencies-0.9.0-py2.py3-none-any.whl

---
+++ packages/dials-dependencies/dials_dependencies-0.9.0-py2.py3-none-any.whl/dials_dependencies/test_dependencies.py
@@ -0,0 +1,49 @@
+from __future__ import absolute_import, division, print_function
+
+
+def test_jinja2():
+    import jinja2
+
+
+def test_mock():
+    import mock
+
+
+def test_msgpack():
+    import msgpack
+
+
+def test_orderedset():
+    import orderedset
+
+
+def test_procrunner():
+    import procrunner
+
+
+def test_pytest():
+    import pytest
+
+
+def test_pytest_mock():
+    import pytest_mock
+
+
+def test_pytest_xdist():
+    import xdist
+
+
+def test_scikit_learn():
+    import sklearn
+
+
+def test_scipy():
+    import scipy
+
+
+def test_six():
+    import six
+
+
+def test_tqdm():
+    import tqdm
