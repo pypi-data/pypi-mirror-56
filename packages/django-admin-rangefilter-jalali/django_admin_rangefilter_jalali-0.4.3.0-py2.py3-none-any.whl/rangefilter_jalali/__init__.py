6adbceaf6e990694639720172dff1ba3751a9c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:59 +0000

    Add package django-admin-rangefilter-jalali/0.4.3.0/django_admin_rangefilter_jalali-0.4.3.0-py2.py3-none-any.whl

---
+++ packages/django-admin-rangefilter-jalali/django_admin_rangefilter_jalali-0.4.3.0-py2.py3-none-any.whl/rangefilter_jalali/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+
+__author__ = 'Mohammad Faraji'
+__version__ = '0.4.3.0'
+
+
+default_app_config = 'rangefilter_jalali.apps.RangeFilterConfig'
+
+
+VERSION = __version__
