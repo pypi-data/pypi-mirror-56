645c82816579b20992ad3f03bcbbefc50eeb4a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:38 +0000

    Add package django-vb-baseapp/1.0.3/django-vb-baseapp-1.0.3.tar.gz

---
+++ packages/django-vb-baseapp/django-vb-baseapp-1.0.3.tar.gz/django-vb-baseapp-1.0.3/vb_baseapp/management/template_structures/admins/basemodel.py
@@ -0,0 +1,28 @@
+"""
+CustomBaseModel template for model generator
+"""
+
+TEMPLATE_ADMIN_BASEMODEL = """import logging
+
+from django.contrib import admin
+
+from console import console
+
+from vb_baseapp.admin import CustomBaseModelAdmin
+
+from ..models import {model_name_title}
+
+__all__ = ['{model_name_title}Admin']
+
+logger = logging.getLogger('app')
+console = console(source=__name__)
+
+
+@admin.register({model_name_title})
+class {model_name_title}Admin(CustomBaseModelAdmin):
+    pass
+
+"""
+
+
+__all__ = ['TEMPLATE_ADMIN_BASEMODEL']
