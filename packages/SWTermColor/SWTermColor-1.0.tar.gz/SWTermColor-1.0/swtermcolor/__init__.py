8583d92e5ecdb3b86ffb251f6782d8093d15a24b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:21 +0000

    Add package SWTermColor/1.0/SWTermColor-1.0.tar.gz

---
+++ packages/SWTermColor/SWTermColor-1.0.tar.gz/SWTermColor-1.0/swtermcolor/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2.7
+# _*_ coding:UTF-8 _*_
+"""
+__author__ = 'shede333'
+"""
+
+from .swtermcolor import SWTermColor
+
+# 等价的类名
+ColorPrint = SWTermColor
+PrintColor = SWTermColor
