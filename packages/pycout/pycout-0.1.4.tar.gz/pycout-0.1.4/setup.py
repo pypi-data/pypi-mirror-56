4643b693c7b53e6c16ca9651cae25a0c1b7ef6b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:47 +0000

    Add package pycout/0.1.4/pycout-0.1.4.tar.gz

---
+++ packages/pycout/pycout-0.1.4.tar.gz/pycout-0.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pycout',
+    version='0.1.4',
+    packages=['pycout'],
+    url='http://github.com/EnzoBnl/pycout',
+    license='Apache 2.',
+    author='Enzo Bonnal',
+    author_email='enzobonnal@gmail.com',
+    description='Python porting of: cout << "Hello" << " World !" << endl;'
+)
