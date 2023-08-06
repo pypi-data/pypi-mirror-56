669903f91b8ff670cfb46cac4c0afe04f717b1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:29 +0000

    Add package scrapy-redis-ironman/0.1.0/scrapy_redis_ironman-0.1.0-py3-none-any.whl

---
+++ packages/scrapy-redis-ironman/scrapy_redis_ironman-0.1.0-py3-none-any.whl/scrapy_redis_bloomfilter/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from .connection import (  # NOQA
+    get_redis,
+    get_redis_from_settings,
+)
+
+
+__author__ = 'Aberstone'
+__email__ = 'aberstone.hk@gmail.com'
+__version__ = '0.1.0'
