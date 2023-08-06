3d46de4081197e24439d4b8314d56b479851e658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:58 +0000

    Add package astrology/0.0.1dev/astrology-0.0.1dev.tar.gz

---
+++ packages/astrology/astrology-0.0.1dev.tar.gz/astrology-0.0.1dev/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='astrology',
+    version='0.0.1dev',
+    author='the future',
+    description='',
+    long_description='',
+    packages=['astrology',],
+    install_requires=['numpy>=1.5','astropy>3']
+)
