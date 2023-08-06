5d040df6c0bdb0e434fb04329c4992a44a472424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:24 +0000

    Add package deepfitv/0.2/deepfitv-0.2-py2-none-any.whl

---
+++ packages/deepfitv/deepfitv-0.2-py2-none-any.whl/deepfitv/DeepFit.py
@@ -0,0 +1,23 @@
+class Deepfit:
+    
+    def readVideo(data):
+        return data
+    
+    def videotoimageconverter(data):
+        return data
+    
+    def imagepreprocessing(data):
+        print("hi")
+        return data
+    
+    def moduletotagimages(data):
+        return data
+    
+    def adanet(data):
+        return data
+    
+    def noiseremoval-shapely(data):
+        return data
+        
+    def subgrouptuning-multiaccuracy(data):
+        return data
