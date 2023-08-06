6bdbfa22e81011a60c187038c761ecc4b95f8089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:54 +0000

    Add package tfs-seon-client/2.0.5/tfs_seon_client-2.0.5-py3-none-any.whl

---
+++ packages/tfs-seon-client/tfs_seon_client-2.0.5-py3-none-any.whl/tfs_seon_client/service/tfs_service.py
@@ -0,0 +1,27 @@
+import pika
+import uuid
+import os
+import json
+from pprint import pprint
+from rabbitmqX.patterns.client.work_queue_task_client import Work_Queue_Task_Client
+from rabbitmqX.journal.journal import Journal
+import json 
+
+class TFS_Service (Work_Queue_Task_Client):
+    
+    def __init__(self, type):
+        Work_Queue_Task_Client.__init__(self,'integration.tfs')
+        self.type = type
+
+    def integrate(self, organization_id, tfs_key, tfs_url, application, external_id=None,external_project_id = None):
+
+        data = {'organization_id': organization_id, 
+                "tfs_key": tfs_key, 
+                "tfs_url": tfs_url,
+                "application": application,
+                "external_id": external_id,
+                "external_project_id": external_project_id}      
+        journal = Journal(organization_id,self.type,data,"integration")
+        self.send(journal)
+        
+
