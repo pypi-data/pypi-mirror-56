715d924529d6d7c655e10efcb9e6f1e2ba356f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:55 +0000

    Add package swh.lister/0.0.42/swh.lister-0.0.42-py3-none-any.whl

---
+++ packages/swh.lister/swh.lister-0.0.42-py3-none-any.whl/swh/lister/pypi/tests/test_lister.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2019 The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+
+def test_pypi_lister(lister_pypi, requests_mock_datadir):
+    lister_pypi.run()
+
+    r = lister_pypi.scheduler.search_tasks(task_type='load-pypi')
+    assert len(r) == 4
+
+    for row in r:
+        assert row['type'] == 'load-pypi'
+        # arguments check
+        args = row['arguments']['args']
+        assert len(args) == 2
+
+        project = args[0]
+        url = args[1]
+        assert url == 'https://pypi.org/project/%s/' % project
+
+        # kwargs
+        kwargs = row['arguments']['kwargs']
+        meta_url = kwargs['project_metadata_url']
+        assert meta_url == 'https://pypi.org/pypi/%s/json' % project
+
+        assert row['policy'] == 'recurring'
+        assert row['priority'] is None
