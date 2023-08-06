398119e692bf34bd53809b9552fbfde19faf203d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:35 +0000

    Add package ravenrpc/0.2/ravenrpc-0.2.tar.gz

---
+++ packages/ravenrpc/ravenrpc-0.2.tar.gz/ravenrpc-0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ravenrpc", # Replace with your own username
+    version="0.2",
+    author="JonPizza",
+    author_email="jon@jon.network",
+    description="Ravencoin RPC client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JonPizza/ravenrpc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
