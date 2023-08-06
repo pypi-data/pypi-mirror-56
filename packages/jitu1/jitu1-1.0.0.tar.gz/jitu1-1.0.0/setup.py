393d6f282e28f3eab09e85f0e640f88f5cdb339d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:04 +0000

    Add package jitu1/1.0.0/jitu1-1.0.0.tar.gz

---
+++ packages/jitu1/jitu1-1.0.0.tar.gz/jitu1-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'jitu1',
+    version = '1.0.0',
+    py_modules = ['jitu1'],
+    author = 'jiten',
+    author_email = 'jitenbnsl@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
