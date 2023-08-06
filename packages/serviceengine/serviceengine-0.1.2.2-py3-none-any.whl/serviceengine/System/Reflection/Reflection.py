1cabea5007214054302304f198ac7e335b14e172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:18 +0000

    Add package serviceengine/0.1.2.2/serviceengine-0.1.2.2-py3-none-any.whl

---
+++ packages/serviceengine/serviceengine-0.1.2.2-py3-none-any.whl/serviceengine/System/Reflection/Reflection.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import inspect
+import sys
+import importlib
+
+
+def GetFunctionsFromPath(filePath):
+    filePath = f"serviceengine.{filePath}"
+    print(f"filepath={filePath}")
+    importlib.import_module(f"{filePath}")
+    return inspect.getmembers(sys.modules[filePath], inspect.isclass)
+
+
+def GetFunctionsWithAttributeFromPath(filePath, attribute):
+    return [mem for mem in GetFunctionsFromPath(filePath) if hasattr(mem[1], attribute)]
+
+
+def GetPublicFacingFunctionsFromPath(filePath):
+    return GetFunctionsWithAttributeFromPath(filePath, "PublicFacing")
