28e83be42c2872a5afcd9e922de0a1ee370fbcfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:44 +0000

    Add package qgpu/0.0.1/qgpu-0.0.1.tar.gz

---
+++ packages/qgpu/qgpu-0.0.1.tar.gz/qgpu-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = "qgpu",
+    version = "0.0.1",
+    author = "thl",
+    author_email = "happysky111999@gmail.com",
+    url = "https://www.python.org/",
+    description = "2D QG on GPU",
+    packages = ['qgpu'],
+    install_requires = [],
+    )
+
