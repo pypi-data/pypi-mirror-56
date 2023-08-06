a35ef2254d1d7171f409d7e689fd09559fde26f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:19 +0000

    Add package faust-avro/0.2.0/faust_avro-0.2.0-py3-none-any.whl

---
+++ packages/faust-avro/faust_avro-0.2.0-py3-none-any.whl/faust_avro/app.py
@@ -0,0 +1,46 @@
+import click
+import faust
+
+from faust_avro.record import Record
+from faust_avro.serializers import AvroSchemaRegistry
+
+
+class App(faust.App):
+    def __init__(self, *args, registry_url="http://localhost:8081", **kwargs):
+        """Create a new Avro enabled Faust app.
+
+        :param registry_url: The base URL to the schema registry.
+        """
+        registry = AvroSchemaRegistry(registry_url=registry_url)
+        schema = kwargs.setdefault("Schema", registry)
+        super().__init__(*args, **kwargs)
+
+        @self.service
+        class AvroSchemaRegistryService(faust.Service):
+            async def on_start(_):
+                """Fetch faust_avro.Record schema ids from the schema registry."""
+                await schema.sync()
+
+        @self.command()
+        async def register(_):
+            """Register faust_avro.Record schemas with the schema registry."""
+            await schema.register()
+
+        @self.command(faust.cli.argument('model'))
+        async def schema(app, model):
+            """Dump the schema of a faust_avro.Record model."""
+            record = app.import_relative_to_app(model)
+            if isinstance(record, type) and issubclass(record, Record):
+                app.say(await registry.to_avro(record))
+            else:
+                raise click.Abort(f"{model} is not an avro-based Record.")
+
+    def topic(self, *args, **kwargs):
+        topic = super().topic(*args, **kwargs)
+
+        for which in ["key", "value"]:
+            record = kwargs.get(f"{which}_type")
+            if record:
+                topic.schema.define(topic.topics, which, record)
+
+        return topic
