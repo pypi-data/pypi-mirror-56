c861e5eea47e3135ee26b5bd236be61dda67bea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:23 +0000

    Add package remote-runner/0.1.0/remote-runner-0.1.0.tar.gz

---
+++ packages/remote-runner/remote-runner-0.1.0.tar.gz/remote-runner-0.1.0/remote_runner/utility.py
@@ -0,0 +1,31 @@
+import os
+import tempfile
+import contextlib
+from pathlib import Path
+import threading
+
+
+class ChangeDirectory:
+
+    def __init__(self, dirname: Path = None):
+        assert threading.current_thread() is threading.main_thread()
+        assert dirname is None or dirname.is_dir()
+
+        if dirname is None:
+            dirname = Path.cwd().absolute()
+        self.dirname = dirname
+        self.prev_dir = Path.cwd().absolute()
+
+    def __enter__(self):
+        self.prev_dir = Path.cwd().absolute()
+        os.chdir(str(self.dirname))
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        os.chdir(str(self.prev_dir))
+
+
+@contextlib.contextmanager
+def ChangeToTemporaryDirectory():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        with ChangeDirectory(Path(temp_dir)):
+            yield None
