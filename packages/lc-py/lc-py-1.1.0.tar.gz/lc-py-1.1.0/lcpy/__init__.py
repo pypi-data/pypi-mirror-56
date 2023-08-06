4bf3aafb85cfc854a24bacdc659ef43acf1b82e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:57 +0000

    Add package lc-py/1.1.0/lc-py-1.1.0.tar.gz

---
+++ packages/lc-py/lc-py-1.1.0.tar.gz/lc-py-1.1.0/lcpy/__init__.py
@@ -0,0 +1,10 @@
+"""
+Copyright (c) 2019-present Reece Dunham.
+You should have received a copy of the
+MIT License with this program/distribution.
+"""
+
+true = True
+false = False
+none = None
+exception = Exception
