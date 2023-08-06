f735cb8dee4597b4640aab9d46c3fb3ab9726dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:11 +0000

    Add package tsforest/0.2.3/tsforest-0.2.3.tar.gz

---
+++ packages/tsforest/tsforest-0.2.3.tar.gz/tsforest-0.2.3/tsforest/forecaster.py
@@ -0,0 +1,28 @@
+import h2o
+from tsforest.forest import H2OGBMRegressor, LightGBMRegressor, CatBoostRegressor, XGBoostRegressor
+from tsforest.forecaster_base import ForecasterBase
+
+class H2OGBMForecaster(ForecasterBase):
+    def __init__(self, *args, **kwargs):
+        super(H2OGBMForecaster, self).__init__(*args, **kwargs)
+        # init the cluster if is not already up
+        if h2o.cluster() is None: h2o.init(nthreads=-1)
+        self.model = H2OGBMRegressor(kwargs['model_params'])
+
+class LightGBMForecaster(ForecasterBase):
+    def __init__(self, *args, **kwargs):
+        super(LightGBMForecaster, self).__init__(*args, **kwargs)
+        self.model = LightGBMRegressor(kwargs['model_params'])
+
+class CatBoostForecaster(ForecasterBase):
+    def __init__(self, *args, **kwargs):
+        super(CatBoostForecaster, self).__init__(*args, **kwargs)
+        self.model = CatBoostRegressor(kwargs['model_params'])
+
+class XGBoostForecaster(ForecasterBase):
+    def __init__(self, *args, **kwargs):
+        super(XGBoostForecaster, self).__init__(*args, **kwargs)
+        self.model = XGBoostRegressor(kwargs['model_params'])
+        for feature,encoding in self.categorical_features.items():
+            if encoding == "default":
+                self.categorical_features[feature] = "CatBoostEncoder"
