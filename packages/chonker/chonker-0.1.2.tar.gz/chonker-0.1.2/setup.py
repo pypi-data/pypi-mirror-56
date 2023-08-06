c4f016a2aee7a750f353277c85d45bc815d82ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:30 +0000

    Add package chonker/0.1.2/chonker-0.1.2.tar.gz

---
+++ packages/chonker/chonker-0.1.2.tar.gz/chonker-0.1.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='chonker',  
+     version='0.1.2',
+     scripts=['chonker'] ,
+     author="C.M. Downey",
+     author_email="cmdowney@uw.edu",
+     description="A utility package for NLP and Machine Learning",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/cmdowney88/chonker",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+         "Operating System :: OS Independent",
+     ],
+ )
