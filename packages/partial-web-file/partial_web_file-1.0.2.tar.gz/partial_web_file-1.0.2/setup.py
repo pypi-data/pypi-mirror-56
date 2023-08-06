81cf2d0ac0032070ea5fbc278e6537bccdc0e82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:58 +0000

    Add package partial-web-file/1.0.2/partial_web_file-1.0.2.tar.gz

---
+++ packages/partial-web-file/partial_web_file-1.0.2.tar.gz/partial_web_file-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+ 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+desc = 'Utilities to get the partial content of the web file or to unarchive a single file from a huge remote web zip.'
+
+setup(
+    name='partial_web_file',
+    version='1.0.2',
+    url='https://github.com/alexappsnet/pypartialwebfile',
+    license='MIT',
+    author='Alex Malko',
+    author_email='alex@alexapps.net',
+    description=desc,
+    packages=find_packages(exclude=['tests']),
+    long_description=desc,
+    long_description_content_type="text/plain"
+)
