69c2e76795a89c369d5b1e6003fb74c9fe72b383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:39:29 +0000

    Add package larq-zoo/0.5.0/larq_zoo-0.5.0-py3-none-any.whl

---
+++ packages/larq-zoo/larq_zoo-0.5.0-py3-none-any.whl/larq_zoo/__init__.py
@@ -0,0 +1,27 @@
+from larq_zoo.binarynet import BinaryAlexNet
+from larq_zoo.birealnet import BiRealNet
+from larq_zoo.xnornet import XNORNet
+from larq_zoo.resnet_e import BinaryResNetE18
+from larq_zoo.densenet import (
+    BinaryDenseNet28,
+    BinaryDenseNet37,
+    BinaryDenseNet37Dilated,
+    BinaryDenseNet45,
+)
+from larq_zoo.dorefanet import DoReFaNet
+from larq_zoo.data import preprocess_input
+from larq_zoo.utils import decode_predictions
+
+__all__ = [
+    "BinaryAlexNet",
+    "BiRealNet",
+    "XNORNet",
+    "BinaryResNetE18",
+    "BinaryDenseNet28",
+    "BinaryDenseNet37",
+    "BinaryDenseNet37Dilated",
+    "BinaryDenseNet45",
+    "DoReFaNet",
+    "decode_predictions",
+    "preprocess_input",
+]
