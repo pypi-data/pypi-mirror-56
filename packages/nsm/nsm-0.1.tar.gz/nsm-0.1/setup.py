acf7c9918200eb159d8c2570c3094599f4763e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:08 +0000

    Add package nsm/0.1/nsm-0.1.tar.gz

---
+++ packages/nsm/nsm-0.1.tar.gz/nsm-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nsm',
+      version='0.1',
+      description='A NS-3 Simulation Manager',
+      url='http://github.com/rgrunbla/nsm',
+      author='Rémy Grünblatt',
+      author_email='remy@grunblatt.org',
+      license='MIT',
+      packages=['nsm'],
+      zip_safe=False)
