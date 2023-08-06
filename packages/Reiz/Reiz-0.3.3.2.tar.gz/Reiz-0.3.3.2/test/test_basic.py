b071e0b31986458f39209b894f8fb9808a8d0049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:43 +0000

    Add package Reiz/0.3.3.2/Reiz-0.3.3.2.tar.gz

---
+++ packages/Reiz/Reiz-0.3.3.2.tar.gz/Reiz-0.3.3.2/test/test_basic.py
@@ -0,0 +1,71 @@
+import reiz
+
+
+def test_canvas_keypress():
+    from pyglet.window import key
+    canvas = reiz.Canvas()
+    canvas.open()
+    assert canvas.start_run == False
+    canvas.window.dispatch_event('on_key_press', key.F5, 0)
+    canvas.flip()
+    assert canvas.start_run == True
+    canvas.close()
+
+
+def test_canvas_available():
+    canvas = reiz.Canvas(size=(5, 5))
+    assert canvas.available == False
+    canvas.open()
+    assert canvas.available == True
+    canvas.close()
+    assert canvas.available == False
+
+
+def test_canvas_properties():
+    w, h = (10, 9)
+    canvas = reiz.Canvas(size=(w, h))
+    canvas.open()
+    canvas.clear()
+    canvas.get_diag()
+    canvas.estimate_fps()
+    assert h == canvas.height
+    assert w == canvas.width
+    canvas.close()
+
+
+def test_clock_sleep():
+    limit = 1.5 * 10**(-4)  # exact within a millisecond
+    desired = .1
+    actual = reiz.clock.sleep(desired)
+    deviance = abs(desired-actual)
+    assert deviance < limit
+
+
+def test_clock_sleep_debiased():
+    import random
+    import time
+    limit = 1.5 * 10**(-4)  # exact within a millisecond
+    desired = .01
+    reiz.clock.reset()
+    for i in range(1, 11):
+        time.sleep(random.random()*0.005)
+        reiz.clock.sleep_debiased(desired)
+    actual = reiz.clock.now()
+    deviance = abs(desired*i-actual)
+    assert deviance < limit
+
+
+def test_cue():
+    c = reiz.Canvas((10, 10))
+    cue = reiz.Cue(canvas=c,
+                   audiostim=reiz.audio.library.beep, visualstim=reiz.visual.library.go)
+    N = reiz.Cue(canvas=c,
+                 audiostim=None,
+                 visualstim=None)
+    reiz.audio.library.beep.volume = 0
+    c.open()
+    cue.show(0.05)
+    N.show(canvas=c)
+    N.show()
+    cue.show(None)
+    c.close()
