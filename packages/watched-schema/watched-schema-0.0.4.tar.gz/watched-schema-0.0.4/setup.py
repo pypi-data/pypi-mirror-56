dd736ef806c09c21c5831f13dd4a7d221a4e9306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:05 +0000

    Add package watched-schema/0.0.4/watched-schema-0.0.4.tar.gz

---
+++ packages/watched-schema/watched-schema-0.0.4.tar.gz/watched-schema-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import os
+import json
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as f:
+    README = f.read()
+
+package = json.load(open("package.json", "r"))
+
+setup(
+    name="watched-schema",
+    version=package['version'],
+    description=package['description'],
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url=package['repository'],
+    author="WATCHED",
+    author_email="dev@watched.com",
+    packages=find_packages(),
+    install_requires=["pyyaml", "jsonschema"],
+    package_data={"src": ["schema.yaml"]},
+    python_requires=">=3.4",
+)
