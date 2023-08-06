03b970262f2f91c820ad7250946c1f8d93503775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:30 +0000

    Add package pytorch-ignite/0.3.0.dev20191129/pytorch_ignite-0.3.0.dev20191129-py2.py3-none-any.whl

---
+++ packages/pytorch-ignite/pytorch_ignite-0.3.0.dev20191129-py2.py3-none-any.whl/ignite/handlers/__init__.py
@@ -0,0 +1,4 @@
+from ignite.handlers.checkpoint import ModelCheckpoint, Checkpoint, DiskSaver
+from ignite.handlers.timing import Timer
+from ignite.handlers.early_stopping import EarlyStopping
+from ignite.handlers.terminate_on_nan import TerminateOnNan
