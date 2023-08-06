254c6e4d38294f83c597f1025b3d6fe0b7fdd291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:05 +0000

    Add package GaugeRnR/0.1.4/GaugeRnR-0.1.4.tar.gz

---
+++ packages/GaugeRnR/GaugeRnR-0.1.4.tar.gz/GaugeRnR-0.1.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    packages=['GaugeRnR'],
+    install_requires=requirements
+)
