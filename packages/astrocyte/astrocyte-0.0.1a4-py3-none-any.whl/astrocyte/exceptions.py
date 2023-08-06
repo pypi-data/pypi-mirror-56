6c0b9d19847b142de20795716a7aaa7910001d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:34 +0000

    Add package astrocyte/0.0.1a4/astrocyte-0.0.1a4-py3-none-any.whl

---
+++ packages/astrocyte/astrocyte-0.0.1a4-py3-none-any.whl/astrocyte/exceptions.py
@@ -0,0 +1,14 @@
+class AstroError(Exception):
+    pass
+
+class StructureError(AstroError):
+    pass
+
+class UploadError(AstroError):
+    pass
+
+class InvalidDistributionError(UploadError):
+    pass
+
+class InvalidMetaError(UploadError):
+    pass
