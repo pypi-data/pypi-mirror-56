7028b826f34c241735e158e169f71a1bbcf94e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:37 +0000

    Add package aiohttp-swagger3/0.2.5/aiohttp-swagger3-0.2.5.tar.gz

---
+++ packages/aiohttp-swagger3/aiohttp-swagger3-0.2.5.tar.gz/aiohttp-swagger3-0.2.5/aiohttp_swagger3/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ("SwaggerDocs", "SwaggerFile", "__version__")
+__version__ = "0.2.5"
+__author__ = "Valetov Konstantin"
+
+from .swagger_docs import SwaggerDocs
+from .swagger_file import SwaggerFile
