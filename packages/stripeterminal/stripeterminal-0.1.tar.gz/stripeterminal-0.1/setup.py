7eb74c180a9e7e462f9ce1bcf571bf5a5283e54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:53 +0000

    Add package stripeterminal/0.1/stripeterminal-0.1.tar.gz

---
+++ packages/stripeterminal/stripeterminal-0.1.tar.gz/stripeterminal-0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name="stripeterminal",
+    packages=["stripeterminal"],
+    version="0.1",
+    license="GPL v3",
+    description="Access Stripe's JavaScript terminal SDK from python",
+    author="Kevin Lai",
+    author_email="kevinlai31@gmail.com",
+    url="https://github.com/bnetbutter/stripeterminal",
+    download_url="https://github.com/bnetbutter/stripeterminal/v_01.tar.gz",
+    install_requires=[
+        "selenium",
+        "websockets",
+        "stripe",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
