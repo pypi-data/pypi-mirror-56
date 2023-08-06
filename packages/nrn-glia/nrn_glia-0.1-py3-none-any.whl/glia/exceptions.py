ea52d703daaf67aee64b393bd5b6c9fdf8d2921c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:25 +0000

    Add package nrn-glia/0.1/nrn_glia-0.1-py3-none-any.whl

---
+++ packages/nrn-glia/nrn_glia-0.1-py3-none-any.whl/glia/exceptions.py
@@ -0,0 +1,26 @@
+class GliaError(Exception):
+    pass
+
+class CompileError(GliaError):
+    pass
+
+class LibraryError(GliaError):
+    pass
+
+class ResolveError(GliaError):
+    pass
+
+class TooManyMatchesError(ResolveError):
+    pass
+
+class NoMatchesError(ResolveError):
+    pass
+
+class UnknownAssetError(ResolveError):
+    pass
+
+class PackageError(GliaError):
+    pass
+
+class PackageModError(GliaError):
+    pass
