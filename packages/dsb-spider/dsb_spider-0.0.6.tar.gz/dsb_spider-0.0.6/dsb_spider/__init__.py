4a099ef26140cae11f020c8ef7d319dabe01f063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:27 +0000

    Add package dsb-spider/0.0.6/dsb_spider-0.0.6.tar.gz

---
+++ packages/dsb-spider/dsb_spider-0.0.6.tar.gz/dsb_spider-0.0.6/dsb_spider/__init__.py
@@ -0,0 +1,4 @@
+from dsb_spider import log
+from dsb_spider.log import ex, getLogger
+from dsb_spider.sched import TaskListener
+from dsb_spider.tasker import BaseTask, TaskState, TaskSingleton, ImageTaskSingleton, ActivityTaskSingleton
