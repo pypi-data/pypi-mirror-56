43bac195565dfcdb2dd0239adc44a3efe4c7b8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:21 +0000

    Add package SchemaChecker/0.0.3.dev91/SchemaChecker-0.0.3.dev91.tar.gz

---
+++ packages/SchemaChecker/SchemaChecker-0.0.3.dev91.tar.gz/SchemaChecker-0.0.3.dev91/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='SchemaChecker',
+    version='0.0.3.dev91',
+    package_dir={'': 'src'},
+    packages=['schemachecker',
+              'schemachecker.edo',
+              'schemachecker.fns',
+              'schemachecker.stat',
+              'schemachecker.pfr',
+              'schemachecker.fss'],
+    install_requires=open('requirements.txt').read().splitlines(),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
