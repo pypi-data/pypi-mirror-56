8ae61a20b544ea72789686a11410c53a760f43d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:32 +0000

    Add package web-dict/0.1.1/web_dict-0.1.1.tar.gz

---
+++ packages/web-dict/web_dict-0.1.1.tar.gz/web_dict-0.1.1/web_dict/__init__.py
@@ -0,0 +1,5 @@
+from .core.factory import CollinsDictionary
+from .core.prviders.collinsdictionary import CollinsWeb
+from .core.prviders.lexico import Lexico
+
+__all__ = (CollinsDictionary, Lexico, CollinsWeb)
