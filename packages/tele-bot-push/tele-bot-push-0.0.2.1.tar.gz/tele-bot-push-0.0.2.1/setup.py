d2ea743cc179c3ba3c0d53c70e75c4c2278512f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:26 +0000

    Add package tele-bot-push/0.0.2.1/tele-bot-push-0.0.2.1.tar.gz

---
+++ packages/tele-bot-push/tele-bot-push-0.0.2.1.tar.gz/tele-bot-push-0.0.2.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding='utf-8') as fh:
+	long_description = fh.read()
+
+setup(
+	name='tele-bot-push',
+	version='0.0.2.1',
+	author='BuGTEa',
+	author_email='noreply@sir7878.com',
+	url='https://github.com/BuGTEa/Telegram-Bot-push',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	description='Telegram机器人发送消息',
+	packages=['telegram_bot_push'],
+	install_requires=['requests'],
+)
