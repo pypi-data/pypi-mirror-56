11aa39efe6577800861d7b50e67c46e46c47680a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:55 +0000

    Add package reproject/0.6/reproject-0.6-cp38-cp38-manylinux2010_i686.whl

---
+++ packages/reproject/reproject-0.6-cp38-cp38-manylinux2010_i686.whl/reproject/spherical_intersect/setup_package.py
@@ -0,0 +1,27 @@
+import os
+from distutils.core import Extension
+
+REPROJECT_ROOT = os.path.relpath(os.path.dirname(__file__))
+
+
+def get_extensions():
+
+    libraries = []
+
+    sources = []
+    sources.append(os.path.join(REPROJECT_ROOT, "_overlap.c"))
+    sources.append(os.path.join(REPROJECT_ROOT, "overlapArea.c"))
+    sources.append(os.path.join(REPROJECT_ROOT, "reproject_slice_c.c"))
+
+    include_dirs = ['numpy']
+    include_dirs.append(REPROJECT_ROOT)
+
+    extension = Extension(
+        name="reproject.spherical_intersect._overlap",
+        sources=sources,
+        include_dirs=include_dirs,
+        libraries=libraries,
+        language="c",
+        extra_compile_args=['-O2'])
+
+    return [extension]
