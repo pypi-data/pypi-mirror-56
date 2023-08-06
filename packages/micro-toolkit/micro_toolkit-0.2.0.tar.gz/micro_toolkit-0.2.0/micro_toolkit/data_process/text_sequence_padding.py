c22e38817a5056b36cc4f2fd775697ee66c20d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:34 +0000

    Add package micro-toolkit/0.2.0/micro_toolkit-0.2.0.tar.gz

---
+++ packages/micro-toolkit/micro_toolkit-0.2.0.tar.gz/micro_toolkit-0.2.0/micro_toolkit/data_process/text_sequence_padding.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class TextSequencePadding:
+    def __init__(self, length: int, value: str):
+        self.length = length
+        self.value = value
+
+    def fit(self, input_data: List[List[str]]) -> List[List[str]]:
+        result_list = []
+        for line_data in input_data:
+            result = self._process_line(line_data)
+            result_list.append(result)
+
+        return result_list
+
+    def _process_line(self, line_data: List[str]) -> List[str]:
+        # early stop
+        if len(line_data) == self.length:
+            return line_data
+
+        if len(line_data) > self.length:  # truncate
+            return line_data[:self.length]
+
+        # need padding now
+        padding_list = [self.value] * (self.length - len(line_data))
+
+        return line_data + padding_list
+
