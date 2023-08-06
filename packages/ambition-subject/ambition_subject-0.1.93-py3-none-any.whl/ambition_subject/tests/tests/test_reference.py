56c0dfa66c99277bcd33a6a7db5d9c69f8eac384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:42 +0000

    Add package ambition-subject/0.1.93/ambition_subject-0.1.93-py3-none-any.whl

---
+++ packages/ambition-subject/ambition_subject-0.1.93-py3-none-any.whl/ambition_subject/tests/tests/test_reference.py
@@ -0,0 +1,10 @@
+from ambition_rando.tests import AmbitionTestCaseMixin
+from django.test import TestCase, tag
+from django.test.utils import override_settings
+from edc_reference.site_reference import site_reference_configs
+
+
+@override_settings(SITE_ID="10")
+class TestReference(AmbitionTestCaseMixin, TestCase):
+    def test_(self):
+        site_reference_configs.check()
