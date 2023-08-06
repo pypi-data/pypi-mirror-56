fe1b38b7a52afdf87156c8e880d143ff90f4c028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:37 +0000

    Add package guanoctl/0.1.3/guanoctl-0.1.3.tar.gz

---
+++ packages/guanoctl/guanoctl-0.1.3.tar.gz/guanoctl-0.1.3/src/guanoctl/api/__init__.py
@@ -0,0 +1,9 @@
+""" Application commands common to all interfaces.
+
+"""
+# from .hello import main as hello
+from .readwav import main as readwav
+from .writewav import main as writewav
+
+
+__all__ = "readwav", "writewav" # "hello",
