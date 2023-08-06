f504e4b3f855c7e7623fa449198843bbbba4c7c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:15 +0000

    Add package ndml-probability-distributions/0.1/ndml_probability_distributions-0.1.tar.gz

---
+++ packages/ndml-probability-distributions/ndml_probability_distributions-0.1.tar.gz/ndml_probability_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ndml_probability_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ndml_probability_distributions'],
+      author = 'Vortana Say',
+      author_email = 'sayvortana.itc@gmail.com',
+      zip_safe=False)
