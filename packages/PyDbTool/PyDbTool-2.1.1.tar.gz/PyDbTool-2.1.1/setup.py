5cc731dba49ae3fd4b5f3093f725a1b91534664a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:54 +0000

    Add package PyDbTool/2.1.1/PyDbTool-2.1.1.tar.gz

---
+++ packages/PyDbTool/PyDbTool-2.1.1.tar.gz/PyDbTool-2.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='PyDbTool',
+      version='2.1.1',
+      description='Python all in one data-driver',
+      long_description=readme(),
+      url='https://pypi.org/project/PyDbTool/',
+      author='Prabir Ghosh',
+      author_email='mymail.prabir@gmail.com',
+      license='MIT',
+      packages=['PyDbTool'],
+      install_requires=["pandas","psycopg2","cassandra-driver","pymongo"],
+      zip_safe=False)
