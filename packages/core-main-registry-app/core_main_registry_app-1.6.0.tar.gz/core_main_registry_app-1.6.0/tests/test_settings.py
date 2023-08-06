8429cb0e1018a73d279e149e8b2e61f760106a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:51 +0000

    Add package core-main-registry-app/1.6.0/core_main_registry_app-1.6.0.tar.gz

---
+++ packages/core-main-registry-app/core_main_registry_app-1.6.0.tar.gz/core_main_registry_app-1.6.0/tests/test_settings.py
@@ -0,0 +1,36 @@
+from core_main_app.utils.databases.mongoengine_database import Database
+
+SECRET_KEY = 'fake-key'
+
+INSTALLED_APPS = [
+    # Django apps
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sites",
+    # Local app
+    "core_main_registry_app",
+    "tests",
+]
+
+# IN-MEMORY TEST DATABASE
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
+
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
+)
+
+MOCK_DATABASE_NAME = 'db_mock'
+MOCK_DATABASE_HOST = 'mongomock://localhost'
+
+database = Database(MOCK_DATABASE_HOST, MOCK_DATABASE_NAME)
+database.connect()
