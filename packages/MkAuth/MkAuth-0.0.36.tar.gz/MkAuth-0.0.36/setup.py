53e360662b3a4d977e974c19bf3cc2644610e628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:39 +0000

    Add package MkAuth/0.0.36/MkAuth-0.0.36.tar.gz

---
+++ packages/MkAuth/MkAuth-0.0.36.tar.gz/MkAuth-0.0.36/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MkAuth",
+    version="0.0.36",
+    author="Mark Cartagena",
+    author_email="mark@mknxgn.com",
+    description="MkNxGn User Management. Free for use.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://mknxgn.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
