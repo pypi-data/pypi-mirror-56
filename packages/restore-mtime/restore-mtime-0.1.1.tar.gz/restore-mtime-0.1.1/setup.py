6867cf5a4337107e1762d2a770eefffdb6d12973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:54 +0000

    Add package restore-mtime/0.1.1/restore-mtime-0.1.1.tar.gz

---
+++ packages/restore-mtime/restore-mtime-0.1.1.tar.gz/restore-mtime-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='restore-mtime',
+    version='0.1.1',
+    author='Eero Rikalainen',
+    author_email='eerorika@gmail.com',
+    url='https://github.com/eerorika/restore-mtime',
+    description='Command line utility for resotration of modification time (mtime) of files from tar archive into filesystem if the content matches',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'restore-mtime = restore_mtime.restore_mtime:main'
+        ]
+    },
+    zip_safe=False
+)
