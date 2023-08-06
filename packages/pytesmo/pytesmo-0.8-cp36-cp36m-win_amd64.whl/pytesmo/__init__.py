2bc43e26ae25537562eff5404d5e6986dbf038c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:42 +0000

    Add package pytesmo/0.8/pytesmo-0.8-cp36-cp36m-win_amd64.whl

---
+++ packages/pytesmo/pytesmo-0.8-cp36-cp36m-win_amd64.whl/pytesmo/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ['metrics', 'scaling', 'temporal_matching',
+           'timedate', 'time_series',
+           'grid', 'colormaps']
+
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except:
+    __version__ = 'unknown'
