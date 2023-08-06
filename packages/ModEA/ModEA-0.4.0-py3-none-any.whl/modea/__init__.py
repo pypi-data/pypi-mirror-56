873efea2951b8cd3bf3de6b1b82e2d365a1e6651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:37 +0000

    Add package ModEA/0.4.0/ModEA-0.4.0-py3-none-any.whl

---
+++ packages/ModEA/ModEA-0.4.0-py3-none-any.whl/modea/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+name = 'modea'
+__author__ = 'Sander van Rijn <svr003@gmail.com>'
+__version__ = '0.4.0'
