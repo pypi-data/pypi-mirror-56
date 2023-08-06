2b0987c1bb9d52f70e40b34cb8cbc396821298d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:27 +0000

    Add package deveal/1.0.0/deveal-1.0.0.tar.gz

---
+++ packages/deveal/deveal-1.0.0.tar.gz/deveal-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name = 'deveal',
+      version = '1.0.0',
+      description = 'A reveal.js helper tool',
+      long_description=readme(),
+      author='Jean-Christophe Fabre',
+      author_email='jean-christophe.fabre@inra.fr',
+      url='http://github.com/fabrejc/deveal',
+      license='GPLv3',
+      packages = ['deveal'],
+      package_data={
+          'deveal': ['skeleton/*']
+      },
+      include_package_data=True,
+      entry_points = {
+          'console_scripts': [
+              'deveal = deveal.__main__:main'
+          ]
+      },
+      install_requires = [
+          'argparse',
+          'jinja2 >= 2.7',
+          'PyYAML',
+          'watchdog'
+      ],
+      python_requires='~=3.5',
+     )
