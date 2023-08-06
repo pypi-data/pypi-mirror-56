f7cb8de11927a336a469de2c1447afa78e138a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:20 +0000

    Add package aiocian/0.0.3/aiocian-0.0.3.tar.gz

---
+++ packages/aiocian/aiocian-0.0.3.tar.gz/aiocian-0.0.3/aiocian/constants.py
@@ -0,0 +1,94 @@
+MOSCOW = "moscow"
+SPB = "spb"
+REGIONS = {
+    MOSCOW: 1,
+    SPB: 2,
+}
+
+AREND_DAILY = "arend_daily"
+AREND_MONTHLY = "arend_monthly"
+BUY = "buy"
+ACTIONS = (AREND_DAILY, AREND_MONTHLY, BUY)
+
+BED = "bed"
+BUSINESS = "business"
+BUILDING = "building"
+CAR_SERVICE = "car_service"
+COMMERCIAL_LAND = "commercial_land"
+DOMESTIC_SERVICES = "domestic_services"
+FLAT = "flat"
+FREE_SPACE = "free_space"
+GARAGE = "garage"
+HOMESTEAD = "homestead"
+HOUSE = "house"
+HOUSE_PART = "house_part"
+MANUFACTURE = "manufacture"
+OFFICE = "office"
+PART = "part"
+PUBLIC_CATERING = "public_catering"
+ROOM = "room"
+STOCK = "stock"
+TOWNHOUSE = "townhouse"
+TRADE_AREA = "trade_area"
+PLACES = {
+    BED: 10,
+    BUSINESS: 10,
+    BUILDING: 11,
+    CAR_SERVICE: 9,
+    COMMERCIAL_LAND: None,
+    DOMESTIC_SERVICES: 12,
+    FLAT: None,
+    FREE_SPACE: 5,
+    GARAGE: 6,
+    HOMESTEAD: 3,
+    HOUSE: 1,
+    HOUSE_PART: 2,
+    MANUFACTURE: 7,
+    OFFICE: 1,
+    PART: 8,
+    PUBLIC_CATERING: 4,
+    ROOM: 0,
+    STOCK: 3,
+    TOWNHOUSE: 4, 
+    TRADE_AREA: 2,
+}
+
+ONE_ROOM = "one_room"
+TWO_ROOMS = "two_rooms"
+THREE_ROOMS = "three_rooms"
+FOUR_ROOMS = "four_rooms"
+FIVE_ROOMS = "five_rooms"
+SIX_ROOMS = "six_rooms"
+FREE_LAYOUT = "free_layout"
+STUDIO = "studio"
+ROOMS = {
+    ONE_ROOM: 1,
+    TWO_ROOMS: 2,
+    THREE_ROOMS: 3,
+    FOUR_ROOMS: 4,
+    FIVE_ROOMS: 5,
+    SIX_ROOMS: 6,
+    FREE_LAYOUT: 7,
+    STUDIO: 9,
+}
+
+OLD = "old"
+NEW = "new"
+USED = {
+    OLD: 1,
+    NEW: 2,
+}
+
+DEVELOPER = "developer"
+OWNERS_AGENTS = "owners_agents"
+OWNERS = {
+    DEVELOPER: 1,
+    OWNERS_AGENTS: 2,
+}
+
+DOLLAR = "dollar"
+EURO = "euro"
+CURRENCIES = {
+    DOLLAR: 1,
+    EURO: 3,
+}
