b3d6ab7984a6348709941e3f84b7b0346393bca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:20 +0000

    Add package deepbay/0.8.2/deepbay-0.8.2.tar.gz

---
+++ packages/deepbay/deepbay-0.8.2.tar.gz/deepbay-0.8.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+version="v0.8.2"
+print("\nPackages: " + str(setuptools.find_packages()) + "\n")
+
+setuptools.setup(
+	name="deepbay",
+	version=version,
+	author="Whitman Bohorquez",
+	author_email="whitman-2@hotmail.com",
+	description="Tensorflow/Keras Plug-N-Play Deep Learning Models Compilation",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/ElPapi42/DeepBay",
+	download_url = "https://github.com/ElPapi42/DeepBay/archive/" + version + ".tar.gz",
+	keywords = ['Keras', 'Model', 'Plug And Play', "Tensorflow"],
+	packages=setuptools.find_packages(),
+	install_requires=[],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
