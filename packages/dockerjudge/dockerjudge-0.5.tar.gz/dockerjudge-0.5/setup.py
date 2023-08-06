50f02bcd0b70faeb450548e02230efb54b84ddbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:24 +0000

    Add package dockerjudge/0.5/dockerjudge-0.5.tar.gz

---
+++ packages/dockerjudge/dockerjudge-0.5.tar.gz/dockerjudge-0.5/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+from dockerjudge import __version__
+
+
+setuptools.setup(
+    name='dockerjudge',
+    version=__version__,
+    author='汪心禾',
+    author_email='wangxinhe06@gmail.com',
+    description='A Docker Based Online Judge Engine',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/wangxinhe2006/dockerjudge',
+    packages=['dockerjudge'],
+    install_requires=[
+        'docker[tls]>=3.7',
+        'ruamel.yaml'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: '
+        'GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: OS Independent',
+    ],
+)
