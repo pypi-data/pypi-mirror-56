e4995ab5952aa608b57b199410590de08dcd5c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:42 +0000

    Add package RSSReader/0.0.8/RSSReader-0.0.8.tar.gz

---
+++ packages/RSSReader/RSSReader-0.0.8.tar.gz/RSSReader-0.0.8/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RSSReader", # Replace with your own username
+    version="0.0.8",
+    author="tim",
+    author_email="tim_xia@icloud.com",
+    description="a simple Schedule of RSS Reader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
