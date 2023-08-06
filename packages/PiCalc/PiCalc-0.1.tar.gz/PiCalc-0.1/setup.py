2bc029501dd0375d0084708a93aef441f9da967a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:24 +0000

    Add package PiCalc/0.1/PiCalc-0.1.tar.gz

---
+++ packages/PiCalc/PiCalc-0.1.tar.gz/PiCalc-0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+setup(name="PiCalc",
+version="0.1",
+description="This is code which will find value of pi",
+long_description = "Hello Guys I have worked very hard on finding various method to find PI.",
+author="Dhaval",
+packages=['PiCalc'],
+install_requires=[])
+
