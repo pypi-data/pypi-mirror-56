74ad0f15f4de99bbc1eeeaa6653332c2f1b4eaec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:52 +0000

    Add package lethargy/1.2.0/lethargy-1.2.0.tar.gz

---
+++ packages/lethargy/lethargy-1.2.0.tar.gz/lethargy-1.2.0/lethargy/__init__.py
@@ -0,0 +1,26 @@
+from .lethargy import (
+    ArgsError,
+    Argv,
+    MissingOption,
+    Opt,
+    OptionError,
+    argv,
+    eprint,
+    print_if,
+    take_debug,
+    take_verbose,
+)
+
+__version__ = "1.2.0"
+__all__ = (
+    "OptionError",
+    "ArgsError",
+    "MissingOption",
+    "Opt",
+    "Argv",
+    "argv",
+    "take_debug",
+    "take_verbose",
+    "eprint",
+    "print_if",
+)
