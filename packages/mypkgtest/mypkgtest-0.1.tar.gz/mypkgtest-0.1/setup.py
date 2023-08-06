9b521f18416d806592c73b4a76de12856771b2c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:34 +0000

    Add package mypkgtest/0.1/mypkgtest-0.1.tar.gz

---
+++ packages/mypkgtest/mypkgtest-0.1.tar.gz/mypkgtest-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mypkgtest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/sean-bai/mypkgtest',
+      author='sean-bai',
+      author_email='zhihengbai@gmail.com',
+      license='MIT',
+      packages=['mypkgtest'],
+      zip_safe=False)
