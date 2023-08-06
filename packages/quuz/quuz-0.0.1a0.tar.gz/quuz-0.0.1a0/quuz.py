5c633bd0940c58ae737865f2e76c5202824b2ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:24 +0000

    Add package quuz/0.0.1a0/quuz-0.0.1a0.tar.gz

---
+++ packages/quuz/quuz-0.0.1a0.tar.gz/quuz-0.0.1a0/quuz.py
@@ -0,0 +1,5 @@
+"""
+Hello, world.
+
+This is a placeholder package.
+"""
---
+++ packages/quuz/quuz-0.0.1a0.tar.gz/quuz-0.0.1a0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="quuz",
+    version="0.0.1a",
+    author="EclecticIQ",
+    author_email="engineering@eclecticiq.com",
+    url="https://github.com/eclecticiq/quuz",
+    py_modules=["quuz"],
+)
