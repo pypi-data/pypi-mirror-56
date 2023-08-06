2aa3e065e7eb8bf2c263d0b98471e3501cedc7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:23 +0000

    Add package batching/1.0.7/batching-1.0.7.tar.gz

---
+++ packages/batching/batching-1.0.7.tar.gz/batching-1.0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="batching",
+    version="1.0.7",
+    description="Batching is a set of tools to format data for training sequence models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cirick/batching",
+    download_url="https://github.com/cirick/batching/archive/v1.0.4.tar.gz",
+    author="Charles Irick",
+    author_email="cirick@gmail.com",
+    include_package_data=True,
+    license="MIT",
+    packages=["batching"],
+    install_requires=[
+        "numpy>=1.17.4",
+        "pandas>=0.25.3",
+        "scikit-learn>=0.21.3",
+        "tensorflow>=2.0.0",
+        "boto3>=1.10.28"
+    ],
+    zip_safe=False,
+)
