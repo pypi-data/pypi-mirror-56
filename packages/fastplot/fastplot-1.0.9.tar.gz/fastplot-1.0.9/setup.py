e9230fb3f44943aa96b10c422f25ac0e70461148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:56 +0000

    Add package fastplot/1.0.9/fastplot-1.0.9.tar.gz

---
+++ packages/fastplot/fastplot-1.0.9.tar.gz/fastplot-1.0.9/setup.py
@@ -0,0 +1,26 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="fastplot",
+    description="Are you bored of re-writing code for each plot? Create publication-quality plots with a simple interface over matplotlib.",
+    license="GNU GENERAL PUBLIC LICENSE v3",
+    version="1.0.9",
+    author="Martino Trevisan",
+    author_email="martino.trevisan@polito.it",
+    maintainer="Martino Trevisan",
+    maintainer_email="martino.trevisan@polito.it",
+    url="https://github.com/marty90/fastplot",
+    download_url = 'https://github.com/marty90/fastplot/tarball/1.0.9',
+    packages=['fastplot'],
+    install_requires=['matplotlib', 'numpy', 'pandas', 'statsmodels', 'scipy', 'seaborn']
+)
+
+# Upload on pip with:
+# python setup.py sdist
+# twine upload dist/*
+
+# Test it locally removing the pip installed one
