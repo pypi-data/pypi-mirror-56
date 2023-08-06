7cb4115ae955b0ffcb6deca3faf71f2f61644d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:51 +0000

    Add package yahoo-historical/0.3.2/yahoo_historical-0.3.2.tar.gz

---
+++ packages/yahoo-historical/yahoo_historical-0.3.2.tar.gz/yahoo_historical-0.3.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="yahoo_historical",
+    packages=["yahoo_historical"],
+    version="0.3.2",
+    description="Fetches historical EOD (end of day) prices from yahoo finance",
+    author="Andrew Porter",
+    author_email="porter.r.andrew@gmail.com",
+    license="MIT License",
+    url="https://github.com/AndrewRPorter/yahoo-historical",
+    download_url="https://github.com/AndrewRPorter/yahoo-historical/releases",
+    install_requires=["setuptools", "pandas", "requests"],
+)
