693702ee6dc41413d981e0210259e7038846a8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:44 +0000

    Add package mkstorm/0.0.7/mkstorm-0.0.7.tar.gz

---
+++ packages/mkstorm/mkstorm-0.0.7.tar.gz/mkstorm-0.0.7/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+import os
+
+from setuptools import setup, find_packages
+
+try:
+    with open('README.rst') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+# version
+here = os.path.dirname(os.path.abspath(__file__))
+version = '0.0.7'
+
+setup(
+    name="mkstorm",
+    version=version,
+    url='https://github.com/Shochan024/mkstorm',
+    author='shochan',
+    author_email='jadetech0024@gmail.com',
+    maintainer='kinpira',
+    maintainer_email='jadetech0024@gmail.com',
+    description='Package Dependency: Validates package requirements',
+    long_description=readme,
+    packages=find_packages(),
+    install_requires=_requires_from_file('requirements.txt'),
+    license="MIT",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: MIT License',
+    ],
+    entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      pkgdep = pypipkg.scripts.command:main
+    """,
+)
