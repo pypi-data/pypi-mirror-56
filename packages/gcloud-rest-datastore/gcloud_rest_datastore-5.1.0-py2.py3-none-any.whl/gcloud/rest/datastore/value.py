c0073222b3cbb068d4e51ef0fee09cbd102a8b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:05 +0000

    Add package gcloud-rest-datastore/5.1.0/gcloud_rest_datastore-5.1.0-py2.py3-none-any.whl

---
+++ packages/gcloud-rest-datastore/gcloud_rest_datastore-5.1.0-py2.py3-none-any.whl/gcloud/rest/datastore/value.py
@@ -0,0 +1,92 @@
+from datetime import datetime
+from typing import Any
+from typing import Dict
+
+from gcloud.rest.datastore.constants import TypeName
+from gcloud.rest.datastore.constants import TYPES
+from gcloud.rest.datastore.key import Key
+from gcloud.rest.datastore.lat_lng import LatLng
+
+
+# https://cloud.google.com/datastore/docs/reference/data/rest/v1/projects/runQuery#value
+class Value(object):  # pylint:disable=useless-object-inheritance
+    key_kind = Key
+
+    def __init__(self, value     , exclude_from_indexes       = False)        :
+        self.value = value
+        self.excludeFromIndexes = exclude_from_indexes
+
+    def __eq__(self, other     )        :
+        if not isinstance(other, Value):
+            return False
+
+        return bool(
+            self.excludeFromIndexes == other.excludeFromIndexes
+            and self.value == other.value)
+
+    def __repr__(self)       :
+        return str(self.to_repr())
+
+    @classmethod
+    def from_repr(cls, data                )           :
+        supported_types = cls._get_supported_types()
+        for value_type, type_name in supported_types.items():
+            json_key = type_name.value
+            if json_key in data:
+                if json_key == 'nullValue':
+                    value = None
+                elif value_type == datetime:
+                    value = datetime.strptime(data[json_key],
+                                              '%Y-%m-%dT%H:%M:%S.%f000Z')
+                elif value_type == cls.key_kind:
+                    value = cls.key_kind.from_repr(data[json_key])
+                elif value_type == LatLng:
+                    value = LatLng.from_repr(data[json_key])
+                else:
+                    value = value_type(data[json_key])
+                break
+        else:
+            supported = [name.value for name in supported_types.values()]
+            raise NotImplementedError(
+                '{vals} does not contain a supported value type'
+                ' (any of: {supported})'.format(vals=data.keys(),
+                                                supported=supported))
+
+        # Google may not populate that field. This can happen with both
+        # indexed and non-indexed fields.
+        exclude_from_indexes = bool(data.get('excludeFromIndexes', False))
+
+        return cls(value=value, exclude_from_indexes=exclude_from_indexes)
+
+    def to_repr(self)                  :
+        value_type = self._infer_type(self.value)
+        if value_type in {TypeName.GEOPOINT, TypeName.KEY}:
+            value = self.value.to_repr()
+        elif value_type == TypeName.TIMESTAMP:
+            value = self.value.strftime('%Y-%m-%dT%H:%M:%S.%f000Z')
+        else:
+            value = 'NULL_VALUE' if self.value is None else self.value
+        return {
+            'excludeFromIndexes': self.excludeFromIndexes,
+            value_type.value: value,
+        }
+
+    def _infer_type(self, value     )            :
+        # Compatibility for python2
+        kind = str if 'unicode' in value.__class__.__name__ else type(value)
+        supported_types = self._get_supported_types()
+
+        try:
+            return supported_types[kind]
+        except KeyError:
+            raise NotImplementedError(
+                '{} is not a supported value type (any of: '
+                '{})'.format(kind, supported_types))
+
+    @classmethod
+    def _get_supported_types(cls):
+        supported_types = TYPES
+        supported_types.update({
+            cls.key_kind: TypeName.KEY,
+        })
+        return supported_types
