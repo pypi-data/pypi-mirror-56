d7d4fd7f085b00285fa24c90064357bef92e2ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:39:29 +0000

    Add package ridi-cms-sdk/3.0.2/ridi-cms-sdk-3.0.2.tar.gz

---
+++ packages/ridi-cms-sdk/ridi-cms-sdk-3.0.2.tar.gz/ridi-cms-sdk-3.0.2/README.md
@@ -0,0 +1,13 @@
+# Ridibooks CMS SDK - Python
+
+## Requirements
+
+- python 3.4 or higher
+
+## Setup
+
+`pip install ridi-cms-sdk`
+
+## Usage
+
+Check out our [sample project](https://github.com/ridi/cms-bootstrap-python) for more details.
---
+++ packages/ridi-cms-sdk/ridi-cms-sdk-3.0.2.tar.gz/ridi-cms-sdk-3.0.2/ridi/cms/cms_client.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from urllib.parse import quote_plus
+from thrift.transport import THttpClient
+from thrift.protocol import TMultiplexedProtocol
+from thrift.protocol import TJSONProtocol
+from ridi.cms.thrift.AdminAuth import AdminAuthService
+from ridi.cms.thrift.AdminMenu import AdminMenuService
+from ridi.cms.thrift.AdminTag import AdminTagService
+from ridi.cms.thrift.AdminUser import AdminUserService
+from ridi.cms.config import Config
+
+def _createProtocol(service_name, config: Config):
+    client = THttpClient.THttpClient(config.RPC_URL)
+    if config.RPC_SECRET:
+        (cf_id, cf_secret, *dummy) = config.RPC_SECRET.split(',')
+        client.setCustomHeaders({
+            'CF-Access-Client-Id': cf_id,
+            'CF-Access-Client-Secret': cf_secret
+        })
+    protocol = TJSONProtocol.TJSONProtocol(client)
+    protocol = TMultiplexedProtocol.TMultiplexedProtocol(protocol, service_name)
+    return protocol
+
+class AdminAuth(AdminAuthService.Client):
+    def __init__(self, config: Config):
+        super().__init__(_createProtocol('AdminAuth', config))
+        self.config = config
+
+    def getAuthorizeUrl(self, return_url: str = None) -> str:
+        param = '?return_url=%s' % quote_plus(return_url) if return_url else ''
+        return '/login' + param
+
+    def authorize(self, admin_id: str, check_url: str) -> bool:
+        return self.hasHashAuth(None, check_url, admin_id)
+
+class AdminMenu(AdminMenuService.Client):
+    def __init__(self, config: Config):
+        super().__init__(_createProtocol('AdminMenu', config))
+
+class AdminTag(AdminTagService.Client):
+    def __init__(self, config: Config):
+        super().__init__(_createProtocol('AdminTag', config))
+
+class AdminUser(AdminUserService.Client):
+    def __init__(self, config: Config):
+        super().__init__(_createProtocol('AdminUser', config))
