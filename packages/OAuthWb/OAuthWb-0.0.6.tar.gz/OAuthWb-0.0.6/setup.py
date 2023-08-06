fc809447a3607b6c2ef4c68d1b08dc38688efe8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:02 +0000

    Add package OAuthWb/0.0.6/OAuthWb-0.0.6.tar.gz

---
+++ packages/OAuthWb/OAuthWb-0.0.6.tar.gz/OAuthWb-0.0.6/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="OAuthWb",  # Replace with your own username
+    version="0.0.6",
+    author="DAHANGZAIYA",
+    author_email="y17505251998@163.com",
+    description="weibo_login",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yinbohang/weibo_login",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
