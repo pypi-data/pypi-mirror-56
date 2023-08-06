36dee4ea3e2ad724ddb8aba1a817813c1c676a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:36 +0000

    Add package DAStk/0.3.2/DAStk-0.3.2-py2.py3-none-any.whl

---
+++ packages/DAStk/DAStk-0.3.2-py2.py3-none-any.whl/DAStk/__init__.py
@@ -0,0 +1,16 @@
+import DAStk.process_atac as p
+import DAStk.differential_md_score as d
+import DAStk.barcode_plot as b
+import DAStk.ma_plot as m
+
+def process_atac():
+    p.main()
+
+def differential_md_score():
+    d.main()
+    
+def barcode_plot():
+    b.main() 
+    
+def ma_plot():
+    m.main()     
