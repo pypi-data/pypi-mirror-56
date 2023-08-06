d2674c822c5126c161d77461e5c057bb6bfad4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:00 +0000

    Add package pyOpenRPA/1.0.31/pyOpenRPA-1.0.31.tar.gz

---
+++ packages/pyOpenRPA/pyOpenRPA-1.0.31.tar.gz/pyOpenRPA-1.0.31/pyOpenRPA/Robot/__init__.py
@@ -0,0 +1,13 @@
+r"""
+
+The OpenRPA package (from UnicodeLabs)
+
+"""
+__all__ = [
+    'UIDesktop', 'Clipboard', 'IntegrationOrchestrator', 'Window', 'ProcessCommunicator'
+]
+__author__ = 'Ivan Maslov <ivan.maslov@unicodelabs.ru>'
+#from . import UIDesktop
+from . import Clipboard
+from . import Window
+from . import ProcessCommunicator
