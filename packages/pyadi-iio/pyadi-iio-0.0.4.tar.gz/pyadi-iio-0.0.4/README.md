7d255aaaa98542b455487da8012a5a98d7640b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:30 +0000

    Add package pyadi-iio/0.0.4/pyadi-iio-0.0.4.tar.gz

---
+++ packages/pyadi-iio/pyadi-iio-0.0.4.tar.gz/pyadi-iio-0.0.4/README.md
@@ -0,0 +1,63 @@
+# pyadi-iio: Analog Devices python interfaces for hardware with Industrial I/O drivers
+
+[![Build Status](https://travis-ci.org/analogdevicesinc/pyadi-iio.svg?branch=master)](https://travis-ci.org/analogdevicesinc/pyadi-iio)
+[![PyPI version](https://badge.fury.io/py/pyadi-iio.svg)](https://badge.fury.io/py/pyadi-iio) [![Codacy Badge](https://api.codacy.com/project/badge/Grade/4bd027bfc5774029a30a9e1cedf5a434)](https://www.codacy.com/app/travis.collins/pyadi-iio?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=analogdevicesinc/pyadi-iio&amp;utm_campaign=Badge_Grade) ![PyPI - Python Version](https://img.shields.io/pypi/pyversions/pyadi-iio)
+
+[[Docs](http://analogdevicesinc.github.io/pyadi-iio/)]
+[[Support](http://ez.analog.com)]
+[[Wiki](https://wiki.analog.com/resources/tools-software/linux-software/pyadi-iio)]
+
+pyadi-iio is a python abstraction module for ADI hardware with IIO drivers to make them easier to use. The libIIO interface although extremely flexible can be cumbersome to use due to the amount of boilerplate code required for even simple examples, especially when interfacing with buffers. This module has custom interfaces classes for specific parts and development systems which can generally make them easier to understand and use. To get up and running with a device can be as simple as a few lines of code:
+```python
+import adi
+# Create device from specific uri address
+sdr = adi.ad9361(uri="ip:192.168.2.1")
+# Get data from transceiver
+data = sdr.rx()
+```
+
+### Currently supported hardware
+- AD936X (Pluto, FMComms2/3/4, ADRV936X)
+- AD9371
+- ADRV9009
+- ADRV9009-ZU11EG
+- AD9144
+- AD9680
+- DAQ2
+- ADIS16460
+- AD7124
+
+### Dependencies
+- [libiio with python bindings](https://wiki.analog.com/resources/tools-software/linux-software/libiio)
+- [numpy](https://scipy.org/install.html)
+
+### Installing from source
+```
+tcollins@jeeves:~$ git clone https://github.com/analogdevicesinc/pyadi-iio.git
+tcollins@jeeves:~$ cd pyadi-iio
+tcollins@jeeves:~$ (sudo) python setup.py install
+```
+### Installing from pip
+```
+tcollins@jeeves:~$ (sudo) pip install pyadi-iio
+```
+### Building doc
+Install necessary tools
+```
+tcollins@jeeves:~$ (sudo) pip install -r requirements_doc.txt
+```
+Build actual doc with sphinx
+```
+tcollins@jeeves:~$ cd doc
+tcollins@jeeves:~$ make html
+```
+### Developing
+Install necessary tools
+```
+tcollins@jeeves:~$ (sudo) pip install -r requirements_dev.txt
+```
+
+Running pre-commit checks
+```
+tcollins@jeeves:~$ pre-commit run --all-files
+```
---
+++ packages/pyadi-iio/pyadi-iio-0.0.4.tar.gz/pyadi-iio-0.0.4/adi/ad9371.py
@@ -0,0 +1,132 @@
+# Copyright (C) 2019 Analog Devices, Inc.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#     - Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     - Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#     - Neither the name of Analog Devices, Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#     - The use of this software may or may not infringe the patent rights
+#       of one or more patent holders.  This license does not release you
+#       from the requirement that you obtain separate licenses from these
+#       patent holders to use this software.
+#     - Use of the software either in source or binary form, must be run
+#       on or directly connected to an Analog Devices Inc. component.
+#
+# THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED.
+#
+# IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, INTELLECTUAL PROPERTY
+# RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from adi.context_manager import context_manager
+from adi.rx_tx import rx_tx
+
+
+class ad9371(rx_tx, context_manager):
+    """ AD9371 Transceiver """
+
+    _complex_data = True
+    _rx_channel_names = ["voltage0_i", "voltage0_q", "voltage1_i", "voltage1_q"]
+    _tx_channel_names = ["voltage0", "voltage1", "voltage2", "voltage3"]
+    _device_name = ""
+
+    def __init__(self, uri=""):
+
+        context_manager.__init__(self, uri, self._device_name)
+
+        self._ctrl = self._ctx.find_device("ad9371-phy")
+        self._rxadc = self._ctx.find_device("axi-ad9371-rx-hpc")
+        self._rxobs = self._ctx.find_device("axi-ad9371-rx-obs-hpc")
+        self._txdac = self._ctx.find_device("axi-ad9371-tx-hpc")
+
+        rx_tx.__init__(self)
+
+    @property
+    def gain_control_mode(self):
+        """gain_control_mode: Mode of receive path AGC. Options are:
+        automatic, hybrid, manual"""
+        return self._get_iio_attr_str("voltage0", "gain_control_mode", False)
+
+    @gain_control_mode.setter
+    def gain_control_mode(self, value):
+        self._set_iio_attr("voltage0", "gain_control_mode", False, value)
+
+    @property
+    def rx_hardwaregain(self):
+        """rx_hardwaregain: Gain applied to RX path. Only applicable when
+        gain_control_mode is set to 'manual'"""
+        return self._get_iio_attr("voltage0", "hardwaregain", False)
+
+    @rx_hardwaregain.setter
+    def rx_hardwaregain(self, value):
+        if self.gain_control_mode == "manual":
+            self._set_iio_attr("voltage0", "hardwaregain", False, value)
+
+    @property
+    def tx_hardwaregain(self):
+        """tx_hardwaregain: Attenuation applied to TX path"""
+        return self._get_iio_attr("voltage0", "hardwaregain", True)
+
+    @tx_hardwaregain.setter
+    def tx_hardwaregain(self, value):
+        self._set_iio_attr("voltage0", "hardwaregain", True, value)
+
+    @property
+    def rx_rf_bandwidth(self):
+        """rx_rf_bandwidth: Bandwidth of front-end analog filter of RX path"""
+        return self._get_iio_attr("voltage0", "rf_bandwidth", False)
+
+    @property
+    def tx_rf_bandwidth(self):
+        """tx_rf_bandwidth: Bandwidth of front-end analog filter of TX path"""
+        return self._get_iio_attr("voltage0", "rf_bandwidth", True)
+
+    @property
+    def rx_sample_rate(self):
+        """rx_sample_rate: Sample rate RX path in samples per second"""
+        return self._get_iio_attr("voltage0", "sampling_frequency", False)
+
+    @property
+    def tx_sample_rate(self):
+        """tx_sample_rate: Sample rate TX path in samples per second"""
+        return self._get_iio_attr("voltage0", "sampling_frequency", True)
+
+    @property
+    def rx_lo(self):
+        """rx_lo: Carrier frequency of RX path"""
+        return self._get_iio_attr("altvoltage0", "RX_LO_frequency", True)
+
+    @rx_lo.setter
+    def rx_lo(self, value):
+        self._set_iio_attr("altvoltage0", "RX_LO_frequency", True, value)
+
+    @property
+    def tx_lo(self):
+        """tx_lo: Carrier frequency of TX path"""
+        return self._get_iio_attr("altvoltage1", "TX_LO_frequency", True)
+
+    @tx_lo.setter
+    def tx_lo(self, value):
+        self._set_iio_attr("altvoltage1", "TX_LO_frequency", True, value)
+
+    @property
+    def sn_lo(self):
+        """sn_lo: Carrier frequency of Sniffer/ORx path"""
+        return self._get_iio_attr("altvoltage2", "RX_SN_LO_frequency", True)
+
+    @sn_lo.setter
+    def sn_lo(self, value):
+        self._set_iio_attr("altvoltage2", "RX_SN_LO_frequency", True, value)
