c2b0d32b71b34c84ae01d633687342726edd7901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:56 +0000

    Add package dsb-spider/0.0.2/dsb_spider-0.0.2.tar.gz

---
+++ packages/dsb-spider/dsb_spider-0.0.2.tar.gz/dsb_spider-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+
+setup(
+    name='dsb_spider',
+    version='0.0.2',
+    author='cjxh',
+    author_email='chenjianxihang@duoshoubang.cn',
+    url='http://m.fine3q.com/home-w/index.html',
+    description=u'666',
+    packages=['dsb_spider'],
+    install_requires=[],
+    python_requires='>=3.6'
+)
