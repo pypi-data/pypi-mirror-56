191cce6e20ee93c2e80d9e66f7d0561352aca120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:06 +0000

    Add package dictipy/0.0.1/dictipy-0.0.1.tar.gz

---
+++ packages/dictipy/dictipy-0.0.1.tar.gz/dictipy-0.0.1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read_file(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
+def upload_to_pypi():
+    os.system("twine upload dist/*")
+
+
+setup(
+    name='dictipy',
+    version="0.0.1",
+    author='Gioele Crispo',
+    author_email='crispogioele@gmail.com',
+    package_dir={'dictipy': 'dictipy'},
+    packages=find_packages('.'),
+    # scripts=['bin/script1', 'bin/script2'],
+    url='https://github.com/gioelecrispo/dictipy.git',
+    license='MIT',
+    license_file='LICENSE',
+    platform='any',
+    description='Dictipy creates the right dict also for nested objects using recursion.',
+    long_description=read_file('README.rst'),
+    install_requires=read_file('requirements.txt').splitlines(),
+    python_requires='>=3',
+    package_data={
+        # '': ['package_data.dat'],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
+
+upload_to_pypi()
+
