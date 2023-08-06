605e5068532c9c64f106687aa5df34201cdbc778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:11 +0000

    Add package bosch-thermostat-http-client/0.5.2/bosch-thermostat-http-client-0.5.2.tar.gz

---
+++ packages/bosch-thermostat-http-client/bosch-thermostat-http-client-0.5.2.tar.gz/bosch-thermostat-http-client-0.5.2/README.md
@@ -0,0 +1,37 @@
+
+
+# bosch-thermostat-http-client-python
+Python3 asyncio package to talk to Bosch Thermostats via their gateway, especially for Buderus KM200/KM50 and Nefit IP modules used in Enviline heat pumps. These gateways support Http access in local network.
+
+example :
+```python
+import asyncio 
+import aiohttp
+
+from buderus.gateway import Gateway
+async def main():
+    async with aiohttp.ClientSession() as session:
+        gateway =  Gateway(session, '<Local IP of gateway>','<gateway password>','<user password>')
+        await gateway.initialize()
+       
+      
+        print(gateway.info.uuid)
+
+        for sensor in gateway.sensors.items:
+            print (sensor.name, sensor.value)
+
+asyncio.get_event_loop().run_until_complete(main())
+```
+
+To run this code do the following:
+
+* create file data_file.txt and insert like this:
+```
+ip
+access_key
+password
+```
+replace strings with proper values
+
+* run in dir `python3 -m venv .`
+* run `python3 test.py`
---
+++ packages/bosch-thermostat-http-client/bosch-thermostat-http-client-0.5.2.tar.gz/bosch-thermostat-http-client-0.5.2/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from setuptools import setup
+
+VERSION = '0.5.2'
+
+REQUIRES = [
+    'pyaes>=1.6.1',
+    'aiohttp'
+    ]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='bosch-thermostat-http-client',
+    version=VERSION,
+    description='Python API for talking to Bosch™ Heating gateway using HTTP ',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Ludovic Laurent',
+    author_email='ludovic.laurent@gmail.com',
+    maintainer='Ludovic Laurent',
+    maintainer_email='ludovic.laurent@gmail.com',
+    url='https://github.com/moustic999/bosch-thermostat-http-client-python.git',
+    download_url='https://github.com/moustic999/bosch-thermostat-http-client-python/archive/{}.zip'.format(VERSION),
+    packages=["bosch_thermostat_http"],
+    install_requires=REQUIRES,
+    include_package_data=True,
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: Console',
+        'Topic :: Other/Nonlisted Topic',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+)
