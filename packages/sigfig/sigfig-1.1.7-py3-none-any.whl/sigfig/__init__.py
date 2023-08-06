9f8143672d7a5d78bdb703209568e75fe6d9dded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:08 +0000

    Add package sigfig/1.1.7/sigfig-1.1.7-py3-none-any.whl

---
+++ packages/sigfig/sigfig-1.1.7-py3-none-any.whl/sigfig/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['sigfig']
+from sigfig import sigfig.round as round
+from sigfig import sigfig.roundit as roundit
+from sigfig import sigfig.round_unc as round_unc
+from sigfig import sigfig.round_sf as round_sf
