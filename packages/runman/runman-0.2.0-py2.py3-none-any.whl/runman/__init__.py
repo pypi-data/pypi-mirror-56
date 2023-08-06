57af44a6ad6e6a28c14176a28a71be452090db42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:10 +0000

    Add package runman/0.2.0/runman-0.2.0-py2.py3-none-any.whl

---
+++ packages/runman/runman-0.2.0-py2.py3-none-any.whl/runman/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Run Manager."""
+
+__author__ = "David Williams"
+__email__ = "cdavew@alleninstitute.org"
+# Do not edit this string manually, always use bumpversion
+__version__ = "0.2.0"
+
+from .runman import Run  # noqa: F401
