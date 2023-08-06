506c447ce202fccdc46d300e71c7162cc767db62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:48 +0000

    Add package aiida-optimize/0.3.0/aiida-optimize-0.3.0.tar.gz

---
+++ packages/aiida-optimize/aiida-optimize-0.3.0.tar.gz/aiida-optimize-0.3.0/aiida_optimize/engines/_bisection.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+
+# © 2017-2019, ETH Zurich, Institut für Theoretische Physik
+# Author: Dominik Gresch <greschd@gmx.ch>
+"""
+Defines a 1D bisection optimization engine.
+"""
+
+from fsc.export import export
+from aiida.orm import Float
+
+from ._base import OptimizationEngineImpl, OptimizationEngineWrapper
+
+
+class _BisectionImpl(OptimizationEngineImpl):
+    """
+    Implementation class for the bisection optimization engine.
+    """
+    def __init__(
+        self,
+        *,
+        lower: float,
+        upper: float,
+        tol: float,
+        input_key: str,
+        result_key: str,
+        target_value: float,
+        logger,
+        result_state=None,
+        initialized=False
+    ):
+        super(_BisectionImpl, self).__init__(logger=logger, result_state=result_state)
+        self.lower = lower
+        self.upper = upper
+        self.initialized = initialized
+        self.tol = tol
+        self.input_key = input_key
+        self.result_key = result_key
+        self.target_value = target_value
+
+    @property
+    def _state(self):
+        return {k: v for k, v in self.__dict__.items() if k not in ['_result_mapping', '_logger']}
+
+    @property
+    def is_finished(self):
+        return abs(self.upper - self.lower) < self.tol
+
+    @property
+    def average(self):
+        return (self.upper + self.lower) / 2.
+
+    def _create_inputs(self):
+        if not self.initialized:
+            return [{self.input_key: Float(self.lower)}, {self.input_key: Float(self.upper)}]
+        return [{self.input_key: Float(self.average)}]
+
+    def _update(self, outputs):
+        output_values = outputs.values()
+        num_vals = len(output_values)
+        if not self.initialized:
+            self.initialized = True
+            assert num_vals == 2
+            # initial step: change upper such that the _result_ of upper is higher
+            results = [val[self.result_key] for val in output_values]
+            lower_val, upper_val = results
+            if lower_val > upper_val:
+                self.lower, self.upper = self.upper, self.lower
+            if min(results) > self.target_value:
+                # TODO: add exit code
+                raise ValueError(
+                    "Target value '{}' is outside range '{}'".format(self.target_value, results)
+                )
+            if max(results) < self.target_value:
+                # TODO: add exit code
+                raise ValueError(
+                    "Target value '{}' is outside range '{}'".format(self.target_value, results)
+                )
+        else:
+            assert num_vals == 1
+            res = next(iter(output_values))[self.result_key]
+            if (res - self.target_value) > 0:
+                self.upper = self.average
+            else:
+                self.lower = self.average
+
+    def _get_optimal_result(self):
+        """
+        Return the index and optimization value of the best evaluation workflow.
+        """
+        output_values = {
+            key: value.output[self.result_key]
+            for key, value in self._result_mapping.items()
+        }
+        return min(output_values.items(), key=lambda item: abs(item[1].value - self.target_value))
+
+    @property
+    def result_value(self):
+        return self._get_optimal_result()[1]
+
+    @property
+    def result_index(self):
+        return self._get_optimal_result()[0]
+
+
+@export
+class Bisection(OptimizationEngineWrapper):
+    """
+    Optimization engine that performs a bisection.
+
+    :param lower: Lower boundary for the bisection.
+    :type lower: float
+
+    :param upper: Upper boundary for the bisection.
+    :type upper: float
+
+    :param tol: Tolerance in the input value.
+    :type tol: float
+
+    :param input_key: Name of the input to be varied in the optimization.
+    :type input_key: str
+
+    :param result_key: Name of the output which contains the evaluated function.
+    :type result_key: str
+
+    :param target_value: Target value of the function towards which it should be optimized.
+    :type target_value: float
+    """
+
+    _IMPL_CLASS = _BisectionImpl
+
+    def __new__(
+        cls,
+        lower: float,
+        upper: float,
+        *,
+        tol: float = 1e-6,
+        input_key: str = 'x',
+        result_key: str = 'result',
+        target_value: float = 0.,
+        logger=None
+    ):  # pylint: disable=arguments-differ
+        return cls._IMPL_CLASS(
+            lower=lower,
+            upper=upper,
+            tol=tol,
+            input_key=input_key,
+            result_key=result_key,
+            target_value=target_value,
+            logger=logger
+        )
---
+++ packages/aiida-optimize/aiida-optimize-0.3.0.tar.gz/aiida-optimize-0.3.0/aiida_optimize/engines/_result_mapping.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# © 2017-2019, ETH Zurich, Institut für Theoretische Physik
+# Author: Dominik Gresch <greschd@gmx.ch>
+"""
+Defines the datastructures used by optimization engines to keep track of results.
+"""
+
+from fsc.export import export
+
+
+@export
+class Result(object):
+    """
+    Data object for storing the input created by the optimization engine, and the output from the evaluation process corresponding to that input.
+    """
+    def __init__(self, input_, output=None):
+        self.input = input_
+        self.output = output
+
+
+@export
+class ResultMapping(object):
+    """
+    Maps the keys used to identify evaluations to their inputs / outputs.
+    """
+    def __init__(self):
+        self._results = {}
+
+    @property
+    def state(self):
+        """
+        Uniquely defines the state of the object. This can be used to create an identical copy.
+        """
+        return self._results
+
+    @classmethod
+    def from_state(cls, state):
+        """
+        Create a :class:`ResultMapping` instance from a state.
+        """
+        instance = cls()
+        if state is not None:
+            instance._results = state  # pylint: disable=protected-access
+        return instance
+
+    def add_inputs(self, inputs_list):
+        """
+        Adds a list of inputs to the mapping, generating new keys. Returns a dict mapping the keys to the inputs.
+        """
+        keys = []
+        for input_value in inputs_list:
+            key = self._get_new_key()
+            keys.append(key)
+            self._results[key] = Result(input_=input_value)
+
+        return {k: self._results[k].input for k in keys}
+
+    def _get_new_key(self):
+        try:
+            return max(self._results.keys()) + 1
+        except ValueError:
+            return 0
+
+    def add_outputs(self, outputs):
+        for key, out in outputs.items():
+            self._results[key].output = out
+
+    def __getattr__(self, key):
+        return getattr(self._results, key)
+
+    def __getitem__(self, key):
+        return self._results[key]
+
+    def __len__(self):
+        return len(self._results)
