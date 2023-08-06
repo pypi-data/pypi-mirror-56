5cfe72b997eb2e1fbc8f85b12761b4f263d1aaec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:12 +0000

    Add package Webbean/0.0.1/Webbean-0.0.1.tar.gz

---
+++ packages/Webbean/Webbean-0.0.1.tar.gz/Webbean-0.0.1/webbean/bank.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from os import path
+import argparse
+import logging
+
+
+from webbean.modules import MODULES
+from webbean.modules.beancount import beancount  # noqa
+from webbean.modules.weboob import weboob  # noqa
+
+
+DAYS_SYNCED = 30
+
+
+def parse_arguments(args):
+
+    accounts = []
+    for module in MODULES.ALL:
+        files = args.__dict__[module] or []
+        for filename in files:
+            if not path.isfile(filename):
+                logging.error(f"the file {filename} doesn' not exist")
+                return False
+
+            klass = eval(module)
+            account = klass(filename)
+            accounts.append(account)
+
+    return accounts
+
+
+def add_modules_arguments(parser):
+    for module in MODULES.ALL:
+        parser.add_argument(f'--{module}', nargs='*')
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Synchronise account")
+    add_modules_arguments(parser)
+
+    args = parser.parse_args()
+
+    accounts = parse_arguments(args)
+    if not accounts:
+        parser.print_help()
+        exit()
+    beancount_transactions = accounts[0].get_all_transactions(days_synced=DAYS_SYNCED)
+
+    w = weboob("plop")
+    weboob_transactions = w.get_all_transactions(days_synced=DAYS_SYNCED)
+
+    missing_transactions = []
+    for wb_transaction in weboob_transactions:
+        if not [True for tr in beancount_transactions if tr == wb_transaction]:
+            missing_transactions.append(wb_transaction)
+
+    accounts[0].write_transactions(list(missing_transactions))
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/Webbean/Webbean-0.0.1.tar.gz/Webbean-0.0.1/webbean/core/Transaction.py
@@ -0,0 +1,20 @@
+from datetime import timedelta
+
+
+DELTA_DAY = 3
+
+
+class Transaction:
+
+    def __init__(self, date=None, amount=None, account=None, label=None):
+        self.date = date
+        self.amount = amount
+        self.account = account
+        self.label = label
+
+    def __str__(self):
+        return "{}: {}".format(self.date, self.amount)
+
+    def __eq__(self, other):
+        delta = self.date - other.date
+        return abs(delta) < timedelta(days=DELTA_DAY) and self.amount == other.amount
