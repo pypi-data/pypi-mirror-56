6943a893ac7811774afc25b9cedd88969f75cdbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:51 +0000

    Add package davtelepot/2.3.7/davtelepot-2.3.7.tar.gz

---
+++ packages/davtelepot/davtelepot-2.3.7.tar.gz/davtelepot-2.3.7/davtelepot/__init__.py
@@ -0,0 +1,35 @@
+"""Information about this package.
+
+See `bot.py` for information about Bot class.
+    ```python3.5+
+    from davtelepot.bot import Bot
+    help(Bot)
+    ```
+
+Legacy `custombot.py` is kept for backward compatibility but will finally
+    be deprecated.
+"""
+
+__author__ = "Davide Testa"
+__email__ = "davide@davte.it"
+__credits__ = ["Marco Origlia", "Nick Lee @Nickoala"]
+__license__ = "GNU General Public License v3.0"
+__version__ = "2.3.7"
+__maintainer__ = "Davide Testa"
+__contact__ = "t.me/davte"
+
+import logging
+
+# Legacy module; please use `from davtelepot.bot import Bot` from now on
+from davtelepot.custombot import Bot
+
+# Prevent aiohttp from encoding file names as URLs, replacing ' ' with '%20'
+# Thanks @Nickoala (Nick Lee) for this hack from his archived `telepot` repo
+try:
+    import aiohttp
+    from davtelepot.hacks import hacked_content_disposition_header
+    aiohttp.payload.content_disposition_header = (
+        hacked_content_disposition_header
+    )
+except (ImportError, AttributeError) as e:
+    logging.error(f"{e}")
