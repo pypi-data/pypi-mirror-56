aef666f3fc0bf5f25bcdbccc4b85ead4da56f2a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:26 +0000

    Add package social-ethosa/0.4.6/social_ethosa-0.4.6-py3-none-any.whl

---
+++ packages/social-ethosa/social_ethosa-0.4.6-py3-none-any.whl/social_ethosa/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# author: ethosa
+
+from .ConfigReader import *
+from .botwrapper import *
+from .UtilsManager import *
+
+
+__version__ = "0.4.6"
+__authors__ = ["Ethosa"]
+__copyright__ = "2019"
+__changelog__ = ["social_ethosa.net.vkcom - optimization", "social_ethosa.botwrapper.BotBase - fix", "social_ethosa.graphics - init"]
