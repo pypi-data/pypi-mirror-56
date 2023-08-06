33f4aa749f4a93d9f19f3e77145085be947987cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:44 +0000

    Add package newsscraper/0.1.2/newsscraper-0.1.2.tar.gz

---
+++ packages/newsscraper/newsscraper-0.1.2.tar.gz/newsscraper-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import newsscraper
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'r') as fh:
+    requirements = fh.read().splitlines()
+
+setup(name='newsscraper',
+      version=newsscraper.__version__,
+      description='News scraping and reporting.',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
+      url='https://github.com/wichern/newsscraper',
+      author='Paul Wichern',
+      author_email='paul@menphis.de',
+      license='MIT',
+      keywords=['python3', 'webscraping', 'selenium', 'beautifulsoup4'],
+      package_data={'': ['newscraper/report.html']},
+      include_package_data=True,
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 3',
+          'Operating System :: OS Independent',
+      ],
+      python_requires='>=3.6',
+      packages=['newsscraper'],
+      install_requires=requirements)
