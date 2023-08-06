fe7675eb0ea54e603f3296cdfaa596660c39d4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:57 +0000

    Add package eagle-cli/0.4/eagle_cli-0.4-py3-none-any.whl

---
+++ packages/eagle-cli/eagle_cli-0.4-py3-none-any.whl/eagle/meta.py
@@ -0,0 +1,7 @@
+CONFIG = {
+    "package_name": "eagle",
+    "version": "0.4",
+    "description": "Simple TODO tool for CLI.",
+    "author": "n1",
+    "homepage": "https://github.com/im-n1/eagle",
+}
