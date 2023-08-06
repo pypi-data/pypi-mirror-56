7a9b54b4c4d402487e9cbcb11d6f6fa2d03ff0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:02 +0000

    Add package sspo-client/1.1.11/sspo_client-1.1.11-py3-none-any.whl

---
+++ packages/sspo-client/sspo_client-1.1.11-py3-none-any.whl/sspo_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from sspo_client.api.artifact_api import ArtifactApi
+from sspo_client.api.core_api import CoreApi
+from sspo_client.api.process_api import ProcessApi
+from sspo_client.api.stakeholder_api import StakeholderApi
