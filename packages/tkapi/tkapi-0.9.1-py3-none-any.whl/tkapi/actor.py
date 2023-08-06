8847dce32253cfe5d31c7b91b81e8ad8c5e25c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:38 +0000

    Add package tkapi/0.9.1/tkapi-0.9.1-py3-none-any.whl

---
+++ packages/tkapi/tkapi-0.9.1-py3-none-any.whl/tkapi/actor.py
@@ -0,0 +1,25 @@
+import tkapi
+
+
+class Actor(tkapi.TKItemRelated, tkapi.TKItem):
+    url = NotImplementedError
+
+    @property
+    def zaken(self):
+        from tkapi.zaak import Zaak
+        return self.related_items(Zaak)
+
+    @property
+    def documenten(self):
+        from tkapi.document import Document
+        return self.related_items(Document)
+
+    @property
+    def stemming(self):
+        from tkapi.stemming import Stemming
+        return self.related_item(Stemming)
+
+    @property
+    def commissie(self):
+        from tkapi.commissie import Commissie
+        return self.related_item(Commissie)
