8f825895de321f29a61d31b85068ab10c4eee74c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:02 +0000

    Add package prompt-tr-free/0.0.1/prompt-tr-free-0.0.1.tar.gz

---
+++ packages/prompt-tr-free/prompt-tr-free-0.0.1.tar.gz/prompt-tr-free-0.0.1/setup.py
@@ -0,0 +1,43 @@
+r'''
+"promtone translate"
+'''
+# pylint: disable=invalid-name
+from pathlib import Path
+import re
+
+from setuptools import setup, find_packages
+
+name = """prompt-tr-free"""
+# description = ' '.join(name.split('-'))
+description = name.replace('-tr-', ' translate for ')
+dir_name, = find_packages()
+
+version, = re.findall(r"\n__version__\W*=\W*'([^']+)'", open(f'{dir_name}/__init__.py').read())
+targz = 'v_' + version.replace('.', '') + '.tar.gz'
+install_requires = Path('requirements.txt').read_text()
+install_requires = [elm for elm in install_requires.split('\n') if not elm.startswith('#')]
+
+README_rst = f'{Path(__file__).parent}/README.md'
+long_description = open(README_rst, encoding='utf-8').read() if Path(README_rst).exists() else ''
+
+setup(
+    name=name,
+    packages=find_packages(),
+    version=version,
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['machine translation', 'free', 'scraping', ],
+    author="mikeee",
+    url=f'http://github.com/ffreemt/{name}',
+    download_url='https://github.com/ffreemt/prompt-tr-free/archive/' + targz,
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+    license='MIT License',
+)
