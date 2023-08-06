7e31d989a2d28cab366ed23ac5f5f9b5efba11ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:13 +0000

    Add package turbustat/1.1.0/turbustat-1.1.0.tar.gz

---
+++ packages/turbustat/turbustat-1.1.0.tar.gz/turbustat-1.1.0/turbustat/__init__.py
@@ -0,0 +1,9 @@
+
+from ._astropy_init import *
+
+# if not _ASTROPY_SETUP_:
+#     from .statistics import *
+#     from .analysis import *
+#     # from .cube_tools import *
+#     from .data_reduction import *
+#     from .io import *
