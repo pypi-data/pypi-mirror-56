3cfc7b5c3d966990206fc367aa519dcf5b11fc26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:04 +0000

    Add package Authomatic/0.2.0a3/Authomatic-0.2.0a3.tar.gz

---
+++ packages/Authomatic/Authomatic-0.2.0a3.tar.gz/Authomatic-0.2.0a3/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    packages=find_packages(),
+    package_data={'': ['*.txt', '*.rst']},
+    extras_require={
+        'OpenID': [
+            "python-openid ; python_version<'3'",
+            "python3-openid ; python_version>='3'",
+        ],
+    },
+)
