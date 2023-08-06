32cae9a24f7404939af18084956962b62fa220a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:44 +0000

    Add package instrument-server/1.3.2/instrument_server-1.3.2-py2.py3-none-any.whl

---
+++ packages/instrument-server/instrument_server-1.3.2-py2.py3-none-any.whl/instrument_server/command/error.py
@@ -0,0 +1,24 @@
+from .base  import Base
+from .mixin import ErrorQueueMixin, ParserMixin
+
+class Error(ErrorQueueMixin, ParserMixin, Base):
+    def __init__(self, devices, state, **settings):
+        Base           .__init__(self, devices, state, **settings)
+        ParserMixin    .__init__(self, command='errors?')
+        ErrorQueueMixin.__init__(self)
+        self.state['__errors__'] = []
+
+    @property
+    def help_str(self):
+        return 'Returns errors currently in queue:\n `errors?`'
+
+    def execute(self, received_command):
+        args = self.args(received_command)
+        prev_errors = self.error_queue.copy()
+        self.error_queue.clear()
+        return prev_errors
+
+# # Declare as device plugin
+# # for auto-import
+IS_COMMAND_PLUGIN = True
+plugin            = Error
