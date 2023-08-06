2e0f222fcfcc88abb7bbd484f2f5df07d61bdc6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:53 +0000

    Add package epicenter/1.6/epicenter-1.6.tar.gz

---
+++ packages/epicenter/epicenter-1.6.tar.gz/epicenter-1.6/epicenter/__init__.py
@@ -0,0 +1,10 @@
+"""
+Imports here are performed so that "from epicenter import Epicenter" works as
+an import in models.
+"""
+from __future__ import absolute_import
+
+from .epicenter import Epicenter
+
+# Version number is tracked on PyPi
+__version__ = 1.6
