db5ae3ca1b0a988baadc14a143a176f0fc4d9a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:21 +0000

    Add package ssh-menu/2.0.5/ssh-menu-2.0.5.tar.gz

---
+++ packages/ssh-menu/ssh-menu-2.0.5.tar.gz/ssh-menu-2.0.5/setup.py
@@ -0,0 +1,30 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+config = {
+    "keywords": "ssh menu interactive",
+    "author": "Anton Andersson",
+    "description": "A very simple interactive ssh profile menu",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "url": "https://antonjah.github.io/ssh-menu/",
+    "download_url": "https://antonjah.github.io/ssh-menu/",
+    "author_email": "contact@antonandersson.se",
+    "version": "2.0.5",
+    "install_requires": ["bullet"],
+    "packages": ["sshmenu", "sshmenu.decorators", "sshmenu.tmux"],
+    "entry_points": {
+        "console_scripts": ['sshmenu = sshmenu.sshmenu:main']
+    },
+    "name": "ssh-menu",
+    "classifiers": [
+        "Programming Language :: Python :: 3.6",
+    ],
+}
+
+setup(**config)
