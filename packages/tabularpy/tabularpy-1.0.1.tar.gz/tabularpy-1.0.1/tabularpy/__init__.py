29139e52e31df48c19613b7d3941b72ad7f91f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:25 +0000

    Add package tabularpy/1.0.1/tabularpy-1.0.1.tar.gz

---
+++ packages/tabularpy/tabularpy-1.0.1.tar.gz/tabularpy-1.0.1/tabularpy/__init__.py
@@ -0,0 +1,20 @@
+from .settings import Settings
+from .tables import (
+    CsvTable,
+    ExcelTable,
+    HtmlTable,
+    ListOfDictsTable,
+    ListOfListsTable,
+    SqlAlcTable,
+    Table,
+)
+from .util import (
+    avg_aggr,
+    count_aggr,
+    get_sql_query_types,
+    max_aggr,
+    min_aggr,
+    open_xls_as_xlsx,
+    product_aggr,
+    sum_aggr,
+)
