de8b58ff56d336929a993ab8202bf18d86acc794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:23 +0000

    Add package polygonlite/0.0.8.5/polygonlite-0.0.8.5.tar.gz

---
+++ packages/polygonlite/polygonlite-0.0.8.5.tar.gz/polygonlite-0.0.8.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='polygonlite',
+    version='0.0.8.5',
+    packages=['polygonlite'],
+    url='https://github.com/pranavtotla/polygonlite',
+    license='MIT',
+    author='Pranav Totla',
+    author_email='pranav@totla.tech',
+    description='A lightweight module for polygon, edge and point operations.',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+    ],
+    download_url='https://github.com/pranavtotla/polygonlite/archive/v0.0.8.5.tar.gz'
+)
