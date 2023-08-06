b496a4d6703bc66757c23816d55196c6fa4745ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:43 +0000

    Add package aiomail/0.0.1/aiomail-0.0.1.tar.gz

---
+++ packages/aiomail/aiomail-0.0.1.tar.gz/aiomail-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aiomail",
+    version="0.0.1",
+    author="fxdmhtt",
+    author_email="yuanjunkang@gmail.com",
+    description="easy and async e-mail package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fxdmhtt/aiomail.git",
+    packages=list(filter(lambda x: x.startswith('aiomail'), setuptools.find_packages())),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
