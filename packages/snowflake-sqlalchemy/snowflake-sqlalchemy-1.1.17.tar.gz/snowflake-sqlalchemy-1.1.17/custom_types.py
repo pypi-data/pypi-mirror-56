52c27ecb4de678ffee29b39de8c166001b6ebd07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:47 +0000

    Add package snowflake-sqlalchemy/1.1.17/snowflake-sqlalchemy-1.1.17.tar.gz

---
+++ packages/snowflake-sqlalchemy/snowflake-sqlalchemy-1.1.17.tar.gz/snowflake-sqlalchemy-1.1.17/custom_types.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012-2019 Snowflake Computing Inc. All right reserved.
+#
+
+import sqlalchemy.types as sqltypes
+
+TEXT = sqltypes.VARCHAR
+CHARACTER = sqltypes.CHAR
+DEC = sqltypes.DECIMAL
+DOUBLE = sqltypes.FLOAT
+FIXED = sqltypes.DECIMAL
+NUMBER = sqltypes.DECIMAL
+BYTEINT = sqltypes.SMALLINT
+STRING = sqltypes.VARCHAR
+TINYINT = sqltypes.SMALLINT
+VARBINARY = sqltypes.BINARY
+
+
+class VARIANT(sqltypes.TypeEngine):
+    __visit_name__ = 'VARIANT'
+
+
+class OBJECT(sqltypes.TypeEngine):
+    __visit_name__ = 'OBJECT'
+
+
+class ARRAY(sqltypes.TypeEngine):
+    __visit_name__ = 'ARRAY'
+
+
+class TIMESTAMP_TZ(sqltypes.TIMESTAMP):
+    __visit_name__ = 'TIMESTAMP_TZ'
+
+
+class TIMESTAMP_LTZ(sqltypes.TIMESTAMP):
+    __visit_name__ = 'TIMESTAMP_LTZ'
+
+
+class TIMESTAMP_NTZ(sqltypes.TIMESTAMP):
+    __visit_name__ = 'TIMESTAMP_NTZ'
