a389e6fac9da1f41ee0feb1b48314106e1320b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:04 +0000

    Add package django-jsonfield-hotfix/1.3.1.post2/django-jsonfield-hotfix-1.3.1.post2.tar.gz

---
+++ packages/django-jsonfield-hotfix/django-jsonfield-hotfix-1.3.1.post2.tar.gz/django-jsonfield-hotfix-1.3.1.post2/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'jsonfield', 'VERSION')) as fp:
+    version = fp.read().strip()
+
+with open("README.rst") as fp:
+    long_description = fp.read()
+
+setup(
+    name="django-jsonfield-hotfix",
+    version=version,
+    description="JSONField for django models",
+    long_description=long_description,
+    url="https://github.com/ChouBaoDxs/django-jsonfield",
+    author="Matthew Schinckel",
+    author_email="matt@schinckel.net",
+    maintainer="Adam Johnson",
+    maintainer_email="me@adamj.eu",
+    packages=[
+        "jsonfield",
+    ],
+    include_package_data=True,
+    test_suite='tests.main',
+    install_requires=[
+        'Django>=1.9',
+    ],
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        'Framework :: Django',
+        "Framework :: Django :: 1.9",
+        "Framework :: Django :: 1.10",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+)
