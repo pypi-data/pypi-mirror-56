99c394b7ea2725c5d6309e78e03cdb9e225cfcba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:50 +0000

    Add package pco/0.0.15/pco-0.0.15-py3-none-any.whl

---
+++ packages/pco/pco-0.0.15-py3-none-any.whl/pco/camera.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: ziegler
+"""
+
+import numpy as np
+import time
+
+
+class camera:
+
+    def __init__(self):
+        print('This class is under construction.')
+
+    def __str__(self):
+        return 'pco camera'
+
+    def __repr__(self):
+        return 'pco camera'
