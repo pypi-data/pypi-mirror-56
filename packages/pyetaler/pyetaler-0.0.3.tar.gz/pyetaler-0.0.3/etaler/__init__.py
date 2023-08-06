796bdf32f6a07b2821b18fd2abdd311c5fc8997e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:50 +0000

    Add package pyetaler/0.0.3/pyetaler-0.0.3.tar.gz

---
+++ packages/pyetaler/pyetaler-0.0.3.tar.gz/pyetaler-0.0.3/etaler/__init__.py
@@ -0,0 +1,13 @@
+import cppyy
+import os
+
+src_dir = os.path.dirname(os.path.realpath(__file__))
+cppyy.load_reflection_info(os.path.join(src_dir, "etaler_rflx.so"))
+
+
+from cppyy.gbl import et
+from cppyy.gbl import std
+
+# Override the default __repr__ with Etaler's function
+et.Shape.__repr__ = lambda self: cppyy.gbl.cling.printValue(self)
+et.Tensor.__repr__ = lambda self: cppyy.gbl.cling.printValue(self)
