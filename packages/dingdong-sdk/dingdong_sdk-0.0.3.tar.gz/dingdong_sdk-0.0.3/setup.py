180c4049a17d0cd7bbaa7e615fdd83e662e9d903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:04 +0000

    Add package dingdong-sdk/0.0.3/dingdong_sdk-0.0.3.tar.gz

---
+++ packages/dingdong-sdk/dingdong_sdk-0.0.3.tar.gz/dingdong_sdk-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='dingdong_sdk',
+     version='0.0.3',
+     scripts=['dingdong_sdk/'] ,
+     author="Centre of Technology and Innovation",
+     author_email="cti@apiit.edu.my",
+     description="The DingDong SDK",
+     long_description="The DingDong SDK allows CTI developers to interact with the endpoints with ease by using simple functions provided.",
+   long_description_content_type="text/markdown",
+     url="https://bitbucket.org/ctiteam/dingdong-sdk",
+     install_requires=[
+       "requests"
+     ],
+     packages=setuptools.find_packages(),
+     classifiers=[
+        "Intended Audience :: Developers",
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
