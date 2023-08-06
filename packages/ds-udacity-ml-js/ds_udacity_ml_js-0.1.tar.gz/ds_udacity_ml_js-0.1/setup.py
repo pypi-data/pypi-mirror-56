dae94f73bce768f531b409810d7f21d871b8722e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:24 +0000

    Add package ds-udacity-ml-js/0.1/ds_udacity_ml_js-0.1.tar.gz

---
+++ packages/ds-udacity-ml-js/ds_udacity_ml_js-0.1.tar.gz/ds_udacity_ml_js-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ds_udacity_ml_js',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ds_udacity_ml_js'],
+      zip_safe=False)
