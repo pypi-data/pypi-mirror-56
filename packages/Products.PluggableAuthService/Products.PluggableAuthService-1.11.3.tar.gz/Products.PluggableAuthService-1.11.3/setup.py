fcce919d8bad6c6512a4b1bbe9cace1b161938d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:18 +0000

    Add package Products.PluggableAuthService/1.11.3/Products.PluggableAuthService-1.11.3.tar.gz

---
+++ packages/Products.PluggableAuthService/Products.PluggableAuthService-1.11.3.tar.gz/Products.PluggableAuthService-1.11.3/setup.py
@@ -0,0 +1,66 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+NAME = 'PluggableAuthService'
+
+here = os.path.abspath(os.path.dirname(__file__))
+package = os.path.join(here, 'Products', NAME)
+
+def _package_doc(name):
+    f = open(os.path.join(package, name))
+    return f.read()
+
+_boundary = '\n\n'
+
+with open('README.rst') as f:
+    README = f.read()
+
+with open('CHANGES.rst') as f:
+    CHANGES = f.read()
+
+README = ( README + _boundary + CHANGES)
+
+setup(name='Products.%s' % NAME,
+      version=_package_doc('version.txt').strip(),
+      description='Pluggable Zope2 authentication / authorization framework',
+      long_description=README,
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Zope :: 2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: System :: Systems Administration :: "
+                "Authentication/Directory",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2 :: Only",
+        "Programming Language :: Python :: Implementation",
+        "Programming Language :: Python :: Implementation :: CPython",
+        ],
+      keywords='web application server zope zope2',
+      author="Zope Foundation and Contributors",
+      author_email="zope-cmf@zope.org",
+      url="http://pypi.python.org/pypi/Products.PluggableAuthService",
+      license="ZPL 2.1 (http://www.zope.org/Resources/License/ZPL-2.1)",
+      packages=find_packages(),
+      include_package_data=True,
+      namespace_packages=['Products'],
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.PluginRegistry <1.5',
+          'Products.GenericSetup <1.9',
+          'Zope2 <4',
+          ],
+      extras_require={'ip_range': ['IPy'],
+                     },
+      entry_points="""
+      [zope2.initialize]
+      Products.%s = Products.%s:initialize
+      """ % (NAME, NAME),
+      )
