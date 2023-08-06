fc86a0f4744639fced802e7e2d2dc29c0b0b70aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:34 +0000

    Add package mlwrapper/0.1/mlwrapper-0.1.tar.gz

---
+++ packages/mlwrapper/mlwrapper-0.1.tar.gz/mlwrapper-0.1/setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="mlwrapper",
+    description="Wrapper for machine learning experiments",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    version="0.1",
+    author="Albert Swiecicki",
+    author_email="albertswiecicki@gmail.com",
+    url="https://github.com/SlavicMate/mlwrapper",
+    download_url="https://github.com/SlavicMate/mlwrapper/archive/v0.1.0.tar.gz",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+    packages=["mlwrapper", "mlwrapper.tests"],
+    python_requires=">=3.6",
+    setup_requires=["setuptools>=38.6.0"],
+    install_requires=["numpy", "mlflow", "tensorboard", "tensorflow"],
+)
