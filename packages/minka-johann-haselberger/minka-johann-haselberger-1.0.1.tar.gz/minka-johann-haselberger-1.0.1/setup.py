7ad302a306469dcfb9283c2654ece30a62b0cdf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:54 +0000

    Add package minka-johann-haselberger/1.0.1/minka-johann-haselberger-1.0.1.tar.gz

---
+++ packages/minka-johann-haselberger/minka-johann-haselberger-1.0.1.tar.gz/minka-johann-haselberger-1.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="minka-johann-haselberger", # Replace with your own username
+    version="1.0.1",
+    author="Johann Haselberger",
+    author_email="johann.haselberger@hs-kempten.de",
+    description="The minka package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    install_requires=required,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
