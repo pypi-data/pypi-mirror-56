b229cc980ddad362806ccba8e5993a598e9f1ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:16 +0000

    Add package fine-grained-segmentation/0.1.9/fine_grained_segmentation-0.1.9.tar.gz

---
+++ packages/fine-grained-segmentation/fine_grained_segmentation-0.1.9.tar.gz/fine_grained_segmentation-0.1.9/fine_grained_segmentation/visualize.py
@@ -0,0 +1,169 @@
+"""
+Mask R-CNN
+Display and Visualization Functions.
+
+Copyright (c) 2017 Matterport, Inc.
+Licensed under the MIT License (see LICENSE for details)
+Written by Waleed Abdulla
+"""
+
+import random
+import colorsys
+
+import numpy as np
+import skimage
+from skimage.measure import find_contours
+import matplotlib.pyplot as plt
+from matplotlib import patches
+from matplotlib.patches import Polygon
+
+
+random.seed(42)
+
+def random_colors(N, bright=True):
+    """
+    Generate random colors.
+    To get visually distinct colors, generate them in HSV space then
+    convert to RGB.
+    """
+    brightness = 1.0 if bright else 0.7
+    hsv = [(i / N, 1, brightness) for i in range(N)]
+    colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
+    random.shuffle(colors)
+    return colors
+    
+
+def apply_mask(image, mask, color, alpha=0.5):
+    """Apply the given mask to the image.
+    """
+    for c in range(3):
+        image[:, :, c] = np.where(mask == 1,
+                                  image[:, :, c] *
+                                  (1 - alpha) + alpha * color[c] * 255,
+                                  image[:, :, c])
+    return image
+    
+
+def remove_white_borders(image):
+    ''' Remove white borders around an image, and only keep the cropped center '''
+    
+    # sum pixel values over all 3 color channels
+    image_sum_channels = np.sum(image, axis=-1, keepdims=True)
+    
+    # initialize filter to extract non-white regions
+    nonwhite_filter = np.ones(image_sum_channels.shape)
+    
+    # if image pixel is white [255, 255, 255], set filter value to nan
+    nonwhite_filter[image_sum_channels == 255*3] = 0
+    
+    # duplicate filter on each channels of the image
+    nonwhite_filter = np.repeat(nonwhite_filter, 3, axis=-1)
+    
+    # apply filter and only keep non-white regions
+    image = image[nonwhite_filter == 1]
+    
+    # get non-white region height by counting the number of non-white pixels at the center of the image across its height
+    nonwhite_height = np.count_nonzero(nonwhite_filter[:,nonwhite_filter.shape[1]//2,0])
+    
+    # get non-white region width by counting the number of non-white pixels at the center of the image across its width
+    nonwhite_width = np.count_nonzero(nonwhite_filter[nonwhite_filter.shape[0]//2,:,0])
+    
+    # reconstruct filtered image with the correct dimensions
+    image = image.reshape(nonwhite_height, nonwhite_width, -1)
+    
+    return image
+    
+    
+def display_instances(image, boxes, masks, class_ids, class_names,
+                      scores=None, title="",
+                      figsize=(16, 16), ax=None,
+                      show_mask=True, show_bbox=True,
+                      colors=None, captions=None):
+    """
+    boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
+    masks: [height, width, num_instances]
+    class_ids: [num_instances]
+    class_names: list of class names of the dataset
+    scores: (optional) confidence scores for each box
+    title: (optional) Figure title
+    show_mask, show_bbox: To show masks and bounding boxes or not
+    figsize: (optional) the size of the image
+    colors: (optional) An array or colors to use with each object
+    captions: (optional) A list of strings to use as captions for each object
+    """
+    OUTPUT_FILENAME = "result.png"
+    
+    # Number of instances
+    N = boxes.shape[0]
+    if not N:
+        print("\n*** No instances to display *** \n")
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
+
+    # If no axis is passed, create one and automatically call show()
+    auto_show = False
+    if not ax:
+        _, ax = plt.subplots(1, figsize=figsize)
+        auto_show = True
+
+    # Generate random colors
+    colors = colors or random_colors(N)
+
+    # Show area outside image boundaries.
+    height, width = image.shape[:2]
+    ax.set_ylim(height + 10, -10)
+    ax.set_xlim(-10, width + 10)
+    ax.axis('off')
+    ax.set_title(title)
+
+    masked_image = image.astype(np.uint32).copy()
+    for i in range(N):
+        color = colors[i]
+
+        # Bounding box
+        if not np.any(boxes[i]):
+            # Skip this instance. Has no bbox. Likely lost in image cropping.
+            continue
+        y1, x1, y2, x2 = boxes[i]
+        if show_bbox:
+            p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
+                                alpha=0.7, linestyle="dashed",
+                                edgecolor=color, facecolor='none')
+            ax.add_patch(p)
+
+        # Label
+        if not captions:
+            class_id = class_ids[i]
+            score = scores[i] if scores is not None else None
+            label = class_names[class_id]
+            caption = "{} {:.3f}".format(label, score) if score else label
+        else:
+            caption = captions[i]
+        ax.text(x1, y1 + 8, caption,
+                color='lime', size=11, backgroundcolor="none")
+
+        # Mask
+        mask = masks[:, :, i]
+        if show_mask:
+            masked_image = apply_mask(masked_image, mask, color)
+
+        # Mask Polygon
+        # Pad to ensure proper polygons for masks that touch image edges.
+        padded_mask = np.zeros(
+            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+        padded_mask[1:-1, 1:-1] = mask
+        contours = find_contours(padded_mask, 0.5)
+        for verts in contours:
+            # Subtract the padding and flip (y, x) to (x, y)
+            verts = np.fliplr(verts) - 1
+            p = Polygon(verts, facecolor="none", edgecolor=color)
+            ax.add_patch(p)
+    ax.imshow(masked_image.astype(np.uint8))
+    if auto_show:
+        plt.savefig(OUTPUT_FILENAME)
+    
+    # crop white borders in the output image
+    image = skimage.io.imread(OUTPUT_FILENAME)[:,:,:3]
+    image = remove_white_borders(image)
+    skimage.io.imsave(OUTPUT_FILENAME, image)
+        
---
+++ packages/fine-grained-segmentation/fine_grained_segmentation-0.1.9.tar.gz/fine_grained_segmentation-0.1.9/setup.py
@@ -0,0 +1,60 @@
+"""
+The build/compilations setup
+
+>> pip install -r requirements.txt
+>> python setup.py install
+"""
+import pip
+import logging
+import pkg_resources
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    
+try:
+    from pip._internal import download
+except ImportError:
+    from pip import download
+
+
+def _parse_requirements(file_path):
+    pip_ver = pkg_resources.get_distribution('pip').version
+    pip_version = list(map(int, pip_ver.split('.')[:2]))
+    if pip_version >= [6, 0]:
+        raw = parse_requirements(file_path,
+                                         session=download.PipSession())
+    else:
+        raw = parse_requirements(file_path)
+    return [str(i.req) for i in raw]
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+try:
+    install_reqs = _parse_requirements("requirements.txt")
+except Exception:
+    logging.warning('Fail load requirements file, so using default ones.')
+    install_reqs = []
+
+setup(
+    name='fine_grained_segmentation',
+    version='0.1.9',
+    url='https://github.com/vinny-palumbo/fine_grained_segmentation',
+    author='Vincent Palumbo',
+    author_email='vinnypalumbo.com@gmail.com',
+    license='MIT',
+    description='Mask R-CNN for Fine-Grained segmentation',
+    packages=["fine_grained_segmentation"],
+    install_requires=install_reqs,
+    include_package_data=True,
+    python_requires='>=3.4',
+    long_description=open('README.md').read(),
+    entry_points = {
+        'console_scripts': ['fashion-segmentator=fine_grained_segmentation.command_line:main'],
+    }
+)
