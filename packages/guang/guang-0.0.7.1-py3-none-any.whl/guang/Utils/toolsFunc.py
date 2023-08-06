c68c58998f945811c465cd085d076eb0fbab2967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:00 +0000

    Add package guang/0.0.7.1/guang-0.0.7.1-py3-none-any.whl

---
+++ packages/guang/guang-0.0.7.1-py3-none-any.whl/guang/Utils/toolsFunc.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+def broadcast(func):
+    '''
+        example:
+        @broadcast
+        def f(x):
+            # A function that can map only a single element
+            if x==1 or x==0:
+                return x
+            else:
+                return f(x-1)+f(x-2)
+            
+        >> f([2,4,10])
+        >> array([1, 3, 832040], dtype=object)
+    '''
+    def wrap(*args, **kwargs):
+        '''
+        Takes an arbitrary Python function and returns a NumPy ufunc
+        Can be used, for example, to add broadcasting to a built-in Python function
+        return: only one out, 
+        type:numpy object
+
+        '''
+        nin, nout = len(args)+len(kwargs), 1
+        return np.frompyfunc(func,nin, nout)(*args, **kwargs)
+    return wrap
+
+# :Enables the dictionary to be dot operated
+class _Dict_enhance(dict):
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+        self.__dict__ = self 
+
+def dict_dotable(dic):
+    '''
+    : input: a dictionary
+    : output: an enhanced dictionary
+    Example:
+        enhance_dic = dict_dotable(dic)
+    then, you can operate an enhanced dictionary like this:
+        enhance_dic.key1.key2. ...
+    '''
+    dic = _Dict_enhance(dic)
+    for i in dic:
+        if type(dic[i]) == dict:
+            dic[i] = dict_dotable(dic[i])
+    return dic
+
+
+# define a constant like C language.
+class Cons:
+    '''
+    `object.__setattr__(self, name, value)`
+    this built-in function will called when assigning values to properties of the class
+    
+    `object.__dict__` holds all writable attributes in object, 
+    key as variable name and value as variable value.
+    '''
+    def __setattr__(self, name,value):
+        if hasattr(self,name):
+            raise " "
+        else:
+            self.__dict__[name] = value
+
