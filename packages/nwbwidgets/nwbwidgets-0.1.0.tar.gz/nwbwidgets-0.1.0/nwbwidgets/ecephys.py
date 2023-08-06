307127ec37135f7e01298b6d6f95197d9d44621b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:05 +0000

    Add package nwbwidgets/0.1.0/nwbwidgets-0.1.0.tar.gz

---
+++ packages/nwbwidgets/nwbwidgets-0.1.0.tar.gz/nwbwidgets-0.1.0/nwbwidgets/ecephys.py
@@ -0,0 +1,115 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from ipywidgets import widgets
+from scipy.signal import stft
+from pynwb.ecephys import LFP
+from .base import fig2widget, nwb2widget
+
+
+def show_lfp(node: LFP, neurodata_vis_spec: dict):
+    lfp = list(node.electrical_series.values())[0]
+    return nwb2widget(lfp, neurodata_vis_spec)
+
+
+def show_voltage_traces(lfp):
+    # Produce figure
+    def control_plot(x0, x1, ch0, ch1):
+        fig, ax = plt.subplots(figsize=(18, 10))
+        istart = int(x0 * lfp.rate)
+        istop = int(x1 * lfp.rate)
+        data = lfp.data[istart:istop, ch0:ch1+1]
+        tt = np.linspace(x0, x1, istop-istart)
+        mu_array = np.nanmean(data, 0)
+        sd_array = np.nanstd(data, 0)
+        offset = np.nanmean(sd_array)*5
+        yticks = [i*offset for i in range(ch1+1-ch0)]
+        for i in range(ch1+1-ch0):
+            ax.plot(tt, data[:, i] - mu_array[i] + yticks[i])
+        ax.set_xlabel('Time (s)', fontsize=20)
+        ax.set_ylabel('Ch #', fontsize=20)
+        ax.set_yticks(yticks)
+        ax.set_yticklabels([str(i) for i in range(ch0, ch1+1)])
+        ax.tick_params(axis='both', which='major', labelsize=16)
+        ax.set_xlim((x0, x1))
+        plt.show()
+        return fig2widget(fig)
+
+    fs = lfp.rate
+    nSamples = lfp.data.shape[0]
+    nChannels = lfp.data.shape[1]
+
+    # Controls
+    field_lay = widgets.Layout(max_height='40px', max_width='100px',
+                               min_height='30px', min_width='70px')
+    x0 = widgets.BoundedFloatText(value=0, min=0, max=nSamples/fs, layout=field_lay)
+    x1 = widgets.BoundedFloatText(value=min(10, nSamples/fs), min=0, max=nSamples/fs,
+                                  layout=field_lay)
+    ch0 = widgets.BoundedIntText(value=0, min=0, max=int(nChannels-1), layout=field_lay)
+    ch1 = widgets.BoundedIntText(value=min(10, int(nChannels-1)), min=0,
+                                 max=int(nChannels-1), layout=field_lay)
+
+    controls = {
+        'x0': x0,
+        'x1': x1,
+        'ch0': ch0,
+        'ch1': ch1
+    }
+    out_fig = widgets.interactive_output(control_plot, controls)
+
+    # Assemble layout box
+    lbl_x = widgets.Label('Time (s):', layout=field_lay)
+    lbl_ch = widgets.Label('Ch #:', layout=field_lay)
+    lbl_blank = widgets.Label('    ', layout=field_lay)
+    hbox0 = widgets.HBox(children=[lbl_x, x0, x1, lbl_blank, lbl_ch, ch0, ch1])
+    vbox = widgets.VBox(children=[hbox0, out_fig])
+    return vbox
+
+
+def show_spectrogram(neurodata, channel=0, **kwargs):
+    fig, ax = plt.subplots()
+    f, t, Zxx = stft(neurodata.data[:, channel], neurodata.rate, nperseg=2*17)
+    ax.imshow(np.log(np.abs(Zxx)), aspect='auto', extent=[0, max(t), 0, max(f)], origin='lower')
+    ax.set_ylim(0, 50)
+    ax.set_xlabel('time')
+    ax.set_ylabel('frequency')
+    plt.show(ax.figure())
+
+
+def show_spike_event_series(ses, **kwargs):
+    def control_plot(spk_ind):
+        fig, ax = plt.subplots(figsize=(9, 5))
+        data = ses.data[spk_ind, :, :]
+        for ch in range(nChannels):
+            ax.plot(data[:, ch], color='#d9d9d9')
+        ax.plot(np.mean(data, axis=1), color='k')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Amplitude')
+        plt.show()
+        return fig2widget(fig)
+
+    nChannels = ses.data.shape[2]
+    nSpikes = ses.data.shape[0]
+
+    # Controls
+    field_lay = widgets.Layout(max_height='40px', max_width='100px',
+                               min_height='30px', min_width='70px')
+    spk_ind = widgets.BoundedIntText(value=0, min=0, max=nSpikes-1,
+                                     layout=field_lay)
+    controls = {'spk_ind': spk_ind}
+    out_fig = widgets.interactive_output(control_plot, controls)
+
+    # Assemble layout box
+    lbl_spk = widgets.Label('Spike ID:', layout=field_lay)
+    lbl_nspks0 = widgets.Label('N° spikes:', layout=field_lay)
+    lbl_nspks1 = widgets.Label(str(nSpikes), layout=field_lay)
+    lbl_nch0 = widgets.Label('N° channels:', layout=field_lay)
+    lbl_nch1 = widgets.Label(str(nChannels), layout=field_lay)
+    hbox0 = widgets.HBox(children=[lbl_spk, spk_ind])
+    vbox0 = widgets.VBox(children=[
+        widgets.HBox(children=[lbl_nspks0, lbl_nspks1]),
+        widgets.HBox(children=[lbl_nch0, lbl_nch1]),
+        hbox0
+    ])
+    hbox1 = widgets.HBox(children=[vbox0, out_fig])
+
+    return hbox1
---
+++ packages/nwbwidgets/nwbwidgets-0.1.0.tar.gz/nwbwidgets-0.1.0/nwbwidgets/ophys.py
@@ -0,0 +1,173 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pynwb.ophys import RoiResponseSeries, DfOverF, PlaneSegmentation, TwoPhotonSeries, ImageSegmentation
+from pynwb.base import NWBDataInterface
+from ndx_grayscalevolume import GrayscaleVolume
+from .utils.cmaps import linear_transfer_function
+import ipywidgets as widgets
+from .base import show_neurodata_base
+from scipy.spatial import ConvexHull
+import plotly.graph_objects as go
+
+
+color_wheel = ['red', 'blue', 'green', 'black', 'magenta', 'yellow']
+
+
+def show_two_photon_series(indexed_timeseries: TwoPhotonSeries, neurodata_vis_spec: dict):
+    output = widgets.Output()
+
+    if len(indexed_timeseries.data.shape) == 3:
+        def show_image(index=0):
+            fig, ax = plt.subplots(subplot_kw={'xticks': [], 'yticks': []})
+            ax.imshow(indexed_timeseries.data[index], cmap='gray')
+            output.clear_output(wait=True)
+            with output:
+                plt.show(fig)
+    elif len(indexed_timeseries.data.shape) == 4:
+        import ipyvolume.pylab as p3
+
+        def show_image(index=0):
+            fig = p3.figure()
+            p3.volshow(indexed_timeseries.data[index], tf=linear_transfer_function([0, 0, 0], max_opacity=.3))
+            output.clear_output(wait=True)
+            with output:
+                p3.show()
+    else:
+        raise NotImplementedError
+
+    def on_index_change(change):
+        show_image(change.new)
+
+    slider = widgets.IntSlider(value=0, min=0,
+                               max=indexed_timeseries.data.shape[0] - 1,
+                               orientation='horizontal')
+    slider.observe(on_index_change, names='value')
+    show_image()
+
+    return widgets.VBox([output, slider])
+
+
+def show_df_over_f(df_over_f: DfOverF, neurodata_vis_spec: dict):
+    if len(df_over_f.roi_response_series) == 1:
+        title, input = list(df_over_f.roi_response_series.items())[0]
+        return neurodata_vis_spec[RoiResponseSeries](input, neurodata_vis_spec, title=title)
+    else:
+        return neurodata_vis_spec[NWBDataInterface](df_over_f, neurodata_vis_spec)
+
+
+def show_roi_response_series(roi_response_series: RoiResponseSeries, neurodata_vis_spec: dict,
+                             nchans: int = 30, title: str = None):
+    """
+
+    :param roi_response_series: pynwb.ophys.RoiResponseSeries
+    :param neurodata_vis_spec: OrderedDict
+    :param nchans: int
+    :param title: str
+    :return: matplotlib.pyplot.Figure
+    """
+    tt = roi_response_series.timestamps
+    data = roi_response_series.data
+    if data.shape[1] == len(tt):  # fix of orientation is incorrect
+        mini_data = data[:nchans, :].T
+    else:
+        mini_data = data[:, :nchans]
+
+    gap = np.median(np.nanstd(mini_data, axis=0)) * 20
+    offsets = np.arange(nchans) * gap
+
+    fig, ax = plt.subplots()
+    ax.plot(tt, mini_data + offsets)
+    ax.set_xlabel('time (s)')
+    ax.set_ylabel('traces (first 30)')
+    if np.isfinite(gap):
+        ax.set_ylim(-gap, offsets[-1] + gap)
+        ax.set_xlim(tt[0], tt[-1])
+        ax.set_yticks(offsets)
+        ax.set_yticklabels(np.arange(mini_data.shape[1]))
+
+    if title is not None:
+        ax.set_title(title)
+
+    return fig
+
+
+def show_image_segmentation(img_seg: ImageSegmentation, neurodata_vis_spec: dict):
+    if len(img_seg.plane_segmentations) == 1:
+        return show_plane_segmentation(next(iter(img_seg.plane_segmentations.values())), neurodata_vis_spec)
+    else:
+        return show_neurodata_base(ImageSegmentation, neurodata_vis_spec)
+
+
+def show_plane_segmentation(plane_seg: PlaneSegmentation, neurodata_vis_spec: dict):
+    nrois = len(plane_seg)
+
+    if 'voxel_mask' in plane_seg:
+        import ipyvolume.pylab as p3
+
+        dims = np.array([max(max(plane_seg['voxel_mask'][i][dim]) for i in range(nrois))
+                         for dim in ['x', 'y', 'z']]).astype('int') + 1
+        fig = p3.figure()
+        for icolor, color in enumerate(color_wheel):
+            vol = np.zeros(dims)
+            sel = np.arange(icolor, nrois, len(color_wheel))
+            for isel in sel:
+                dat = plane_seg['voxel_mask'][isel]
+                vol[tuple(dat['x'].astype('int')),
+                    tuple(dat['y'].astype('int')),
+                    tuple(dat['z'].astype('int'))] = 1
+            p3.volshow(vol, tf=linear_transfer_function(color, max_opacity=.3))
+        return fig
+    elif 'image_mask' in plane_seg:
+        if 'neuron_type' in plane_seg:
+            neuron_types = np.unique(plane_seg['neuron_type'][:])
+        data = plane_seg['image_mask'].data
+        nUnits = data.shape[0]
+        fig = go.FigureWidget()
+        aux_leg = []
+        for i in range(nUnits):
+            if plane_seg['neuron_type'][i] not in aux_leg:
+                show_leg = True
+                aux_leg.append(plane_seg['neuron_type'][i])
+            else:
+                show_leg = False
+            c = color_wheel[np.where(neuron_types==plane_seg['neuron_type'][i])[0][0]]
+            # hover text
+            hovertext = '<b>roi_id</b>: '+str(plane_seg['roi_id'][i])
+            rois_cols = list(plane_seg.colnames)
+            rois_cols.remove('roi_id')
+            sec_str = '<br>'.join([col+': '+str(plane_seg[col][i]) for col in rois_cols if isinstance(plane_seg[col][i], (int, float, np.integer, np.float, str))])
+            hovertext += '<br>'+sec_str
+            # form cell borders
+            y, x = np.where(plane_seg['image_mask'][i])
+            arr = np.vstack((x, y)).T
+            hull = ConvexHull(arr)
+            vertices = np.append(hull.vertices, hull.vertices[0])
+            fig.add_trace(
+                go.Scatter(
+                    x=arr[vertices, 0],
+                    y=arr[vertices, 1],
+                    fill='toself',
+                    mode='lines',
+                    line_color=c,
+                    name=plane_seg['neuron_type'][i],
+                    legendgroup=plane_seg['neuron_type'][i],
+                    showlegend=show_leg,
+                    text=hovertext,
+                    hovertext='text',
+                    line=dict(width=.5),
+                )
+            )
+            fig.update_layout(
+                width=700, height=500,
+                margin=go.layout.Margin(l=60, r=60, b=60, t=60, pad=1),
+                plot_bgcolor="rgb(245, 245, 245)",
+            )
+        return fig
+
+
+def show_grayscale_volume(vol: GrayscaleVolume, neurodata_vis_spec: dict):
+    import ipyvolume.pylab as p3
+
+    fig = p3.figure()
+    p3.volshow(vol.data, tf=linear_transfer_function([0, 0, 0], max_opacity=.1))
+    return fig
