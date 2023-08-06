24313a25ae9b9c74cf311263b978ad3d750002ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:40 +0000

    Add package testwizard.web/3.1.12/testwizard.web-3.1.12.tar.gz

---
+++ packages/testwizard.web/testwizard.web-3.1.12.tar.gz/testwizard.web-3.1.12/setup.py
@@ -0,0 +1,56 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testwizard.web",
+    version="3.1.12",
+    author="Eurofins Digital Testing - Belgium",
+    author_email="support-be@eurofins.com",
+    description="Testwizard for Web testobjects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['testwizard.web'],
+    install_requires=[
+        'testwizard.core==3.1.12',
+        'testwizard.testobjects-core==3.1.12',
+        'testwizard.commands-audio==3.1.12',
+        'testwizard.commands-video==3.1.12',
+        'testwizard.commands-web==3.1.12'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
