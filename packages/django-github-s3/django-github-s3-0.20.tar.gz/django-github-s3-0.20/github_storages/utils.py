bc6a0844b6f6a1d3b3addc0c7b8e7bf50d87d313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:45 +0000

    Add package django-github-s3/0.20/django-github-s3-0.20.tar.gz

---
+++ packages/django-github-s3/django-github-s3-0.20.tar.gz/django-github-s3-0.20/github_storages/utils.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+
+def get_url(name):
+	'''
+	Helper function to construct the url of the file.
+	'''
+	url = "https://api.github.com/repos/"
+	url += str(settings.GITHUB_HANDLE) + "/"
+	url += str(settings.GITHUB_REPO_NAME) 
+	url += "/contents/"
+	url += str(name)
+
+	return url
+
