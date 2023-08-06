53756f9ea92c1284319987188efe086a1218ee4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:28 +0000

    Add package openstep-parser/1.4.0/openstep_parser-1.4.0.tar.gz

---
+++ packages/openstep-parser/openstep_parser-1.4.0.tar.gz/openstep_parser-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(name='openstep_parser',
+    author='Ignacio Calderon',
+    description='OpenStep plist reader into python objects',
+    url="http://github.com/kronenthaler/openstep-parser",
+    version='1.4.0',
+    install_requires=['future'],
+    license='BSD License',
+    packages=find_packages(exclude=['tests']))
