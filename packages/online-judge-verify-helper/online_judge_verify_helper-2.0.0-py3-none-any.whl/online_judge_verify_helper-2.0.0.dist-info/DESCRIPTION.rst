bfdcf20796a80c68ead3716a2606ae9d3b32df64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:16 +0000

    Add package online-judge-verify-helper/2.0.0/online_judge_verify_helper-2.0.0-py3-none-any.whl

---
+++ packages/online-judge-verify-helper/online_judge_verify_helper-2.0.0-py3-none-any.whl/online_judge_verify_helper-2.0.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,50 @@
+# 競プロライブラリ verify 用 すごい CI script (WIP)
+
+## 使い方
+
+### かんたん Web 上インストール
+
+<https://kmyk.github.io/online-judge-verify-helper/>
+
+### ターミナル上で実行
+
+まず `oj-verify` コマンドを使えるようにします。
+これには `pip3 install git+https://github.com/kmyk/online-judge-verify-helper` を実行します。
+
+テストを書いていきましょう。
+`.test.cpp` という拡張子を持つ C++ のファイルを作って、その中で [Aizu Online Judge](http://judge.u-aizu.ac.jp/onlinejudge/) や [Library Checker](https://judge.yosupo.jp/) の問題の URL を `#define PROBLEM "URL"` のように指定しておきます  (例: [example.test.cpp](https://github.com/kmyk/online-judge-verify-helper/blob/master/example.test.cpp))。
+そして `oj-verify run` を実行すると、そのようなファイルを自動で検出し、指定された問題に対し AC を得るかどうかを自動で判定してくれます。
+
+最後に GitHub 上で自動で verify してくれるように設定しましょう。
+`oj-verify init` を実行すると `.github/workflow/verify.yml` というファイルが作られます。
+これを commit して GitHub に push しておけば終了です。
+以後なにか push するたびに毎回自動で verify をしてくれます。
+
+まとめると、以下を実行すればよいということになります。
+
+``` console
+$ pip3 install git+https://github.com/kmyk/online-judge-verify-helper
+$ wget https://raw.githubusercontent.com/kmyk/online-judge-verify-helper/master/example.test.cpp
+$ oj-verify run
+$ oj-verify init
+$ git add .github/workflow/verify.yml
+$ git add example.test.cpp
+$ git commit -m "Add .github/workflow/verify.yml and example.test.cpp"
+$ git push origin master
+```
+
+加えて `README.md` に `[![Actions Status](https://github.com/kmyk/online-judge-verify-helper/workflows/verify/badge.svg)](https://github.com/kmyk/online-judge-verify-helper/actions)` のように書いておくとよいでしょう。URL は適切に書き換えてください。
+[![Actions Status](https://github.com/kmyk/online-judge-verify-helper/workflows/verify/badge.svg)](https://github.com/kmyk/online-judge-verify-helper/actions) のようなバッチが貼られ、verify に成功したかが簡単に確認できます。
+
+## License
+
+MIT License
+
+## Authors
+
+-   committer: [@kmyk](https://github.com/kmyk) (AtCoder: [kimiyuki](https://atcoder.jp/users/kimiyuki)): pip での配布や [online-judge-tools](https://github.com/kmyk/online-judge-tools) などその他の諸々の担当
+-   committer: [@beet-aizu](https://github.com/beet-aizu) (AtCoder: [beet](https://atcoder.jp/users/beet)): verify 機能担当
+-   committer: [@tsutaj](https://github.com/tsutaj) (AtCoder: [Tsuta_J](https://atcoder.jp/users/Tsuta_J)): documents 生成担当
+-   special thanks: [@yosupo06](http://github.com/yosupo06) (AtCoder: [yosupo](https://atcoder.jp/users/yosupo)): verify のためのオンラインジャッジ [Library Checker](https://judge.yosupo.jp/) を管理運用してくれている
+
+
---
+++ packages/online-judge-verify-helper/online_judge_verify_helper-2.0.0-py3-none-any.whl/onlinejudge_verify/data/test.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+set -e
+
+which oj > /dev/null || { echo 'ERROR: please install `oj'\'' with: $ pip3 install --user -U online-judge-tools=='\''6.*'\''' >& 1 ; exit 1 ; }
+
+if [ -n "$CXX" ] ; then
+    CXX_LIST="$CXX"
+else
+    CXX_LIST="g++ clang++"
+fi
+CXXFLAGS="${CXXFLAGS:--std=c++17 -O2 -Wall -g}"
+ulimit -s unlimited || true
+
+
+list-dependencies() {
+    file="$1"
+    $CXX $CXXFLAGS -I . -MD -MF /dev/stdout -MM "$file" | sed '1s/[^:].*: // ; s/\\$//' | xargs -n 1
+}
+
+list-defined() {
+    file="$1"
+    $CXX $CXXFLAGS -I . -dM -E "$file"
+}
+
+get-url() {
+    file="$1"
+    list-defined "$file" | grep '^#define PROBLEM ' | sed 's/^#define PROBLEM "\(.*\)"$/\1/'
+}
+
+get-last-commit-date() {
+    file="$1"
+    list-dependencies "$file" | xargs git log -1 --date=iso --pretty=%ad
+}
+
+get-error() {
+    file="$1"
+    list-defined "$file" | grep '^#define ERROR ' | sed 's/^#define ERROR "\(.*\)"$/\1/'
+}
+
+is-verified() {
+    file="$1"
+    cache=test/timestamp/$(echo -n "$CXX/$file" | md5sum | sed 's/ .*//')
+    timestamp="$(get-last-commit-date "$file")"
+    [[ -e $cache ]] && [[ $timestamp = $(cat $cache) ]]
+}
+
+mark-verified() {
+    file="$1"
+    cache=test/timestamp/$(echo -n "$CXX/$file" | md5sum | sed 's/ .*//')
+    mkdir -p test/timestamp
+    timestamp="$(get-last-commit-date "$file")"
+    echo $timestamp > $cache
+}
+
+list-recently-updated() {
+    for file in $(find . -name \*.test.cpp) ; do
+        list-dependencies "$file" | xargs -n 1 | while read f ; do
+            git log -1 --format="%ci	${file}" "$f"
+        done | sort -nr | head -n 1
+    done | sort -nr | head -n 20 | cut -f 2
+}
+
+run() {
+    file="$1"
+    echo "$ CXX=$CXX ./test.sh $file"
+
+    url="$(get-url "$file")"
+    dir=test/$(echo -n "$url" | md5sum | sed 's/ .*//')
+    mkdir -p ${dir}
+
+    # ignore if IGNORE is defined
+    if list-defined "$file" | grep '^#define IGNORE ' > /dev/null ; then
+        return
+    fi
+
+    if ! is-verified "$file" ; then
+        # compile
+        $CXX $CXXFLAGS -I . -o ${dir}/a.out "$file"
+        if [[ -n ${url} ]] ; then
+            # download
+            echo "$ oj d -a $url"
+            if [[ ! -e ${dir}/test ]] ; then
+                sleep 2
+                oj download --system "$url" -d ${dir}/test
+            fi
+            # test
+            echo '$ oj t'
+            if list-defined "$file" | grep '^#define ERROR ' > /dev/null ; then
+                error=$(get-error "$file")
+                oj test -e ${error} -c ${dir}/a.out -d ${dir}/test
+            else
+                oj test -c ${dir}/a.out -d ${dir}/test
+            fi
+        else
+            # run
+            echo "$ ./a.out"
+            time ${dir}/a.out
+        fi
+        mark-verified "$file"
+    fi
+}
+
+
+if [[ $# -eq 1 && ( $1 = -h || $1 = --help || $1 = -? ) ]] ; then
+    echo Usage: $0 '[FILE ...]'
+    echo 'Compile and Run specified C++ code.'
+    echo 'If the given code contains macro like `#define PROBLEM "https://..."'\'', Download test cases of the problem and Test with them.'
+    echo
+    echo 'Features:'
+    echo '-   glob files with "**/*.test.cpp" if no arguments given.'
+    echo '-   cache results of tests, analyze "#include <...>" relations, and execute tests if and only if necessary.'
+    echo '-   on CI environment (i.e. $CI is defined), only recently modified files are tested (without cache).'
+    echo '-   use both CXX=g++ and CXX=clang++ when $CXX is not given.'
+
+elif [[ $# -eq 0 ]] ; then
+    if [[ $CI ]] ; then
+        # CI
+        message="$(git log -1 | tail -1 | awk '{print $1}')"
+        if [[ "${message}" != '[auto-verifier]' ]] ; then
+            for f in $(list-recently-updated) ; do
+                for CXX in $CXX_LIST ; do
+                    run $f
+                done
+            done
+        fi
+    elif [[ $GITHUB_ACTIONS ]] ; then
+        # GitHub Actions
+        username=$(git remote get-url origin | sed -e 's/\(.*github.com\/\)\(.*\)\/\(.*\)/\2/')
+        reponame=$(git remote get-url origin | sed -e 's/\(.*github.com\/\)\(.*\)\/\(.*\)/\3/')
+
+        git config --global user.name ${username}
+        git config --global user.email "online-judge-verify-helper@example.com"
+
+        echo "https://${username}:"'${GITHUB_TOKEN}'"@github.com/${username}/${reponame}"
+        echo "${GITHUB_REF##*/}"
+        git remote set-url origin https://${username}:${GITHUB_TOKEN}@github.com/${username}/${reponame}
+        git checkout "${GITHUB_REF##*/}"
+        
+        for f in $(find . -name \*.test.cpp) ; do
+            for CXX in $CXX_LIST ; do
+                run $f
+            done            
+            if [[ $SECONDS -gt 600 ]] ; then
+                break
+            fi
+        done
+        
+        git status -s
+        if [[ -n "$(git status -s)" ]]; then
+            last_commit="$(git log -1 | head -1 | awk '{print $2}')"
+            git add test/timestamp/
+            git commit -m "[auto-verifier] verify commit ${last_commit}"
+            git push origin HEAD
+        fi
+    else
+        # local
+        for f in $(find . -name \*.test.cpp) ; do
+            for CXX in $CXX_LIST ; do
+                run $f
+            done
+        done
+    fi
+else
+    # specified
+    for f in "$@" ; do
+        for CXX in $CXX_LIST ; do
+            run $f
+        done
+    done
+fi
