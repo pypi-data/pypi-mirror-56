0c950def4e07259a0f9b66e54463b180eea530e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:50 +0000

    Add package openplc-desktop/0.8.1/openplc_desktop-0.8.1-py3-none-any.whl

---
+++ packages/openplc-desktop/openplc_desktop-0.8.1-py3-none-any.whl/desktop/runtime/runtime_commander.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+import paramiko
+from Qt import QtCore, QtWidgets, QtNetwork, Qt, pyqtSignal
+
+from img import Ico
+
+import cwidgets
+
+
+class RuntimeCommanderWidget(QtWidgets.QWidget):
+
+    def __init__(self, parent, serverConn):
+        QtWidgets.QWidget.__init__(self, parent)
+
+        assert serverConn != None
+        self.serverConn = serverConn
+
+        self.serverConn.sigRuntimeMessage.connect(self.on_runtime_message)
+
+
+        self.mainLayout = cwidgets.vlayout()
+        self.setLayout(self.mainLayout)
+
+        # self.toolbar = QtWidgets.QToolBar()
+        # self.mainLayout.addWidget(self.toolbar)
+
+
+
+        # ===============
+        midLay = cwidgets.hlayout()
+        self.mainLayout.addLayout(midLay)
+        #grp = cwidgets.ToolBarGroup(title="Commands")
+        #self.toolbar.addWidget(grp)
+
+        self.txt = QtWidgets.QTextEdit()
+        midLay.addWidget(self.txt)
+
+
+        rightLay = cwidgets.vlayout(spacing=5)
+        midLay.addLayout(rightLay)
+
+        grp = cwidgets.ToolBarGroup(title="RunTime Connect")
+        rightLay.addWidget(grp)
+        self.buttConnect = cwidgets.XToolButton(ico=Ico.start, text="Connect", callback=self.on_connect)
+        grp.addWidget(self.buttConnect)
+
+        self.buttConnect = cwidgets.XToolButton(ico=Ico.start, text="SSH", callback=self.on_ssh)
+        grp.addWidget(self.buttConnect)
+
+        self.lblStatus = cwidgets.XLabel()
+        grp.addWidget(self.lblStatus)
+
+
+
+        rightLay.addSpacing(10)
+        lbl = cwidgets.XLabel(text="Commands")
+        rightLay.addWidget(lbl)
+
+        self.buttGroup = QtWidgets.QButtonGroup(self)
+        self.buttGroup.setExclusive(False)
+        self.buttGroup.buttonClicked.connect(self.on_exec_button)
+        for cmd in ["runtime_logs()", "exec_time()",
+                    "start_modbus()", "stop_modbus()",
+                    "start_dnp3()", "stop_dnp3()",
+                    "start_enip()", "stop_enip()",
+                    "start_pstorage()", "stop_pstorage()",
+                    "quit()"
+                    ]:
+            butt = cwidgets.XToolButton(text=cmd, autoRaise=False)
+            rightLay.addWidget(butt)
+            self.buttGroup.addButton(butt)
+            butt.setFixedWidth(200)
+            butt.setDisabled(True)
+
+        rightLay.addStretch(10)
+
+        self.show_status(False)
+
+
+    def init_load(self):
+        pass
+
+
+    def on_connect(self):
+        self.serverConn.connect_runtime()
+
+    def on_runtime_message(self, mess):
+        print(mess)
+        self.show_status(mess.connected)
+        if mess.message:
+            self.txt.setPlainText(mess.message)
+        for b in self.buttGroup.buttons():
+            b.setEnabled(mess.connected)
+
+    def on_exec_button(self, butt):
+        cmd = butt.text()
+        self.serverConn.send_runtime(cmd)
+
+
+    def show_status(self, connected):
+        self.lblStatus.setText("Connected" if connected else "Disconnected")
+        sty = "background-color: %s" % ("#EDFFDA" if connected else "#FFDFDA")
+        self.lblStatus.setStyleSheet(sty)
+
+    def on_ssh(self):
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.WarningPolicy)
+
+        print("on_ssh")
+        client.connect("192.168.1.222", port=22, username="pi", password="raspberry")
+        print("connect")
+        stdin, stdout, stderr = client.exec_command("ls -al")
+        #print( stdout.read(), )
+        s = str(stdout.read(), encoding="ascii")
+        print(s)
+        self.txt.setText(s)
---
+++ packages/openplc-desktop/openplc_desktop-0.8.1-py3-none-any.whl/desktop/server/server_panel.py
@@ -0,0 +1,198 @@
+
+
+
+
+
+from Qt import QtCore, QtWidgets, Qt, pyqtSignal
+
+import cwidgets
+from img import Ico
+
+from server import server_conn, server_dialog
+from slave_devices import slave_devices_grid, modbus_widgets
+from users import users_grid
+from programs import programs_grid
+from network import websocket_panel
+from runtime import settings_dialog, runtime_commander, ssh_terminal
+#from runtime import pyqterm_test
+
+class ServerPanel(QtWidgets.QMainWindow):
+
+
+
+    def __init__(self, parent=None, server=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+
+        self.server = server
+        self.serverConn = server_conn.ServerConn(self, server_address=server["address"])
+
+
+
+
+
+        self.toolbar = QtWidgets.QToolBar()
+        self.toolbar.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+        self.addToolBar(Qt.TopToolBarArea, self.toolbar)
+
+        # Creds -------------------------
+        self.tbServer = cwidgets.ToolBarGroup(self, title="Server Connection")
+        self.toolbar.addWidget(self.tbServer)
+
+        self.lblServerName = cwidgets.XLabel()
+        self.lblServerName.setFixedWidth(160)
+        self.tbServer.addWidget(self.lblServerName)
+
+        self.lblServerStatus = cwidgets.XLabel()
+        self.lblServerStatus.setFixedWidth(160)
+        self.tbServer.addWidget(self.lblServerStatus)
+
+        self.buttServer = cwidgets.XToolButton(self, ico=Ico.server, both=False, callback=self.on_server_edit)
+        self.tbServer.addWidget(self.buttServer)
+
+        self.buttWs = cwidgets.XToolButton(self, ico=Ico.save, both=False, callback=self.on_ws)
+        self.tbServer.addWidget(self.buttWs)
+
+        self.toolbar.addSeparator()
+
+
+        # Runtime
+        self.tbRuntime = cwidgets.ToolBarGroup(self, title="OpenPLC Runtime", is_group=True, toggle_icons=False)
+        self.toolbar.addWidget(self.tbRuntime)
+
+        self.buttStop = cwidgets.XToolButton(self, ico=Ico.stop, text="Stop", callback=self.on_runtime_stop)
+        self.tbRuntime.addWidget(self.buttStop)
+
+        self.buttStart = cwidgets.XToolButton(self, ico=Ico.start, text="Start", callback=self.on_runtime_start)
+        self.tbRuntime.addWidget(self.buttStart)
+
+
+        self.lblRuntimeStatus = cwidgets.XLabel("Status")
+        self.tbRuntime.addWidget(self.lblRuntimeStatus)
+
+        self.toolbar.addSeparator()
+
+        # Actions
+        self.tbActions = cwidgets.ToolBarGroup(self, title="Actions")
+        self.toolbar.addWidget(self.tbActions)
+
+        self.buttSettings = cwidgets.XToolButton(self, ico=Ico.settings, text="Settings", callback=self.on_settings)
+        self.tbActions.addWidget(self.buttSettings)
+
+        self.buttMbConfig = cwidgets.XToolButton(self, ico=Ico.mbconfig, text="mbconfig.cfg", callback=self.on_mbconfig)
+        self.tbActions.addWidget(self.buttMbConfig)
+
+        self.mainWidget = QtWidgets.QWidget()
+        self.mainLayout = cwidgets.vlayout()
+        self.mainWidget.setLayout(self.mainLayout)
+        self.setCentralWidget(self.mainWidget)
+
+        self.mainLayout.addSpacing(10)
+
+
+        ## Midx widgets
+        self.tabWidget = QtWidgets.QTabWidget()
+        self.mainLayout.addWidget(self.tabWidget)
+
+
+        #self.pyQTerm = pyqterm_test.PyQTermWidget(self, serverConn=self.serverConn)
+        #self.tabWidget.addTab(self.pyQTerm, Ico.icon(Ico.terminal), "PyQTermWidget")
+
+        self.sshTerminal = ssh_terminal.SshTerminalWidget(self, serverConn=self.serverConn)
+        self.tabWidget.addTab(self.sshTerminal, Ico.icon(Ico.terminal), "SSH Terminal")
+
+        self.runCommander = runtime_commander.RuntimeCommanderWidget(self, serverConn=self.serverConn)
+        self.tabWidget.addTab(self.runCommander, Ico.icon(Ico.commander), "Commander")
+
+        self.programsGrid = programs_grid.ProgramsGrid(self, serverConn=self.serverConn)
+        self.tabWidget.addTab(self.programsGrid, Ico.icon(Ico.programs), "Programs")
+
+        self.usersGrid = users_grid.UsersGrid(self, serverConn=self.serverConn)
+        self.tabWidget.addTab(self.usersGrid, Ico.icon(Ico.users), "Users")
+
+
+        self.slavesGrid = slave_devices_grid.SlaveDevicesGrid(self, serverConn=self.serverConn)
+        self.tabWidget.addTab(self.slavesGrid, Ico.icon(Ico.slaves), "Slaves")
+
+        self.lblServerName.setText("-")
+        self.lblServerStatus.setText("-")
+
+
+        ## Docks
+        dock = QtWidgets.QDockWidget("Web Socket")
+        self.addDockWidget(Qt.LeftDockWidgetArea, dock)
+        self.webSockPanel = websocket_panel.WebSocketPanel(self, serverConn=self.serverConn)
+        dock.setWidget(self.webSockPanel)
+
+
+    def on_ws(self):
+        #self.serverConn.connect_websocket()
+
+        self.serverConn.connect_websocket()
+
+
+    def init_load(self):
+
+        # self.fetch()
+        self.on_login()
+
+    def on_login(self):
+        creds = dict(login=self.server['login'], password=self.server['password'])
+        self.serverConn.post(self, "/login", data=creds, tag="login")
+
+
+    def on_server_reply(self, reply):
+
+        if reply.error:
+
+            self.lblServerStatus.setText(reply.error)
+            return
+            self.statusBar.set_reply(reply)
+
+        if reply.tag == "login":
+
+            self.lblServerName.setText("-")
+            self.lblServerStatus.setText("-")
+
+            if reply.data and "logged_in" in reply.data and reply.data["logged_in"] == True:
+                self.on_logged_in()
+                self.lblServerName.setText(self.server['address'])
+                self.lblServerStatus.setText("Connected")
+
+                # &*^&^^&*^^**
+                #self.serverConn.connect_websocket()
+
+        if reply.tag == "runtime":
+
+            self.lblServerName.setText("-")
+            self.lblServerStatus.setText("-")
+
+            self.serverConn.connect_runtime()
+
+
+
+
+    def on_mbconfig(self):
+        dial = modbus_widgets.MbConfigDialog(self, serverConn=self.serverConn)
+        dial.exec_()
+
+    def on_logged_in(self):
+        for idx in range(0, self.tabWidget.count()):
+            self.tabWidget.widget(idx).init_load()
+
+    def on_server_edit(self):
+        dial = server_dialog.ServerDialog(self, server=self.server)
+        dial.exec_()
+
+
+    def on_settings(self):
+        dial = settings_dialog.SettingsDialog(self, serverConn=self.serverConn)
+        dial.exec_()
+
+
+    def on_runtime_start(self):
+        print("start")
+        self.serverConn.post(self, "/runtime/start", tag="runtime")
+
+    def on_runtime_stop(self):
+        print("stop")
+        self.serverConn.post(self, "/runtime/stop", tag="runtime")
