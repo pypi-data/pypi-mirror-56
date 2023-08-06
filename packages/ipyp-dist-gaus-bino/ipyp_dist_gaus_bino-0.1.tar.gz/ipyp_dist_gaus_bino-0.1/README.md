81ddd5ef8d1d3a299c252278e6d3f3d4de97a380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:59 +0000

    Add package ipyp-dist-gaus-bino/0.1/ipyp_dist_gaus_bino-0.1.tar.gz

---
+++ packages/ipyp-dist-gaus-bino/ipyp_dist_gaus_bino-0.1.tar.gz/ipyp_dist_gaus_bino-0.1/README.md
@@ -0,0 +1,3 @@
+This package used for student practice, so don't expect too much.
+It has Gaussioan and Binomial Distrubution-Class drived from a general distribution class.
+and use for practicing pip install.
---
+++ packages/ipyp-dist-gaus-bino/ipyp_dist_gaus_bino-0.1.tar.gz/ipyp_dist_gaus_bino-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ipyp_dist_gaus_bino',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ipyp_dist_gaus_bino'],
+      zip_safe=False)
