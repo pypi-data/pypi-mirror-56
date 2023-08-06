b15e26c14a65b61d9ac52b12212fe3236fe4fd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:46 +0000

    Add package uncover-ml/0.1.0/uncover_ml-0.1.0-py2.py3-none-any.whl

---
+++ packages/uncover-ml/uncover_ml-0.1.0-py2.py3-none-any.whl/uncoverml/__init__.py
@@ -0,0 +1,5 @@
+__author__ = 'Geoscience Australia Mineral Systems Group, ' \
+             'NICTA Spatial Inference Systems Team'
+__email__ = 'daniel.steinberg@nicta.com.au, basaks@gmail.com'
+__version__ = '0.1.0'
+
