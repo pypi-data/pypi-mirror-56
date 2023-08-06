92cb442aa9c4f80f7c99496d0b192a1ac63ad2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:42 +0000

    Add package autostack/1.1.1/autostack-1.1.1.tar.gz

---
+++ packages/autostack/autostack-1.1.1.tar.gz/autostack-1.1.1/autostack/__init__.py
@@ -0,0 +1,9 @@
+'''
+Authors: Elijah Sawyers, Benjamin Sanders
+Emails: elijahsawyers@gmail.com, ben.sanders97@gmail.com
+Date: 03/17/2019
+Overview: The autostack module contains the code necessary to
+listen for python errors outputed in a terminal.
+'''
+
+name = "autostack"  # pylint: disable=invalid-name
