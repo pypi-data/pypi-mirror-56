28ec376816312d8f83a5ce7eb9408bf8c3791b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:00 +0000

    Add package genderClassifier/1.7/genderClassifier-1.7.tar.gz

---
+++ packages/genderClassifier/genderClassifier-1.7.tar.gz/genderClassifier-1.7/setup.py
@@ -0,0 +1,27 @@
+# SWAMI KARUPPASWAMI THUNNAI
+
+import setuptools
+
+def read():
+	with open("ReadMe.md", "r") as file:
+		content = file.read()
+	return content
+
+setuptools.setup(
+     name="genderClassifier",  
+     version="1.7",
+     author="Visweswaran N",
+     author_email="visweswaran.nagasivam98@gmail.com",
+     description="A simple gender classifier",
+     long_description=read(),
+     long_description_content_type="text/markdown",
+     url="https://gender-classifier.visweswaran.com",
+     packages=["gender_classifier"],
+     install_requires=["numpy", "tensorflow", "keras"],
+     include_package_data=True,
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ]
+ )
