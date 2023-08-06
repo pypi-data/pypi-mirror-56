0896fa077d1093d0bbbe8347ab6ad6b428402c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:19 +0000

    Add package resseg/0.1.0/resseg-0.1.0.tar.gz

---
+++ packages/resseg/resseg-0.1.0.tar.gz/resseg-0.1.0/resseg/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for resseg."""
+
+__author__ = """Fernando Perez-Garcia"""
+__email__ = 'fernando.perezgarcia.17@ucl.ac.uk'
+__version__ = '0.1.0'
+
+from .resseg import resseg
