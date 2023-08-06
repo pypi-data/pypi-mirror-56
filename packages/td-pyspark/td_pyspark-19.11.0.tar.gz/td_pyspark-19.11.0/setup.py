3ead4214a119114a35ec742cd0d77b22fba5f5e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:48 +0000

    Add package td-pyspark/19.11.0/td_pyspark-19.11.0.tar.gz

---
+++ packages/td-pyspark/td_pyspark-19.11.0.tar.gz/td_pyspark-19.11.0/setup.py
@@ -0,0 +1,35 @@
+import os
+import setuptools
+
+with open("README_public.md", "r") as fh:
+    long_description = fh.read()
+
+with open("PYSPARK_VERSION", "r") as f:
+    pyspark_version = f.read()
+
+version = os.environ['TD_PYSPARK_VERSION']
+
+setuptools.setup(
+    name="td_pyspark",
+    version=version,
+    description="Treasure Data extension for pyspark",
+    author="Arm Treasure Data",
+    author_email="dev+pypi@treasure-data.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://support.treasuredata.com/hc/en-us/sections/360000317028-Data-Science-and-SQL-Tools",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={"": ["*.jar"]},
+    license="Apache 2",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="Spark PySpark TreasureData",
+    extras_require={
+        "spark": ['pyspark>={}'.format(pyspark_version)],
+        "docs": ["sphinx>=2.2.0", "sphinx_rtd_theme>=0.4.3", "recommonmark"],
+    },
+)
