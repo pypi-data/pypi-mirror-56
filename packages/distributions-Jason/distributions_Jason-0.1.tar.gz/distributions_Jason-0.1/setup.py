89d4b8235f5646a8234af3a45c77b7b993f70418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:41 +0000

    Add package distributions-Jason/0.1/distributions_Jason-0.1.tar.gz

---
+++ packages/distributions-Jason/distributions_Jason-0.1.tar.gz/distributions_Jason-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_Jason',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_Jason'],
+      author = 'JasonShin',
+      author_email = 'kimgaul.js@gmail.com',
+      zip_safe=False)
