7d56ee1a2e3c3a2b0769eb531613298f71bfade0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:50 +0000

    Add package django-vb-baseapp/1.0.5/django-vb-baseapp-1.0.5.tar.gz

---
+++ packages/django-vb-baseapp/django-vb-baseapp-1.0.5.tar.gz/django-vb-baseapp-1.0.5/vb_baseapp/management/template_structures/admins/softdelete.py
@@ -0,0 +1,31 @@
+"""
+CustomBaseModelWithSoftDelete template for model generator
+"""
+
+TEMPLATE_ADMIN_SOFTDELETEMODEL = """import logging
+
+from django.contrib import admin
+
+from console import console
+
+from vb_baseapp.admin import (
+    CustomBaseModelAdminWithSoftDelete,
+)
+
+from ..models import {model_name_title}
+
+__all__ = ['{model_name_title}Admin']
+
+logger = logging.getLogger('app')
+console = console(source=__name__)
+
+
+@admin.register({model_name_title})
+class {model_name_title}Admin(CustomBaseModelAdminWithSoftDelete):
+    pass
+    # hide_deleted_at = False
+
+"""
+
+
+__all__ = ['TEMPLATE_ADMIN_SOFTDELETEMODEL']
