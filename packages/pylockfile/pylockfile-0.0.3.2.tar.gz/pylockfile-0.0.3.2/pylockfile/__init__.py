b716a0889713216383ac7b22057063273d1a9e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:36 +0000

    Add package pylockfile/0.0.3.2/pylockfile-0.0.3.2.tar.gz

---
+++ packages/pylockfile/pylockfile-0.0.3.2.tar.gz/pylockfile-0.0.3.2/pylockfile/__init__.py
@@ -0,0 +1,1 @@
+from .pylockfile import pylockfile
---
+++ packages/pylockfile/pylockfile-0.0.3.2.tar.gz/pylockfile-0.0.3.2/pylockfile/pylockfile.py
@@ -0,0 +1,48 @@
+#! -*-encoding:utf-8-*-
+
+import os
+import sys
+import re
+
+class pylockfile(object):
+
+	def __init__(self, path):
+		self.path = path
+	
+	def create(self):
+		path = self.path
+		if re.match(r'/\w+/\w+/\w+',path) is None:
+			print('Wrong path!' %path)
+			print('Sample usage: /<DIR>/<DIR>/lockfile')
+			sys.exit()
+		else:
+			directory = path[:path.rfind("/") + 1]
+			if os.path.exists(directory) == False:
+				os.mkdir(directory)
+			if os.path.isfile(path) == False:
+				os.mknod(path)
+			else:
+				print("%s already exits!" %self.path)
+				sys.exit()
+	
+	def delete(self):
+		path = self.path 
+		if re.match(r'/\w+/\w+/\w+',path) is None:
+			print('Wrong path! %s' %path)
+			print('Sample usage: /<DIR>/<DIR>/lockfile')
+			sys.exit()
+		else:			
+			os.remove(path)
+
+if __name__ == '__main__':
+	print('''USAGE:
+In your script paste
+
+import sys
+from pylockfile import *
+
+lock = pylockfile('<PATH TO LOCKFILE>') #Initialize module
+
+lock.create() # create lockfile
+lock.delete # delete lockfile
+''')
