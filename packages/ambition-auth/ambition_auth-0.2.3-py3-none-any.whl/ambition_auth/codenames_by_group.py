ee7f7f16de4ad835d8e976a9cadbef5806cd4d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:07 +0000

    Add package ambition-auth/0.2.3/ambition_auth-0.2.3-py3-none-any.whl

---
+++ packages/ambition-auth/ambition_auth-0.2.3-py3-none-any.whl/ambition_auth/codenames_by_group.py
@@ -0,0 +1,22 @@
+from edc_auth import (
+    AUDITOR,
+    CLINIC,
+    SCREENING,
+    UNBLINDING_REQUESTORS,
+    UNBLINDING_REVIEWERS,
+    codenames_by_group,
+)
+
+from .codenames import (
+    auditor,
+    clinic,
+    screening,
+)
+
+unblinding_requestors = []
+unblinding_reviewers = []
+codenames_by_group[AUDITOR] = auditor
+codenames_by_group[CLINIC] = clinic
+codenames_by_group[SCREENING] = screening
+codenames_by_group[UNBLINDING_REQUESTORS] = unblinding_requestors
+codenames_by_group[UNBLINDING_REVIEWERS] = unblinding_reviewers
