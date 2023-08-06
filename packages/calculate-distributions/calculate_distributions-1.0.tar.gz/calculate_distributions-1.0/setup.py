5b764d422d0ae66ab2bb0f5a409c9e005e47129c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:36 +0000

    Add package calculate-distributions/1.0/calculate_distributions-1.0.tar.gz

---
+++ packages/calculate-distributions/calculate_distributions-1.0.tar.gz/calculate_distributions-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='calculate_distributions',
+      version='1.0',
+      description='Find Binomaial and Gaussian distributions',
+      packages=['calculate_distributions'],
+      zip_safe=False)
