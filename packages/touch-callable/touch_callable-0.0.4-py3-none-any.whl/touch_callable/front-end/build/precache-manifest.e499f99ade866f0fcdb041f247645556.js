a90dc1ac0ea5195612ce41ab1f2390c96d1ee41d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:07 +0000

    Add package touch-callable/0.0.4/touch_callable-0.0.4-py3-none-any.whl

---
+++ packages/touch-callable/touch_callable-0.0.4-py3-none-any.whl/touch_callable/front-end/build/precache-manifest.e499f99ade866f0fcdb041f247645556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8fd79e3844f5948cfae764ad1fe8447",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c04adb198ee5ccbf7924",
+    "url": "/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "86102f704ef1283a962c",
+    "url": "/static/css/main.6e6e8990.chunk.css"
+  },
+  {
+    "revision": "c04adb198ee5ccbf7924",
+    "url": "/static/js/2.9cce7296.chunk.js"
+  },
+  {
+    "revision": "86102f704ef1283a962c",
+    "url": "/static/js/main.7834c7a5.chunk.js"
+  },
+  {
+    "revision": "a4a2c54e6b5e2d190c40",
+    "url": "/static/js/runtime~main.e543b2a5.js"
+  }
+]);
