0ac9d230974344d58c33cb66ce825a86f8721c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:00 +0000

    Add package OpenTrepWrapper/0.7.4.post2/OpenTrepWrapper-0.7.4.post2.tar.gz

---
+++ packages/OpenTrepWrapper/OpenTrepWrapper-0.7.4.post2.tar.gz/OpenTrepWrapper-0.7.4.post2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Main installation script.
+'''
+
+from setuptools import setup
+
+setup(
+    name = 'OpenTrepWrapper',
+    version = '0.7.4.post2',
+    author = 'Alex Prengere',
+    author_email = 'alex.prengere@gmail.com',
+    url = 'https://github.com/trep/wrapper',
+    description = 'A Python wrapper module for OpenTrep',
+    long_description = 'file: README.md',
+    entry_points = {
+        'console_scripts' : [
+            'OpenTrep = OpenTrepWrapperMain:main',
+        ]
+    },
+    py_modules = [
+        'OpenTrepWrapper',
+        'OpenTrepWrapperMain'
+    ],
+    install_requires = [
+        #Public
+        #'simplejson', # --> json now
+    ],
+    #sanitize_lib = ['-lasan'] if cc == 'gcc' and not is_macos else [],
+)
+
