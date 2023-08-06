61145c4f652b3c869da5222ac9fde13f312df004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:48 +0000

    Add package nanome/0.10.0/nanome-0.10.0-py2.py3-none-any.whl

---
+++ packages/nanome/nanome-0.10.0-py2.py3-none-any.whl/nanome/_internal/_ui/_io/_image_json.py
@@ -0,0 +1,17 @@
+from .. import _Image
+from nanome.util.color import Color
+
+def parse_json(content_json):
+    image = _Image._create()
+    image._color = Color.from_int(int(float((content_json["color"]))))
+    image._file_path = str(content_json["file_path"])
+    if ("scaling_option" in content_json):
+        image._scaling_option = _Image.ScalingOptions(int(float(content_json["scaling_option"])))
+    return image
+
+def write_json(image):
+    content_json = {}
+    content_json["color"] = image._color._color
+    content_json["file_path"] = image._file_path
+    content_json["scaling_option"] = image._scaling_option
+    return content_json
---
+++ packages/nanome/nanome-0.10.0-py2.py3-none-any.whl/nanome/api/ui/io/menu_io.py
@@ -0,0 +1,44 @@
+import json
+from nanome.util import Logs
+from nanome._internal._ui._io import _menu_json
+from nanome._internal import _Addon
+
+class MenuIO(_Addon):
+    def __init__(self, base_object=None):
+        _Addon.__init__(self, base_object)
+
+    def to_json(self, path):
+        menu_json = _menu_json.write_json(self.base_object)
+        menu_string = json.dumps(menu_json)
+        try:
+            with open(path, "w") as f:
+                f.write(menu_string)
+        except:
+            Logs.error("Could not write to file: " + path)
+            raise
+
+    def from_json(self, path):
+        try:
+            with open(path, "r") as f:
+                menu_string = f.read()
+                menu_json = json.loads(menu_string)
+        except:
+            Logs.error("Could not read json file: " + path)
+            raise
+        try:
+            return _menu_json.parse_json(menu_json)
+        except:
+            Logs.error("Json does not correctly represent a menu.")
+            raise
+            
+
+    def update_json(self, path):
+        """
+        | Updates a menu written for an old version of the library.
+        | Call once before reading and run once. Then you can remove the call.
+
+        :param path: path to the menu you wish to update.
+        :type path: str
+        """
+        menu = self.from_json(path)
+        menu.io.to_json(path)
