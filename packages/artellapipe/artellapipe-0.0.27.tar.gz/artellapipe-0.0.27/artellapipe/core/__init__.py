5c3b6367366cbc6ff8fc37e4827b29692c00246b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:00 +0000

    Add package artellapipe/0.0.27/artellapipe-0.0.27.tar.gz

---
+++ packages/artellapipe/artellapipe-0.0.27.tar.gz/artellapipe-0.0.27/artellapipe/core/__init__.py
@@ -0,0 +1,11 @@
+order = [
+    'artellapipe.core.defines',
+    'artellapipe.core.abstract',
+    'artellapipe.core.config',
+    'artellapipe.core.assetinfo',
+    'artellapipe.core.assetfile',
+    'artellapipe.core.asset',
+    'artellapipe.core.syncdialog',
+    'artellapipe.core.sequence',
+    'artellapipe.core.shot'
+]
