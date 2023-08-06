eb2dbcae7e8e2b5e9e673fc4f1c0a9b5d15265c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:22 +0000

    Add package netcdf-segy/1.0.1/netcdf_segy-1.0.1.tar.gz

---
+++ packages/netcdf-segy/netcdf_segy-1.0.1.tar.gz/netcdf_segy-1.0.1/netcdf_segy/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Convert SEG-Y files to NetCDF.
+"""
+
+__author__ = """Alan Richardson"""
+__email__ = "arichar@tcd.ie"
+__version__ = "1.0.1"
+
+import netcdf_segy.segy2netcdf
---
+++ packages/netcdf-segy/netcdf_segy-1.0.1.tar.gz/netcdf_segy-1.0.1/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'Click>=6.0',
+    'segyio',
+    'netCDF4',
+    'numpy'
+]
+
+test_requirements = [
+    'pytest'
+]
+
+setup(
+    name='netcdf_segy',
+    version='1.0.1',
+    description="Convert between SEG-Y and NetCDF",
+    long_description=readme,
+    author="Alan Richardson",
+    author_email='arichar@tcd.ie',
+    url='https://github.com/ar4/netcdf_segy',
+    packages=[
+        'netcdf_segy',
+    ],
+    package_dir={'netcdf_segy':
+                 'netcdf_segy'},
+    entry_points={
+        'console_scripts': [
+            'segy2netcdf=netcdf_segy.segy2netcdf:cli'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    zip_safe=False,
+    keywords='netcdf_segy',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
