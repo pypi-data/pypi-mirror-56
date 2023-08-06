260e510dd16b1d4748595b370b6acf9435bd2ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:32 +0000

    Add package django-smmapdfs/1.0.3/django_smmapdfs-1.0.3-py3-none-any.whl

---
+++ packages/django-smmapdfs/django_smmapdfs-1.0.3-py3-none-any.whl/smmapdfs/email.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 o.s. Auto*Mat
+import re
+from datetime import datetime
+
+from django.core.mail import send_mail
+from django.template import engines
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.sites.shortcuts import get_current_site
+
+from .models import PdfSandwichEmail
+
+
+def get_base_url(request):
+    return '%s://%s' % (
+        request.scheme,
+        request.META['HTTP_HOST'],
+    )
+
+def send_pdfsandwich(pdfsandwich, base_url):
+    language = pdfsandwich.get_language()
+    email_template = PdfSandwichEmail.objects.get(language=language, pdfsandwich_type=pdfsandwich.pdfsandwich_type)
+    django_template = engines['django'].from_string(email_template.template)
+    context = pdfsandwich.get_context(base_url=base_url)
+    context['download_link'] = pdfsandwich.get_pdf_url(base_url)
+    html_message = django_template.render(context)
+    # https://stackoverflow.com/questions/9662346/python-code-to-remove-html-tags-from-a-string#12982689
+    cleanr = re.compile('<.*?>')
+    message = re.sub(cleanr, '', html_message)
+    send_mail(
+        email_template.subject,
+        message,
+        None,
+        [pdfsandwich.get_email()],
+        fail_silently=False,
+        html_message=html_message,
+        attachments=pdfsandwich.pdf,
+    )
+    pdfsandwich.sent_time = datetime.now()
+    pdfsandwich.save()
