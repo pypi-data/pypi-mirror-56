f98d3c757627d9659d4006d3c37e68caef128582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:34 +0000

    Add package TerminalPrinter/1.5.2/TerminalPrinter-1.5.2-py2.7.egg

---
+++ packages/TerminalPrinter/TerminalPrinter-1.5.2-py2.7.egg/printer/painter.py
@@ -0,0 +1,195 @@
+# coding=utf8
+import sys
+import random
+from os import popen, path
+
+from PIL import Image, ImageFont, ImageDraw
+
+
+FONT_LIST = ['shuyan.ttf',
+             'letter.ttf',
+             'Haibaoyuanyuan.ttf',
+             'fengyun.ttf',
+             'huakangbold.otf']
+
+_font_prefix = "https://raw.githubusercontent.com/hellflame/terminal_printer/" \
+               "808004a7cd41b4383bfe6aa310c491c69d9b2556/fonts/"
+
+FONT_URL = {
+    f: _font_prefix + f for f in FONT_LIST
+}
+
+FONT_DIR = path.join(path.expanduser('~'), ".terminal_fonts")
+
+
+if sys.version_info.major == 2:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    MESS_FILTERS = ''.join([unichr(i) for i in range(32, 256)])
+else:
+    MESS_FILTERS = ''.join([chr(i) for i in range(32, 256)])
+    unicode = lambda s: s
+
+try:
+    DEFAULT_SIZE = tuple(reversed([int(s) for s in popen("stty size").read().split()]))
+except:
+    DEFAULT_SIZE = 50, 30  # width, height
+
+
+class ImageMap(object):
+    def __init__(self):
+        self._image = {}
+
+    def __setitem__(self, key, value):
+        self._image[key] = value
+
+    def __getitem__(self, key):
+        return self._image[key]
+
+
+def make_terminal_img(img, filter_type=None, width=None,
+                      height=None, dye=None, reverse=False,
+                      keep_ratio=False, gray=True, strip_white=False):
+    """
+    在终端输出字符图片
+    :param img: 图像
+    :param filter_type: 填充字符
+    :param width: 输出宽度
+    :param height: 输出高度
+    :param dye: 上色
+    :param reverse: 是否反色
+    :param keep_ratio: 是否保持比例
+    :param gray: 如果处理图片，是否转换为灰度图
+    :param strip_white: 是否删除(文字打印)模式下的空白行
+    :return: 图像字符
+    """
+    if not img:
+        # 如果文字画布生成失败，img为空
+        return ''
+    if not keep_ratio:
+        if width is None or height is None:
+            img = img.resize(DEFAULT_SIZE)
+        else:
+            img = img.resize((width, height))
+    else:
+        size = img.size
+        if width is None or height is None:
+            ratio = min(float(DEFAULT_SIZE[0]) / size[0], float(DEFAULT_SIZE[1]) / size[1])
+            img = img.resize((int(size[0] * ratio), int(size[1] * ratio)))
+        else:
+            ratio = min(float(width) / size[0], float(height) / size[1])
+            img = img.resize((int(size[0] * ratio), int(size[1] * ratio)))
+    width, height = img.size
+    pix = img.load()
+
+    if strip_white:
+        image_map = ImageMap()
+        white_lines = 0
+        after_h = 0
+        is_start_line = True
+        for h in range(height):
+            if all([pix[w, h] == 255 for w in range(width)]):
+                if is_start_line:
+                    # skip first white line
+                    is_start_line = False
+                elif h == height - 2:
+                    # skip last white line
+                    pass
+                else:
+                    white_lines += 1
+                    continue
+            for w in range(width):
+                image_map[w, after_h] = pix[w, h]
+            after_h += 1
+        height = after_h
+        pix = image_map
+
+    if gray:
+        def render_pix(x, y):
+            if filter_type:
+                if reverse:
+                    return MESS_FILTERS[(255 - pix[x, y]) * filter_type // 255]
+                return MESS_FILTERS[pix[x, y] * filter_type // 255]
+            else:
+                if reverse:
+                    return MESS_FILTERS[(255 - pix[x, y]) * 4 // 255]
+                return MESS_FILTERS[pix[x, y] * 4 // 255]
+    else:
+        def render_pix(x, y):
+            # 如果这里也用和灰度图一样的处理方法的话，会显得很乱，终端中的颜色也难以显示出来
+            return '\033[0;38;2;%s;%s;%sm' % pix[x, y][:3] + MESS_FILTERS[filter_type]
+
+    if type(dye) is int:
+        # 特定颜色绘制
+        result = '\033[01;{}m'.format(dye) + '\n'.join([''.join([render_pix(w, h) for w in range(width)])
+                                                        for h in range(height)])
+    elif type(dye) is str:
+        # 随机颜色绘制
+        result = '\n'.join([''.join(["\033[01;{}m{}".format(random.randrange(30, 40),
+                                                            render_pix(w, h))
+                                     for w in range(width)])
+                            for h in range(height)])
+
+    else:
+        # 黑白
+        result = '\n'.join([''.join([render_pix(w, h)
+                                     for w in range(width)])
+                            for h in range(height)])
+    img.close()
+    return result + '\033[00m'
+
+
+def get_img(file_path, gray=False):
+    """
+    获取输入图片
+    :param file_path: 图片文件位置
+    :param gray: 是否转换为灰度图
+    :return: img
+    """
+    try:
+        img = Image.open(file_path)
+        if gray:
+            img = img.convert('L')
+        return img
+    except Exception:
+        print("不支持的图片格式")
+        return None
+
+
+def text_drawer(text, fonts=None):
+    """
+    将文字书写在白色画布上
+    :param text: 要书写的文字
+    :param fonts: 字体选择，索引或路径
+    :return: img
+    """
+    im = Image.new("1", (1, 1), 'white')  # 初始画布大小没有关系
+    draw = ImageDraw.Draw(im)
+    if type(fonts) is int:
+        font = path.join(FONT_DIR, FONT_LIST[fonts if len(FONT_LIST) - 1 >= fonts >= 0 else 0])
+        # print(font)
+    elif fonts is None:
+        font = path.join(FONT_DIR, FONT_LIST[0])
+    else:
+        font = fonts
+
+    try:
+        font = ImageFont.truetype(font, 20)
+    except IOError:
+        print("字体文件损坏，请使用其他字体或重新初始化")
+        return None
+    except:
+        target = path.join(FONT_DIR, FONT_LIST[0])
+        if path.exists(target):
+            font = ImageFont.truetype(target, 20)
+        else:
+            print("字体缺失，请初始化字体!")
+            return None
+
+    text_size = draw.textsize(unicode(text), font=font)
+    im = im.resize(text_size)
+
+    draw = ImageDraw.Draw(im)
+    draw.text((0, 0), unicode(text), font=font)
+    return im
+
---
+++ packages/TerminalPrinter/TerminalPrinter-1.5.2-py2.7.egg/printer/resource.py
@@ -0,0 +1,83 @@
+# coding=utf8
+
+import os
+import sys
+import time
+import shutil
+
+from printer.http import HTTPCons, SockFeed, unit_change
+
+if sys.version_info.major == 2:
+    reload(sys)
+    sys.setdefaultencoding("utf8")
+
+__all__ = ['font_downloader', 'font_handle']
+
+
+def font_downloader(font_link, font_dir):
+    """
+    字体下载
+    :param font_link: 字体名称
+    :param font_dir: 字体保存路径
+    :return:
+    """
+    font_name = font_link.split("/")[-1]
+    save_path = os.path.join(font_dir, font_name)
+    downloader = HTTPCons()
+    downloader.request(font_link)
+    feed = SockFeed(downloader)
+    start = time.time()
+    feed.http_response(save_path, chunk=4096)
+    end = time.time()
+    if int(feed.status['code']) == 200:
+        size = os.stat(save_path).st_size
+        print("\033[01;31m{}\033[00m downloaded @speed \033[01;32m{}/s\033[00m"
+              .format(font_name,
+                      unit_change(size / (end - start))))
+    else:
+        print("\033[01;31m{}\033[00m 下载失败".format(font_name))
+    return True
+
+
+def font_handle(font_dir, fonts_url, show_prompt=True):
+    """
+    字体下载管理，如果没有缺失字体依然执行，将提示重新下载所有字体
+    :param font_dir: 字体路径
+    :param fonts_url: 字体链接
+    :param show_prompt: 显示提示信息
+    :return:
+    """
+    target = [fonts_url[f] for f in fonts_url if not os.path.exists(os.path.join(font_dir, f))]
+    if not target:
+        # 如果字体完整依然执行初始化，则提示删除原有字体目录
+        if show_prompt:
+            # 如果不显示提示信息，则直接删除
+            prompt = "当前字体数据完整，是否继续初始化? y/n "
+            try:
+                if sys.version_info.major == 2:
+                    if not raw_input(prompt).lower().startswith('y'):
+                        return False
+                else:
+                    if not input(prompt).lower().startswith('y'):
+                        return False
+            except KeyboardInterrupt:
+                exit(0)
+        shutil.rmtree(font_dir)
+        target = fonts_url.values()
+
+    if not os.path.exists(font_dir):
+        # 创建字体目录
+        os.makedirs(font_dir)
+
+    print("Start Downloading {} fonts".format(len(target)))
+    for font in target:
+        font_downloader(font, font_dir)
+
+    print("下载完成")
+
+
+if __name__ == '__main__':
+    from printer import painter
+    font_handle(painter.FONT_DIR, painter.FONT_URL, show_prompt=False)
+
+
