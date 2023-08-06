e82b7afc7fffd55a690274677b75138c16d684a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:20 +0000

    Add package django-frontera/0.0.3a1/django-frontera-0.0.3a1.tar.gz

---
+++ packages/django-frontera/django-frontera-0.0.3a1.tar.gz/django-frontera-0.0.3a1/django_frontera/__init__.py
@@ -0,0 +1,8 @@
+from django_frontera.contrib.utils.version import get_semver_version, get_version
+
+VERSION = (0, 0, 3, 'alpha', 1)
+
+__version__ = get_version(VERSION)
+
+# Required for npm package for frontend
+__semver__ = get_semver_version(VERSION)
