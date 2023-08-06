0f0977219a4ebb9eda9121cff2f6d027c58f4bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:56 +0000

    Add package invenio-app-rdm/1.0.0a4/invenio_app_rdm-1.0.0a4-py2.py3-none-any.whl

---
+++ packages/invenio-app-rdm/invenio_app_rdm-1.0.0a4-py2.py3-none-any.whl/invenio_app_rdm/theme/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 CERN.
+# Copyright (C) 2019 Northwestern University,
+#                    Galter Health Sciences Library & Learning Center.
+#
+# Invenio App RDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio App RDM."""
+
+from __future__ import absolute_import, print_function
+
+from ..version import __version__
+
+__all__ = ('__version__', )
