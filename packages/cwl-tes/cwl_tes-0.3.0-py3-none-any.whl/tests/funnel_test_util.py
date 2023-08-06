daa51024abddc1f46210b129bf3155d138fd5300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:12 +0000

    Add package cwl-tes/0.3.0/cwl_tes-0.3.0-py3-none-any.whl

---
+++ packages/cwl-tes/cwl_tes-0.3.0-py3-none-any.whl/tests/funnel_test_util.py
@@ -0,0 +1,134 @@
+from __future__ import print_function
+
+import logging
+import os
+import signal
+import subprocess
+import sys
+import tempfile
+import time
+import unittest
+import yaml
+
+
+def cmd_exists(program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return True
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return True
+
+    return False
+
+
+def popen(*args, **kwargs):
+    kwargs['preexec_fn'] = os.setsid
+    return subprocess.Popen(*args, **kwargs)
+
+
+def kill(p):
+    try:
+        os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+        p.wait()
+    except OSError:
+        pass
+
+
+def temp_config(dir, config):
+    configFile = tempfile.NamedTemporaryFile(dir=dir, mode='w', delete=False)
+    yaml.dump(config, configFile)
+    return configFile
+
+
+def check_version(cmd, version_str):
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                         universal_newlines=True)
+    stdout, stderr = p.communicate()
+    return version_str in stdout
+
+
+class SimpleServerTest(unittest.TestCase):
+    def setUp(self):
+        self.tmpdir = None
+        self.task_server = None
+        self.addCleanup(self.cleanup)
+
+        if not cmd_exists("funnel"):
+            print(
+                "-bash: funnel: command not found\n",
+                "see https://ohsu-comp-bio.github.io/funnel/download/",
+                "for instuctions on how to install",
+                file=sys.stdout)
+            raise RuntimeError
+
+        # if not check_version(["funnel", "version"], "version: 0.7.0"):
+        #     raise RuntimeError("expected Funnel version 0.7.0")
+
+        self.rootprojectdir = os.path.dirname(
+            os.path.dirname(os.path.realpath(__file__)))
+
+        self.testdir = os.path.join(self.rootprojectdir, "tests")
+        if not os.path.exists(os.path.join(self.testdir, "test_tmp")):
+            os.mkdir(os.path.join(self.testdir, "test_tmp"))
+        self.tmpdir = tempfile.mkdtemp(
+            dir=os.path.join(self.testdir, "test_tmp"),
+            prefix="conformance_test_v1.0_")
+        os.environ['TMPDIR'] = self.tmpdir
+
+        f, db_path = tempfile.mkstemp(dir=self.tmpdir, prefix="tes_task_db.")
+        os.close(f)
+        funnel_work_dir = os.path.join(self.tmpdir, "funnel-work-dir")
+        os.mkdir(funnel_work_dir)
+        logFile = os.path.join(self.tmpdir, "funnel_log.txt")
+
+        # Build server config file (YAML)
+        rate = "500ms"
+        configFile = temp_config(
+            dir=self.tmpdir,
+            config={
+                "Compute": "local",
+                "Database": "boltdb",
+                "EventWriters": ["log"],
+                "BoltDB": {
+                    "Path": db_path
+                },
+                "Server": {
+                    "HostName": "localhost",
+                    "HTTPPort": "8000",
+                    "RPCPort": "9090"
+                },
+                "Worker": {
+                    "WorkDir": funnel_work_dir,
+                    "PollingRate": rate,
+                    "LogUpdateRate": rate
+                },
+                "LocalStorage": {
+                    "AllowedDirs": [self.testdir, "/tmp", "/private"]
+                },
+                "Logger": {
+                    "Level": "info",
+                    "OutputFile": logFile
+                },
+            })
+
+        # Start server
+        cmd = ["funnel", "server", "run", "--config", configFile.name]
+        logging.info("Running %s" % (" ".join(cmd)))
+        self.task_server = popen(cmd)
+        signal.signal(signal.SIGINT, self.cleanup)
+        time.sleep(1)
+
+    # We're using this instead of tearDown because python doesn't call tearDown
+    # if setUp fails. Since our setUp is complex, that means things don't get
+    # properly cleaned up (e.g. processes are orphaned).
+    def cleanup(self, *args):
+        if self.task_server is not None:
+            kill(self.task_server)
---
+++ packages/cwl-tes/cwl_tes-0.3.0-py3-none-any.whl/tests/test_conformance.py
@@ -0,0 +1,57 @@
+from __future__ import print_function, unicode_literals
+
+import glob
+import os
+import shutil
+import subprocess
+import sys
+import time
+import psutil
+
+from .funnel_test_util import SimpleServerTest, popen
+
+
+class TestConformance(SimpleServerTest):
+
+    def test_conformance(self):
+        cwl_testdir = os.path.join(self.testdir, "schemas/v1.0")
+        ctest_def = os.path.join(cwl_testdir, "conformance_test_v1.0.yaml")
+        tool_entry = os.path.join(self.rootprojectdir, "cwl-tes")
+
+        cmd = [
+            "cwltest", "--test", ctest_def, "--basedir", self.tmpdir,
+            "--tool", tool_entry, "-j", str(psutil.cpu_count()),
+            "-n", "1-5,7-49,52-79,81-96,99-102,104-107", "--", "--tes=http://localhost:8000"
+        ]
+
+        testlog = os.path.join(
+            self.tmpdir,
+            "conformance_test_results_%s.txt" % (time.strftime("%m%d%Y"))
+        )
+        print("RUNNNING:", " ".join(cmd))
+        p = popen(
+            cmd,
+            cwd=os.path.join(self.testdir, "schemas/v1.0"),
+            stderr=subprocess.STDOUT,
+            stdout=subprocess.PIPE,
+            bufsize=1
+        )
+        with p.stdout, open(testlog, 'ab') as fh:
+            for line in iter(p.stdout.readline, b''):
+                print(line.decode('utf8').strip("\n"), file=sys.stderr)
+                fh.write(line)
+        p.wait()
+
+        ctest_dirs = glob.glob(cwl_testdir + "[a-zA-Z0-9_]*")
+        cleanup_tmpdirs(ctest_dirs)
+
+        assert p.returncode == 0
+
+
+def cleanup_tmpdirs(*args):
+    for d in args:
+        if isinstance(d, list):
+            for sd in d:
+                shutil.rmtree(sd, True)
+        else:
+            shutil.rmtree(d, True)
