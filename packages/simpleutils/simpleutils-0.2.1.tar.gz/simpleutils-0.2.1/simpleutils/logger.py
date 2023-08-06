a816d185e4df7ab6a8bdcaadb3651fe269ab2cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:41 +0000

    Add package simpleutils/0.2.1/simpleutils-0.2.1.tar.gz

---
+++ packages/simpleutils/simpleutils-0.2.1.tar.gz/simpleutils-0.2.1/simpleutils/logger.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2017-5-7
+
+@author: cheng.li
+"""
+
+import logging
+
+
+class CustomLogger:
+    def __init__(self,
+                 logger_name: str,
+                 log_level: str):
+        """
+        Initialize the logger
+
+        :param logger_name: str
+        :param log_level: srt
+        """
+        self.logger = logging.getLogger(logger_name)
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+        self.set_level(log_level)
+
+    def set_level(self, log_level: str):
+        if log_level.lower() == "info":
+            self.logger.setLevel(logging.INFO)
+        elif log_level.lower() == "warning":
+            self.logger.setLevel(logging.WARNING)
+        elif log_level.lower() == 'critical':
+            self.logger.setLevel(logging.CRITICAL)
+        elif log_level.lower() == 'debug':
+            self.logger.setLevel(logging.DEBUG)
+
+    def info(self, msg: str):
+        self.logger.info(msg)
+
+    def warning(self, msg: str):
+        self.logger.warning(msg)
+
+    def critical(self, msg: str):
+        self.logger.critical(msg)
+
+    def debug(self, msg: str):
+        self.logger.debug(msg)
