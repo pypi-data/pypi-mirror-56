1c33ea887ca644e6676d24c92da2a53cea04a593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:37 +0000

    Add package global-utils-88/0.0.5/global_utils_88-0.0.5.tar.gz

---
+++ packages/global-utils-88/global_utils_88-0.0.5.tar.gz/global_utils_88-0.0.5/global_utils/__main__.py
@@ -0,0 +1,11 @@
+# __main__.py
+
+import global_utils
+from global_utils import global_utils_functions
+
+def main():
+    """."""
+    print("Using global utils lib")
+
+if __name__ == "__main__":
+    main()
