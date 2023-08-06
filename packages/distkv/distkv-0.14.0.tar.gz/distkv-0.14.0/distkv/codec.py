ba19e9ec43c9abb3c152b9f8838d8583d19ca1b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:05 +0000

    Add package distkv/0.14.0/distkv-0.14.0.tar.gz

---
+++ packages/distkv/distkv-0.14.0.tar.gz/distkv-0.14.0/distkv/codec.py
@@ -0,0 +1,24 @@
+"""
+This module contains helper functions for packing+unpacking of single messages,
+plus an unpacker factory for streams.
+"""
+
+import msgpack
+from functools import partial
+
+from .util import attrdict
+
+__all__ = ["packer", "unpacker", "stream_unpacker"]
+
+# single message packer
+packer = msgpack.Packer(strict_types=False, use_bin_type=True).pack
+
+# single message unpacker
+unpacker = partial(
+    msgpack.unpackb, object_pairs_hook=attrdict, raw=False, use_list=False
+)
+
+# stream unpacker factory
+stream_unpacker = partial(msgpack.Unpacker,
+    object_pairs_hook=attrdict, raw=False, use_list=False
+)
