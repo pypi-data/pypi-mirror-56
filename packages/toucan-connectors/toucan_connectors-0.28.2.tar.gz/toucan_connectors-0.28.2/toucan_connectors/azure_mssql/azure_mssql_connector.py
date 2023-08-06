185b9394e7cf8b7b2f9479c2534d0def032eecf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:09 +0000

    Add package toucan-connectors/0.28.2/toucan_connectors-0.28.2.tar.gz

---
+++ packages/toucan-connectors/toucan_connectors-0.28.2.tar.gz/toucan_connectors-0.28.2/toucan_connectors/azure_mssql/azure_mssql_connector.py
@@ -0,0 +1,50 @@
+import re
+
+import pandas as pd
+import pyodbc
+from pydantic import constr
+
+from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
+
+CLOUD_HOST = 'database.windows.net'
+
+
+class AzureMSSQLDataSource(ToucanDataSource):
+    database: str
+    query: constr(min_length=1)
+
+
+class AzureMSSQLConnector(ToucanConnector):
+    """
+    Import data from Microsoft Azure SQL Server.
+    """
+
+    data_source_model: AzureMSSQLDataSource
+
+    host: str
+    user: str
+    password: str
+    connect_timeout: int = None
+
+    def get_connection_params(self, *, database=None):
+        base_host = re.sub(f'.{CLOUD_HOST}$', '', self.host)
+        user = f'{self.user}@{base_host}' if '@' not in self.user else self.user
+
+        con_params = {
+            'driver': '{ODBC Driver 17 for SQL Server}',
+            'server': f'{base_host}.{CLOUD_HOST}',
+            'database': database,
+            'user': user,
+            'password': self.password,
+            'timeout': self.connect_timeout,
+        }
+        # remove None values
+        return {k: v for k, v in con_params.items() if v is not None}
+
+    def _retrieve_data(self, datasource: AzureMSSQLDataSource) -> pd.DataFrame:
+        connection = pyodbc.connect(**self.get_connection_params(database=datasource.database))
+
+        df = pd.read_sql(datasource.query, con=connection)
+
+        connection.close()
+        return df
