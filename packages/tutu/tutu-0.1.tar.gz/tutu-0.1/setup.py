0eb2de4bcb1502a455aeb3f6f1e8c77ce08081c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:47 +0000

    Add package tutu/0.1/tutu-0.1.tar.gz

---
+++ packages/tutu/tutu-0.1.tar.gz/tutu-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tutu',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['tutu'],
+      author = 'Changyu (Steven) Lin',
+      author_email = 'changyu.lin2011@gmail.com',
+      zip_safe=False)
