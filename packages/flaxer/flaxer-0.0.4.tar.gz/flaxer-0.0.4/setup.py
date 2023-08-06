ce9c23d5c5174778b1bba53f917522cd037db350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:39 +0000

    Add package flaxer/0.0.4/flaxer-0.0.4.tar.gz

---
+++ packages/flaxer/flaxer-0.0.4.tar.gz/flaxer-0.0.4/setup.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+import setuptools
+
+print(setuptools.find_packages())
+setuptools.setup(
+    name="flaxer",
+    version="0.0.4",
+    author="yana",
+    author_email="yana021022@example.com",
+    description="A python fixer",
+    url="https://github.com/yangyangxcf/flaxer",
+    packages=setuptools.find_packages(),
+    install_requires=["click", "astunparse"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={"console_scripts": ["flaxer=flaxer:main"]},
+    python_requires=">=3.6",
+)
