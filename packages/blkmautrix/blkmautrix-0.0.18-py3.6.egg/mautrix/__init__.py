9bf8e61925ee081d85e7aa217aa040e96a7ca80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:59 +0000

    Add package blkmautrix/0.0.18/blkmautrix-0.0.18-py3.6.egg

---
+++ packages/blkmautrix/blkmautrix-0.0.18-py3.6.egg/mautrix/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.4.0.dev78"
+__blink_version__ = "0.0.18"
+__author__ = "Tulir Asokan <tulir@maunium.net>"
+__all__ = ["api", "appservice", "bridge", "client", "errors", "util", "types"]
