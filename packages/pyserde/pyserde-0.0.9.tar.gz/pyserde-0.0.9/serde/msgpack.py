792037d7f3cd1054b8f415aa4db54e9d654b7d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:44 +0000

    Add package pyserde/0.0.9/pyserde-0.0.9.tar.gz

---
+++ packages/pyserde/pyserde-0.0.9.tar.gz/pyserde-0.0.9/serde/msgpack.py
@@ -0,0 +1,29 @@
+from typing import Any, Type
+
+import msgpack
+
+from .core import T, logger
+from .de import Deserializer, from_dict
+from .se import Serializer, asdict
+
+
+class MsgPackSerializer(Serializer):
+    @classmethod
+    def serialize(cls, obj: Any, **opts) -> str:
+        return msgpack.packb(asdict(obj), use_bin_type=True, **opts)
+
+
+class MsgPackDeserializer(Deserializer):
+    @classmethod
+    def deserialize(cls, s, **opts):
+        unp = msgpack.unpackb(s, raw=False, use_list=False, **opts)
+        logger.debug('unpack from msgpack: {unp}')
+        return unp
+
+
+def to_msgpack(obj: Any, serializer=MsgPackSerializer, **opts) -> bytes:
+    return obj.__serde_serialize__(serializer, **opts)
+
+
+def from_msgpack(c: Type[T], s: str, de: Type[Deserializer] = MsgPackDeserializer, **opts) -> Type[T]:
+    return from_dict(c, de.deserialize(s, **opts))
