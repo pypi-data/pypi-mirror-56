ec4049411a2e2d2bb11153f6434262d47a1c578d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:52 +0000

    Add package cmplot/1.0.1/cmplot-1.0.1.tar.gz

---
+++ packages/cmplot/cmplot-1.0.1.tar.gz/cmplot-1.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README_brief.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cmplot",
+    version="1.0.1",
+    author="Dr Giuseppe Insana",
+    author_email="insana@insana.net",
+    description="Cloudy Mountain Plot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/g-insana/cmplot.py",
+    license="AGPL",
+    py_modules=["cmplot"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "plotly",
+        "numpy",
+        "pandas",
+        "scipy"
+    ],
+    python_requires=">=3.5",
+)
