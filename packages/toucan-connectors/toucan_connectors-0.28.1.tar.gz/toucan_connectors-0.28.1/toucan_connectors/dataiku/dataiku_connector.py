6468663c7f02d4cc6ee9f3b24858e2e8e133a3a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:55 +0000

    Add package toucan-connectors/0.28.1/toucan_connectors-0.28.1.tar.gz

---
+++ packages/toucan-connectors/toucan_connectors-0.28.1.tar.gz/toucan_connectors-0.28.1/toucan_connectors/dataiku/dataiku_connector.py
@@ -0,0 +1,29 @@
+from io import StringIO
+
+import dataikuapi
+import pandas as pd
+
+from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
+
+
+class DataikuDataSource(ToucanDataSource):
+    dataset: str
+
+
+class DataikuConnector(ToucanConnector):
+    """
+    This is a basic connector for [Dataiku](https://www.dataiku.com/) using their
+    [DSS API](https://doc.dataiku.com/dss/2.0/api/index.html).
+    """
+
+    data_source_model: DataikuDataSource
+
+    host: str
+    apiKey: str
+    project: str
+
+    def _retrieve_data(self, data_source: DataikuDataSource) -> pd.DataFrame:
+        client = dataikuapi.DSSClient(self.host, self.apiKey)
+        data_url = f'/projects/{self.project}/datasets/{data_source.dataset}/data/'
+        stream = client._perform_raw("GET", data_url, params={"format": "tsv-excel-header"})
+        return pd.read_csv(StringIO(stream.text), sep='\t')
---
+++ packages/toucan-connectors/toucan_connectors-0.28.1.tar.gz/toucan_connectors-0.28.1/toucan_connectors/google_credentials.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, validator
+
+
+class GoogleCredentials(BaseModel):
+    type: str
+    project_id: str
+    private_key_id: str
+    private_key: str
+    client_email: str
+    client_id: str
+    auth_uri: str
+    token_uri: str
+    auth_provider_x509_cert_url: str
+    client_x509_cert_url: str
+
+    @validator('private_key')
+    def unescape_break_lines(cls, v):
+        """
+        `private_key` is a long string like
+        '-----BEGIN PRIVATE KEY-----\nxxx...zzz\n-----END PRIVATE KEY-----\n
+        As the breaking line are often escaped by the client,
+        we need to be sure it's unescaped
+        """
+        return v.replace('\\n', '\n')
+
+
+def get_google_oauth2_credentials(google_credentials):
+    from google.oauth2.service_account import Credentials
+
+    return Credentials.from_service_account_info(google_credentials.dict())
