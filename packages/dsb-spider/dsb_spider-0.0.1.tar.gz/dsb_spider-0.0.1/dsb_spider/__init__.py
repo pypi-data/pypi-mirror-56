7ea179832a140bbdd403c8f8f72e08117436f7cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:58 +0000

    Add package dsb-spider/0.0.1/dsb_spider-0.0.1.tar.gz

---
+++ packages/dsb-spider/dsb_spider-0.0.1.tar.gz/dsb_spider-0.0.1/dsb_spider/__init__.py
@@ -0,0 +1,3 @@
+from .log import getLogger
+from .sched import TaskListener
+from .task import BaseTask, TaskState, TaskSingleton, ImageTaskSingleton, ActivityTaskSingleton
---
+++ packages/dsb-spider/dsb_spider-0.0.1.tar.gz/dsb_spider-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dsb_spider',
+    version='0.0.1',
+    author='cjxh',
+    author_email='chenjianxihang@duoshoubang.cn',
+    url='http://m.fine3q.com/home-w/index.html',
+    description=u'666',
+    packages=['dsb_spider'],
+    install_requires=[],
+    python_requires='>=3.6'
+)
