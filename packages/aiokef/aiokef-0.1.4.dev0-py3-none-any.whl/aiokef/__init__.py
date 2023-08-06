25e673a6e21efd59f11561336f847fd519cffdbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:24 +0000

    Add package aiokef/0.1.4.dev0/aiokef-0.1.4.dev0-py3-none-any.whl

---
+++ packages/aiokef/aiokef-0.1.4.dev0-py3-none-any.whl/aiokef/__init__.py
@@ -0,0 +1,7 @@
+"""A module for asynchronously interacting with KEF wireless speakers."""
+
+__version__ = "0.1.4.dev0"
+
+from aiokef.aiokef import AsyncKefSpeaker, SyncKefSpeaker
+
+__all__ = ["AsyncKefSpeaker", "SyncKefSpeaker"]
