8ce9cfd076d2d09b7cb441748d60d4abb7f17049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:15 +0000

    Add package touch-callable/0.0.5/touch_callable-0.0.5-py3-none-any.whl

---
+++ packages/touch-callable/touch_callable-0.0.5-py3-none-any.whl/touch_callable/front-end/build/precache-manifest.92b9efaa4df499a355b21311a598eae5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6be618ce244a1f0e9a268d4ba6a685",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1aff34fe279e459deaf",
+    "url": "/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "06f7b87e49b36aacb6b6",
+    "url": "/static/css/main.6e6e8990.chunk.css"
+  },
+  {
+    "revision": "d1aff34fe279e459deaf",
+    "url": "/static/js/2.07ffdd84.chunk.js"
+  },
+  {
+    "revision": "06f7b87e49b36aacb6b6",
+    "url": "/static/js/main.e733cb33.chunk.js"
+  },
+  {
+    "revision": "a4a2c54e6b5e2d190c40",
+    "url": "/static/js/runtime~main.e543b2a5.js"
+  },
+  {
+    "revision": "3dedce739d214a926193692b6ac34be3",
+    "url": "/static/media/logo.3dedce73.svg"
+  },
+  {
+    "revision": "edef1c3c214e77394ac6ed8f382be54b",
+    "url": "/static/media/touch-callable-text.edef1c3c.svg"
+  }
+]);
