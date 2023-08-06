16f60f60e356da37a211c73ecb4fc51cb5d98a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:24 +0000

    Add package FIDL/1.1/FIDL-1.1.tar.gz

---
+++ packages/FIDL/FIDL-1.1.tar.gz/FIDL-1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='FIDL',
+      version='1.1',
+      description='Wrapper for the IDA decompiler API',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Disassemblers',
+      ],
+      keywords='ida decompiler api vulnerability research reversing malware',
+      url='https://github.com/fireeye/FIDL',
+      author='FireEye FLARE Team',
+      author_email='carlos.garcia@fireeye.com',
+      license='MIT',
+      install_requires=[
+        'networkx',
+        'six',
+      ],
+      extras_require={
+        'dev': [
+          'pytest',
+          'pytest-pycodestyle',
+          'sphinx_rtd_theme',
+        ]
+      },
+      packages=['FIDL'],
+      include_package_data=True,
+      zip_safe=False)
