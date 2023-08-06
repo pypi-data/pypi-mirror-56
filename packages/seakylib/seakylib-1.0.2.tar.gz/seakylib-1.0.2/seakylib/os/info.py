d91cdcbdcbe73cb82add606de7b9141da876e672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:38 +0000

    Add package seakylib/1.0.2/seakylib-1.0.2.tar.gz

---
+++ packages/seakylib/seakylib-1.0.2.tar.gz/seakylib-1.0.2/seakylib/os/info.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Seaky
+# @Date:   2019/8/12 10:46
+
+import os
+import re
+import shutil
+import socket
+import traceback
+from pathlib import Path
+
+import psutil
+
+TERM_WIDTH, TERM_DEPTH = shutil.get_terminal_size((80, 20))
+LIB_DIR = 'seakylib'
+
+
+def get_hostname():
+    return socket.gethostname()
+
+
+def get_if(name=None):
+    d = psutil.net_if_addrs()
+    if name:
+        return {k: v for k, v in d.items() if k.startswith(name)}
+    return d
+
+
+def get_pwd():
+    '''返回程序所在目录'''
+    return get_caller().parent
+
+
+def get_caller(top=True):
+    '''返回用户调用函数，过滤lib'''
+    es = traceback.extract_stack()
+    l = es if top else es[::-1]
+    for x in l:
+        if not re.search('({}|pydev|pycharm)'.format(LIB_DIR), x.filename):
+            return Path(x.filename)
