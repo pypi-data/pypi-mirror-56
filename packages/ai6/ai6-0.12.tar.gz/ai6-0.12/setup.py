4d96c2a64028126ffe5616b0c8bfb2c5f53637fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:43 +0000

    Add package ai6/0.12/ai6-0.12.tar.gz

---
+++ packages/ai6/ai6-0.12.tar.gz/ai6-0.12/setup.py
@@ -0,0 +1,54 @@
+# from distutils.core import setup
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+	long_description = f.read()
+
+setup(
+	name = 'ai6',
+	packages = ['ai6'],
+	version = '0.12',
+	license='MIT',
+	description = 'Artificial Intelligence Examples',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	author = 'ccckmit',
+	author_email = 'ccckmit@gmail.com',
+	url = 'https://github.com/ccc-py/ai6',
+	download_url = 'https://github.com/ccc-py/ai6/archive/v0.12.tar.gz',
+	keywords = ['ai', 'nn', 'neural'],
+	install_requires=[
+			'numpy',
+		],
+	classifiers=[  # Optional
+		# How mature is this project? Common values are
+		#   3 - Alpha
+		#   4 - Beta
+		#   5 - Production/Stable
+		'Development Status :: 3 - Alpha',
+
+		# Indicate who your project is intended for
+		'Intended Audience :: Developers',
+		'Topic :: Software Development :: Build Tools',
+
+		# Pick your license as you wish
+		'License :: OSI Approved :: MIT License',
+
+		# Specify the Python versions you support here. In particular, ensure
+		# that you indicate whether you support Python 2, Python 3 or both.
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+	],
+)
