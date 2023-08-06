47b5e144fd8e4515dbbe48f173be69db4d6ced80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:09 +0000

    Add package shaonutil/0.0.0.5.1/shaonutil-0.0.0.5.1.tar.gz

---
+++ packages/shaonutil/shaonutil-0.0.0.5.1.tar.gz/shaonutil-0.0.0.5.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+  name = 'shaonutil',
+  packages = ['shaonutil'],
+  version = '0.0.0.5.1',
+  author = 'Shaon Majumder',
+  author_email = 'smazoomder@gmail.com',
+  url = 'https://github.com/ShaonMajumder/shaonutil',
+  download_url = 'https://github.com/ShaonMajumder/shaonutil/archive/0.0.0.5.1.tar.gz',
+  keywords = ['shaon', 'utility', 'statistics'], 
+  classifiers = [],
+)
