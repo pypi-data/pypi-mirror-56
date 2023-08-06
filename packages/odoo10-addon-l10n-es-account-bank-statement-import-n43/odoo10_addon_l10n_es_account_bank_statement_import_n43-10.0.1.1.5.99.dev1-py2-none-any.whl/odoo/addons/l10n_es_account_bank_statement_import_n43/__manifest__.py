fa8dca0d3ea07be3d8c90090096e7e704dd9a157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:56 +0000

    Add package odoo10-addon-l10n-es-account-bank-statement-import-n43/10.0.1.1.5.99.dev1/odoo10_addon_l10n_es_account_bank_statement_import_n43-10.0.1.1.5.99.dev1-py2-none-any.whl

---
+++ packages/odoo10-addon-l10n-es-account-bank-statement-import-n43/odoo10_addon_l10n_es_account_bank_statement_import_n43-10.0.1.1.5.99.dev1-py2-none-any.whl/odoo/addons/l10n_es_account_bank_statement_import_n43/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2013-2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Importación de extractos bancarios españoles (Norma 43)',
+    'category': 'Accounting & Finance',
+    'version': '10.0.1.1.5',
+    'license': 'AGPL-3',
+    'author': 'Spanish Localization Team,'
+              'Tecnativa,'
+              'Odoo Community Association (OCA)',
+    "website": "https://github.com/OCA/l10n-spain",
+    'depends': [
+        'account_bank_statement_import',
+    ],
+    'external_dependencies': {
+        'python': [
+            'chardet',
+        ],
+    },
+    'data': [
+        'views/account_journal_views.xml',
+        'wizards/account_bank_statement_import_view.xml',
+    ],
+    'installable': True,
+}
