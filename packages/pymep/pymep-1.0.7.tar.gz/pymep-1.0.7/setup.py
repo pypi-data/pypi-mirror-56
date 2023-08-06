ff1b2cf19e35a0e9c3cb97d2922a7eadccd5690a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:26 +0000

    Add package pymep/1.0.7/pymep-1.0.7.tar.gz

---
+++ packages/pymep/pymep-1.0.7.tar.gz/pymep-1.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymep",
+    version="1.0.7",
+    author="Sergio Besada",
+    author_email="sergio.besada@gmail.com",
+    description="Python Math Expression Parser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sbesada/python.math.expression.parser.pymep",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+
+)
