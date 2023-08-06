4c725f77d2ff8760f8d6b20f10c16d03dd823965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:33 +0000

    Add package tfs-seon-client/2.0.7/tfs_seon_client-2.0.7-py3-none-any.whl

---
+++ packages/tfs-seon-client/tfs_seon_client-2.0.7-py3-none-any.whl/tfs_seon_client/factory/service_enum.py
@@ -0,0 +1,10 @@
+from enum import Enum
+ 
+class Service(Enum):
+    Project = "Project"
+    Team_Member = "Team_Member"
+    Project_Team = "Project_Team"
+    Sprint = "Sprint"
+    User_Story = "User_Story"
+    Product_backlog_Sprint_Backlog = "Product_backlog_Sprint_Backlog"
+    Task = "Task"
