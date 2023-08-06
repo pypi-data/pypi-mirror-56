84c2883afaf0e31fd05ff560f74cd056006a3747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:08 +0000

    Add package cfa-taz/0.2.0/cfa-taz-0.2.0.tar.gz

---
+++ packages/cfa-taz/cfa-taz-0.2.0.tar.gz/cfa-taz-0.2.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+from taz._version import __version__
+
+
+long_description = 'SEE README file'
+
+# install is run by using: python setup.py install
+
+# exec(compile(open(
+#     os.path.join(
+#       os.path.dirname(os.path.abspath(__file__)),
+#       'taz',
+#       '_version.py')).read(), 'fbd_tools/_version.py', 'exec'))
+
+
+setup(name='cfa-taz',  # name your package
+      packages=find_packages(exclude=['tests/config.py']),
+      version=__version__,
+      description='Hight level API for azure',
+      long_description=long_description,
+      package_dir={'taz': 'taz'},
+      author='Christophe Fauchard',
+      author_email='christophe.fauchard@gmail.com',
+      license='MIT',  # MIT, GPL, BSD ??
+      install_requires=[
+          'azure-cli',
+          'nose',
+	  'pandas'
+      ],
+)
