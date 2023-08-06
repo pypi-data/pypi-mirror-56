6e8c71ca87041e0ccf10992a2cc6bb7f4420c555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:41 +0000

    Add package TracPlantUml/2.3/TracPlantUml-2.3.tar.gz

---
+++ packages/TracPlantUml/TracPlantUml-2.3.tar.gz/TracPlantUml-2.3/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010 Polar Technologies - www.polartech.es
+# Copyright (C) 2010 Alvaro J Iradier <alvaro.iradier@polartech.es>
+# Copyright (C) 2012-2017 Ryan J Ollos <ryan.j.ollos@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+from setuptools import setup
+
+setup(
+    name="TracPlantUml",
+    version=2.3,
+    packages=['plantuml'],
+    author="Alvaro J. Iradier",
+    author_email="alvaro.iradier@polartech.es",
+    maintainer="Ryan J Ollos",
+    maintainer_email='ryan.j.ollos@gmail.com',
+    description="A macro to include diagrams from PlantUML",
+    license="3-Clause BSD",
+    keywords="trac macro uml plantuml embed include",
+    url="https://trac-hacks.org/wiki/PlantUmlMacro",
+    install_requires=['Trac'],
+    classifiers = ['Framework :: Trac'],
+    entry_points={
+        "trac.plugins": [
+            "plantuml.macro = plantuml.macro"
+        ]
+    }
+)
