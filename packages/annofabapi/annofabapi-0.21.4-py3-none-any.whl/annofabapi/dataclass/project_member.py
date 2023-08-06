d1ec86d1932e0c4e02594349849bad4db32c5a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:51 +0000

    Add package annofabapi/0.21.4/annofabapi-0.21.4-py3-none-any.whl

---
+++ packages/annofabapi/annofabapi-0.21.4-py3-none-any.whl/annofabapi/dataclass/project_member.py
@@ -0,0 +1,54 @@
+# flake8: noqa: W291
+# pylint: disable=too-many-lines,trailing-whitespace
+"""
+annofabapiのmodelをDataClassで定義したクラス
+
+Note:
+    このファイルはopenapi-generatorで自動生成される。詳細は generate/README.mdを参照.
+    oneOf, allOfなどは正しく表現できない可能性がある。
+"""
+
+import warnings  # pylint: disable=unused-import
+from dataclasses import dataclass
+from typing import Any, Dict, List, NewType, Optional, Tuple, Union  # pylint: disable=unused-import
+
+from dataclasses_json import dataclass_json
+
+from annofabapi.models import ProjectMemberRole, ProjectMemberStatus
+
+
+@dataclass_json
+@dataclass
+class ProjectMember:
+    """
+    
+    """
+    project_id: str
+    """プロジェクトID。[値の制約についてはこちら。](#section/API-Convention/APIID) """
+
+    account_id: str
+    """"""
+
+    user_id: str
+    """"""
+
+    username: str
+    """"""
+
+    member_status: ProjectMemberStatus
+    """"""
+
+    member_role: ProjectMemberRole
+    """"""
+
+    updated_datetime: Optional[str]
+    """"""
+
+    created_datetime: Optional[str]
+    """"""
+
+    sampling_inspection_rate: Optional[int]
+    """メンバー固有の抜取検査率（0-100のパーセント値）。"""
+
+    sampling_acceptance_rate: Optional[int]
+    """メンバー固有の抜取受入率（0-100のパーセント値）。"""
