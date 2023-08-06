29545eaa68f402ead9c52116042c3367c77b0f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:03 +0000

    Add package bitmex-trio-websocket/0.2.8/bitmex_trio_websocket-0.2.8-py3-none-any.whl

---
+++ packages/bitmex-trio-websocket/bitmex_trio_websocket-0.2.8-py3-none-any.whl/bitmex_trio_websocket/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for BitMEX Trio-Websocket."""
+
+from typing import Union, Iterable
+from .websocket import connect
+from .storage import Storage
+from trio_websocket import WebSocketConnection
+
+__author__ = """Anders Ellenshøj Andersen"""
+__email__ = 'andersa@atlab.dk'
+__version__ = '0.2.8'
+
+class BitMEXWebsocket:
+    def __init__(self, endpoint: str, symbol: Union[str, Iterable[str]]=None, api_key: str=None, api_secret: str=None):
+        self.endpoint = endpoint
+        self.symbol = symbol
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.storage = Storage()
+        self.ws = None
+    
+    async def start(self):
+        stream  = self.storage.process(connect(self.endpoint, self.symbol, self.api_key, self.api_secret))
+        self.ws = await stream.asend(None)
+        async for message in stream:
+            yield message
+    
