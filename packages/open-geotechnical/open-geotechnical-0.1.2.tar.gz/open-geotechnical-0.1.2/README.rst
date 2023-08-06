74cf5d1657a4795edc5691a5460ded31009eb445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:22 +0000

    Add package open-geotechnical/0.1.2/open-geotechnical-0.1.2.tar.gz

---
+++ packages/open-geotechnical/open-geotechnical-0.1.2.tar.gz/open-geotechnical-0.1.2/README.rst
@@ -0,0 +1,12 @@
+ogt-py = Open GeoTechnical in python
+=========================================
+
+- Docs at https://open-geotechnical.gitlab.io/ogt-py/
+
+Install
+===========================
+
+- see https://open-geotechnical.gitlab.io/ogt-py/install.html
+
+
+
---
+++ packages/open-geotechnical/open-geotechnical-0.1.2.tar.gz/open-geotechnical-0.1.2/setup.py
@@ -0,0 +1,53 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+with open("version.txt", "r") as fh:
+    version = fh.read()
+
+setuptools.setup(
+    name="open-geotechnical", # Replace with your own username
+    version=version,
+
+    author="Pedro Morgan",
+    author_email="ogt@daffodil.uk.com",
+
+    description="Open Geotechnical",
+    long_description=long_description,
+
+    url="https://open-geotechnical.gitlab.io/ogt_py/",
+    project_urls={
+        "Bug Tracker": "https://gitlab.com/open-geotechnical/ogt_py/issues",
+        "Documentation": "https://open-geotechnical.gitlab.io/ogt_py/",
+        "Source Code": "https://gitlab.com/open-geotechnical/ogt_py",
+    },
+
+    packages=setuptools.find_packages(exclude="docs"),
+    include_package_data=True,
+    #package_data={
+    #    '': ['*.png', '*.md', "*.html", "*.ini"],
+    #},
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+
+    python_requires='>=3.4',
+    install_requires=[
+       'mistune',
+       'openpyxl',
+        "pyyaml",
+        "geojson",
+        "flask"
+    ],
+
+    scripts=[
+        'ogt-cli.py',
+        'ogt-desktop.py',
+        'ogt-server.sh'
+    ],
+
+)
