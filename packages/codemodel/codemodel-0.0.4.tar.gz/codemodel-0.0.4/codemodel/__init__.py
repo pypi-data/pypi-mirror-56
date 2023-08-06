1920d9943aa33ca8800e32df8d4c3be5bf491caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:16 +0000

    Add package codemodel/0.0.4/codemodel-0.0.4.tar.gz

---
+++ packages/codemodel/codemodel-0.0.4.tar.gz/codemodel-0.0.4/codemodel/__init__.py
@@ -0,0 +1,20 @@
+# from . import type_validation
+
+from . import asttools
+from .code_model import CodeModel, Instance
+from .json_stack import Parameter, Package, load_json
+from .generate_json import make_package, codemodel_from_callable
+from . import dag
+
+try:
+    from . import version
+except ImportError:  # pragma: no cover
+    from . import _version as version
+
+# docstring helpers aren't required
+try:
+    import numpydoc
+except ImportError:
+    pass
+else:
+    from . import numpydoc_helper
