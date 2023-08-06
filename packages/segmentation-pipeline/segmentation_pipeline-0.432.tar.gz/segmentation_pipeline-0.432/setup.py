c12b2e9be51f8a9fc59c136b28647e56b84cb0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:41 +0000

    Add package segmentation-pipeline/0.432/segmentation_pipeline-0.432.tar.gz

---
+++ packages/segmentation-pipeline/segmentation_pipeline-0.432.tar.gz/segmentation_pipeline-0.432/setup.py
@@ -0,0 +1,25 @@
+import io
+import os
+import sys
+
+from setuptools import setup
+import setuptools
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+setup(name='segmentation_pipeline',
+      version='0.432',
+      description='Segmentation support piepeline for Musket ML',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/musket-ml/segmentation_training_pipeline',
+      author='Petrochenko Pavel',
+      author_email='petrochenko.pavel.a@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      include_package_data=True,
+      #dependency_links=['https://github.com/aleju/imgaug'],
+      install_requires=["musket_core"],
+      zip_safe=False)
