d1269aa3f988e0e92d7d3fd991319849289ed6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:55 +0000

    Add package gsat/0.1a4/gsat-0.1a4.tar.gz

---
+++ packages/gsat/gsat-0.1a4.tar.gz/gsat-0.1a4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gsat", 
+    version="0.1a4",
+    author="Michael J. Axtell",
+    author_email="mja18@psu.edu",
+    description="General Small RNA-seq Analysis Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MikeAxtell/gsat",
+    scripts=['bin/gsat'],
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>3.5, <4',
+    install_requires=['biopython','python-Levenshtein'],
+)
