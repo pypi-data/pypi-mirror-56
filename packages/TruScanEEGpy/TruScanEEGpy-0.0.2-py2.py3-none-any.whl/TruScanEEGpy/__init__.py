2500bd9693c69f8c3497714c2f29f6b8de1bc267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:19 +0000

    Add package TruScanEEGpy/0.0.2/TruScanEEGpy-0.0.2-py2.py3-none-any.whl

---
+++ packages/TruScanEEGpy/TruScanEEGpy-0.0.2-py2.py3-none-any.whl/TruScanEEGpy/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for TruScanEEGpy."""
+
+# =============================================================================
+# Basic
+# =============================================================================
+import os
+
+__author__ = """TruScanEEGpy developpment team"""
+__email__ = 'dom.makowski@gmail.com'
+__version__ = '0.0.2'
+__modulepath__ = os.path.split(__file__)[0]
+
+
+
+
+# =============================================================================
+# Imports
+# =============================================================================
+from .layout import *
+from .montage import *
