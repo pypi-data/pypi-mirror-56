2a3a12eb45aa712b90c62f9f85fb7feaa99c2564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:16 +0000

    Add package fsds-100719/0.4.43/fsds_100719-0.4.43.tar.gz

---
+++ packages/fsds-100719/fsds_100719-0.4.43.tar.gz/fsds_100719-0.4.43/fsds_100719/ft/__init__.py
@@ -0,0 +1,12 @@
+"""A collection of submodules by online-ds-ft-100719.
+Maintained by James Irving (GitHub: jirvingphd)
+james.irving@flatironschool.com
+"""
+# from ..imports import *
+# from fsds_100719.ft import *
+
+# from ..ft import *
+import fsds_100719.ft.hakkeray
+import fsds_100719.ft.jirving
+# def placeholder():
+#     pass
