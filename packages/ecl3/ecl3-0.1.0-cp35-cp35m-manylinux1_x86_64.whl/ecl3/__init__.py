fa869a3758c3d253b1721f11738f54505f8e457e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:51 +0000

    Add package ecl3/0.1.0/ecl3-0.1.0-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/ecl3/ecl3-0.1.0-cp35-cp35m-manylinux1_x86_64.whl/ecl3/__init__.py
@@ -0,0 +1,11 @@
+try:
+    import pkg_resources
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:
+    pass
+
+from . import summary
+
+__all__ = [
+    'summary',
+]
