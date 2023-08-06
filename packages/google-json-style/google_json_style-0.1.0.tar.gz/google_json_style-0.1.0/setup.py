eafb5afa76946d210eca5bef977fc58fcdfdf703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:51 +0000

    Add package google-json-style/0.1.0/google_json_style-0.1.0.tar.gz

---
+++ packages/google-json-style/google_json_style-0.1.0.tar.gz/google_json_style-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='google_json_style',
+    version='0.1.0',
+    packages=['google_json_style'],
+    url='https://gitlab.com/dmantis',
+    license='MIT',
+    author='dmitry',
+    author_email='dmitry.mantis@protonmail.com',
+    description='Create Google JSON Style response bodies'
+)
