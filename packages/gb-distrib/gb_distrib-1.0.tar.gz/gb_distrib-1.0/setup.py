4efbe62c43ef9852cc5abcd2aea4f6319b912ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:20 +0000

    Add package gb-distrib/1.0/gb_distrib-1.0.tar.gz

---
+++ packages/gb-distrib/gb_distrib-1.0.tar.gz/gb_distrib-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gb_distrib',
+         version='1.0',
+         description='Gaussian and Binomial Distributions',
+         packages=['gb_distrib'],
+         author='Andrei Ungureanu via Udacity',
+         zip_safe=False)
