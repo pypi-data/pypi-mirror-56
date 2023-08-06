8f9b66f08e499d26e9d94734993e1d750667dd20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:16 +0000

    Add package vcfparser/0.1.11/vcfparser-0.1.11.tar.gz

---
+++ packages/vcfparser/vcfparser-0.1.11.tar.gz/vcfparser-0.1.11/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+from setuptools import Extension
+from Cython.Build import cythonize
+
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+# extensions = [Extension(['vcfparser/*.pyx']]
+
+setup(
+    author="Kiran Bishwa",
+    author_email='everestial007@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    description="Minimaistic VCf parser in python",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme ,
+    include_package_data=True,
+    keywords='vcfparser',
+    name='vcfparser',
+    packages=find_packages(include=['vcfparser']),
+    
+    ext_modules = cythonize('**/*.pyx', language_level = '3'),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/everestial/vcfparser',
+    version='0.1.11',
+    zip_safe=False,
+)
+
