1b664ffae41a08758cd40aba42df3d4e74b56abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:10 +0000

    Add package Auptimizer/1.1/Auptimizer-1.1-py2-none-any.whl

---
+++ packages/Auptimizer/Auptimizer-1.1-py2-none-any.whl/aup/__init__.py
@@ -0,0 +1,9 @@
+"""
+Copyright (c) 2018 LG Electronics Inc.
+SPDX-License-Identifier: GPL-3.0-or-later
+"""
+from .EE.Experiment import Experiment
+from .aup import BasicConfig
+from .aup import print_result
+
+__version__ = "1.1"
