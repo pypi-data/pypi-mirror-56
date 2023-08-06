11b816ad225d0bf89e0d795b9a52dac948966fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:26 +0000

    Add package cana-asteroids/0.1/cana_asteroids-0.1-py3-none-any.whl

---
+++ packages/cana-asteroids/cana_asteroids-0.1-py3-none-any.whl/cana/__init__.py
@@ -0,0 +1,5 @@
+from .spec import loadspec, Spectrum, stack_spec
+from . import datasets
+from . import spectools
+from .spectools import slope, Slope, taxonomy, Taxonomy, \
+                       depth, Depth, Continuum
---
+++ packages/cana-asteroids/cana_asteroids-0.1-py3-none-any.whl/cana/spectools/__init__.py
@@ -0,0 +1,5 @@
+# pylint: disable=missing-docstring,import-outside-toplevel
+from .uncertainties import SpecError
+from .slopetool import slope, Slope
+from .taxonomytool import taxonomy, Taxonomy
+from .specband import depth, Depth, Continuum
