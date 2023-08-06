253a56eca9a96cc74afd1a72e7a5df512eee66b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:07 +0000

    Add package touch-callable/0.0.4/touch-callable-0.0.4.tar.gz

---
+++ packages/touch-callable/touch-callable-0.0.4.tar.gz/touch-callable-0.0.4/touch_callable/front-end/build/precache-manifest.a347dd9a5f8cdacafb3ba961b878083a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a0b4821796113a399fa3671d47a588",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c9098500f7e607c602d",
+    "url": "/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "24d43566d06906978294",
+    "url": "/static/css/main.6e6e8990.chunk.css"
+  },
+  {
+    "revision": "0c9098500f7e607c602d",
+    "url": "/static/js/2.b7a65e9f.chunk.js"
+  },
+  {
+    "revision": "24d43566d06906978294",
+    "url": "/static/js/main.8caa01fa.chunk.js"
+  },
+  {
+    "revision": "a4a2c54e6b5e2d190c40",
+    "url": "/static/js/runtime~main.e543b2a5.js"
+  },
+  {
+    "revision": "3dedce739d214a926193692b6ac34be3",
+    "url": "/static/media/logo.3dedce73.svg"
+  },
+  {
+    "revision": "edef1c3c214e77394ac6ed8f382be54b",
+    "url": "/static/media/touch-callable-text.edef1c3c.svg"
+  }
+]);
