50eaeb5de8cf0570dbbccd325fe107aa68a05117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:24 +0000

    Add package societeinfo/0.1.0/societeinfo-0.1.0.tar.gz

---
+++ packages/societeinfo/societeinfo-0.1.0.tar.gz/societeinfo-0.1.0/CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.1.0
+
+**Minor:**
+
+- First version, including calls to get credits details, autocomplete companies, and get a company by its identifier.
---
+++ packages/societeinfo/societeinfo-0.1.0.tar.gz/societeinfo-0.1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_namespace_packages, setup
+
+
+setup(
+    name='societeinfo',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    description='Python wrapper to SOCIETE INFO api.',
+    author='Pierre Kephas',
+    url='https://gitlab.com/PierreKephas/societeinfo-python',
+    packages=find_namespace_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    keywords=[
+    ],
+    install_requires=[
+        'requests>=2,<3',
+    ],
+    extras_require={
+        'dev': [
+            'flake8',
+            'flake8-import-order',
+            'pytest',
+            'pytest-cov',
+            'pytest-xdist',
+            'requests-mock',
+        ],
+    },
+)
