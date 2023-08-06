a02954d86c1be68c0d2d1b4288486e89c682be92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:18 +0000

    Add package lyricsprocessor/0.1.4/lyricsprocessor-0.1.4.tar.gz

---
+++ packages/lyricsprocessor/lyricsprocessor-0.1.4.tar.gz/lyricsprocessor-0.1.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+setup(
+    name='lyricsprocessor',
+    packages = ['lyricsprocessor'], 
+    python_requires='>=3.7',
+    version='0.1.4',
+    description='Lyrics processing code',
+    long_description=readme(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    keywords='lyricsgenius genius lyrics',
+    url='https://github.com/Mitchwatts93/lyrics_generator',
+    author='https://github.com/Mitchwatts93',
+    author_email='',
+    license='MIT',
+    install_requires = [
+    'pandas>=0.23.4',
+    'argparse>=1.1',
+    're',
+    'json',
+    'lyricsgenius>=1.8.0'
+        ],
+    include_package_data=True,
+    test_suite='nose.collector',
+    tests_require=['nose']
+    )
