29bc7a67da7d47a1fdb90c89334010e1dabfd03b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:02 +0000

    Add package py-ecg-detectors/1.0.0/py-ecg-detectors-1.0.0.tar.gz

---
+++ packages/py-ecg-detectors/py-ecg-detectors-1.0.0.tar.gz/py-ecg-detectors-1.0.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='py-ecg-detectors',
+    version='1.0.0',
+    description="Seven ECG heartbeat detection algorithms and heartrate variability analysis",
+    long_description=long_description,
+    author='Luis Howell, Bernd Porr',
+    author_email='luisbhowell@gmail.com, bernd.porr@glasgow.ac.uk',
+    py_modules=['ecgdetectors','hrv'],
+    include_package_data=True,
+    install_requires=['numpy',
+                      'pathlib2;python_version<"3.4"',
+                      'scipy',
+                      'gatspy',
+                      'pywavelets'],
+    zip_safe=False,
+    url='https://github.com/berndporr/py-ecg-qrs-detectors',
+    license='GPL 3.0',
+    data_files=[('templates',['templates/template_250hz.csv','templates/template_360hz.csv'])],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+)
