f499d492422175dc1006b3df3a279fa73b7a37c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:40 +0000

    Add package featurize-package/0.0.20/featurize-package-0.0.20.tar.gz

---
+++ packages/featurize-package/featurize-package-0.0.20.tar.gz/featurize-package-0.0.20/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+setuptools.setup(
+    name='featurize-package',
+    description='Official packages for featurize.',
+    version='0.0.20',
+    packages=setuptools.find_packages(),
+    url="https://github.com/louis-she/featurize-package",
+    author='louis',
+    author_email='chenglu.she@gmail.com',
+    keywords='pytorch minecraft',
+    include_package_data=True,
+    install_requires=[
+        'featurize-jupyterlab',
+        'torchvision',
+        'albumentations',
+        'torch',
+        'pandas',
+        'numpy',
+        'opencv-python',
+        'scikit-learn',
+        'pretrainedmodels',
+        'kaggle'
+    ],
+)
