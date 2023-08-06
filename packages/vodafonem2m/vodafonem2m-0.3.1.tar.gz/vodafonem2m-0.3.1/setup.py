4331d187f64fd4ee4b21d6a59cb5ffd0008698d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:56 +0000

    Add package vodafonem2m/0.3.1/vodafonem2m-0.3.1.tar.gz

---
+++ packages/vodafonem2m/vodafonem2m-0.3.1.tar.gz/vodafonem2m-0.3.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'vodafonem2m',
+  packages = ['vodafonem2m'],
+  version = '0.3.1',
+  license='GNU GENERAL PUBLIC LICENSE',
+  description = 'Provides access to Vodafone M2M endpoints via the REST API.',
+  author = 'James K Bowler',
+  author_email = 'james.bowler@datacentauri.com',
+  url = 'https://github.com/JamesKBowler/vodafonem2m',
+  download_url = 'https://github.com/JamesKBowler/vodafonem2m/archive/0.1.tar.gz',
+  keywords = ['Vodafone M2M Python API', 'Vodafone M2M', 'Python M2M'],
+  install_requires=[
+        'requests'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
