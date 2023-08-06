ba63f19e007991c68bbc01165ce194760c66a375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:48 +0000

    Add package jdTranslationHelper/2.0/jdTranslationHelper-2.0.tar.gz

---
+++ packages/jdTranslationHelper/jdTranslationHelper-2.0.tar.gz/jdTranslationHelper-2.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from setuptools import find_packages, setup
+
+with open("README.md","r",encoding="utf-8") as f:
+    description = f.read()
+
+setup(name='jdTranslationHelper',
+    version='2.0',
+    description='A simple API for translating your programs',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author='JakobDev',
+    author_email='jakobdev@gmx.de',
+    url='https://gitlab.com/JakobDev/jdTranslationHelper',
+    include_package_data=True,
+    packages=find_packages(),
+    license='BSD',
+    keywords=['JakobDev','translation','localization'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Environment :: Other Environment',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: OS Independent',
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+ )
