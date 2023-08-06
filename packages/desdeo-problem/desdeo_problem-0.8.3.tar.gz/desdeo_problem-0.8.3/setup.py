bcf9fc5bcc53de615f6e4ac0b25d25a8e8acf926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:03 +0000

    Add package desdeo-problem/0.8.3/desdeo_problem-0.8.3.tar.gz

---
+++ packages/desdeo-problem/desdeo_problem-0.8.3.tar.gz/desdeo_problem-0.8.3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['desdeo_problem', 'desdeo_problem.testproblems']
+
+package_data = \
+{'': ['*'], 'desdeo_problem': ['surrogatemodels/*']}
+
+install_requires = \
+['diversipy>=0.8.0,<0.9.0',
+ 'numpy>=1.17,<2.0',
+ 'optproblems>=1.2,<2.0',
+ 'pandas>=0.25.1,<0.26.0',
+ 'scikit-learn>=0.21.3,<0.22.0']
+
+setup_kwargs = {
+    'name': 'desdeo-problem',
+    'version': '0.8.3',
+    'description': 'This package contains the problem classes for desdeo framework.',
+    'long_description': None,
+    'author': 'Bhupinder Saini',
+    'author_email': 'bhupinder.s.saini@jyu.fi',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
