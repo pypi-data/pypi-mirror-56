d67a44bf0fae67828ea4b46a3fa68027b8287c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:40 +0000

    Add package testwizard.testobjects-core/3.1.11/testwizard.testobjects-core-3.1.11.tar.gz

---
+++ packages/testwizard.testobjects-core/testwizard.testobjects-core-3.1.11.tar.gz/testwizard.testobjects-core-3.1.11/setup.py
@@ -0,0 +1,48 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testwizard.testobjects-core",
+    version="3.1.11",
+    author="Eurofins Digital Testing - Belgium",
+    author_email="support-be@eurofins.com",
+    description="Testwizard core components for testobjects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['testwizard.testobjects_core'],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
