41d240cab7076369615785417221334776221847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:41 +0000

    Add package coding-test-foo/1.8.0/coding-test-foo-1.8.0.tar.gz

---
+++ packages/coding-test-foo/coding-test-foo-1.8.0.tar.gz/coding-test-foo-1.8.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='coding-test-foo',
+    packages=['bar'],
+    description='funny foo',
+    version='1.8.0',
+    url='http://github.com/user/foo',
+    author='ericchan77',
+    author_email='1305584643@qq.com'
+)
