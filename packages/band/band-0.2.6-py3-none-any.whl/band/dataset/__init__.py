868d826ef837045a9df575c620a9dd48fd3d5740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:51 +0000

    Add package band/0.2.6/band-0.2.6-py3-none-any.whl

---
+++ packages/band/band-0.2.6-py3-none-any.whl/band/dataset/__init__.py
@@ -0,0 +1,11 @@
+"""
+@author: SunYanCN
+@contact: sunyanhust@163.com
+@blog: https://sunyancn.github.io
+@version: 1.0
+@license: MIT Licence
+@file: __init__.py
+@time: 2019-11-24 21:24:09
+"""
+
+from .masa_ner import MSRA_NER
