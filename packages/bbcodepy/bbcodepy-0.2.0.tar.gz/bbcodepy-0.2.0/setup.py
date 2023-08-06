3784bd0a7995bb5357ca8d512116eaf65c996461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:41 +0000

    Add package bbcodepy/0.2.0/bbcodepy-0.2.0.tar.gz

---
+++ packages/bbcodepy/bbcodepy-0.2.0.tar.gz/bbcodepy-0.2.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = "Python Library for rendering BBCode."
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
+VERSION = '0.2.0'
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+setup(name='bbcodepy',
+    version=VERSION,
+    packages=find_packages(),
+    author='Stanislav Vishnevskiy',
+    author_email='vishnevskiy@gmail.com',
+    url='https://github.com/vishnevskiy/bbcodepy',
+    license='MIT',
+    include_package_data=True,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    platforms=['any'],
+    classifiers=CLASSIFIERS,
+    test_suite='tests',
+)
