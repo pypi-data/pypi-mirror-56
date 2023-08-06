20e089671a3cc8b414aed55761dea93d920424c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:46 +0000

    Add package tweetpy-janmarkuslanger/0.0.3/tweetpy-janmarkuslanger-0.0.3.tar.gz

---
+++ packages/tweetpy-janmarkuslanger/tweetpy-janmarkuslanger-0.0.3.tar.gz/tweetpy-janmarkuslanger-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    dependencies = f.read().splitlines()
+
+setuptools.setup(
+    name="tweetpy-janmarkuslanger",
+    version="0.0.3",
+    author="Jan-Markus Langer",
+    author_email="janmarkuslanger10121994@gmail.com",
+    description="Create your own twitter bot without the api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/janmarkuslanger/tweetpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=dependencies,
+    python_requires='>=3.6',
+)
