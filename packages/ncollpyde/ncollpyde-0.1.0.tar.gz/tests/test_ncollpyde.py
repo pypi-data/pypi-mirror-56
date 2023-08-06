f20d51996653c4a25a50d0054be563bc9f83b9dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:57 +0000

    Add package ncollpyde/0.1.0/ncollpyde-0.1.0.tar.gz

---
+++ packages/ncollpyde/ncollpyde-0.1.0.tar.gz/tests/test_ncollpyde.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `ncollpyde` package."""
+
+import pytest
+import numpy as np
+
+from ncollpyde import Volume
+
+
+points_expected = [
+    ([-2.3051376, -4.1556454,  1.9047838], True),  # internal
+    ([-0.35222054, -0.513299, 7.6191354], False),  # external but in AABB
+    ([0.13970017, 0.0928266, 4.7073355], True),  # vertex
+    ([10, 10, 10], False),  # out of AABB
+]
+
+
+@pytest.mark.parametrize(["point", "expected"], points_expected)
+def test_single(mesh, point, expected):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    vol = Volume(*mesh)
+    assert (point in vol) == expected
+
+
+def test_corner(mesh):
+    vol = Volume(*mesh)
+    assert mesh[0][0] in vol
+
+
+def test_many(mesh):
+    points = []
+    expected = []
+    for p, e in points_expected:
+        points.append(p)
+        expected.append(e)
+
+    vol = Volume(*mesh)
+    assert np.array_equal(vol.contains(points), expected)
