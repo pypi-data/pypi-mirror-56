5d458309f962a72c657b2ad58aa0e0293983a21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:19 +0000

    Add package hid/1.0.4/hid-1.0.4.tar.gz

---
+++ packages/hid/hid-1.0.4.tar.gz/hid-1.0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+
+version = '1.0.4'
+
+setup(
+    name='hid',
+    version=version,
+    description='ctypes bindings for hidapi',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='',
+    author='Austin Morton',
+    author_email='amorton@juvsoft.com',
+    url='https://github.com/apmorton/pyhidapi',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    test_suite='nose.collector'
+)
