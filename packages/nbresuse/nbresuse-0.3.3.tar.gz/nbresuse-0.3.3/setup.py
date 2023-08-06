dd9b16ded2c68b9b2d335acdf826a59bf5c8f6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:56 +0000

    Add package nbresuse/0.3.3/nbresuse-0.3.3.tar.gz

---
+++ packages/nbresuse/nbresuse-0.3.3.tar.gz/nbresuse-0.3.3/setup.py
@@ -0,0 +1,22 @@
+from glob import glob
+import setuptools
+
+setuptools.setup(
+    name="nbresuse",
+    version='0.3.3',
+    url="https://github.com/yuvipanda/nbresuse",
+    author="Yuvi Panda",
+    description="Simple Jupyter extension to show how much resources (RAM) your notebook is using",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'psutil',
+        'notebook',
+    ],
+    data_files=[
+        ('share/jupyter/nbextensions/nbresuse', glob('nbresuse/static/*')),
+        ('etc/jupyter/jupyter_notebook_config.d', ['nbresuse/etc/serverextension.json']),
+        ('etc/jupyter/nbconfig/notebook.d', ['nbresuse/etc/nbextension.json'])
+    ],
+    zip_safe=False,
+    include_package_data=True
+)
