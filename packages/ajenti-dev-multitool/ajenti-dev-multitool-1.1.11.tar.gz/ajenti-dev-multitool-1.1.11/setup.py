20601164a5ede32c64d6a7bbc1ae7c3c5f09ff46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:06 +0000

    Add package ajenti-dev-multitool/1.1.11/ajenti-dev-multitool-1.1.11.tar.gz

---
+++ packages/ajenti-dev-multitool/ajenti-dev-multitool-1.1.11.tar.gz/ajenti-dev-multitool-1.1.11/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='ajenti-dev-multitool',
+    version='1.1.11',
+    install_requires=[
+        'coloredlogs',
+        'pyyaml',
+        'gevent',
+    ],
+    description='-',
+    author='Eugene Pankov',
+    author_email='e@ajenti.org',
+    url='http://ajenti.org/',
+    scripts=['ajenti-dev-multitool'],
+)
