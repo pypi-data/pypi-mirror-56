9a896a82c985d71191d3e20215dc2c16f911ba2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:58 +0000

    Add package 6D657461666C6F77/0.0.10/6D657461666C6F77-0.0.10.tar.gz

---
+++ packages/6D657461666C6F77/6D657461666C6F77-0.0.10.tar.gz/6D657461666C6F77-0.0.10/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+version = '0.0.10'
+
+setup(name='6D657461666C6F77',
+      version=version,
+      description='6D657461666C6F77 is a microframework for Data Science projects',
+      author='Machine Learning Infrastructure team',
+      author_email='mli@netflix.com',
+      license='Apache License 2.0',
+      packages=find_packages(exclude=['metaflow_test']),
+      py_modules=['metaflow', ],
+      package_data={'metaflow' : ['examples/*/*']},
+      entry_points='''
+        [console_scripts]
+        metaflow=metaflow.main_cli:main
+      ''',
+      install_requires = [
+        'click',
+        'requests',
+        'boto3'
+      ],
+      tests_require = [
+        'coverage'
+      ])
