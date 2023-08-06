4c7635e2fe1e8f80fb805d074a751b26ef7ec651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:55 +0000

    Add package django-loose-fk/0.6.0/django_loose_fk-0.6.0-py2.py3-none-any.whl

---
+++ packages/django-loose-fk/django_loose_fk-0.6.0-py2.py3-none-any.whl/testapp/api.py
@@ -0,0 +1,67 @@
+from django_filters.rest_framework.backends import DjangoFilterBackend
+from django_filters.rest_framework.filterset import FilterSet
+from rest_framework import routers, serializers, viewsets
+
+from .models import Zaak, ZaakObject, ZaakType
+
+# Serializers
+
+
+class ZaakTypeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ZaakType
+        fields = ("url", "name")
+
+
+class ZaakSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Zaak
+        fields = ("url", "zaaktype", "name")
+
+
+class ZaakObjectSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ZaakObject
+        fields = ("url", "zaak", "name")
+
+
+# Filters
+
+
+class ZaakFilterSet(FilterSet):
+    class Meta:
+        model = Zaak
+        fields = ("zaaktype",)
+
+
+# Viewsets
+
+
+class NoAuthMixin:
+    def perform_authentication(self, request):
+        pass
+
+
+class ZaakTypeViewSet(NoAuthMixin, viewsets.ModelViewSet):
+    queryset = ZaakType.objects.all()
+    serializer_class = ZaakTypeSerializer
+
+
+class ZaakViewSet(NoAuthMixin, viewsets.ModelViewSet):
+    queryset = Zaak.objects.all()
+    serializer_class = ZaakSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = ZaakFilterSet
+
+
+class ZaakObjectViewSet(NoAuthMixin, viewsets.ModelViewSet):
+    queryset = ZaakObject.objects.all()
+    serializer_class = ZaakObjectSerializer
+
+
+# URL routing
+
+router = routers.DefaultRouter()
+router.register("zaaktypes", ZaakTypeViewSet)
+router.register("zaken", ZaakViewSet)
+router.register("zaakobjecten", ZaakObjectViewSet)
