81fab62eeec6dc054675894e58707870e1bb3bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:16 +0000

    Add package interspace/0.0.3/interspace-0.0.3.tar.gz

---
+++ packages/interspace/interspace-0.0.3.tar.gz/interspace-0.0.3/src/interspace.py
@@ -0,0 +1,16 @@
+# calculate the Euclidean distance between two vectors
+def euclidean(vector_1, vector_2):
+    distance = 0.0
+    for i in range(len(vector_1)):
+            distance += (vector_1[i] - vector_2[i])**2 
+    return (distance)**0.5
+
+def distance(vector_1, vector_2):
+    if len(vector_1) != len(vector_2):
+        return "Vector lengths mismatch"
+    
+    distances = {}
+    
+    distances['Euclidean'] = euclidean(vector_1, vector_2)
+        
+    return distances
