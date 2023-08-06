84817ac152ef538ff1d0dbe7550972e177ab755a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:30 +0000

    Add package webvis/0.0.4/webvis-0.0.4.tar.gz

---
+++ packages/webvis/webvis-0.0.4.tar.gz/webvis-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'webvis',
+    packages = find_packages(),
+    version = '0.0.4',
+    description = 'A live two-way data binder for python',
+    author = 'Danil Lykov',
+    author_email = 'lkvdan@gmail.com',
+    url = 'https://github.com/DaniloZZZ/pywebviz',
+    install_requires=['matplotlib', 'numpy', 'requests',
+                      'mpld3','trio','trio-websocket'],
+    python_requires='>=3.3',
+    include_package_data=True,
+    license='MIT',
+    keywords = ['tools', 'data', 'framework', 'visualization'], # arbitrary keywords
+    classifiers = [],
+)
---
+++ packages/webvis/webvis-0.0.4.tar.gz/webvis-0.0.4/webvis/websocket/ws_server.py
@@ -0,0 +1,48 @@
+import trio
+from trio_websocket import serve_websocket, ConnectionClosed
+from logging import getLogger
+from .message_gen import message_gen
+
+log = getLogger("Websocket")
+
+class StopServer(Exception):
+    pass
+
+async def ws_serve(addr, port, iterable_fn, on_connect=lambda x:None, nursery=None):
+    async def server(request):
+
+        ws = await request.accept()
+        on_connect(ws)
+
+        async for message in iterable_fn(message_gen(ws)):
+            try:
+                await ws.send_message(message)
+            except ConnectionClosed:
+                log.warning("Connection closed")
+                break
+
+    await serve_websocket(server, addr, port, ssl_context=None, handler_nursery=nursery)
+    log.info("Websocket terminates")
+
+def start_server(addr, port, handler_func=print, on_connect=lambda x: None):
+    log.info(f"Starting ws server at {addr}:{port}")
+    try:
+        trio.run(ws_serve, addr, port, handler_func, on_connect)
+    except ConnectionClosed as e:
+        log.warning(f"Connection closed: {e}")
+        return
+
+def start_server_old(addr, port, handler_func):
+    async def handler(client_messages):
+        async for message in client_messages:
+            yield str(handler_func(message))
+    start_server(addr, port, handler)
+
+def stop():
+    print("You found a stub!")
+
+def main():
+    start_server('127.0.0.1',8000)
+
+if __name__=='__main__':
+    main()
