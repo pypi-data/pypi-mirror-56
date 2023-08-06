79a5ce7bdce56d9f660f28cd908a9dbf0aae9a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:41 +0000

    Add package pylockfile/0.0.3.3/pylockfile-0.0.3.3.tar.gz

---
+++ packages/pylockfile/pylockfile-0.0.3.3.tar.gz/pylockfile-0.0.3.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+      name='pylockfile',
+      version='0.0.3.3',
+      packages=find_packages(),
+      author='Kusakin Anton @iiiparadoxxxiii',
+      author_email='iiiparadoxxxiii@gmail.com',
+      url='https://github.com/iiiparadoxxxiii/lockfile',
+      description="Создание файла блокировки, для предотвращения выполнения нескольких копий скрипта",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      )
