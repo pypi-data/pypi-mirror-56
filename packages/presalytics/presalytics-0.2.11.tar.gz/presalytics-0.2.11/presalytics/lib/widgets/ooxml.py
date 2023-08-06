02d8d929c9691679f8eb79fe3985544d3f8fc755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:59 +0000

    Add package presalytics/0.2.11/presalytics-0.2.11.tar.gz

---
+++ packages/presalytics/presalytics-0.2.11.tar.gz/presalytics-0.2.11/presalytics/lib/widgets/ooxml.py
@@ -0,0 +1,17 @@
+import presalytics.story.components
+import presalytics.client
+
+# class OoxmlWidget(presalytics.story.components.WidgetBase):
+#     __component_kind__ = 'ooxml'
+
+#     __plugins__ = [
+#         {
+#             'name': 'external_scripts',
+#             'type': 'script',
+#             'config': {
+#                 'approved_scripts_key': 'ooxml'
+#         }
+#     ]
+
+#     def to_html(self, **kwargs):
+        
