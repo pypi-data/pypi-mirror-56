52ad605e5e3b7fc6ab2427da095fb7d1eb4ab576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:06 +0000

    Add package testone/0.0.1/testone-0.0.1.tar.gz

---
+++ packages/testone/testone-0.0.1.tar.gz/testone-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='testone',
+    version='0.0.1',
+    py_modules=['testone'],
+    author='wupunci',
+    author_email='302538094@qq.com',
+    url="http://www.wupunci.com",
+    description='A simple printer of nester lists',
+    )
---
+++ packages/testone/testone-0.0.1.tar.gz/testone-0.0.1/testone.py
@@ -0,0 +1,11 @@
+def print_lol2(the_list,level):
+    """这个函数有两个参数，一个是任何python列表
+    一个是参数level，用来遇到嵌套列表时插入制表符"""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol2(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(each_item)
+            #这又是注释
