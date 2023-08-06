2e23a09bedfa69a3ab17550b488ea082068107ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:40 +0000

    Add package pty_good/1.0/pty_good-1.0.tar.gz

---
+++ packages/pty_good/pty_good-1.0.tar.gz/pty_good-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+name='pty_good', # 对外我们模块的名字
+version='1.0', # 版本号
+description='这是第一个对外发布的模块，测试哦', #描述
+author='pty', # 作者
+author_email='2294776022@qq.com', py_modules=['pty_programming.demo1','pty_programming.demo2'] # 要发布的模块
+)
