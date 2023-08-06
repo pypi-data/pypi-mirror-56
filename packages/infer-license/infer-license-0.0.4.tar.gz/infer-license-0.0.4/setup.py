9a03e04eabdb1c60ce64afffadbed84f09cbd2f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:11 +0000

    Add package infer-license/0.0.4/infer-license-0.0.4.tar.gz

---
+++ packages/infer-license/infer-license-0.0.4.tar.gz/infer-license-0.0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    use_scm_version=True,
+    install_requires=["dataclasses >= 0.7; python_version < '3.7'",],
+    entry_points={"console_scripts": ["infer_license = infer_license.cmdline:main"]},
+    include_package_data=True,
+)
