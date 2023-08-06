d171774ffe1d94fad38c5deaacd45cd9d6cfe072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:30 +0000

    Add package GooseHDF5/0.1.1/GooseHDF5-0.1.1-py3-none-any.whl

---
+++ packages/GooseHDF5/GooseHDF5-0.1.1-py3-none-any.whl/GooseHDF5/cli/G5check.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+'''G5check
+  Try reading datasets. In case of reading failure the path is printed (otherwise nothing is
+  printed).
+
+Usage:
+  G5check <source> [options]
+
+Arguments:
+  <source>        HDF5-file.
+
+Options:
+  -b, --basic     Only try getting a list of datasets, skip trying to read them.
+  -h, --help      Show help.
+      --version   Show version.
+
+(c - MIT) T.W.J. de Geus | tom@geus.me | www.geus.me | github.com/tdegeus/GooseHDF5
+'''
+
+# ==================================================================================================
+
+# temporary fix: suppress warning from h5py
+import warnings
+warnings.filterwarnings("ignore")
+
+import numpy as np
+import sys
+import os
+import re
+import h5py
+import docopt
+import pkg_resources
+
+__version__ = pkg_resources.require("GooseHDF5")[0].version
+
+from .. import *
+
+# ==================================================================================================
+
+def check_isfile(fname):
+  r'''
+Check if a file exists, quit otherwise.
+  '''
+
+  if not os.path.isfile(fname):
+    raise IOError('"{0:s}" does not exist'.format(fname))
+
+# ==================================================================================================
+
+def read(filename, check):
+  r'''
+Read (and check) all datasets
+  '''
+
+  with h5py.File(filename, 'r') as source:
+
+    paths = getdatasets(source)
+
+    if check:
+      verify(source, paths, error=True)
+
+# ==================================================================================================
+
+def main():
+
+  # parse command-line options
+  args = docopt.docopt(__doc__, version=__version__)
+
+  # check that file exists
+  check_isfile(args['<source>'])
+
+  read(args['<source>'], not args['--basic'])
+
+  # read datasets
+  try:
+    read(args['<source>'], not args['--basic'])
+  except:
+    print(args['<source>'])
