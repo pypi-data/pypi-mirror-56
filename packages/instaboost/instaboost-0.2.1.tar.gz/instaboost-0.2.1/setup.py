92cc811d744288681e9fb59c72ae717143c14cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:29 +0000

    Add package instaboost/0.2.1/instaboost-0.2.1.tar.gz

---
+++ packages/instaboost/instaboost-0.2.1.tar.gz/instaboost-0.2.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='instaboost',
+    version='0.2.1',
+    description='a python implementation for paper: "InstaBoost: Boosting Instance Segmentation Via Probability Map GuidedCopy-Pasting"',
+    author='Haoshu Fang, Jianhua Sun, Runzhong Wang,  Minghao Gou, Yonglu Li, Cewu Lu',
+    author_email='1751196720@qq.com',
+    url='https://github.com/GothicAi/InstaBoost-pypi',
+    packages=find_packages(),
+    install_requires=[
+      'numpy',
+      'opencv-python',
+      'scipy',
+      'Cython',
+      'pycocotools',
+      'matplotlib',
+      'Pillow',
+      'scikit-image',
+      'opencv-mat'
+    ]
+)
