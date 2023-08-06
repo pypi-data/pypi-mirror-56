bc452974cb9173b1652efcc9378e2a1d6e90f3ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:45 +0000

    Add package intuitive/0.5.0/intuitive-0.5.0.tar.gz

---
+++ packages/intuitive/intuitive-0.5.0.tar.gz/intuitive-0.5.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['intuitive']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'intuitive',
+    'version': '0.5.0',
+    'description': '',
+    'long_description': None,
+    'author': 'John Smith',
+    'author_email': 'John.Smith@example.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
