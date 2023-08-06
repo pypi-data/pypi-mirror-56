5fee8f8895445aa6f3cc554059c96b5dedfad825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:14 +0000

    Add package pd-sql-loader/1.4.2/pd_sql_loader-1.4.2.tar.gz

---
+++ packages/pd-sql-loader/pd_sql_loader-1.4.2.tar.gz/pd_sql_loader-1.4.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name='pd_sql_loader',
+      version='1.4.2',
+      license='MIT',
+      author='Karev Vitaliy',
+      author_email='Vitaliy.Karev@mvideo.ru',
+      description='To optimization load DataFrame from databases',
+      packages=find_packages(exclude=['tests', 'debug']),
+      #packages=['pd_sql_loader'],
+      include_package_data=True,
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      install_requires=requirements,
+      test_suite='tests')
