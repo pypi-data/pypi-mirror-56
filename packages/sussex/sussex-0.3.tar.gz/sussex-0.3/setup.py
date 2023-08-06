1ede90dd04484fd2b973355a3f93b6b95dd521ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:21 +0000

    Add package sussex/0.3/sussex-0.3.tar.gz

---
+++ packages/sussex/sussex-0.3.tar.gz/sussex-0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sussex',
+    version='0.3',
+    license='MIT',
+    author = 'Simon Sorensen',
+    author_email='hello@simse.io',
+    url='https://simse.io',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'requests',
+        'BeautifulSoup4',
+        'yaspin',
+        'pickledb',
+        'lxml'
+    ],
+    entry_points='''
+        [console_scripts]
+        sussex=sussex.cli:cli
+    ''',
+)
