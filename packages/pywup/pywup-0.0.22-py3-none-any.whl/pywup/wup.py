06373ab3eb61b0148df260a1ef8f49fb1a89444a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:25 +0000

    Add package pywup/0.0.22/pywup-0.0.22-py3-none-any.whl

---
+++ packages/pywup/pywup-0.0.22-py3-none-any.whl/pywup/wup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from sys import argv
+
+
+def wup(debug=False):
+    args = argv[1:]
+    
+    if len(args) == 0:
+        return print("Missing wup command")
+    
+    cmd = args[0]
+    
+    if cmd == "heatmap":
+        if debug:
+            from cmdtools.heatmap import main
+        else:
+            from pywup.cmdtools.heatmap import main
+    
+    elif cmd == "collect":
+        if debug:
+            from cmdtools.collect import main
+        else:
+            from pywup.cmdtools.collect import main
+    
+    elif cmd == "bars":
+        if debug:
+            from cmdtools.bars import main
+        else:
+            from pywup.cmdtools.bars import main
+    
+    elif cmd == "backup":
+        if debug:
+            from cmdtools.backup import main
+        else:
+            from pywup.cmdtools.backup import main
+    
+    else:
+        return print("Unknown wup command:", cmd)
+    
+    main(args[1:])
+
+
+if __name__ == "__main__":
+    wup(True)
