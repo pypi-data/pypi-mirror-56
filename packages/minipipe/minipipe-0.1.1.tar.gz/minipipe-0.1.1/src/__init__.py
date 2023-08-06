17c1c82c47d221225e32f8fd75ed7ead9ad7e2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:54 +0000

    Add package minipipe/0.1.1/minipipe-0.1.1.tar.gz

---
+++ packages/minipipe/minipipe-0.1.1.tar.gz/minipipe-0.1.1/src/__init__.py
@@ -0,0 +1,12 @@
+"""Minipipe is a machine learning mini-batch pipeline tool for out-of-memory workflows. """
+
+from .base import Sentinel
+from .base import Logger
+from .base import Stream
+from .base import Pipe
+from .base import PipeSystem
+from .base import PipeLine
+from .parent import Source 
+from .parent import Sink
+from .parent import Transform
+from .parent import Regulator
