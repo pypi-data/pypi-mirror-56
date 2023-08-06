a9424b21e5a01545aec8d65bf4d9f8395b90421b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:50 +0000

    Add package core-explore-keyword-app/1.6.0/core_explore_keyword_app-1.6.0.tar.gz

---
+++ packages/core-explore-keyword-app/core_explore_keyword_app-1.6.0.tar.gz/core_explore_keyword_app-1.6.0/core_explore_keyword_app/urls.py
@@ -0,0 +1,19 @@
+""" Url router for the explore keyword application
+"""
+
+from django.conf.urls import url
+
+from core_explore_keyword_app.views.user import views as user_views, ajax as user_ajax
+
+urlpatterns = [
+    url(r'^$', user_views.KeywordSearchView.as_view(),
+        name='core_explore_keyword_app_search'),
+    url(r'^suggestions$', user_ajax.SuggestionsKeywordSearchView.as_view(),
+        name='core_explore_keyword_suggestions'),
+    url(r'^get-persistent-query-url$', user_ajax.CreatePersistentQueryUrlKeywordView.as_view(),
+        name='core_explore_keyword_get_persistent_query_url'),
+    url(r'^results-redirect/(?P<persistent_query_id>\w+)', user_views.ResultQueryRedirectKeywordView.as_view(),
+        name='core_explore_keyword_results_redirect'),
+    url(r'^(?P<query_id>\w+)$', user_views.KeywordSearchView.as_view(),
+        name='core_explore_keyword_app_search'),
+]
