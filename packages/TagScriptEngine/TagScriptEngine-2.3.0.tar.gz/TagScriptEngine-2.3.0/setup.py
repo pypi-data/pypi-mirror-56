e6a91352a173fda2515c7e990e60975d889721d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:34 +0000

    Add package TagScriptEngine/2.3.0/TagScriptEngine-2.3.0.tar.gz

---
+++ packages/TagScriptEngine/TagScriptEngine-2.3.0.tar.gz/TagScriptEngine-2.3.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="TagScriptEngine",
+    url="https://github.com/JonSnowbd/TagScript/tree/v2",
+    author="PySnow",
+    author_email="vasti009@gmail.com",
+    version="2.3.0",
+    packages=[
+        "TagScriptEngine",
+        "TagScriptEngine.adapter",
+        "TagScriptEngine.block",
+        "TagScriptEngine.interface",
+    ],
+)
