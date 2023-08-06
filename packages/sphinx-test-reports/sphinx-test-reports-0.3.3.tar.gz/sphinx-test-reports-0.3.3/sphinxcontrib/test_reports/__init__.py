6537e968dc795f3c3f01a38bab391cf135b23d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:09 +0000

    Add package sphinx-test-reports/0.3.3/sphinx-test-reports-0.3.3.tar.gz

---
+++ packages/sphinx-test-reports/sphinx-test-reports-0.3.3.tar.gz/sphinx-test-reports-0.3.3/sphinxcontrib/test_reports/__init__.py
@@ -0,0 +1,2 @@
+# from sphinxcontrib.test_reports.needs.dyn_functions import Results4Needs
+from sphinxcontrib.test_reports.test_reports import setup
---
+++ packages/sphinx-test-reports/sphinx-test-reports-0.3.3.tar.gz/sphinx-test-reports-0.3.3/sphinxcontrib/test_reports/exceptions.py
@@ -0,0 +1,25 @@
+from sphinx.errors import SphinxError, SphinxWarning
+
+
+class TestReportFileNotSetException(SphinxError):
+    """
+    Raised if a needed test_file path is not given in directive.
+    """
+
+
+class TestReportFileInvalidException(SphinxError):
+    """
+    Raised if the given path is not valid.
+    """
+
+
+class TestReportInvalidOption(SphinxError):
+    """
+    Raised if an option is not given or invalid.
+    """
+
+
+class TestReportIncompleteConfiguration(SphinxWarning):
+    """
+    Raised if given arguments / options are not correct configured
+    """
