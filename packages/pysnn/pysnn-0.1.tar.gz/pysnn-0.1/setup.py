673f13280f5c9d86ac1c7e98c7f22d68d2d30bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:50 +0000

    Add package pysnn/0.1/pysnn-0.1.tar.gz

---
+++ packages/pysnn/pysnn-0.1.tar.gz/pysnn-0.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+requirements = [
+    "torch>=1.2.0",
+    "torchvision",
+    "matplotlib",
+    "numpy",
+    "pandas",
+    "sklearn",
+    "pre-commit",
+]
+
+extras = {
+    "examples": [
+        "gym",
+        "tensorflow>=2.0.0",
+        "ray[tune]",
+        "ray[debug]",
+        "requests",
+        "bayesian-optimization",
+    ]
+}
+
+setup(
+    name="pysnn",
+    version="0.1",
+    description="Framework for engineering and simulating spiking neural networks, built on top of PyTorch.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Bas Buller",
+    author_email="bas.buller@gmail.com",
+    packages=find_packages(exclude=("tests", "examples")),
+    install_requires=requirements,
+    extras_require=extras,
+    tests_require=["pytest"],
+    python_requires=">=3.6.0",
+)
