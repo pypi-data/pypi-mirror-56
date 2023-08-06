79ab3e815660defa8989fb0a4d8f7c92cda88765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:00 +0000

    Add package wyliozero/0.0.29/wyliozero-0.0.29.tar.gz

---
+++ packages/wyliozero/wyliozero-0.0.29.tar.gz/wyliozero-0.0.29/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wyliozero",
+    version="0.0.29",
+    author="Serban Razvan",
+    author_email="razvan.serban@wyliodrin.com",
+    description="Library for Wyliodrin Lab",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Wyliodrin/wyliozero",
+    packages=setuptools.find_packages(),
+    install_requires= ['gpiozero', 'Adafruit_DHT', 'pymata_aio', 'RPi.GPIO', 'Adafruit_CharLCD'],
+    classifiers=[
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+        "Topic :: Education"
+    ],
+    python_requires='>=3.6'
+)
