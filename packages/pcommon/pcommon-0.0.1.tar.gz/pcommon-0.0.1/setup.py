77001f4f9cc2dadfc2695b28ba2cc1188f20eec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:46 +0000

    Add package pcommon/0.0.1/pcommon-0.0.1.tar.gz

---
+++ packages/pcommon/pcommon-0.0.1.tar.gz/pcommon-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name = "pcommon",
+    version = "0.0.1",
+    author = "shijianliangs",
+    author_email = "shijianliangs@gmail.com",
+    description = "A python tool package",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/shijianliangs/utils-1019",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.6',
+)
