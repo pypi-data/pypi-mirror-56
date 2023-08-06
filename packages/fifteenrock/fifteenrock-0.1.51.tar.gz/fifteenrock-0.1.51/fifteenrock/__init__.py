9576f5fbc2fdac60dfa059ccd5c0992b9c03f953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:36 +0000

    Add package fifteenrock/0.1.51/fifteenrock-0.1.51.tar.gz

---
+++ packages/fifteenrock/fifteenrock-0.1.51.tar.gz/fifteenrock-0.1.51/fifteenrock/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for fifteenrock."""
+
+__author__ = """Rajiv Abraham"""
+__email__ = 'rajiv.abraham@15rock.com'
+__version__ = '0.1.51'
+
+from fifteenrock.core.core import database
+from fifteenrock.core.compute_client import compute
+from fifteenrock.core.fr_notebook import deploy_form
+
