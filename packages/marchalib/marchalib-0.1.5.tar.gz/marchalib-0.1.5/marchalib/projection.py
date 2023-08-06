e8459d43581b90ce7c188823a0ba40d7b5a1c11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:22 +0000

    Add package marchalib/0.1.5/marchalib-0.1.5.tar.gz

---
+++ packages/marchalib/marchalib-0.1.5.tar.gz/marchalib-0.1.5/marchalib/projection.py
@@ -0,0 +1,10 @@
+import numpy as np
+from astropy import wcs
+
+def wcs2D(hdr):
+    w = wcs.WCS(naxis=2)
+    w.wcs.crpix = [hdr['CRPIX1'], hdr['CRPIX2']]
+    w.wcs.cdelt = np.array([hdr['CDELT1'], hdr['CDELT2']])
+    w.wcs.crval = [hdr['CRVAL1'], hdr['CRVAL2']]
+    w.wcs.ctype = [hdr['CTYPE1'], hdr['CTYPE2']]
+    return w
---
+++ packages/marchalib/marchalib-0.1.5.tar.gz/marchalib-0.1.5/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-                                                                                                                                                                                                                             
+from setuptools import setup, find_packages
+
+with open('/home/amarchal/library/marchalib/Readme.md') as f:
+        readme = f.read()
+
+with open('/home/amarchal/library/marchalib/LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='marchalib',
+    version='0.1.5',
+    description='A. Marchal library',
+    long_description=readme,
+    classifiers=[
+        'Development status :: 1 - Alpha',
+        'License :: CC-By-SA2.0',
+        'Programming Language :: Python',
+        'Topic :: Data Analysis'
+    ],
+    author='Antoine Marchal',
+    author_email='amarchal@cita.utoronto.ca',
+    url='',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+            'numpy',
+	    'statsmodels',
+	    'scipy',
+	    'astropy',
+	    'colorcet',
+    ],
+    include_package_data=True
+)
