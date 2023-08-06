93d8d0f4e1800dc7164ec41331c0da51bba38a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:59 +0000

    Add package ScopeSim-Templates/0.1.dev0/ScopeSim_Templates-0.1.dev0.tar.gz

---
+++ packages/ScopeSim-Templates/ScopeSim_Templates-0.1.dev0.tar.gz/ScopeSim_Templates-0.1.dev0/README.md
@@ -0,0 +1,27 @@
+# ScopeSim Templates
+
+[![Build Status](https://travis-ci.org/astronomyk/ScopeSim_Templates.svg?branch=master)](https://travis-ci.org/astronomyk/ScopeSim_Templates)
+[![Documentation Status](https://readthedocs.org/projects/scopesim-templates/badge/?version=latest)](https://scopesim-templates.readthedocs.io/en/latest/?badge=latest)
+
+A package that contains functions to create ``Source`` objects for ``ScopeSim``.
+
+Please feel free to add new templates by submitting a pull request to github 
+
+## Current package structure
+- galaxy.py
+  |
+  |- elliptical()
+  |- spiral()
+  |- spiral_two_component()
+
+- stars.py 
+  |
+  |- stars()
+  |- star_field()
+  |- cluster() 
+
+- misc.py
+  |
+  |- empty_sky()
+  
+ 
---
+++ packages/ScopeSim-Templates/ScopeSim_Templates-0.1.dev0.tar.gz/ScopeSim_Templates-0.1.dev0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+ScopeSim: A python package to simulate telescope observations
+"""
+
+from datetime import datetime
+from distutils.core import setup
+from setuptools import find_packages
+
+# Version number
+MAJOR = 0
+MINOR = 1
+ATTR = 'dev0'
+
+VERSION = '%d.%d%s' % (MAJOR, MINOR, ATTR)
+
+
+def setup_package():
+    setup(name='ScopeSim_Templates',
+          version=VERSION,
+          description="On-sky source templates for ScopeSim",
+          author="Kieran Leschinski",
+          author_email="kieran.leschinski@unive.ac.at",
+          url="https://github.com/astronomyk/ScopeSim_Templates",
+          package_dir={'scopesim_templates': 'scopesim_templates'},
+          packages=find_packages(),
+          include_package_data=True,
+          install_requires=["numpy>=1.13",
+                            "scipy>0.17",
+                            "astropy>1.1.2",
+                            "matplotlib>1.5.0",
+                            "requests>2.0",
+                            "pyyaml>3",
+                            "synphot>0.1",
+                            "pyckles",
+                            "scopesim",
+                            ],
+          classifiers=["Programming Language :: Python :: 3",
+                       "License :: OSI Approved :: MIT License",
+                       "Operating System :: OS Independent",
+                       "Intended Audience :: Science/Research",
+                       "Topic :: Scientific/Engineering :: Astronomy", ]
+          )
+
+
+if __name__ == '__main__':
+    setup_package()
