a05795cca2135e800fae6a05e2a2b1a1ece2af5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:36 +0000

    Add package LPCTorch/0.1.3/LPCTorch-0.1.3.tar.gz

---
+++ packages/LPCTorch/LPCTorch-0.1.3.tar.gz/LPCTorch-0.1.3/lpctorch/__init__.py
@@ -0,0 +1,15 @@
+"""The Init File for lpctorch module
+
+The file imports all classes from the lpctorch module to expose them to
+the user for easier API usage.
+It provides a LPCCoefficients class as well as a LPCSlicer class in order
+to apply an windowed lpc to a signal.
+"""
+from lpctorch.lpc import LPCCoefficients
+from lpctorch.lpc import LPCSlicer
+
+__name__ = "LPCTorch"
+__version__ = "0.1.3"
+__author__ = "yliess"
+__url__ = "https://github.com/TowardHumanizedInteraction/LPCTorch"
+__email__ = "hatiyliess86@gmail.com"
