7aefb4a9f7efa49943a75d0c203294e977316d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:52 +0000

    Add package genomedashboard/0.0.59/genomedashboard-0.0.59.tar.gz

---
+++ packages/genomedashboard/genomedashboard-0.0.59.tar.gz/genomedashboard-0.0.59/genomedashboard/convert/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""genomedashboard.convert"""
+
+from genomedashboard.convert import convert as cv
+
+__all__ = ["cv"]
---
+++ packages/genomedashboard/genomedashboard-0.0.59.tar.gz/genomedashboard-0.0.59/genomedashboard/io/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""genomedashboard.io"""
+
+from genomedashboard.io import io as io
+
+__all__ = ["io"]
