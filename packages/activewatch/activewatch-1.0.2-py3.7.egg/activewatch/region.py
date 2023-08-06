cffe6b24523a2b3624bea867cb4538b6eeebf9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:54 +0000

    Add package activewatch/1.0.2/activewatch-1.0.2-py3.7.egg

---
+++ packages/activewatch/activewatch-1.0.2-py3.7.egg/activewatch/region.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from activewatch import NoValue
+
+"""
+    alertlogic.region
+    ~~~~~~~~~~~~~~
+    alertlogic region management
+"""
+"""
+List of known regions
+"""
+ENDPOINTS = {
+    "production": "https://api.global-services.global.alertlogic.com",
+    "integration": "https://api.global-integration.product.dev.alertlogic.com"
+}
+
+class Residency(NoValue):
+    DEFAULT = 'default'
+    US = 'us'
+    EMEA = 'emea'
+
+class EndpointType(NoValue):
+    API = 'api'
+    UI = 'ui'
+
+class Region():
+    """
+    Abstracts an alertlogic region, for now it only represents the api endpoint url
+    """
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_global_endpoint(endpoint):
+        return ENDPOINTS.get(endpoint, endpoint)
+
+    @staticmethod
+    def get_region_from_location(location):
+        return location.split('-')[1]
+
