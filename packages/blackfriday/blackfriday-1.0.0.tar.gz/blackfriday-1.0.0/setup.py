706e3f68f5bcd6a1f1bcf6e5e4cbb2f1be6eb11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:46 +0000

    Add package blackfriday/1.0.0/blackfriday-1.0.0.tar.gz

---
+++ packages/blackfriday/blackfriday-1.0.0.tar.gz/blackfriday-1.0.0/setup.py
@@ -0,0 +1,34 @@
+"""Setup script for realpython-reader"""
+
+import os.path
+from setuptools import setup
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+# This call to setup() does all the work
+setup(
+    name="blackfriday",
+    version="1.0.0",
+    description="Black Friday",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://realpython.com/blackfriday",
+    author="Real Python",
+    author_email="info@realpython.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["blackfriday"],
+    include_package_data=False,
+    install_requires=[],
+    entry_points={"console_scripts": ["realpython=blackfriday:main"]},
+)
