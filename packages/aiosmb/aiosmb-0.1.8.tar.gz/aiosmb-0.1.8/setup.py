df303eafdbdb101cdc1a7af74544fd583d3c0733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:32 +0000

    Add package aiosmb/0.1.8/aiosmb-0.1.8.tar.gz

---
+++ packages/aiosmb/aiosmb-0.1.8.tar.gz/aiosmb-0.1.8/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+setup(
+	# Application name:
+	name="aiosmb",
+
+	# Version number (initial):
+	version="0.1.8",
+
+	# Application author details:
+	author="Tamas Jos",
+	author_email="info@skelsec.com",
+
+	# Packages
+	packages=find_packages(),
+
+	# Include additional files into the package
+	include_package_data=True,
+
+
+	# Details
+	url="https://github.com/skelsec/aiosmb",
+
+	zip_safe = True,
+	#
+	# license="LICENSE.txt",
+	description="Asynchronous SMB protocol implementation",
+
+	# long_description=open("README.txt").read(),
+	python_requires='>=3.7',
+	install_requires=[
+		'minikerberos>=0.0.11',
+		'winsspi>=0.0.2',
+		'six',
+		'aiocmd',
+	],
+	
+	classifiers=(
+		"Programming Language :: Python :: 3.7",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	),
+	entry_points={
+		'console_scripts': [
+			'aiosmbclient = aiosmb.examples.smbclient:main',
+		],
+
+	}
+)
