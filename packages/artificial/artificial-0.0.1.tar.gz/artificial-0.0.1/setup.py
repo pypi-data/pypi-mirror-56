fe2144897ccea7b3e7e95580ac62f79cd825e0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:23 +0000

    Add package artificial/0.0.1/artificial-0.0.1.tar.gz

---
+++ packages/artificial/artificial-0.0.1.tar.gz/artificial-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+requirements = parse_requirements('requirements.txt', session=False)
+
+setuptools.setup(
+    name="artificial",
+    version="0.0.1",
+    author="Artificial team",
+    author_email="info@artificial.com",
+    description="Artificial tools",
+    packages=setuptools.find_packages(),
+    install_requires=[str(x.req) for x in requirements],
+    python_requires='~=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ]
+)
