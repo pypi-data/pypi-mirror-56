6dd1958670a6989e138c37e523b6619d247acfda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:16 +0000

    Add package plush-fabric/0.5.1/plush_fabric-0.5.1-py2.py3-none-any.whl

---
+++ packages/plush-fabric/plush_fabric-0.5.1-py2.py3-none-any.whl/plush/repo_keys.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+from fabric.connection import Connection
+from github import GithubException
+from .oauth_flow import get_api
+from .fabric_commands import get_keyfile
+
+
+def list_repo_keys(repo_full_name):
+    api = get_api()
+
+    repo = api.get_repo(repo_full_name)
+    deploy_keys = repo.get_keys()
+
+    try:
+        for key in deploy_keys:
+            print(key.title)
+    except GithubException as exception:
+        print('Failed with {0}, data: {1}'.format(exception.status, exception.data))
+
+
+def add_repo_key(conn: Connection, repo_full_name: str, key_name: Optional[str] = None):
+    public_keyfile = get_keyfile(repo_full_name)
+    public_keyfile_contents = conn.sudo('cat {0}'.format(public_keyfile)).stdout
+    public_keyfile_contents = public_keyfile_contents.rstrip()
+
+    if not key_name:
+        # Default the key_name to the hostname we are connected to
+        key_name = conn.run('hostname').stdout
+        key_name = key_name.rstrip()
+
+    api = get_api()
+
+    repo = api.get_repo(repo_full_name)
+
+    try:
+        print('key_name: "{0}"'.format(key_name))
+        repo.create_key(key_name, public_keyfile_contents, read_only=True)
+    except GithubException as exception:
+        print('Failed with {0}, data: {1}'.format(exception.status, exception.data))
+        raise exception
