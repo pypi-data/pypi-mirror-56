461e6b6d2e8bed2bf9b8e275adcdb68a8139a083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:49 +0000

    Add package pydsb/2.1.1/pydsb-2.1.1.tar.gz

---
+++ packages/pydsb/pydsb-2.1.1.tar.gz/pydsb-2.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name="pydsb",
+                 version="2.1.1",
+                 description="Python API for DSBmobile",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/sn0wmanmj/pydsb",
+                 author="Moritz Jannasch",
+                 author_email="contact@moritzj.de",
+                 license="MIT",
+                 packages=setuptools.find_packages(),
+                 install_requires=[
+                     "requests"
+                 ],
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 )
