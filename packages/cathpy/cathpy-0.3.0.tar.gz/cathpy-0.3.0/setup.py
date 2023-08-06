0a60d6a0d80dd21749b64d3629cb1c34550d79ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:49 +0000

    Add package cathpy/0.3.0/cathpy-0.3.0.tar.gz

---
+++ packages/cathpy/cathpy-0.3.0.tar.gz/cathpy-0.3.0/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import cathpy
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cathpy",
+    version=cathpy.__version__,
+    author="Ian Sillitoe",
+    author_email="i.sillitoe@ucl.ac.uk",
+    description="CathPy - Python Bioinformatics Toolkit for CATH (Protein Classification).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/UCL/cathpy",
+    packages=['cathpy.core'],
+    test_suite="tests",
+    package_dir={
+        'cathpy': 'cathpy',
+    },
+    package_data={
+        'cathpy': [
+            'tools/GroupSim/*',
+            'tools/*/scorecons',
+            'tools/data/*',
+        ],
+    },
+    scripts=[
+        'scripts/cath-funfhmmer-api',
+        'scripts/cath-align-scorecons',
+        'scripts/cath-align-summary',
+        'scripts/cath-sc-merge-alignment',
+    ],
+    install_requires=[
+        'Click',
+        'requests',
+        'jsonpickle',
+        'tqdm',
+        'dendropy',
+        'redis',
+        'celery',
+    ],
+    entry_points={
+        'console_scripts': 'cath-cli=cathpy.core.scripts.cath_cli:cli',
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
