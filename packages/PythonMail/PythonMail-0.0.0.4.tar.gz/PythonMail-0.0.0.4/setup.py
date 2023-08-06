bacd25ac8286eaa5fdd83a1a24cb676cfda771c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:38 +0000

    Add package PythonMail/0.0.0.4/PythonMail-0.0.0.4.tar.gz

---
+++ packages/PythonMail/PythonMail-0.0.0.4.tar.gz/PythonMail-0.0.0.4/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'PythonMail',
+  packages = ['PythonMail'],
+  version = '0.0.0.4',
+  license='MIT',       
+  description = 'Small Module to send Email',
+  author = 'CoreTaxxe',
+  author_email = 'coretaxxe@gmail.com',      
+  url = 'https://github.com/coretaxxe/sendmail',
+  download_url = '',
+  keywords = ['PYTHON','GMAIL','EMAIL'],
+  install_requires=[    
+      ],
+)
