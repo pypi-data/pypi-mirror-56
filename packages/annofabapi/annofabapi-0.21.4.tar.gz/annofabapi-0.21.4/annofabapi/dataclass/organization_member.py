0f3b46db6707f3e4d61effb4a60bb8f1f38d6507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:51 +0000

    Add package annofabapi/0.21.4/annofabapi-0.21.4.tar.gz

---
+++ packages/annofabapi/annofabapi-0.21.4.tar.gz/annofabapi-0.21.4/annofabapi/dataclass/organization_member.py
@@ -0,0 +1,48 @@
+# flake8: noqa: W291
+# pylint: disable=too-many-lines,trailing-whitespace
+"""
+annofabapiのmodelをDataClassで定義したクラス
+
+Note:
+    このファイルはopenapi-generatorで自動生成される。詳細は generate/README.mdを参照.
+    oneOf, allOfなどは正しく表現できない可能性がある。
+"""
+
+import warnings  # pylint: disable=unused-import
+from dataclasses import dataclass
+from typing import Any, Dict, List, NewType, Optional, Tuple, Union  # pylint: disable=unused-import
+
+from dataclasses_json import dataclass_json
+
+from annofabapi.models import OrganizationMemberRole, OrganizationMemberStatus
+
+
+@dataclass_json
+@dataclass
+class OrganizationMember:
+    """
+    
+    """
+    organization_id: str
+    """"""
+
+    account_id: str
+    """"""
+
+    user_id: str
+    """"""
+
+    username: str
+    """"""
+
+    role: OrganizationMemberRole
+    """"""
+
+    status: OrganizationMemberStatus
+    """"""
+
+    created_datetime: Optional[str]
+    """"""
+
+    updated_datetime: Optional[str]
+    """"""
