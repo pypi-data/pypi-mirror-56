4f3b9e35e89b318b071bf34c690bc6e810a958a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:48 +0000

    Add package oidcmsg/0.6.4/oidcmsg-0.6.4.tar.gz

---
+++ packages/oidcmsg/oidcmsg-0.6.4.tar.gz/oidcmsg-0.6.4/src/oidcmsg/__init__.py
@@ -0,0 +1,25 @@
+__author__ = 'Roland Hedberg'
+__version__ = '0.6.4'
+
+
+def proper_path(path):
+    """
+    Clean up the path specification so it looks like something I could use.
+    "./" <path> "/"
+    """
+    if path.startswith("./"):
+        pass
+    elif path.startswith("/"):
+        path = ".%s" % path
+    elif path.startswith("."):
+        while path.startswith("."):
+            path = path[1:]
+        if path.startswith("/"):
+            path = ".%s" % path
+    else:
+        path = "./%s" % path
+
+    if not path.endswith("/"):
+        path += "/"
+
+    return path
