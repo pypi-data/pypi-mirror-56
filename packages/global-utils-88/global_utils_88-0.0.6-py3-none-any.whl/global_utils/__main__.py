728a7d825c0f4dda8aa13ef7d06b927aabcb42eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:39 +0000

    Add package global-utils-88/0.0.6/global_utils_88-0.0.6-py3-none-any.whl

---
+++ packages/global-utils-88/global_utils_88-0.0.6-py3-none-any.whl/global_utils/__main__.py
@@ -0,0 +1,14 @@
+# __main__.py
+
+import global_utils
+from global_utils import global_utils_functions
+
+def printtry():
+    print("I see!")
+
+def main():
+    """."""
+    print("Using global utils lib")
+
+if __name__ == "__main__":
+    main()
