f818a0678c81abb8a9c67cacdf1b7f92f035b55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:44 +0000

    Add package matrix-algebra11/0.1.1/matrix_algebra11-0.1.1.tar.gz

---
+++ packages/matrix-algebra11/matrix_algebra11-0.1.1.tar.gz/matrix_algebra11-0.1.1/matrix_algebra11/python_code.py
@@ -0,0 +1,20 @@
+import math
+import matplotlib.pyplot as plt
+
+class matrix_algebra:
+    ''' basic matrix algebra such as addition, subtraction, multiplication, matrix inversion, etc. 
+    
+    Attributes:
+    
+    
+    Actions:
+    
+    '''
+    
+    def __init__(self):
+        
+    
+    
+    def addition(self, other):
+        if
+    
---
+++ packages/matrix-algebra11/matrix_algebra11-0.1.1.tar.gz/matrix_algebra11-0.1.1/setup.py
@@ -0,0 +1,15 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+
+from setuptools import setup
+
+setup(name='matrix_algebra11',
+      version='0.1.1',
+      description='basic matrix algebra',   # basic matrix algebra such as addition, subtraction, multiplication, matrix inversion, etc.
+      packages=['matrix_algebra11'],
+      zip_safe=False)
+
