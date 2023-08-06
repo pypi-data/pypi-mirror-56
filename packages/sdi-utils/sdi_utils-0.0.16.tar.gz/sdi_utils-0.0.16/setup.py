670412223201c0f61a411f4d6c11ee1e79b7ab48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:30 +0000

    Add package sdi-utils/0.0.16/sdi_utils-0.0.16.tar.gz

---
+++ packages/sdi-utils/sdi_utils-0.0.16.tar.gz/sdi_utils-0.0.16/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sdi_utils",
+    version="0.0.16",
+    author="Thorsten Hapke",
+    author_email="thorsten.hapke@sap.com",
+    description="Generates solution package when locally developing SAP DI operators",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thhapke/gensolution",
+    keywords = ['SAP Data Intelligence','genjson','tprogress','textfield_parser'],
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'gensolution = sdi_utils.gensolution.py:main'
+        ]
+    },
+    classifiers=[
+    	'Programming Language :: Python :: 3.5',
+    	'Programming Language :: Python :: 3.6',
+    	'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
