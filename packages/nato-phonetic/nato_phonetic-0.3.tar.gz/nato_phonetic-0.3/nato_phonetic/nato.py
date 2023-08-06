1c54b8c516931d551e23082d8520ce19f16fa14b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:23 +0000

    Add package nato-phonetic/0.3/nato_phonetic-0.3.tar.gz

---
+++ packages/nato-phonetic/nato_phonetic-0.3.tar.gz/nato_phonetic-0.3/nato_phonetic/nato.py
@@ -0,0 +1,57 @@
+def nato(in_str):
+	'''
+	This function can be used to convert a single word into its NATO equivalent
+
+	Returns -- A response string as well as a list of words in the respnse string
+	'''
+	phonetic_dict = {
+	"A" : "ALPHA",
+	"B" : "BRAVO",
+	"C" : "CHARLIE",
+	"D" : "DELTA",
+	"E" : "ECHO",
+	"F" : "FOXTROT",
+	"G" : "GOLF",
+	"H" : "HOTEL",
+	"I" : "INDIA",
+	"J" : "JULIETT",
+	"K" : "KILO",
+	"L" : "LIMA",
+	"M" : "MIKE",
+	"N" : "NOVEMBER",
+	"O" : "OSCAR",
+	"P" : "PAPA",
+	"Q" : "QUEBEC",
+	"R" : "ROMEO",
+	"S" : "SIERRA",
+	"T" : "TANGO",
+	"U" : "UNIFORM",
+	"V" : "VICTOR",
+	"W" : "WHISKEY",
+	"X" : "XRAY",
+	"Y" : "YANKEE",
+	"Z" : "ZULU"
+	}
+
+	in_str = in_str.upper()
+	out_str = ""
+	out_list = []
+
+	for ch in in_str:
+		if out_str == "":
+			out_str += phonetic_dict[ch]
+		else:
+			out_str += " " + phonetic_dict[ch]
+
+		out_list.append(phonetic_dict[ch])
+
+
+	return out_str, out_list
+
+
+if __name__ == "__main__":
+	input_string = input("Enter the string : ")
+
+	output_string, output_list = nato(input_string)
+
+	print(output_string)
