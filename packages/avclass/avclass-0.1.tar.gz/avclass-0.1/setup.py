0d8ce3fe67485db628a3aaa6841fd9854f38c238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:06 +0000

    Add package avclass/0.1/avclass-0.1.tar.gz

---
+++ packages/avclass/avclass-0.1.tar.gz/avclass-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='avclass',
+      version='0.1',
+      description='Avclass labeler',
+      url='https://github.com/malicialab/avclass',
+      author='malicialab',
+      author_email='scattell@gmail.com',
+      license='MIT',
+      packages=['avclass'],
+      include_package_data=True,
+      zip_safe=False)
