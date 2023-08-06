d4703e1de90996720f5ce9671ba91c096d006fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:46 +0000

    Add package zdktest/0.0.2/zdktest-0.0.2.tar.gz

---
+++ packages/zdktest/zdktest-0.0.2.tar.gz/zdktest-0.0.2/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name = 'zdktest',
+    version = '0.0.2',
+    keywords = ('simple', 'test'),
+    description = 'just a simple test',
+    license = 'MIT License',
+    install_requires = [],
+    author = 'ZDK',
+    author_email = 'zhoudk@ihep.ac.cn',
+    packages = find_packages(),
+    platforms = 'any',
+)
