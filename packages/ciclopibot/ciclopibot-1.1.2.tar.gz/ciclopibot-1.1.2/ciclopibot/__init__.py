72fe7dd6227623f3d2a1a87dbc6a85cf4b75ad85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:00 +0000

    Add package ciclopibot/1.1.2/ciclopibot-1.1.2.tar.gz

---
+++ packages/ciclopibot/ciclopibot-1.1.2.tar.gz/ciclopibot-1.1.2/ciclopibot/__init__.py
@@ -0,0 +1,8 @@
+"""Provide information about this package."""
+
+__author__ = "Davide Testa"
+__email__ = "davide@davte.it"
+__license__ = "GNU General Public License v3.0"
+__version__ = "1.1.2"
+__maintainer__ = "Davide Testa"
+__contact__ = "t.me/davte"
