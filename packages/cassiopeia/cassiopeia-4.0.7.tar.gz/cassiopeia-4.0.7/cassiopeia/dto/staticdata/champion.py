a89ba798c165eb924acfea1917e6c7f6e268dd5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:08 +0000

    Add package cassiopeia/4.0.7/cassiopeia-4.0.7.tar.gz

---
+++ packages/cassiopeia/cassiopeia-4.0.7.tar.gz/cassiopeia-4.0.7/cassiopeia/dto/staticdata/champion.py
@@ -0,0 +1,17 @@
+from ..common import DtoObject
+
+
+class ChampionDto(DtoObject):
+    pass
+
+
+class ChampionListDto(DtoObject):
+    pass
+
+
+class ChampionReleasesDto(DtoObject):
+    pass
+
+
+class ChampionReleaseDto(DtoObject):
+    pass
