e03e433e961f65b4acda7629ac888a4823b72221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:04 +0000

    Add package distributions-usuieri/0.1/distributions_usuieri-0.1.tar.gz

---
+++ packages/distributions-usuieri/distributions_usuieri-0.1.tar.gz/distributions_usuieri-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_usuieri',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_usuieri'],
+      author = 'Erica Usui',
+      zip_safe=False)
