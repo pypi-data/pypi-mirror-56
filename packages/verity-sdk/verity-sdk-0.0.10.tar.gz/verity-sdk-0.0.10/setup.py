82aa7f18d1668e62c9158523984d75c5a2f046b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:18 +0000

    Add package verity-sdk/0.0.10/verity-sdk-0.0.10.tar.gz

---
+++ packages/verity-sdk/verity-sdk-0.0.10.tar.gz/verity-sdk-0.0.10/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="verity-sdk",
+    version="0.0.10",
+    author="Evernym, Inc.",
+    author_email="dev@evernym.com",
+    description="The official Python SDK for Evernym's Verity",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+)
+
