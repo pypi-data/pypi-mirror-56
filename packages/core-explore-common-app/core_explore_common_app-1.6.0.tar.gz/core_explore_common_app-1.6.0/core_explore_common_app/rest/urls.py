397c9d4270a937f33f1333f3fe188628dcabeb04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:50 +0000

    Add package core-explore-common-app/1.6.0/core_explore_common_app-1.6.0.tar.gz

---
+++ packages/core-explore-common-app/core_explore_common_app-1.6.0.tar.gz/core_explore_common_app-1.6.0/core_explore_common_app/rest/urls.py
@@ -0,0 +1,13 @@
+"""Url router for the REST API
+"""
+from django.conf.urls import url
+
+from core_explore_common_app.rest.query import views as query_views
+from core_explore_common_app.rest.result import views as result_views
+
+urlpatterns = [
+    url(r'^local-query', query_views.ExecuteLocalQueryView.as_view(),
+        name='core_explore_common_local_query'),
+    url(r'^result', result_views.get_result_from_data_id,
+        name='core_explore_common_app_get_result_from_data_id'),
+]
