78931f637bd1b9e2e75ec61183b29dbbaca228d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:49 +0000

    Add package spotii-billing-client/1.0.0/spotii-billing-client-1.0.0.tar.gz

---
+++ packages/spotii-billing-client/spotii-billing-client-1.0.0.tar.gz/spotii-billing-client-1.0.0/spotii_billing_client/billing/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from spotii_billing_client.billing.account_api import AccountApi
+from spotii_billing_client.billing.installments_api import InstallmentsApi
+from spotii_billing_client.billing.invoice_api import InvoiceApi
+from spotii_billing_client.billing.payment_api import PaymentApi
+from spotii_billing_client.billing.payment_method_api import PaymentMethodApi
+from spotii_billing_client.billing.tenant_api import TenantApi
