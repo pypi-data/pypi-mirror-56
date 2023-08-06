d986e9a5663213071d2d37408f374287465fa94c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:27 +0000

    Add package yotest/0.0.5/yotest-0.0.5.tar.gz

---
+++ packages/yotest/yotest-0.0.5.tar.gz/yotest-0.0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distils.core import setup, find_packages
+
+
+
+setuptools.setup(
+    name="yotest",
+    version="0.0.5",
+    author="Yoman",
+    author_email="francis.b.lavoie@usherbrooke.ca",
+    description="Yotest",
+    long_description="iii",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
