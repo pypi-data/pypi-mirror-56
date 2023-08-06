a041e9286198096117d0d64ff59541f5f91a768a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:42 +0000

    Add package minilog/1.3/minilog-1.3.tar.gz

---
+++ packages/minilog/minilog-1.3.tar.gz/minilog-1.3/log/tests/conftest.py
@@ -0,0 +1,31 @@
+"""Unit tests configuration file."""
+
+import logging
+
+import pytest
+
+import log
+
+
+def pytest_configure(config):
+    """Disable verbose output when running tests."""
+    logging.basicConfig(level=logging.DEBUG)
+
+    terminal = config.pluginmanager.getplugin('terminal')
+    base = terminal.TerminalReporter
+
+    class QuietReporter(base):  # type: ignore
+        """Reporter that only shows dots when running tests."""
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.verbosity = 0
+            self.showlongtestinfo = False
+            self.showfspath = False
+
+    terminal.TerminalReporter = QuietReporter
+
+
+@pytest.fixture(autouse=True)
+def reset_state():
+    log.state.default_level = None  # type: ignore
