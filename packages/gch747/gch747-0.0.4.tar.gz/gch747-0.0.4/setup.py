20fad0697fd04410646c2f5ea486b61d7a44158f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:13 +0000

    Add package gch747/0.0.4/gch747-0.0.4.tar.gz

---
+++ packages/gch747/gch747-0.0.4.tar.gz/gch747-0.0.4/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="gch747",
+    version="0.0.4",
+    author="Ryan Gosselin",
+    author_email="ryan.gosselin@usherbrooke.ca",
+    packages=["gch747"],
+    description="misc stuff",
+    long_description="misc stuff",
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
