18ba12bf4a4b5644d5f2e0c553ec49f2f1e5f436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:06 +0000

    Add package YChat/0.1.3/YChat-0.1.3.tar.gz

---
+++ packages/YChat/YChat-0.1.3.tar.gz/YChat-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('README.md', encoding = 'utf-8') as f:
+	readme = f.read()
+
+reqs = [
+	"PyQt5>=5.11.0"
+]
+
+setup(
+	name					= 'YChat',
+	version					= '0.1.3',
+	url 					= 'https://github.com/FFTYYY/YChat',
+	description				= 'A simple chatroom program.',
+	long_description		= readme ,
+	install_requires 		= reqs , 
+	license					= 'MIT License',
+	author					= 'Yang Yongyi',
+	author_email 			= 'yongyiyang17@fudan.edu.cn',
+	python_requires			= '>=3.6',
+	packages				= ['YChat'],
+	entry_points			= {'console_scripts': [
+			'YChat-server=YChat.entry:run_server_cli' ,
+			'YChat-client=YChat.entry:run_client_gui' ,
+		]}
+)
