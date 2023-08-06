8813525b628d34b2f716eec5e4ea458e6d8b7595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:36 +0000

    Add package jun18th/1.0.1/jun18th-1.0.1.tar.gz

---
+++ packages/jun18th/jun18th-1.0.1.tar.gz/jun18th-1.0.1/README.md
@@ -0,0 +1,13 @@
+# Jun18th
+
+    pip install jun18th
+
+Then:
+
+    jun18th
+
+Happy Pythoning!
+
+---
+
+*This is just a silly little script*
---
+++ packages/jun18th/jun18th-1.0.1.tar.gz/jun18th-1.0.1/setup.py
@@ -0,0 +1,52 @@
+#coding=utf-8
+
+import os.path
+from setuptools import setup
+from setuptools.command.install import install
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, "README.md")) as f:
+    README = f.read()
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        try:
+            import webbrowser
+            webbrowser.open("https://www.pycourses.com/double12")
+        except:
+            pass
+        print("\n")
+        print("##############################################")
+        print("#          看看爱湃森最新课程优惠喽          #")
+        print("##############################################")
+        print("\n")
+        install.run(self)
+
+
+setup(
+    name="jun18th",
+    version="1.0.1",
+    description="jun18th",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    py_modules=['jun18th'],
+    url="https://www.pycourses.com/double12",
+    author="dongweiming",
+    author_email="admin@pycourses.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+    include_package_data=False,
+    install_requires=[],
+    entry_points={"console_scripts": ["jun18th=jun18th:main"]},
+)
