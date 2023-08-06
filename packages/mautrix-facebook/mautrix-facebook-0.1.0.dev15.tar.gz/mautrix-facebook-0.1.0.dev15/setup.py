a655b52601dcf3152b82bbb23c88b99b017aa5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:22 +0000

    Add package mautrix-facebook/0.1.0.dev15/mautrix-facebook-0.1.0.dev15.tar.gz

---
+++ packages/mautrix-facebook/mautrix-facebook-0.1.0.dev15.tar.gz/mautrix-facebook-0.1.0.dev15/setup.py
@@ -0,0 +1,65 @@
+import setuptools
+import glob
+
+from mautrix_facebook.get_version import git_tag, git_revision, version, linkified_version
+
+try:
+    long_desc = open("README.md").read()
+except IOError:
+    long_desc = "Failed to read README.md"
+
+with open("mautrix_facebook/version.py", "w") as version_file:
+    version_file.write(f"""# Generated in setup.py
+
+git_tag = {git_tag!r}
+git_revision = {git_revision!r}
+version = {version!r}
+linkified_version = {linkified_version!r}
+""")
+
+setuptools.setup(
+    name="mautrix-facebook",
+    version=version,
+    url="https://github.com/tulir/mautrix-facebook",
+
+    author="Tulir Asokan",
+    author_email="tulir@maunium.net",
+
+    description="A Matrix-Facebook Messenger puppeting bridge.",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[
+        "aiohttp>=3.0.1,<4",
+        "mautrix>=0.4.0rc2,<0.5.0",
+        "ruamel.yaml>=0.15.94,<0.17",
+        "commonmark>=0.8,<0.10",
+        "python-magic>=0.4,<0.5",
+        "fbchat-asyncio>=0.3.0rc3",
+        "SQLAlchemy>=1.2,<2",
+        "alembic>=1,<2",
+    ],
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Topic :: Communications :: Chat",
+        "Framework :: AsyncIO",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    entry_points="""
+        [console_scripts]
+        mautrix-facebook=mautrix_facebook.__main__:main
+    """,
+    data_files=[
+        (".", ["example-config.yaml", "alembic.ini"]),
+        ("alembic", ["alembic/env.py"]),
+        ("alembic/versions", glob.glob("alembic/versions/*.py"))
+    ],
+)
