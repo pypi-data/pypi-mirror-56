9d4d84d013d09fef44d52676a91d426033253d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:15 +0000

    Add package vmpck-distributions/0.1/vmpck_distributions-0.1.tar.gz

---
+++ packages/vmpck-distributions/vmpck_distributions-0.1.tar.gz/vmpck_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='vmpck_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['vmpck_distributions'],
+      zip_safe=False)
