cc424d15746b5c5c5888532f01a0e39bf31162ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:20 +0000

    Add package dafsa/0.2.1/dafsa-0.2.1-py3-none-any.whl

---
+++ packages/dafsa/dafsa-0.2.1-py3-none-any.whl/dafsa/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+
+# Version of the dafsa package
+__version__ = "0.2.1"
+__author__ = "Tiago Tresoldi"
+__email__ = "tresoldi@shh.mpg.de"
+
+# Import Python libraries
+from pathlib import Path
+
+# Build the namespace
+from dafsa.dafsa import DAFSA
