136d6ad5a377eb8defda636dc65127198584ebc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:53 +0000

    Add package autocomplete-yichunshi/0.1/autocomplete_yichunshi-0.1.tar.gz

---
+++ packages/autocomplete-yichunshi/autocomplete_yichunshi-0.1.tar.gz/autocomplete_yichunshi-0.1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 19:16:14 2019
+
+@author: Yichun Shi
+"""
+
+from setuptools import setup
+
+setup(name='autocomplete_yichunshi',
+      version='0.1',
+      description='freakin awesome autocomplete system',
+      url='https://github.com/aliceshiyichun/autocomplete_yichunshi',
+      author='Yichun Shi',
+      author_email='yichunsh@andrew.cmu.edu',
+      license='CMU',
+      packages=['autocomplete_yichunshi'],
+      zip_safe=False)
