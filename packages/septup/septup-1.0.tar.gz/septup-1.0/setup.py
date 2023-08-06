40bd6590d1c738c934787a62f03cf0d9f6804986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:50 +0000

    Add package septup/1.0/septup-1.0.tar.gz

---
+++ packages/septup/septup-1.0.tar.gz/septup-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='septup',
+    version='1.0',
+    packages=['print'],
+    url='',
+    license='internal',
+    author='ashok',
+    author_email='ashoklondhe.in@gmail.com',
+    description='testing hosted app'
+)
