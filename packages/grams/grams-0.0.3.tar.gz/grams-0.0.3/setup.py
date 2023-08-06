5833f6f00d10e9beb4be2926798fe4f163b19a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:32 +0000

    Add package grams/0.0.3/grams-0.0.3.tar.gz

---
+++ packages/grams/grams-0.0.3.tar.gz/grams-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="grams",
+    version="0.0.3",
+    author="Jonasz Rice",
+    author_email="jonaszakr@gmail.com",
+    description="A package for managing histograms.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/escofresco/makeschool_fsp2_realtweets",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "numpy==1.17.3",
+        "dit==1.2.3",
+        "colorama==0.4.1",
+        "coverage==4.5.4",
+    ])
