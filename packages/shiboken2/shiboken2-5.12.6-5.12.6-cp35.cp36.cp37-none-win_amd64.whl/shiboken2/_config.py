4491a0d176440f87ac3700d2c321670d6b3a6407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:14 +0000

    Add package shiboken2/5.12.6/shiboken2-5.12.6-5.12.6-cp35.cp36.cp37-none-win_amd64.whl

---
+++ packages/shiboken2/shiboken2-5.12.6-5.12.6-cp35.cp36.cp37-none-win_amd64.whl/shiboken2/_config.py
@@ -0,0 +1,11 @@
+shiboken_library_soversion = str(5.12)
+
+version = "5.12.6"
+version_info = (5, 12, 6, "", "")
+
+__build_date__ = '2019-11-20T10:46:43+00:00'
+
+
+
+
+__setup_py_package_version__ = '5.12.6'
