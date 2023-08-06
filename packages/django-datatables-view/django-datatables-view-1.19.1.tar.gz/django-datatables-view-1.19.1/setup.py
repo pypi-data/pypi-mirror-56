63da209611b3b17cf610c9bd5cc7f9ebeca721a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:05 +0000

    Add package django-datatables-view/1.19.1/django-datatables-view-1.19.1.tar.gz

---
+++ packages/django-datatables-view/django-datatables-view-1.19.1.tar.gz/django-datatables-view-1.19.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.19.1'
+
+base_dir = os.path.dirname(__file__)
+
+
+setup(name='django-datatables-view',
+      version=version,
+      description='Django datatables view',
+      long_description=open(os.path.join(base_dir, "README.rst")).read(),
+      url='https://bitbucket.org/pigletto/django-datatables-view',
+      classifiers=[
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+      keywords='django datatables view',
+      author='Maciej Wisniowski',
+      author_email='maciej.wisniowski@natcam.pl',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+      dependency_links=[],
+      install_requires=[
+          'setuptools',
+      ]
+      )
