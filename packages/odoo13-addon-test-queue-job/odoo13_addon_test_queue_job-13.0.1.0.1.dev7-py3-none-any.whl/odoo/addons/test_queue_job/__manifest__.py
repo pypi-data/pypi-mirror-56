6c83861ca418a279be4761c13423407db6d359bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:01 +0000

    Add package odoo13-addon-test-queue-job/13.0.1.0.1.dev7/odoo13_addon_test_queue_job-13.0.1.0.1.dev7-py3-none-any.whl

---
+++ packages/odoo13-addon-test-queue-job/odoo13_addon_test_queue_job-13.0.1.0.1.dev7-py3-none-any.whl/odoo/addons/test_queue_job/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2016 Camptocamp SA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+
+{
+    "name": "Queue Job Tests",
+    "version": "13.0.1.0.0",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "category": "Generic Modules",
+    "depends": ["queue_job"],
+    "website": "https://github.com/OCA/queue",
+    "data": ["security/ir.model.access.csv"],
+    "installable": True,
+}
