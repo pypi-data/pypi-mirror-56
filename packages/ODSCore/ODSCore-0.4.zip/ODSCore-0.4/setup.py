30d198e8d3a100849d62184106a95bd5a6975521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:51 +0000

    Add package ODSCore/0.4/ODSCore-0.4.zip

---
+++ packages/ODSCore/ODSCore-0.4.zip/ODSCore-0.4/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'ODSCore',
+  packages = ['ODSCore'],   
+  version = '0.4',      
+  license='MIT',       
+  description = 'Core Components for ODS Database',   
+  author = 'Monty Dimkpa',                   
+  author_email = 'cmdimkpa@gmail.com',      
+  url = 'https://github.com/cmdimkpa/ODSCore',   
+  download_url = 'https://github.com/cmdimkpa/ODSCore/archive/v_04.tar.gz',    
+  keywords = ['ODS', 'S3AppDatabase', 'Database'],   
+  install_requires=[            
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 2.7',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
