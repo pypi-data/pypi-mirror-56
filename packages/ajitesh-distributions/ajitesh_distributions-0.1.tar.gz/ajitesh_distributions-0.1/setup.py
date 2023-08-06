8d530e40e0a0068def65333be72382da1e8bee94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:24 +0000

    Add package ajitesh-distributions/0.1/ajitesh_distributions-0.1.tar.gz

---
+++ packages/ajitesh-distributions/ajitesh_distributions-0.1.tar.gz/ajitesh_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ajitesh_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ajitesh_distributions'],
+      author = "Ajitesh Sakaray",
+      author_email = "sakarayajitesh@gmail.com",
+      zip_safe=False)
