47cd2ce30415a28521cfbe30ebbdf10733e437d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:54 +0000

    Add package TextProcess-Ora/0.0.3/TextProcess_Ora-0.0.3.tar.gz

---
+++ packages/TextProcess-Ora/TextProcess_Ora-0.0.3.tar.gz/TextProcess_Ora-0.0.3/TextProcess/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*-coding:utf-8 -*-
+#Reference:**********************************************
+# @Time     : 2019-09-30 12:34
+# @Author   : 病虎
+# @E-mail   : victor.xsyang@gmail.com
+# @File     : __init__.py
+# @User     : ora
+# @Software: PyCharm
+# @Description: 
+#Reference:**********************************************
+from __future__ import print_function
+from __future__ import division
+from __future__ import unicode_literals
+
+name = "TextProcess"
