da5e4949c938357422476a2873f1c13f360faf51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:47 +0000

    Add package idact-gui/0.2.3/idact_gui-0.2.3-py3-none-any.whl

---
+++ packages/idact-gui/idact_gui-0.2.3-py3-none-any.whl/gui/main.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from gui.helpers.configuration_provider import ConfigurationProvider
+from gui.functionality.main_window import MainWindow
+
+
+def main():
+    app = QApplication([])
+    conf_provider = ConfigurationProvider()
+    if not conf_provider.check_if_conf_file_exists():
+        conf_provider.create_conf_file()
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
