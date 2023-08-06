5e7db298845c74e4ddbc92bc50a65c9adf0c17bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:35 +0000

    Add package Flask-AWSCognito/1.1/Flask-AWSCognito-1.1.tar.gz

---
+++ packages/Flask-AWSCognito/Flask-AWSCognito-1.1.tar.gz/Flask-AWSCognito-1.1/setup.py
@@ -0,0 +1,42 @@
+"""
+Flask-AWSCognito
+-------------
+
+User authentication with AWS Cognito for Flask
+"""
+from setuptools import setup
+
+tests_require = ["pytest", "pytest-mock", "pytest-flask"]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="Flask-AWSCognito",
+    version="1.1",
+    url="https://github.com/cgauge/Flask-AWSCognito/",
+    license="MIT",
+    author="CustomerGauge",
+    author_email="python@customergauge.com",
+    description="Authenticate users with AWS Cognito",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["flask_awscognito"],
+    zip_safe=False,
+    include_package_data=True,
+    platforms="any",
+    install_requires=["Flask", "python-jose", "requests"],
+    tests_require=[tests_require],
+    extras_require={"tests": tests_require},
+    python_requires=">=3.6",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
