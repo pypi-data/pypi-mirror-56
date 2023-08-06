3c4cd4f62c63a536ee632a6b171d1674813a93a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:02 +0000

    Add package airtunnel/0.5.0/airtunnel-0.5-py3-none-any.whl

---
+++ packages/airtunnel/airtunnel-0.5-py3-none-any.whl/airtunnel/__init__.py
@@ -0,0 +1,24 @@
+""" airtunnel – tame your Airflow!"""
+
+__version__ = "0.5"
+
+from airtunnel.data_asset import (
+    BaseDataAsset,
+    SQLDataAsset,
+    PandasDataAsset,
+    PySparkDataAsset,
+    BaseDataAssetIO,
+    PandasDataAssetIO,
+    PySparkDataAssetIO,
+)
+
+__all__ = [
+    "BaseDataAsset",
+    "SQLDataAsset",
+    "PandasDataAsset",
+    "PySparkDataAsset",
+    "BaseDataAssetIO",
+    "PandasDataAssetIO",
+    "PySparkDataAssetIO",
+    "__version__",
+]
