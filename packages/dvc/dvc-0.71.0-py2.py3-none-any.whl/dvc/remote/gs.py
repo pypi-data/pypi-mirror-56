dcd7650a0bc3b815207ca798503ea12f1e113fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:40 +0000

    Add package dvc/0.71.0/dvc-0.71.0-py2.py3-none-any.whl

---
+++ packages/dvc/dvc-0.71.0-py2.py3-none-any.whl/dvc/remote/gs.py
@@ -0,0 +1,186 @@
+from __future__ import unicode_literals, division
+
+import logging
+from datetime import timedelta
+from functools import wraps
+import io
+import os.path
+
+from funcy import cached_property
+
+from dvc.config import Config
+from dvc.exceptions import DvcException
+from dvc.path_info import CloudURLInfo
+from dvc.progress import Tqdm
+from dvc.remote.base import RemoteBASE
+from dvc.scheme import Schemes
+from dvc.utils.compat import FileNotFoundError  # skipcq: PYL-W0622
+
+logger = logging.getLogger(__name__)
+
+
+def dynamic_chunk_size(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        import requests
+        from google.cloud.storage.blob import Blob
+
+        # `ConnectionError` may be due to too large `chunk_size`
+        # (see [#2572]) so try halving on error.
+        # Note: start with 40 * [default: 256K] = 10M.
+        # Note: must be multiple of 256K.
+        #
+        # [#2572]: https://github.com/iterative/dvc/issues/2572
+
+        # skipcq: PYL-W0212
+        multiplier = 40
+        while True:
+            try:
+                # skipcq: PYL-W0212
+                chunk_size = Blob._CHUNK_SIZE_MULTIPLE * multiplier
+                return func(*args, chunk_size=chunk_size, **kwargs)
+            except requests.exceptions.ConnectionError:
+                multiplier //= 2
+                if not multiplier:
+                    raise
+
+    return wrapper
+
+
+@dynamic_chunk_size
+def _upload_to_bucket(
+    bucket,
+    from_file,
+    to_info,
+    chunk_size=None,
+    name=None,
+    no_progress_bar=True,
+):
+    blob = bucket.blob(to_info.path, chunk_size=chunk_size)
+    with Tqdm(
+        desc=name or to_info.path,
+        total=os.path.getsize(from_file),
+        bytes=True,
+        disable=no_progress_bar,
+    ) as pbar:
+        with io.open(from_file, mode="rb") as fobj:
+            raw_read = fobj.read
+
+            def read(size=chunk_size):
+                res = raw_read(size)
+                if res:
+                    pbar.update(len(res))
+                return res
+
+            fobj.read = read
+            blob.upload_from_file(fobj)
+
+
+class RemoteGS(RemoteBASE):
+    scheme = Schemes.GS
+    path_cls = CloudURLInfo
+    REQUIRES = {"google-cloud-storage": "google.cloud.storage"}
+    PARAM_CHECKSUM = "md5"
+
+    def __init__(self, repo, config):
+        super(RemoteGS, self).__init__(repo, config)
+
+        url = config.get(Config.SECTION_REMOTE_URL, "gs:///")
+        self.path_info = self.path_cls(url)
+
+        self.projectname = config.get(Config.SECTION_GCP_PROJECTNAME, None)
+        self.credentialpath = config.get(Config.SECTION_GCP_CREDENTIALPATH)
+
+    @cached_property
+    def gs(self):
+        from google.cloud.storage import Client
+
+        return (
+            Client.from_service_account_json(self.credentialpath)
+            if self.credentialpath
+            else Client(self.projectname)
+        )
+
+    def get_file_checksum(self, path_info):
+        import base64
+        import codecs
+
+        bucket = path_info.bucket
+        path = path_info.path
+        blob = self.gs.bucket(bucket).get_blob(path)
+        if not blob:
+            return None
+
+        b64_md5 = blob.md5_hash
+        md5 = base64.b64decode(b64_md5)
+        return codecs.getencoder("hex")(md5)[0].decode("utf-8")
+
+    def copy(self, from_info, to_info):
+        from_bucket = self.gs.bucket(from_info.bucket)
+        blob = from_bucket.get_blob(from_info.path)
+        if not blob:
+            msg = "'{}' doesn't exist in the cloud".format(from_info.path)
+            raise DvcException(msg)
+
+        to_bucket = self.gs.bucket(to_info.bucket)
+        from_bucket.copy_blob(blob, to_bucket, new_name=to_info.path)
+
+    def remove(self, path_info):
+        if path_info.scheme != "gs":
+            raise NotImplementedError
+
+        logger.debug("Removing gs://{}".format(path_info))
+        blob = self.gs.bucket(path_info.bucket).get_blob(path_info.path)
+        if not blob:
+            return
+
+        blob.delete()
+
+    def _list_paths(self, bucket, prefix):
+        for blob in self.gs.bucket(bucket).list_blobs(prefix=prefix):
+            yield blob.name
+
+    def list_cache_paths(self):
+        return self._list_paths(self.path_info.bucket, self.path_info.path)
+
+    def exists(self, path_info):
+        paths = set(self._list_paths(path_info.bucket, path_info.path))
+        return any(path_info.path == path for path in paths)
+
+    def _upload(self, from_file, to_info, name=None, no_progress_bar=True):
+        bucket = self.gs.bucket(to_info.bucket)
+        _upload_to_bucket(
+            bucket,
+            from_file,
+            to_info,
+            name=name,
+            no_progress_bar=no_progress_bar,
+        )
+
+    def _download(self, from_info, to_file, name=None, no_progress_bar=True):
+        bucket = self.gs.bucket(from_info.bucket)
+        blob = bucket.get_blob(from_info.path)
+        with Tqdm(
+            desc=name or from_info.path,
+            total=blob.size,
+            bytes=True,
+            disable=no_progress_bar,
+        ) as pbar:
+            with io.open(to_file, mode="wb") as fobj:
+                raw_write = fobj.write
+
+                def write(byte_string):
+                    raw_write(byte_string)
+                    pbar.update(len(byte_string))
+
+                fobj.write = write
+                blob.download_to_file(fobj)
+
+    def _generate_download_url(self, path_info, expires=3600):
+        expiration = timedelta(seconds=int(expires))
+
+        bucket = self.gs.bucket(path_info.bucket)
+        blob = bucket.get_blob(path_info.path)
+        if blob is None:
+            raise FileNotFoundError
+        return blob.generate_signed_url(expiration=expiration)
---
+++ packages/dvc/dvc-0.71.0-py2.py3-none-any.whl/tests/func/test_gc.py
@@ -0,0 +1,229 @@
+from __future__ import unicode_literals
+
+import os
+
+import configobj
+import pytest
+from git import Repo
+
+from dvc.exceptions import CollectCacheError
+from dvc.main import main
+from dvc.repo import Repo as DvcRepo
+from dvc.utils.compat import pathlib
+from tests.basic_env import TestDir
+from tests.basic_env import TestDvcGit
+
+
+class TestGC(TestDvcGit):
+    def setUp(self):
+        super(TestGC, self).setUp()
+
+        self.dvc.add(self.FOO)
+        self.dvc.add(self.DATA_DIR)
+        self.good_cache = [
+            self.dvc.cache.local.get(md5) for md5 in self.dvc.cache.local.all()
+        ]
+
+        self.bad_cache = []
+        for i in ["123", "234", "345"]:
+            path = os.path.join(self.dvc.cache.local.cache_dir, i[0:2], i[2:])
+            self.create(path, i)
+            self.bad_cache.append(path)
+
+    def test_api(self):
+        self.dvc.gc()
+        self._test_gc()
+
+    def test_cli(self):
+        ret = main(["gc", "-f"])
+        self.assertEqual(ret, 0)
+        self._test_gc()
+
+    def _test_gc(self):
+        self.assertTrue(os.path.isdir(self.dvc.cache.local.cache_dir))
+        for c in self.bad_cache:
+            self.assertFalse(os.path.exists(c))
+
+        for c in self.good_cache:
+            self.assertTrue(os.path.exists(c))
+
+
+class TestGCBranchesTags(TestDvcGit):
+    def _check_cache(self, num):
+        total = 0
+        for root, dirs, files in os.walk(os.path.join(".dvc", "cache")):
+            total += len(files)
+        self.assertEqual(total, num)
+
+    def test(self):
+        fname = "file"
+
+        with open(fname, "w+") as fobj:
+            fobj.write("v1.0")
+
+        stages = self.dvc.add(fname)
+        self.assertEqual(len(stages), 1)
+        self.dvc.scm.add([".gitignore", stages[0].relpath])
+        self.dvc.scm.commit("v1.0")
+        self.dvc.scm.tag("v1.0")
+
+        self.dvc.scm.checkout("test", create_new=True)
+        self.dvc.remove(stages[0].relpath, outs_only=True)
+        with open(fname, "w+") as fobj:
+            fobj.write("test")
+        stages = self.dvc.add(fname)
+        self.assertEqual(len(stages), 1)
+        self.dvc.scm.add([".gitignore", stages[0].relpath])
+        self.dvc.scm.commit("test")
+
+        self.dvc.scm.checkout("master")
+        self.dvc.remove(stages[0].relpath, outs_only=True)
+        with open(fname, "w+") as fobj:
+            fobj.write("trash")
+        stages = self.dvc.add(fname)
+        self.assertEqual(len(stages), 1)
+        self.dvc.scm.add([".gitignore", stages[0].relpath])
+        self.dvc.scm.commit("trash")
+
+        self.dvc.remove(stages[0].relpath, outs_only=True)
+        with open(fname, "w+") as fobj:
+            fobj.write("master")
+        stages = self.dvc.add(fname)
+        self.assertEqual(len(stages), 1)
+        self.dvc.scm.add([".gitignore", stages[0].relpath])
+        self.dvc.scm.commit("master")
+
+        self._check_cache(4)
+
+        self.dvc.gc(all_tags=True, all_branches=True)
+
+        self._check_cache(3)
+
+        self.dvc.gc(all_tags=False, all_branches=True)
+
+        self._check_cache(2)
+
+        self.dvc.gc(all_tags=True, all_branches=False)
+
+        self._check_cache(1)
+
+
+class TestGCMultipleDvcRepos(TestDvcGit):
+    def _check_cache(self, num):
+        total = 0
+        for root, dirs, files in os.walk(os.path.join(".dvc", "cache")):
+            total += len(files)
+        self.assertEqual(total, num)
+
+    def setUp(self):
+        super(TestGCMultipleDvcRepos, self).setUp()
+        self.additional_path = TestDir.mkdtemp()
+        self.additional_git = Repo.init(self.additional_path)
+        self.additional_dvc = DvcRepo.init(self.additional_path)
+
+        cache_path = os.path.join(self._root_dir, ".dvc", "cache")
+        config_path = os.path.join(
+            self.additional_path, ".dvc", "config.local"
+        )
+        cfg = configobj.ConfigObj()
+        cfg.filename = config_path
+        cfg["cache"] = {"dir": cache_path}
+        cfg.write()
+
+        self.additional_dvc = DvcRepo(self.additional_path)
+
+    def test(self):
+
+        # ADD FILE ONLY IN MAIN PROJECT
+        fname = "only_in_first"
+        with open(fname, "w+") as fobj:
+            fobj.write("only in main repo")
+
+        stages = self.dvc.add(fname)
+        self.assertEqual(len(stages), 1)
+
+        # ADD FILE IN MAIN PROJECT THAT IS ALSO IN SECOND PROJECT
+        fname = "in_both"
+        with open(fname, "w+") as fobj:
+            fobj.write("in both repos")
+
+        stages = self.dvc.add(fname)
+        self.assertEqual(len(stages), 1)
+
+        cwd = os.getcwd()
+        os.chdir(self.additional_path)
+        # ADD FILE ONLY IN SECOND PROJECT
+        fname = os.path.join(self.additional_path, "only_in_second")
+        with open(fname, "w+") as fobj:
+            fobj.write("only in additional repo")
+
+        stages = self.additional_dvc.add(fname)
+        self.assertEqual(len(stages), 1)
+
+        # ADD FILE IN SECOND PROJECT THAT IS ALSO IN MAIN PROJECT
+        fname = os.path.join(self.additional_path, "in_both")
+        with open(fname, "w+") as fobj:
+            fobj.write("in both repos")
+
+        stages = self.additional_dvc.add(fname)
+        self.assertEqual(len(stages), 1)
+
+        os.chdir(cwd)
+
+        self._check_cache(3)
+
+        self.dvc.gc(repos=[self.additional_path])
+        self._check_cache(3)
+
+        self.dvc.gc()
+        self._check_cache(2)
+
+
+def test_all_commits(git, dvc_repo):
+    def add_and_commit():
+        stages = dvc_repo.add(str(testfile))
+        dvc_repo.scm.add([s.relpath for s in stages])
+        dvc_repo.scm.commit("message")
+
+    cache_dir = os.path.join(dvc_repo.root_dir, ".dvc", "cache")
+    testfile = pathlib.Path("testfile")
+
+    testfile.write_text("uncommitted")
+    dvc_repo.add(str(testfile))
+
+    testfile.write_text("committed")
+    add_and_commit()
+
+    testfile.write_text("modified")
+    add_and_commit()
+
+    testfile.write_text("workspace")
+    dvc_repo.add(str(testfile))
+
+    n = _count_files(cache_dir)
+
+    dvc_repo.gc(all_commits=True)
+
+    # Only one uncommitted file should go away
+    assert _count_files(cache_dir) == n - 1
+
+
+def _count_files(path):
+    return sum(len(files) for _, _, files in os.walk(path))
+
+
+def test_gc_no_dir_cache(repo_dir, dvc_repo):
+    dvc_repo.add(repo_dir.FOO)
+    dvc_repo.add(repo_dir.BAR)
+    dir_stage, = dvc_repo.add(repo_dir.DATA_DIR)
+
+    os.unlink(dir_stage.outs[0].cache_path)
+
+    with pytest.raises(CollectCacheError):
+        dvc_repo.gc()
+
+    assert _count_files(dvc_repo.cache.local.cache_dir) == 4
+
+    dvc_repo.gc(force=True)
+
+    assert _count_files(dvc_repo.cache.local.cache_dir) == 2
