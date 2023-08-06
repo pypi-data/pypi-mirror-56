ff679c5f2ad5599dc4821a1a3343493d11518049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:20 +0000

    Add package cropmetapop/1.1.0.1/cropmetapop-1.1.0.1.tar.gz

---
+++ packages/cropmetapop/cropmetapop-1.1.0.1.tar.gz/cropmetapop-1.1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='cropmetapop',
+      version='1.1.0.1',
+      description='The crop metapopulation modelisation software',
+      url='https://sourcesup.renater.fr/www/cmp/',
+      author='Equipe DEAP',
+      author_email='baptiste.rouger@inra.fr',
+      license='GNU GPL v3',
+      packages=['cropmetapop'],
+      install_requires=[
+          'simuPOP',
+          'numpy',
+          'python-igraph'
+      ],
+      scripts=['bin/cropmetapop'],
+      zip_safe=False)
