e96d16be7e9ff15f14f8759ed2e475c9e321db8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:34 +0000

    Add package pypager/0.4/pypager-0.4.tar.gz

---
+++ packages/pypager/pypager-0.4.tar.gz/pypager-0.4/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+import pypager
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    long_description = f.read()
+
+install_requires = [
+    'prompt_toolkit>=2.0.0,<3.1.0',
+    'pygments',
+]
+
+# Argparse became part of the standard library in Python 2.7
+# Include for older versions.
+try:
+    import argparse
+except ImportError:
+    install_requires += ['argparse']
+
+setup(
+    name='pypager',
+    author='Jonathan Slenders',
+    version=pypager.__version__,
+    license='LICENSE',
+    url='https://github.com/jonathanslenders/pypager',
+    description='Pure Python pager (like "more" and "less").',
+    long_description=long_description,
+    packages=find_packages('.'),
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'pypager = pypager.entry_points.run_pypager:run',
+        ]
+    },
+)
