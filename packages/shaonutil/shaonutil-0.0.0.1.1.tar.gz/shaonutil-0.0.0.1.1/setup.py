e3797131ffad428003bb2cb6a45ecf020f5956a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:56 +0000

    Add package shaonutil/0.0.0.1.1/shaonutil-0.0.0.1.1.tar.gz

---
+++ packages/shaonutil/shaonutil-0.0.0.1.1.tar.gz/shaonutil-0.0.0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'shaonutil',
+  packages = ['shaonutil'],
+  version = '0.0.0.1.1',
+  description = 'collection of utility by shaon',
+  author = 'Shaon Majumder',
+  author_email = 'smazoomder@gmail.com',
+  url = 'https://github.com/ShaonMajumder/shaonutil',
+  download_url = 'https://github.com/ShaonMajumder/shaonutil/archive/0.0.0.1.1.tar.gz',
+  keywords = ['shaon', 'utility', 'statistics'], 
+  classifiers = [],
+)
