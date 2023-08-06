5010fa5a394d3580fdd0de134dc4ff5df4e0e37e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:08 +0000

    Add package gumo-datastore/0.1.4a1/gumo-datastore-0.1.4a1.tar.gz

---
+++ packages/gumo-datastore/gumo-datastore-0.1.4a1.tar.gz/gumo-datastore-0.1.4a1/gumo/datastore/infrastructure/__init__.py
@@ -0,0 +1,17 @@
+from gumo.datastore.infrastructure.repository import DatastoreRepositoryMixin
+from gumo.datastore.infrastructure.repository import datastore_transaction
+from gumo.datastore.infrastructure.entity_key_mapper import EntityKeyMapper
+from gumo.datastore.infrastructure.mapper import DatastoreMapperMixin
+from gumo.datastore.infrastructure.model import DataModel
+
+DatastoreEntity = DatastoreRepositoryMixin.DatastoreEntity
+
+
+__all__ = [
+    DatastoreRepositoryMixin.__name__,
+    DatastoreEntity.__name__,
+    datastore_transaction.__name__,
+    EntityKeyMapper.__name__,
+    DatastoreMapperMixin.__name__,
+    DataModel.__name__,
+]
