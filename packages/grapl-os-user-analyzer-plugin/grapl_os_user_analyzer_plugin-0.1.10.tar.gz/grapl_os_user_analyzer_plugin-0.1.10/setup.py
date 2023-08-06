ab891ad69f917cd78acb4ead4f440e9152c46c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:32 +0000

    Add package grapl-os-user-analyzer-plugin/0.1.10/grapl_os_user_analyzer_plugin-0.1.10.tar.gz

---
+++ packages/grapl-os-user-analyzer-plugin/grapl_os_user_analyzer_plugin-0.1.10.tar.gz/grapl_os_user_analyzer_plugin-0.1.10/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grapl_os_user_analyzer_plugin",
+    version="0.1.10",
+    description="OS User Plugin for Grapl Analyzers",
+    url="https://github.com/insanitybit/grapl-os-user-plugin/",
+    author="insanitybit",
+    author_email="insanitybit@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["pydgraph"],
+)
