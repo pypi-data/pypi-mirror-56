0b40b73e091e2605ed01dbd5ce5cff4ffff0376b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:36 +0000

    Add package pm4py/1.2.6/pm4py-1.2.6.tar.gz

---
+++ packages/pm4py/pm4py-1.2.6.tar.gz/pm4py-1.2.6/pm4py/__init__.py
@@ -0,0 +1,12 @@
+import time
+
+time.clock = time.process_time
+
+from pm4py import algo, evaluation, objects, util, visualization, statistics, streaming
+
+__version__ = '1.2.6'
+__doc__ = "Process Mining for Python"
+__author__ = 'Fraunhofer Institute for Applied Technology'
+__author_email__ = 'pm4py@pads.rwth-aachen.de'
+__maintainer__ = 'Fraunhofer Institute for Applied Technology'
+__maintainer_email__ = "pm4py@pads.rwth-aachen.de"
