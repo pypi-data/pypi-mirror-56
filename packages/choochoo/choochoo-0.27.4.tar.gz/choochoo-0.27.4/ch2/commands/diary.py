898ce56c389e6c8b55cc8315b5cf3b9cd69db831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:51 +0000

    Add package choochoo/0.27.4/choochoo-0.27.4.tar.gz

---
+++ packages/choochoo/choochoo-0.27.4.tar.gz/choochoo-0.27.4/ch2/commands/diary.py
@@ -0,0 +1,298 @@
+
+import datetime as dt
+from abc import abstractmethod
+from logging import getLogger
+
+from sqlalchemy import or_
+from urwid import MainLoop, Columns, Pile, Frame, Filler, Text, Divider, WEIGHT, connect_signal, Padding
+
+from .args import DATE, SCHEDULE, FAST
+from ..lib.date import to_date
+from ..lib.io import tui
+from ..lib.schedule import Schedule
+from ..lib.utils import PALETTE_RAINBOW, em, label
+from ..lib.widgets import DateSwitcher
+from ..squeal import PipelineType, Topic, TopicJournal
+from ..squeal.database import ActivityJournal, StatisticJournal
+from ..squeal.utils import add
+from ..stoats.display import display_pipeline
+from ..stoats.display.nearby import nearby_any_time, fmt_nearby
+from ..stoats.pipeline import run_pipeline
+from ..uranus.server import set_controller_session
+from ..uranus.template.activity_details import activity_details
+from ..uranus.template.all_activities import all_activities
+from ..uranus.template.compare_activities import compare_activities
+from ..uranus.template.health import health
+from ..uranus.template.similar_activities import similar_activities
+from ..uweird.fields import PAGE_WIDTH
+from ..uweird.fields.summary import summary_columns
+from ..uweird.tui.decorators import Border, Indent
+from ..uweird.tui.factory import Factory
+from ..uweird.tui.fixed import Fixed
+from ..uweird.tui.tabs import TabList
+from ..uweird.tui.widgets import DividedPile, ArrowMenu, SquareButton
+
+log = getLogger(__name__)
+
+
+@tui
+def diary(args, db):
+    '''
+## diary
+
+    > ch2 diary [DATE]
+
+The date can be an absolute day or the number of days previous.  So `ch2 diary 1` selects yesterday.
+
+Display the daily diary.  Enter information here.
+
+To exit, alt-q (or, without saving, alt-x).
+
+    > ch2 diary (--month | --year | --schedule SCHEDULE) [DATE}
+
+Display a summary for the month / year / schedule.
+    '''
+    date, schedule = args[DATE], args[SCHEDULE]
+    if not date:
+        date = dt.date.today()
+    else:
+        days = None
+        try:
+            # try int first because we need to separate the case of days from years
+            days = int(date)
+            if days > 1000:
+                days = None
+        except ValueError:
+            pass
+        if days is None:
+            date = to_date(date)
+        else:
+            date = dt.date.today() - dt.timedelta(days=days)
+    with db.session_context() as s:
+        TopicJournal.check_tz(log, s)
+    if schedule:
+        schedule = Schedule(schedule)
+        if schedule.start or schedule.finish:
+            raise Exception('Schedule must be open (no start or finish)')
+        MainLoop(ScheduleDiary(log, db, date, schedule), palette=PALETTE_RAINBOW).run()
+    else:
+        MainLoop(DailyDiary(log, db, date), palette=PALETTE_RAINBOW).run()
+        if not args[FAST]:
+            print('\n  Please wait while statistics are updated...')
+            run_pipeline(db, PipelineType.STATISTIC)
+            print('  ...done (thanks!)\n')
+
+
+class Diary(DateSwitcher):
+
+    def __init__(self, log, db, date):
+        super().__init__(log, db, date)
+
+    def save(self):
+        s = self._session
+        self._log.info(f'{bool(s)}')
+        if s:
+            self.__clean(s, s.dirty, delete=True)
+            self.__clean(s, s.new, delete=False)
+        super().save()
+
+    def __clean(self, s, instances, delete=False):
+        self._log.debug(f'{len(instances)}')
+        for instance in instances:
+            if isinstance(instance, StatisticJournal):
+                if instance.value == None:
+                    self._log.debug(f'Discarding {instance}')
+                    if delete:
+                        s.delete(instance)
+                    else:
+                        s.expunge(instance)
+
+    def _build(self, s):
+        self._log.debug('Building diary at %s' % self._date)
+        body, f = [], Factory(TabList())
+        root_topics = list(self._topics(s))
+        for topic in root_topics:
+            body.append(self.__display_topic(s, f, topic))
+        for extra in self._display_pipeline(s, f):
+            body.append(extra)
+        for extra in self._display_gui(s, f):
+            body.append(extra)
+        self._check_body(body)
+        body = Border(Frame(Filler(DividedPile(body), valign='top'),
+                            header=Pile([self._header(), Divider()]),
+                            footer=Pile([Divider(), Text(self.__footer(), align='center')])))
+        return body, f.tabs
+
+    @abstractmethod
+    def _header(self):
+        pass
+
+    def _check_body(self, body):
+        pass
+
+    def __footer(self):
+        footer = ['meta-', em('q'), 'uit/e', em('x'), 'it/', em('s'), 'ave']
+        footer += [' [shift]meta-']
+        for sep, c in enumerate('dwmya'):
+            if sep: footer += ['/']
+            footer += [em(c)]
+        footer += ['ctivity/', em('t'), 'oday']
+        return footer
+
+    def __display_topic(self, s, f, topic):
+        self._log.debug('%s' % topic)
+        body, title = [], None
+        if topic.name:
+            title = Text(topic.name)
+        if topic.description:
+            body.append(Text(topic.description))
+        body += list(self._display_fields(s, f, topic))
+        body += list(self.__display_children(s, f, topic))
+        if not body:
+            return title
+        body = Indent(Pile(body))
+        if title:
+            body = Pile([title, body])
+        return body
+
+    def __display_children(self, s, f, topic):
+        for child in topic.children:
+            if self._filter_child(child):
+                extra = self.__display_topic(s, f, child)
+                if extra:
+                    yield extra
+
+    def _filter_child(self, child):
+        return child.schedule.at_location(self._date)
+
+    def _display_gui(self, s, f):
+        yield from []
+
+
+class DailyDiary(Diary):
+    '''
+    Render the diary at a given date.
+    '''
+
+    def _topics(self, s):
+        for topic in s.query(Topic).filter(Topic.parent == None,
+                                           or_(Topic.start <= self._date, Topic.start == None),
+                                           or_(Topic.finish >= self._date, Topic.finish == None)). \
+                order_by(Topic.sort).all():
+            if topic.schedule.at_location(self._date):
+                yield topic
+
+    def _header(self):
+        return Text(self._date.strftime('%Y-%m-%d - %A'))
+
+    def _display_fields(self, s, f, topic):
+        columns, width = [], 0
+        tjournal = self.__topic_journal(s, topic)
+        tjournal.populate(self._log, s)
+        for field in topic.fields:
+            if field in tjournal.statistics:  # might be outside schedule
+                self._log.debug('%s' % field)
+                display = field.display_cls(self._log, tjournal.statistics[field],
+                                            *field.display_args, **field.display_kargs)
+                self._log.debug('%s' % display)
+                if width + display.width > PAGE_WIDTH:
+                    yield Columns(columns)
+                    columns, width = [], 0
+                columns.append((WEIGHT, display.width, f(display.widget())))
+                width += display.width
+        if width:
+            yield Columns(columns)
+
+    def __topic_journal(self, s, topic):
+        tjournal = s.query(TopicJournal). \
+            filter(TopicJournal.topic == topic,
+                   TopicJournal.date == self._date).one_or_none()
+        if not tjournal:
+            tjournal = add(s, TopicJournal(topic=topic, date=self._date))
+        return tjournal
+
+    def _display_pipeline(self, s, f):
+        yield from display_pipeline(s, f, self._date, self)
+
+    def _display_gui(self, s, f):
+        menus = list(self.__gui_menus(s, f))
+        if menus:
+            yield Pile([Text('Jupyter'), Indent(Pile(menus))])
+
+    def __gui_menus(self, s, f):
+        for aj1 in ActivityJournal.at_date(s, self._date):
+            options = [(None, 'None')] + [(aj2, fmt_nearby(aj2, nb)) for aj2, nb in nearby_any_time(s, aj1)]
+            menu = ArrowMenu(label('%s v ' % aj1.name), dict(options))
+            connect_signal(menu, 'click', self.__show_gui, user_args=[s, aj1])
+            button = SquareButton('All Similar')
+            connect_signal(button, 'click', self.__show_similar, user_args=[s, aj1])
+            yield Columns([f(menu), f(Padding(Fixed(button, 13), width='clip'))])
+        button = SquareButton('Health')
+        connect_signal(button, 'click', self.__show_health, user_args=[s, self._date])
+        yield f(Padding(Fixed(button, 8), width='clip'))
+
+    def __show_gui(self, s, aj1, w):
+        set_controller_session(s)
+        if w.state:
+            compare_activities(aj1.start, w.state.start, aj1.activity_group.name)
+        else:
+            activity_details(aj1.start, aj1.activity_group.name)
+
+    def __show_similar(self, s, aj1, w):
+        set_controller_session(s)
+        similar_activities(aj1.start, aj1.activity_group.name)
+
+    def __show_health(self, s, date, w):
+        log.debug(f'w {w} s {s} date {date}')
+        set_controller_session(s)
+        health()
+
+
+class ScheduleDiary(Diary):
+    '''
+    Display summary data for the given schedule.
+    '''
+
+    def __init__(self, log, db, date, schedule):
+        self._schedule = schedule
+        super().__init__(log, db, self._refine_new_date(date))
+
+    def _header(self):
+        return Text(self._date.strftime('%Y-%m-%d') + ' - Summary for %s' % self._schedule.describe())
+
+    def _check_body(self, body):
+        if len(body) < 4:
+            body.append(Indent(Text('Updating the database automatically deletes summary statistics that cover '
+                                    + 'the modified data.  You probably need to re-generate the statistics by '
+                                    + 'running `ch2 statistics`.')))
+
+    def _refine_new_date(self, date):
+        return self._schedule.start_of_frame(date)
+
+    def _topics(self, s):
+        finish = self._schedule.next_frame(self._date)
+        return s.query(Topic).filter(Topic.parent == None,
+                                     or_(Topic.start < finish, Topic.start == None),
+                                     or_(Topic.finish >= self._date, Topic.finish == None)). \
+            order_by(Topic.sort).all()
+
+    def _filter_child(self, child):
+        finish = self._schedule.next_frame(self._date)
+        return (child.start is None or child.start < finish) and (child.finish is None or child.finish > self._date)
+
+    def _display_fields(self, s, f, topic):
+        names = [field.statistic_name for field in topic.fields]
+        yield from summary_columns(self._log, s, f, self._date, self._schedule, names)
+
+    def _display_pipeline(self, s, f):
+        yield from display_pipeline(s, f, self._date, self, schedule=self._schedule)
+
+    def _display_gui(self, s, f):
+        set_controller_session(s)
+        button = SquareButton('All Activities')
+        connect_signal(button, 'click', self.__show_all)
+        yield Pile([Text('Jupyter'), Indent(f(Padding(Fixed(button, 16), width='clip')))])
+
+    def __show_all(self, w):
+        finish = self._schedule.next_frame(self._date)
+        all_activities(self._date.strftime('%Y-%m-%d'), finish.strftime('%Y-%m-%d'))
---
+++ packages/choochoo/choochoo-0.27.4.tar.gz/choochoo-0.27.4/ch2/data/response.py
@@ -0,0 +1,123 @@
+
+import datetime as dt
+from logging import getLogger
+
+from numpy import polyval, polyfit
+from pandas import DataFrame, Series
+from scipy import optimize
+
+from . import inplace_decay
+from ..stoats.names import FITNESS_D_ANY, FATIGUE_D_ANY, like, _d
+
+log = getLogger(__name__)
+
+IMPULSE_3600 = 'Impulse / 3600s'
+RESPONSE = 'Response'
+
+
+# NOTE - almost everything below uses Series, not DataFrame
+
+
+def sum_to_hour(source, column):
+    data = source.resample('1h', label='right').sum()
+    data = data.loc[:, [column]]
+    data.rename(columns={column: IMPULSE_3600}, inplace=True)
+    initial = DataFrame({IMPULSE_3600: 0}, index=[data.index[0] - dt.timedelta(hours=1)])
+    data = initial.append(data, sort=True)
+    return data
+
+
+def calc_response(data, log10_period):
+    # data is a DataFrame mainly because that's what inplace_decay takes
+    response = data.rename(columns={IMPULSE_3600: RESPONSE})  # copy
+    inplace_decay(response, RESPONSE, 10 ** log10_period)
+    return response[RESPONSE]
+
+
+def calc_measured(response, performances):
+    # we're only interested in the FF model at the times that correspond to performance measurements
+    return [response.reindex(index=performance.index, method='nearest')
+            for performance in performances]
+
+
+def calc_predicted(measureds, performances):
+    # this is a bit tricky.  we don't know the exact relationship between 'fitness' and how fast we are.
+    # all we know is that it should track changes in a useful manner.
+    # so there's some arbitrary transform, which could quite easily be different for different routes
+    # (no reason 'fitness' should numerically predict the speed on *all* routes, obviously).
+    # so we assume that there's an arbitrary linear transform (scaling and offset) from measured speed
+    # (or whatever 'performance' is) to 'fitness'.
+    # we could fit for the best transform as we fit for the period, but it's more efficient to calculate
+    # the 'best' transform for a given period.  this is more efficient because it's equivalent to line fitting
+    # (that linear transform is y = ax + b).  so we can call the (fast) line fitting routine in numpy.
+    return [Series(polyval(polyfit(performance, measured, 1), performance),
+                   performance.index)
+            for measured, performance in zip(measureds, performances)]
+
+
+def calc_residuals(measureds, predicteds, method='L1'):
+    if method == 'L1':
+        # use L1 scaled by amplitude to be reasonably robust.
+        return [abs(measured - predicted) / measured.clip(lower=1e-6)
+                for measured, predicted in zip(measureds, predicteds)]
+    elif method == 'L2':
+        # something like chisq
+        return [((measured - predicted).pow(2) / measured.clip(lower=1e-6))
+                for measured, predicted in zip(measureds, predicteds)]
+    else:
+        raise Exception(f'Unknown method ({method}) - use L1 or L2')
+
+
+def calc_cost(measureds, predicteds, method='L1'):
+    residuals = calc_residuals(measureds, predicteds, method=method)
+    return sum(sum(residual) for residual in residuals)
+
+
+def worst_index(residuals):
+    worst, index = None, None
+    for i, residual in enumerate(residuals):
+        bad = max(residual)
+        if index is None or bad > worst:
+            worst, index = bad, i
+    return index
+
+
+def reject_worst_inplace(log10_period, data, performances, method='L1'):
+    response = calc_response(data, log10_period)
+    measureds = calc_measured(response, performances)
+    predicteds = calc_predicted(measureds, performances)
+    residuals = calc_residuals(measureds, predicteds, method=method)
+    index = worst_index(residuals)
+    print(f'Dropping value at {residuals[index].idxmax()}')
+    performances[index].drop(index=residuals[index].idxmax(), inplace=True)
+
+
+def fit_period(data, log10_period, performances, method='L1', reject=0, **kargs):
+    # data should be a DataFrame with an IMPULSE3600 entry
+    # performances should be Series
+
+    result = None
+
+    def cost(log10_period):
+        response = calc_response(data, log10_period)
+        measureds = calc_measured(response, performances)
+        predicteds = calc_predicted(measureds, performances)
+        return calc_cost(measureds, predicteds, method=method)
+
+    while True:
+        result = optimize.minimize(cost, [log10_period], **kargs)
+        log10_period = result.x[0]
+        print(10 ** log10_period)
+        if not reject: break
+        reject_worst_inplace(log10_period, data, performances, method=method)
+        reject -= 1
+
+    return result
+
+
+def response_stats(df):
+    stats = {}
+    for pattern in FITNESS_D_ANY, FATIGUE_D_ANY:
+        for name in like(pattern, df.columns):
+            stats[_d(name)] = df[name][-1] - df[name][0]
+    return stats
