018627c993f89d563c35936fb5b9ac281e0b5967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:37 +0000

    Add package prefect/0.7.3/prefect-0.7.3-py3-none-any.whl

---
+++ packages/prefect/prefect-0.7.3-py3-none-any.whl/prefect/utilities/exceptions.py
@@ -0,0 +1,22 @@
+class PrefectError(Exception):
+    pass
+
+
+class ContextError(KeyError, PrefectError):
+    pass
+
+
+class SerializationError(PrefectError):
+    pass
+
+
+class PrefectWarning(UserWarning):
+    pass
+
+
+class ClientError(PrefectError):
+    pass
+
+
+class AuthorizationError(ClientError):
+    pass
