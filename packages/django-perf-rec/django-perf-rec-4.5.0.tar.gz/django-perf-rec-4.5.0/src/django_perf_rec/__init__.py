b3201bfcd1288ac2101b63114de20325f7331c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:46 +0000

    Add package django-perf-rec/4.5.0/django-perf-rec-4.5.0.tar.gz

---
+++ packages/django-perf-rec/django-perf-rec-4.5.0.tar.gz/django-perf-rec-4.5.0/src/django_perf_rec/__init__.py
@@ -0,0 +1,12 @@
+"""
+isort:skip_file
+"""
+try:
+    import pytest
+except ImportError:
+    pytest = None
+
+if pytest is not None:
+    pytest.register_assert_rewrite("django_perf_rec.api")
+
+from .api import TestCaseMixin, get_record_name, get_perf_path, record  # noqa: F401
---
+++ packages/django-perf-rec/django-perf-rec-4.5.0.tar.gz/django-perf-rec-4.5.0/tests/test_orm.py
@@ -0,0 +1,21 @@
+import django
+import pytest
+from django.db.models import Q
+from django.test import SimpleTestCase
+
+from django_perf_rec.orm import patch_ORM_to_be_deterministic
+
+
+class PatchORMToBeDeterministicTests(SimpleTestCase):
+    def test_call_it(self):
+        patch_ORM_to_be_deterministic()
+
+    def test_call_it_again(self):
+        patch_ORM_to_be_deterministic()
+
+    @pytest.mark.skipif(django.VERSION < (2, 0), reason="Django 2.0+")
+    def test_q_connector(self):
+        q1 = Q(foo="bar") | Q(bar="foo")
+        _path, args, kwargs = q1.deconstruct()
+        q2 = Q(*args, **kwargs)
+        self.assertEqual(q1, q2)
