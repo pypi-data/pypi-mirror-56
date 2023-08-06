bf39025b31b7ec731adfa849ca0db346b1434bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:17 +0000

    Add package futura/0.0.2/futura-0.0.2-py3-none-any.whl

---
+++ packages/futura/futura-0.0.2-py3-none-any.whl/futura/__init__.py
@@ -0,0 +1,41 @@
+import logging
+# import wurst
+import wurst
+import futura.wurst_monkeypatch as wmp
+#do monkeypatching here
+
+wurst.relink_technosphere_exchanges = wmp.relink_technosphere_exchanges
+wurst.transformations.relink_technosphere_exchanges = wmp.relink_technosphere_exchanges
+wurst.transformations.geo.relink_technosphere_exchanges = wmp.relink_technosphere_exchanges
+wurst.transformations.geo.allocate_inputs = wmp.allocate_inputs
+
+#create w alias for wurst
+w = wurst
+
+# set up logging to file - see previous section for more details
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename=r'C:\temp\futura.log',
+                    filemode='w')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.WARNING)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+
+logger1 = logging.getLogger('futura')
+
+
+def log(MESSAGE, *args, **kwargs):
+    logger1.info(MESSAGE, *args, **kwargs)
+
+
+def warn(MESSAGE, *args, **kwargs):
+    logger1.warning(MESSAGE, *args, **kwargs)
+
+warn('Futura is using a monkeypatched version of Wurst')
---
+++ packages/futura/futura-0.0.2-py3-none-any.whl/futura_ui/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import os
+
+from .app import Application, run_futura
+
+PACKAGE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
