e73abf6da7ed00e2cebf422ef3dbcace018dbeb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:51 +0000

    Add package project-demo/0.1.0/project-demo-0.1.0.tar.gz

---
+++ packages/project-demo/project-demo-0.1.0.tar.gz/project-demo-0.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['project_demo']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pendulum>=2.0,<3.0']
+
+setup_kwargs = {
+    'name': 'project-demo',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Daniel Eitan',
+    'author_email': 'daniel.eitan@whitesourcesoftware.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
