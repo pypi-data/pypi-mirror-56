cc19476a9ea80bab11a13585c924cf93cc0f5e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:51 +0000

    Add package core-oaipmh-harvester-app/1.6.0/core_oaipmh_harvester_app-1.6.0.tar.gz

---
+++ packages/core-oaipmh-harvester-app/core_oaipmh_harvester_app-1.6.0.tar.gz/core_oaipmh_harvester_app-1.6.0/tests/test_settings.py
@@ -0,0 +1,26 @@
+from os.path import dirname, realpath
+
+from core_main_app.utils.databases.mongoengine_database import Database
+
+SECRET_KEY = 'fake-key'
+
+INSTALLED_APPS = [
+    # Django apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+    # Local apps
+    "core_main_app",
+    "tests",
+]
+
+OAI_HARVESTER_ROOT = dirname(realpath(__file__))
+
+SSL_CERTIFICATES_DIR = True
+
+MOCK_DATABASE_NAME = 'db_mock'
+MOCK_DATABASE_HOST = 'mongomock://localhost'
+
+database = Database(MOCK_DATABASE_HOST, MOCK_DATABASE_NAME)
+database.connect()
