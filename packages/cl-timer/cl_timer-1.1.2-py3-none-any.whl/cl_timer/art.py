faaa25ab25cf8966066875258e0110dce7b3758e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:05 +0000

    Add package cl-timer/1.1.2/cl_timer-1.1.2-py3-none-any.whl

---
+++ packages/cl-timer/cl_timer-1.1.2-py3-none-any.whl/cl_timer/art.py
@@ -0,0 +1,52 @@
+DECIMAL_POINT = ' \n \n.\n '
+
+
+DIGITS = [
+    ' __ \n|  |\n|__|\n    ',
+    '    \n|   \n|   \n    ',
+    ' __ \n __|\n|__ \n    ',
+    ' __ \n __|\n __|\n    ',
+    '    \n|__|\n   |\n    ',
+    ' __ \n|__ \n __|\n    ',
+    ' __ \n|__ \n|__|\n    ',
+    ' __ \n|  |\n   |\n    ',
+    ' __ \n|__|\n|__|\n    ',
+    ' __ \n|__|\n   |\n    ',
+]
+
+
+DISCLAIMER = '\
+The timer starts a few moments after you let go of the spacebar,\nbut immedia\
+tely afterwards it catches up to the correct times.\n\nKnow that the time disp\
+layed at the end to each solve is accurate\ndown to the hundredth of a second,\
+ as are all of the other stats\nmade available by this timer.\n\nThanks for re\
+ading, and have fun!'
+
+
+
+STARTING_TIME = ' __     __   __\n|  |   |  | |  |\n|__| . |__| |__|'
+
+
+STATS = '\
+STATS FOR SOLVE %s\n\nTime: %s\nAverage of 5: %s\nAverage of 12: %s\nScramble:\
+ %s\n\n\nPress any key to exit'
+
+
+
+TIMER_BACKGROUND = '\
+------------------------------------------------\n|                          \
+                    |\n|                                              |\n|    \
+                                          |\n|                                \
+              |\n|                                              |\n|          \
+                                    |\n---------------------------------------\
+---------'
+
+
+TITLE_ART = '\
+              #  #  #  #\n        # #      |      .  .\n      #          |    \
+      .\n     #           |           .\n     #           |           .\n    #\
+            |            .\n    #            .-------     .\n    #            \
+             .    Timer\n    #                         .\n     #              \
+         .\n     #                       .\n      #                     .\n   \
+     # #            .  .\n            #  #  #  # \n\n\npress any key to contin\
+ue\n'
