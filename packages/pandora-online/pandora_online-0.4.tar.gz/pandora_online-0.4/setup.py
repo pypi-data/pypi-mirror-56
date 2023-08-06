f90c9137b5625da954d611e22bb370d02d6c3d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:01 +0000

    Add package pandora-online/0.4/pandora_online-0.4.tar.gz

---
+++ packages/pandora-online/pandora_online-0.4.tar.gz/pandora_online-0.4/setup.py
@@ -0,0 +1,17 @@
+
+
+
+from setuptools import setup, find_packages
+
+setup(name='pandora_online',
+      version='0.4',
+      description='The Pandora Online Class Helper',
+      long_description='Really, this class help for pro.p-on.ru requests.',
+      keywords='pandora pandect',
+      url='http://github.com/vladimir1marchenko/pandora_oline',
+      author='Vladimir Marchenko',
+      author_email='vladimir1marchenko@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
