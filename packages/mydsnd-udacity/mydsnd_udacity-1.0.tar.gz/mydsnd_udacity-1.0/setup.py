c2075c02090e3724f749c83b08892ceac9d0a79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:20 +0000

    Add package mydsnd-udacity/1.0/mydsnd_udacity-1.0.tar.gz

---
+++ packages/mydsnd-udacity/mydsnd_udacity-1.0.tar.gz/mydsnd_udacity-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mydsnd_udacity',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['mydsnd_udacity'],
+      author = ['Ante Kristic'],
+      zip_safe=False)
