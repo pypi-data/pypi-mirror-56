1c9c131c522af721f42256012fd60fa7a61b8714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:13 +0000

    Add package talos-api/1.3.0/talos-api-1.3.0.tar.gz

---
+++ packages/talos-api/talos-api-1.3.0.tar.gz/talos-api-1.3.0/talos/template/tpl_tox_ini.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+TEMPLATE = u'''# Tox (https://tox.readthedocs.io/) is a tool for running tests
+# in multiple virtualenvs. This configuration file will run the
+# test suite on all supported python versions. To use it, "pip install tox"
+# and then run "tox" from this directory.
+
+[tox]
+envlist = py27, py36
+
+[testenv]
+commands = python setup.py test
+deps =
+    pytest<4.1
+    pytest-runner
+    pytest-html
+    pytest-cov
+'''
