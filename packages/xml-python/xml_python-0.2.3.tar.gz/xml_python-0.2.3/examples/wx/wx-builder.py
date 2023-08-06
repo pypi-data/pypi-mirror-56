9c539a6b5212c55b386f75f479aac55503d8cec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:01 +0000

    Add package xml-python/0.2.3/xml_python-0.2.3.tar.gz

---
+++ packages/xml-python/xml_python-0.2.3.tar.gz/xml_python-0.2.3/examples/wx/wx-builder.py
@@ -0,0 +1,20 @@
+import wx
+
+from xml_python.ext.wx import builder
+
+if __name__ == '__main__':
+    a = wx.App()
+    f = builder.from_args('frame.xml')
+
+    def login():
+        username = f.username.GetValue()
+        password = f.password.GetValue()
+        wx.MessageBox(
+            f'Pretending to login as {username}:{password}.',
+            caption='Pretend Login', parent=f
+        )
+
+    f.login = login
+    f.Show(True)
+    f.Maximize()
+    a.MainLoop()
