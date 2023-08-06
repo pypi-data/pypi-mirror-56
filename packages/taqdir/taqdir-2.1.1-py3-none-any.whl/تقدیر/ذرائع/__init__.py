b5a6e1033ced070a58e545ffb5c260449f3d50e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:42 +0000

    Add package taqdir/2.1.1/taqdir-2.1.1-py3-none-any.whl

---
+++ packages/taqdir/taqdir-2.1.1-py3-none-any.whl/تقدیر/ذرائع/__init__.py
@@ -0,0 +1,7 @@
+from .جمع import جمع
+from .جےسن import جےسن
+from .دیسات import دیسات, دیسات_ماھانہ
+from .سی_اس_وی import سی_اس_وی
+from .مرکسم٥_جال import مرکسم۵_جال
+from .مرکسم۵ import مرکسم۵
+from .ناسا import ناسا
