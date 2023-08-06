a20f197102c3bcd8e9489984d61b94fb5208c509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:24 +0000

    Add package slither-analyzer/0.6.8/slither-analyzer-0.6.8.tar.gz

---
+++ packages/slither-analyzer/slither-analyzer-0.6.8.tar.gz/slither-analyzer-0.6.8/slither/core/declarations/pragma_directive.py
@@ -0,0 +1,25 @@
+from slither.core.source_mapping.source_mapping import SourceMapping
+
+class Pragma(SourceMapping):
+
+    def __init__(self, directive):
+        super(Pragma, self).__init__()
+        self._directive = directive
+
+    @property
+    def directive(self):
+        '''
+            list(str)
+        '''
+        return self._directive
+
+    @property
+    def version(self):
+        return ''.join(self.directive[1:])
+
+    @property
+    def name(self):
+        return self.version
+
+    def __str__(self):
+        return 'pragma '+''.join(self.directive)
