02eb1d08cdff289eca7cc87962e7c68739ebdf41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:23 +0000

    Add package dendroid/0.2.0/dendroid-0.2.0-py3-none-any.whl

---
+++ packages/dendroid/dendroid-0.2.0-py3-none-any.whl/dendroid/hints.py
@@ -0,0 +1,10 @@
+from typing import TypeVar
+
+from lz import sorting
+from lz.hints import Sortable
+
+Domain = TypeVar('Domain')
+OtherDomain = TypeVar('OtherDomain')
+
+Sortable = Sortable
+SortingKey = sorting.Key
