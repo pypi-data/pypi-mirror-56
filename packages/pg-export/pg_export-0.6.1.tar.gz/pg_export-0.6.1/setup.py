73925aea963ec39ed38f75466b1bbaeb3fcb5205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:11 +0000

    Add package pg-export/0.6.1/pg_export-0.6.1.tar.gz

---
+++ packages/pg-export/pg_export-0.6.1.tar.gz/pg_export-0.6.1/setup.py
@@ -0,0 +1,28 @@
+import os
+
+from distutils.core import setup
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+def get_packages(dirs):
+    packages = []
+    for dir in dirs:
+        for dirpath, dirnames, filenames in os.walk(dir):
+            if '__init__.py' in filenames:
+                packages.append(dirpath)
+    return packages
+
+setup(name = "pg_export",
+      description="pg_dump -> repo",
+      license="""BSD""",
+      version = "0.6.1",
+      maintainer = "Dima Beloborodov",
+      maintainer_email = "d.beloborodov@gmail.com",
+      url = "http://comagic.ru",
+      scripts = ['bin/pg_export'],
+      packages = get_packages(['pg_export']),
+      install_requires = [
+          "psycopg2"
+      ])
