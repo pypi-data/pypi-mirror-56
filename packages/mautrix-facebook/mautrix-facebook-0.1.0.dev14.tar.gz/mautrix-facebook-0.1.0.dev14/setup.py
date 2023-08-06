d38f074193505038996aa9f9176b294cb6fd0c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:35 +0000

    Add package mautrix-facebook/0.1.0.dev14/mautrix-facebook-0.1.0.dev14.tar.gz

---
+++ packages/mautrix-facebook/mautrix-facebook-0.1.0.dev14.tar.gz/mautrix-facebook-0.1.0.dev14/setup.py
@@ -0,0 +1,54 @@
+import setuptools
+import glob
+import mautrix_facebook
+
+try:
+    long_desc = open("README.md").read()
+except IOError:
+    long_desc = "Failed to read README.md"
+
+setuptools.setup(
+    name="mautrix-facebook",
+    version=mautrix_facebook.__version__,
+    url="https://github.com/tulir/mautrix-facebook",
+
+    author="Tulir Asokan",
+    author_email="tulir@maunium.net",
+
+    description="A Matrix-Facebook Messenger puppeting bridge.",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[
+        "aiohttp>=3.0.1,<4",
+        "mautrix>=0.4.0.dev70,<0.5.0",
+        "ruamel.yaml>=0.15.94,<0.17",
+        "commonmark>=0.8,<0.10",
+        "python-magic>=0.4,<0.5",
+        "fbchat-asyncio>=0.3.0rc3",
+        "SQLAlchemy>=1.2,<2",
+        "alembic>=1,<2",
+    ],
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Topic :: Communications :: Chat",
+        "Framework :: AsyncIO",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    entry_points="""
+        [console_scripts]
+        mautrix-facebook=mautrix_facebook.__main__:main
+    """,
+    data_files=[
+        (".", ["example-config.yaml", "alembic.ini"]),
+        ("alembic", ["alembic/env.py"]),
+        ("alembic/versions", glob.glob("alembic/versions/*.py"))
+    ],
+)
