352ee8ed4d8aa52ae468fe88c9a084c9a8035696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:18 +0000

    Add package paco.models/6.2.0/paco.models-6.2.0.tar.gz

---
+++ packages/paco.models/paco.models-6.2.0.tar.gz/paco.models-6.2.0/src/paco/models/exceptions.py
@@ -0,0 +1,26 @@
+class InvalidAimFieldType(Exception):
+    pass
+
+class InvalidAimSchema(Exception):
+    pass
+
+class InvalidAimProjectFile(Exception):
+    pass
+
+class UnusedAimProjectField(Exception):
+    pass
+
+class InvalidAimReference(Exception):
+    pass
+
+class InvalidCFNMapping(Exception):
+    pass
+
+class InvalidAimBucket(Exception):
+    pass
+
+class InvalidAWSResourceName(Exception):
+    pass
+
+class TroposphereConversionError(Exception):
+    pass
