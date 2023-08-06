bdc804d315e7aae9e7f00e7f315bcbb3cf70b5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:48 +0000

    Add package publ/0.5.10/Publ-0.5.10-py3-none-any.whl

---
+++ packages/publ/Publ-0.5.10-py3-none-any.whl/publ/index.py
@@ -0,0 +1,227 @@
+# index.py
+''' Content indexer '''
+
+import concurrent.futures
+import logging
+import os
+import threading
+
+import watchdog.events
+import watchdog.observers
+from pony import orm
+
+from . import category, entry, model, utils
+
+LOGGER = logging.getLogger(__name__)
+
+ENTRY_TYPES = ['.md', '.htm', '.html']
+CATEGORY_TYPES = ['.cat', '.meta']
+
+THREAD_POOL = concurrent.futures.ThreadPoolExecutor(
+    max_workers=1,
+    thread_name_prefix="Indexer")
+
+# Get the _work_queue attribute from the pool, if any
+WORK_QUEUE = getattr(THREAD_POOL, '_work_queue', None)
+
+
+class ConcurrentSet:
+    """ Simple quasi-atomic set """
+
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.set = set()
+
+    def add(self, item):
+        """ Add an item to the set, and return whether it was newly added """
+        with self.lock:
+            if item in self.set:
+                return False
+            self.set.add(item)
+            return True
+
+    def remove(self, item):
+        """ Remove an item from the set, returning if it was present """
+        with self.lock:
+            if item in self.set:
+                self.set.remove(item)
+                return True
+            return False
+
+
+SCHEDULED_FILES = ConcurrentSet()
+
+
+def last_modified():
+    """ information about the most recently modified file """
+    files = model.FileFingerprint.select().order_by(
+        orm.desc(model.FileFingerprint.file_mtime))
+    for file in files:
+        return file.file_mtime, file.file_path
+    return None, None
+
+
+def queue_length():
+    """ Get the approximate length of the indexer work queue """
+    return WORK_QUEUE.qsize() if WORK_QUEUE else None
+
+
+def in_progress():
+    """ Return if there's an index in progress """
+    return queue_length() > 0
+
+
+def scan_file(fullpath, relpath, assign_id):
+    """ Scan a file for the index
+
+    fullpath -- The full path to the file
+    relpath -- The path to the file, relative to its base directory
+    assign_id -- Whether to assign an ID to the file if not yet assigned
+
+    This calls into various modules' scanner functions; the expectation is that
+    the scan_file function will return a truthy value if it was scanned
+    successfully, False if it failed, and None if there is nothing to scan.
+    """
+
+    LOGGER.debug("Scanning file: %s (%s) %s", fullpath, relpath, assign_id)
+
+    def do_scan():
+        """ helper function to do the scan and gather the result """
+        _, ext = os.path.splitext(fullpath)
+
+        try:
+            if ext in ENTRY_TYPES:
+                LOGGER.info("Scanning entry: %s", fullpath)
+                return entry.scan_file(fullpath, relpath, assign_id)
+
+            if ext in CATEGORY_TYPES:
+                LOGGER.info("Scanning meta info: %s", fullpath)
+                return category.scan_file(fullpath, relpath)
+
+            return None
+        except:  # pylint: disable=bare-except
+            LOGGER.exception("Got error parsing %s", fullpath)
+            return False
+
+    result = do_scan()
+    if result is False and not assign_id:
+        LOGGER.info("Scheduling fixup for %s", fullpath)
+        THREAD_POOL.submit(scan_file, fullpath, relpath, True)
+    else:
+        LOGGER.debug("%s complete", fullpath)
+        if result:
+            set_fingerprint(fullpath)
+        SCHEDULED_FILES.remove(fullpath)
+
+
+@orm.db_session
+def get_last_fingerprint(fullpath):
+    """ Get the last known modification time for a file """
+    record = model.FileFingerprint.get(file_path=fullpath)
+    if record:
+        return record.fingerprint
+    return None
+
+
+@orm.db_session(immediate=True)
+def set_fingerprint(fullpath, fingerprint=None):
+    """ Set the last known modification time for a file """
+    try:
+        fingerprint = fingerprint or utils.file_fingerprint(fullpath)
+
+        record = model.FileFingerprint.get(file_path=fullpath)
+        if record:
+            record.set(fingerprint=fingerprint,
+                       file_mtime=os.stat(fullpath).st_mtime)
+        else:
+            record = model.FileFingerprint(
+                file_path=fullpath,
+                fingerprint=fingerprint,
+                file_mtime=os.stat(fullpath).st_mtime)
+        orm.commit()
+    except FileNotFoundError:
+        orm.delete(fp for fp in model.FileFingerprint if fp.file_path == fullpath)
+
+
+class IndexWatchdog(watchdog.events.PatternMatchingEventHandler):
+    """ Watchdog handler """
+
+    def __init__(self, content_dir):
+        super().__init__(ignore_directories=True)
+        self.content_dir = content_dir
+
+    def update_file(self, fullpath):
+        """ Update a file """
+        if SCHEDULED_FILES.add(fullpath):
+            LOGGER.debug("Scheduling reindex of %s", fullpath)
+            relpath = os.path.relpath(fullpath, self.content_dir)
+            THREAD_POOL.submit(scan_file, fullpath, relpath, False)
+
+    def on_created(self, event):
+        """ on_created handler """
+        LOGGER.debug("file created: %s", event.src_path)
+        if not event.is_directory:
+            self.update_file(event.src_path)
+
+    def on_modified(self, event):
+        """ on_modified handler """
+        LOGGER.debug("file modified: %s", event.src_path)
+        if not event.is_directory:
+            self.update_file(event.src_path)
+
+    def on_moved(self, event):
+        """ on_moved handler """
+        LOGGER.debug("file moved: %s -> %s", event.src_path, event.dest_path)
+        if not event.is_directory:
+            self.update_file(event.dest_path)
+
+    def on_deleted(self, event):
+        """ on_deleted handler """
+        LOGGER.debug("File deleted: %s", event.src_path)
+        if not event.is_directory:
+            self.update_file(event.src_path)
+
+
+def background_scan(content_dir):
+    """ Start background scanning a directory for changes """
+    observer = watchdog.observers.Observer()
+    observer.schedule(IndexWatchdog(content_dir),
+                      content_dir, recursive=True)
+    logging.info("Watching %s for changes", content_dir)
+    observer.start()
+
+
+@orm.db_session(immediate=True)
+def prune_missing(table):
+    """ Prune any files which are missing from the specified table """
+    try:
+        for item in table.select():
+            if not os.path.isfile(item.file_path):
+                LOGGER.info("File disappeared: %s", item.file_path)
+                item.delete()
+    except:  # pylint:disable=bare-except
+        LOGGER.exception("Error pruning %s", table)
+
+
+def scan_index(content_dir):
+    """ Scan all files in a content directory """
+
+    def scan_directory(root, files):
+        """ Helper function to scan a single directory """
+        try:
+            for file in files:
+                fullpath = os.path.join(root, file)
+                relpath = os.path.relpath(fullpath, content_dir)
+
+                fingerprint = utils.file_fingerprint(fullpath)
+                last_fingerprint = get_last_fingerprint(fullpath)
+                if fingerprint != last_fingerprint and SCHEDULED_FILES.add(fullpath):
+                    scan_file(fullpath, relpath, False)
+        except:  # pylint:disable=bare-except
+            LOGGER.exception("Got error parsing directory %s", root)
+
+    for root, _, files in os.walk(content_dir, followlinks=True):
+        THREAD_POOL.submit(scan_directory, root, files)
+
+    for table in (model.Entry, model.Category, model.Image, model.FileFingerprint):
+        THREAD_POOL.submit(prune_missing, table)
---
+++ packages/publ/Publ-0.5.10-py3-none-any.whl/publ/utils.py
@@ -0,0 +1,416 @@
+# utils.py
+""" Some useful utilities that don't belong anywhere else """
+
+import html
+import html.parser
+import logging
+import os
+import re
+import typing
+import urllib.parse
+
+import arrow
+import flask
+import slugify
+import werkzeug.routing
+
+from . import config, model
+
+LOGGER = logging.getLogger(__name__)
+
+
+class CallableProxy:
+    """ Wrapper class to make args possible on properties. """
+    # pylint: disable=too-few-public-methods
+
+    def __init__(self, func):
+        """ Construct the property proxy.
+
+        func -- The function to wrap
+        """
+
+        self._func = func if func else (lambda *args, **kwargs: '')
+
+    def __call__(self, *args, **kwargs):
+        # use the new kwargs to override the defaults
+        return self._func(*args, **kwargs)
+
+    def __getattr__(self, name):
+        return getattr(self(), name)
+
+    def __bool__(self):
+        return bool(self())
+
+    def __len__(self):
+        return len(self())
+
+    def __str__(self):
+        return str(self())
+
+    def __iter__(self):
+        return self().__iter__()
+
+    def __getitem__(self, key):
+        return self().__getitem__(key)
+
+
+class TrueCallableProxy(CallableProxy):
+    """ A version of CallableProxy that is always truthy """
+    # pylint: disable=too-few-public-methods
+
+    def __bool__(self):
+        return True
+
+    def __len__(self):
+        return True
+
+
+#: arrow format string for 'day' archives
+DAY_FORMAT = 'YYYY-MM-DD'
+
+#: arrow format string for 'month' archives
+MONTH_FORMAT = 'YYYY-MM'
+
+#: arrow format string for 'year' archives
+YEAR_FORMAT = 'YYYY'
+
+#: arrow format string for 'week' archives
+WEEK_FORMAT = 'YYYYMMDD_w'
+
+
+def parse_date(datestr):
+    """ Parse a date expression into a tuple of:
+
+        (start_date, span_type, span_format)
+
+    Arguments:
+        datestr -- A date specification, in the format of YYYY-MM-DD (dashes optional)
+    """
+
+    match = re.match(
+        r'([0-9]{4})(-?([0-9]{1,2}))?(-?([0-9]{1,2}))?(_w)?$', datestr)
+    if not match:
+        return (arrow.get(datestr,
+                          tzinfo=config.timezone).replace(tzinfo=config.timezone),
+                'day', 'YYYY-MM-DD')
+
+    year, month, day, week = match.group(1, 3, 5, 6)
+    start = arrow.Arrow(year=int(year), month=int(
+        month or 1), day=int(day or 1), tzinfo=config.timezone)
+
+    if week:
+        return start.span('week')[0], 'week', WEEK_FORMAT
+    if day:
+        return start, 'day', DAY_FORMAT
+    if month:
+        return start, 'month', MONTH_FORMAT
+    if year:
+        return start, 'year', YEAR_FORMAT
+
+    raise ValueError("Could not parse date: {}".format(datestr))
+
+
+def find_file(path, search_path):
+    """ Find a file by relative path. Arguments:
+
+    path -- the image's filename
+    search_path -- a list of directories to check in
+
+    Returns: the resolved file path
+    """
+
+    for relative in as_list(search_path):
+        candidate = os.path.normpath(os.path.join(relative, path))
+        if os.path.isfile(candidate):
+            return candidate
+
+    return None
+
+
+def find_entry(rel_path, search_path):
+    """ Find an entry by relative path. Arguments:
+
+    path -- the entry's filename (or entry ID)
+    search_path -- a list of directories to check in
+
+    Returns: the resolved Entry object
+    """
+
+    from . import entry  # pylint:disable=cyclic-import
+
+    try:
+        entry_id = int(rel_path)
+        record = model.Entry.get(id=entry_id)
+        if record:
+            return entry.Entry(record)
+    except ValueError:
+        pass
+
+    if rel_path.startswith('/'):
+        search_path = [config.content_folder]
+        rel_path = '.' + rel_path
+
+    for where in search_path:
+        abspath = os.path.normpath(os.path.join(where, rel_path))
+        record = model.Entry.get(file_path=abspath)
+        if record:
+            return entry.Entry(record)
+    return None
+
+
+def make_slug(title):
+    """ convert a title into a URL-friendly slug """
+    return slugify.slugify(title)
+
+
+def static_url(path, absolute=False):
+    """ Shorthand for returning a URL for the requested static file.
+
+    Arguments:
+
+    path -- the path to the file (relative to the static files directory)
+    absolute -- whether the link should be absolute or relative
+    """
+
+    if os.sep != '/':
+        path = '/'.join(path.split(os.sep))
+
+    return flask.url_for('static', filename=path, _external=absolute)
+
+
+def make_tag(name, attrs, start_end=False):
+    """ Build an HTML tag from the given name and attributes.
+
+    Arguments:
+
+    name -- the name of the tag (p, div, etc.)
+    attrs -- a dict of attributes to apply to the tag
+    start_end -- whether this tag should be self-closing
+
+    If an attribute's value is None it will be written as a standalone attribute,
+    e.g. <audio controls>. To suppress it entirely, make the value explicitly False.
+    """
+
+    text = '<' + name
+
+    if isinstance(attrs, dict):
+        attr_list = attrs.items()
+    elif isinstance(attrs, list):
+        attr_list = attrs
+    elif attrs is not None:
+        raise TypeError("Unhandled attrs type " + str(type(attrs)))
+
+    for key, val in attr_list:
+        if val is not False:
+            text += ' {}'.format(key)
+            if val is not None:
+                import markupsafe
+                escaped = html.escape(str(val), False).replace('"', '&#34;')
+
+                if isinstance(val, CallableProxy):
+                    val = val()
+                if isinstance(val, markupsafe.Markup):
+                    # We just double-escaped all entities...
+                    escaped = re.sub(r'&amp;([a-zA-Z0-9.\-_\:]+;)', r'&\1', val)
+                text += '="{}"'.format(escaped)
+    if start_end:
+        text += ' /'
+    text += '>'
+    return flask.Markup(text)
+
+
+def file_fingerprint(fullpath):
+    """ Get a metadata fingerprint for a file """
+    stat = os.stat(fullpath)
+    return ','.join([str(value) for value in [stat.st_ino, stat.st_mtime, stat.st_size] if value])
+
+
+def remap_args(input_args, remap):
+    """ Generate a new argument list by remapping keys. The 'remap'
+    dict maps from destination key -> priority list of source keys
+    """
+    out_args = input_args
+    for dest_key, src_keys in remap.items():
+        remap_value = None
+
+        for key in as_list(src_keys):
+            if key in input_args:
+                remap_value = input_args[key]
+                break
+
+        if remap_value is not None:
+            if out_args is input_args:
+                out_args = {**input_args}
+            out_args[dest_key] = remap_value
+
+    return out_args
+
+
+def remap_link_target(path, absolute=False):
+    """ remap a link target to a static URL if it's prefixed with @ """
+    if path.startswith('@'):
+        # static resource
+        return static_url(path[1:], absolute=absolute)
+
+    if absolute:
+        # absolute-ify whatever the URL is
+        return urllib.parse.urljoin(flask.request.url, path)
+
+    return path
+
+
+def get_category(filename):
+    """ Get a default category name from a filename in a cross-platform manner """
+    return '/'.join(os.path.dirname(filename).split(os.sep))
+
+
+class HTMLTransform(html.parser.HTMLParser):
+    """ Wrapper to HTMLParser to make it easier to build a SAX-style processor.
+
+    You will probably want to implement:
+        handle_starttag(self, tag, attrs)
+        handle_endtag(self, tag)
+        handle_data(self, data)
+        handle_startendtag(self, tag, attrs)
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = False
+        self._fed = []
+
+    def append(self, item):
+        """ Append some text to the output """
+        self._fed.append(item)
+
+    def get_data(self):
+        """ Concatenate the output """
+        return ''.join(self._fed)
+
+    def handle_entityref(self, name):
+        self.handle_data('&' + name + ';')
+
+    def handle_charref(self, name):
+        self.handle_data('&#' + name + ';')
+
+    def error(self, message):
+        """ Deprecated, per https://bugs.python.org/issue31844 """
+        return message
+
+
+def prefix_normalize(kwargs):
+    """ Given an argument list where one of them is 'prefix', normalize the
+    arguments to convert {prefix}{key} to {key} and remove the prefixed versions
+    """
+
+    prefixed = {}
+    removed = []
+    if 'prefix' in kwargs:
+        for prefix in as_list(kwargs.get('prefix')):
+            for k, val in kwargs.items():
+                if k.startswith(prefix):
+                    prefixed[k[len(prefix):]] = val
+                    removed.append(k)
+
+    normalized = {**kwargs, **prefixed}
+    for k in removed:
+        normalized.pop(k, None)
+    return normalized
+
+
+def is_list(item):
+    """ Return if this is a list-type thing """
+    return isinstance(item, (list, tuple, set))
+
+
+def as_list(item):
+    """ Return list-type things directly; convert other things into a list """
+    if item is None:
+        return []
+
+    if is_list(item):
+        return item
+
+    return [item]
+
+
+class CategoryConverter(werkzeug.routing.PathConverter):
+    """ A version of PathConverter that doesn't accept paths beginning with _ """
+
+    def to_python(self, value):
+        if value[0] == '_':
+            raise werkzeug.routing.ValidationError
+        return super().to_python(value)
+
+
+class TemplateConverter(werkzeug.routing.UnicodeConverter):
+    """ A version of UnicodeConverter that doesn't accept strings beginning with _ """
+
+    def to_python(self, value):
+        if value[0] == '_':
+            raise werkzeug.routing.ValidationError
+        return super().to_python(value)
+
+
+def redir_path(path=None):
+    """ Convert a URI path to a path fragment, suitable for url_for
+
+    :param path: The path to redirect to; uses the current request.full_path if
+        unspecified
+    """
+
+    if path is None:
+        path = flask.request.full_path
+    else:
+        path = str(path)
+
+    # strip off leading slashes
+    path = re.sub(r'^/*', r'', path)
+    # if there's a trailing ? strip that off too
+    path = re.sub(r'\?$', r'', path)
+
+    return path
+
+
+def secure_link(endpoint, *args, **kwargs):
+    """ flask.url_for except it will force the link to be secure if we are
+    configured with AUTH_FORCE_HTTPS """
+    force_ssl = config.auth.get('AUTH_FORCE_HTTPS')
+
+    if force_ssl and flask.request.scheme != 'https':
+        kwargs = {**kwargs,
+                  '_external': True,
+                  '_scheme': 'https'}
+    return flask.url_for(endpoint, *args, **kwargs)
+
+
+def auth_link(endpoint):
+    """ Generates a function that maps an optional redir parameter to the
+    specified endpoint. """
+    def endpoint_link(redir=None, **kwargs):
+        LOGGER.debug("Getting %s for redir=%s kwargs=%s", endpoint, redir, kwargs)
+        redir = redir_path(redir)
+
+        return secure_link(endpoint, redir=redir, **kwargs)
+
+    return CallableProxy(endpoint_link)
+
+
+def stash(key: str) -> typing.Callable:
+    """ Decorator to memoize a function onto the global context.
+
+    :param str key: The memoization key
+    """
+
+    def decorator(func: typing.Callable) -> typing.Callable:
+        def wrapped_func(*args, **kwargs):
+            if key in flask.g:
+                return flask.g.get(key)
+            val = func(*args, **kwargs)
+            setattr(flask.g, key, val)
+            return val
+        return wrapped_func
+    return decorator
