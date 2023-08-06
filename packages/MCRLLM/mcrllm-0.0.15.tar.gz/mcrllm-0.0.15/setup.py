7c9fad937e6ce38309730398fd9ad151a052d982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:05 +0000

    Add package MCRLLM/0.0.15/mcrllm-0.0.15.tar.gz

---
+++ packages/MCRLLM/mcrllm-0.0.15.tar.gz/mcrllm-0.0.15/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="mcrllm",
+    version="0.0.15",
+    author="Ryan Gosselin",
+    author_email="ryan.gosselin@usherbrooke.ca",
+    packages=["mcrllm"],
+    description="MCRLLM: Multivariate Curve Resolution by Log-Likelihood Maximization",
+    long_description="MCRLLM: Multivariate Curve Resolution by Log-Likelihood Maximization. \
+\nLavoie F.B., Braidy N. and Gosselin R. (2016) Including Noise Characteristics in MCR to improve Mapping and Component Extraction from Spectral Images, Chemometrics and Intelligent Laboratory Systems, 153, 40-50.",
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
