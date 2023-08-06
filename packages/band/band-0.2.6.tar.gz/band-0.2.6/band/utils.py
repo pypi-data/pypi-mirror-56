1d2b5e9d8976bf23d2eb6699f92ce4ca107598c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:51 +0000

    Add package band/0.2.6/band-0.2.6.tar.gz

---
+++ packages/band/band-0.2.6.tar.gz/band-0.2.6/band/utils.py
@@ -0,0 +1,51 @@
+"""
+@author: SunYanCN
+@contact: sunyanhust@163.com
+@blog: https://sunyancn.github.io
+@version: 1.0
+@license: MIT Licence
+@file: utils.py
+@time: 2019-11-24 19:13:09
+"""
+import numpy as np
+from tqdm import tqdm
+
+
+def text_length_info(texts: list, language='zh', char_level=True, tokenizer=None):
+    data_number = len(texts)
+    if char_level:
+        length = [len(t) for t in texts]
+    else:
+        if language == 'zh' and tokenizer is None:
+            import logging
+            import jieba
+            jieba.setLogLevel(logging.INFO)
+            length = [len(jieba.lcut(t)) for t in tqdm(texts)]
+
+        elif language == 'en' and (tokenizer is None or tokenizer == 'spacy'):
+            import spacy
+            try:
+                nlp = spacy.load('en')
+            except OSError:
+                nlp = spacy.load('en_core_web_sm')
+            length = [len(list(t)) for t in tqdm(nlp.pipe(texts, batch_size=1000, n_threads=5))]
+        elif tokenizer == 'nltk':
+            import nltk
+            length = [len(nltk.word_tokenize(t)) for t in tqdm(texts)]
+        else:
+            raise Exception("Unsupported tokenizer!")
+
+    max_len, min_len = max(length), min(length)
+    mean_len = int(np.mean(length))
+    recommended_len = int(np.mean(length) + 2 * np.std(length))
+    return [data_number, max_len, min_len, mean_len, recommended_len]
+
+
+if __name__ == '__main__':
+    zh_text = ["我爱中国。"]
+    en_text = ["ILove China."]
+    print(text_length_info(zh_text, language='zh', char_level=True))
+    print(text_length_info(zh_text, language='zh', char_level=False))
+    print(text_length_info(en_text, language='en', char_level=True))
+    print(text_length_info(en_text, language='en', char_level=False ,tokenizer='spacy'))
+    print(text_length_info(en_text, language='en', char_level=False, tokenizer='nltk'))
