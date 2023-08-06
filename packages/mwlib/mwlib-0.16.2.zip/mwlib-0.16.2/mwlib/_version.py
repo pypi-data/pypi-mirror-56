b953ab19c3d70a37891931f0439ecb16ca8b5e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:06 +0000

    Add package mwlib/0.16.2/mwlib-0.16.2.zip

---
+++ packages/mwlib/mwlib-0.16.2.zip/mwlib-0.16.2/mwlib/_version.py
@@ -0,0 +1,13 @@
+__version_info__ = (0, 16, 2)
+display_version = version = __version__ = "0.16.2"
+gitid = gitversion = ""
+
+
+def main():
+    import pkg_resources
+    for r in ("mwlib", "mwlib.rl", "mwlib.ext", "mwlib.hiq"):
+        try:
+            v = pkg_resources.require(r)[0].version
+            print r, v
+        except BaseException:
+            continue
