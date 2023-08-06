372ad590c6fedfb156e690c4fb8c50d8e7cd46da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:07 +0000

    Add package cadishi/1.1.0/cadishi-1.1.0.tar.gz

---
+++ packages/cadishi/cadishi-1.1.0.tar.gz/cadishi-1.1.0/scripts/pypi_upload_helper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#cd ..
+
+python setup.py clean
+
+/opt/apps/anaconda/3/current/bin/python3 setup.py sdist
+#/opt/apps/anaconda/3/4.2.0/bin/python3 setup.py bdist_wheel --universal
+
+#twine upload dist/*
+
