572b248f85923466c5375416c3da8929d2cdce3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:29 +0000

    Add package sequana-fastqc/0.8.0/sequana_fastqc-0.8.0.tar.gz

---
+++ packages/sequana-fastqc/sequana_fastqc-0.8.0.tar.gz/sequana_fastqc-0.8.0/test/test_main.py
@@ -0,0 +1,25 @@
+import easydev
+import os
+import tempfile
+import subprocess
+import sys
+
+
+sequana_path = easydev.get_package_location('sequana_fastqc')
+sharedir = os.sep.join([sequana_path , "sequana", 'data'])
+
+
+def test_standalone_subprocess():
+    directory = tempfile.TemporaryDirectory()
+    cmd = "sequana_pipelines_fastqc --fastq-directory {} "
+    cmd += "--output-directory {} --run-mode local --force"
+    cmd = cmd.format(sharedir, directory.name)
+    subprocess.call(cmd.split())
+
+
+def test_standalone_script():
+    directory = tempfile.TemporaryDirectory()
+    import sequana_pipelines.fastqc.main as m
+    sys.argv = ["test", "--fastq-directory", sharedir, "--output-directory",
+        directory.name, "--run-mode", "local", "--force"]
+    m.main()
