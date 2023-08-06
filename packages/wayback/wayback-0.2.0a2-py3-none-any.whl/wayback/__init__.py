a9515a4b35ab214df68a74adcd002b3728f9ee72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:59 +0000

    Add package wayback/0.2.0a2/wayback-0.2.0a2-py3-none-any.whl

---
+++ packages/wayback/wayback-0.2.0a2-py3-none-any.whl/wayback/__init__.py
@@ -0,0 +1,8 @@
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+from ._client import (  # noqa
+    memento_url_data,
+    WaybackClient,
+    WaybackSession)
