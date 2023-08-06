e5b18c1851d8c4ec14835ec7b28b41f5251f5d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:37 +0000

    Add package easy-ml/0.1/easy_ml-0.1.tar.gz

---
+++ packages/easy-ml/easy_ml-0.1.tar.gz/easy_ml-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='easy_ml',
+      version='0.1',
+      description='Easy way to use ML Distributions',
+      packages=['easy_ml'],
+      author='Sparsh Srivastava',
+      author_email='mailtosparshsri@gmail.com',
+      zip_safe=False)
