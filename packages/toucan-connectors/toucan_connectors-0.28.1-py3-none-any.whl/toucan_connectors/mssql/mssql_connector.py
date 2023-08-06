073e423481e02297bb22d8a6572dd11ae117bdbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:55 +0000

    Add package toucan-connectors/0.28.1/toucan_connectors-0.28.1-py3-none-any.whl

---
+++ packages/toucan-connectors/toucan_connectors-0.28.1-py3-none-any.whl/toucan_connectors/mssql/mssql_connector.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import pymssql
+from pydantic import constr
+
+from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
+
+
+class MSSQLDataSource(ToucanDataSource):
+    # By default SQL Server selects the database which is set
+    # as default for specific user
+    database: str = None
+    query: constr(min_length=1)
+
+
+class MSSQLConnector(ToucanConnector):
+    """
+    Import data from Microsoft SQL Server.
+    """
+
+    data_source_model: MSSQLDataSource
+
+    host: str
+    user: str
+    password: str = None
+    port: int = None
+    connect_timeout: int = None
+
+    def get_connection_params(self, database):
+        con_params = {
+            'server': self.host,
+            'user': self.user,
+            'database': database,
+            'password': self.password,
+            'port': self.port,
+            'login_timeout': self.connect_timeout,
+            'as_dict': True,
+        }
+        # remove None values
+        return {k: v for k, v in con_params.items() if v is not None}
+
+    def _retrieve_data(self, datasource):
+        connection = pymssql.connect(**self.get_connection_params(datasource.database))
+        df = pd.read_sql(datasource.query, con=connection)
+
+        connection.close()
+        return df
---
+++ packages/toucan-connectors/toucan_connectors-0.28.1-py3-none-any.whl/toucan_connectors/postgres/postgresql_connector.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import psycopg2 as pgsql
+from pydantic import constr
+
+from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
+
+
+class PostgresDataSource(ToucanDataSource):
+    database: str
+    query: constr(min_length=1)
+
+
+class PostgresConnector(ToucanConnector):
+    """
+    Import data from PostgreSQL.
+    """
+
+    data_source_model: PostgresDataSource
+
+    user: str
+    host: str = None
+    hostname: str = None
+    charset: str = None
+    password: str = None
+    port: int = None
+    connect_timeout: int = None
+
+    def get_connection_params(self, *, database='postgres'):
+        con_params = dict(
+            user=self.user,
+            host=self.host if self.host else self.hostname,
+            client_encoding=self.charset,
+            dbname=database,
+            password=self.password,
+            port=self.port,
+            connect_timeout=self.connect_timeout,
+        )
+        # remove None values
+        return {k: v for k, v in con_params.items() if v is not None}
+
+    def _retrieve_data(self, data_source):
+        connection = pgsql.connect(**self.get_connection_params(database=data_source.database))
+
+        query_params = data_source.parameters or {}
+        df = pd.read_sql(data_source.query, con=connection, params=query_params)
+
+        connection.close()
+
+        return df
