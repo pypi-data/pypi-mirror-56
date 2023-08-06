efcb07fc0be2099a534ea4c21935fa20acbb9946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:17 +0000

    Add package shaonutil/0.0.0.2.1/shaonutil-0.0.0.2.1.tar.gz

---
+++ packages/shaonutil/shaonutil-0.0.0.2.1.tar.gz/shaonutil-0.0.0.2.1/shaonutil/__init__.py
@@ -0,0 +1,6 @@
+#from shaonutil.stats import ClassA
+import shaonutil.stats
+import shaonutil.network
+import shaonutil.image
+import shaonutil.string
+import shaonutil.file
---
+++ packages/shaonutil/shaonutil-0.0.0.2.1.tar.gz/shaonutil-0.0.0.2.1/shaonutil/stats.py
@@ -0,0 +1,5 @@
+def mean(li):
+	return sum(li)/len(li)
+
+if __name__ == '__main__':
+	pass
