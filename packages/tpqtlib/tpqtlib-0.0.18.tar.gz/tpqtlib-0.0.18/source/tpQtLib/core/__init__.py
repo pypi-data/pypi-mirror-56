602b89981eec90698799435240059c0f011a4619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:06 +0000

    Add package tpqtlib/0.0.18/tpqtlib-0.0.18.tar.gz

---
+++ packages/tpqtlib/tpqtlib-0.0.18.tar.gz/tpqtlib-0.0.18/source/tpQtLib/core/__init__.py
@@ -0,0 +1,16 @@
+order = [
+    'tpQtLib.core.color',
+    'tpQtLib.core.qtutils',
+    'tpQtLib.core.base',
+    'tpQtLib.core.animation',
+    'tpQtLib.core.settings',
+    'tpQtLib.core.style',
+    'tpQtLib.core.statusbar',
+    'tpQtLib.core.tooltips',
+    'tpQtLib.core.buttons',
+    'tpQtLib.core.dragger',
+    'tpQtLib.core.theme',
+    'tpQtLib.core.dialog',
+    'tpQtLib.core.window',
+
+]
