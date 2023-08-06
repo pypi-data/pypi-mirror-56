0b027cb9ff7952cf709c95f04c00923c298fca65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:20 +0000

    Add package coc.py/0.3.1/coc.py-0.3.1.tar.gz

---
+++ packages/coc.py/coc.py-0.3.1.tar.gz/coc.py-0.3.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import os
+
+REQUIREMENTS = []
+with open(os.path.join(os.getcwd(), "requirements.txt")) as f:
+    REQUIREMENTS = f.read().splitlines()
+
+VERSION = "0.3.1"
+
+README = ""
+with open("README.rst") as f:
+    README = f.read()
+
+setup(
+    name="coc.py",
+    author="mathsman5133",
+    url="https://github.com/mathsman5133/coc.py",
+    packages=["coc"],
+    version=VERSION,
+    license="MIT",
+    description="A python wrapper for the Clash of Clans API",
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    python_requires=">=3.5.3",
+    install_requires=REQUIREMENTS,
+    extra_requires={"docs": ["sphinx==1.7.4", "sphinx_rtd_theme"], "lint": ["pylint"]},
+    classifiers={
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    },
+)
