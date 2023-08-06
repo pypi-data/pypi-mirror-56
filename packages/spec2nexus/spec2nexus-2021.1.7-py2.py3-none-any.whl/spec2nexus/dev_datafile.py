7d2e77391001e85efb64d8c6bdaf83d39d444910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:01 +0000

    Add package spec2nexus/2021.1.7/spec2nexus-2021.1.7-py2.py3-none-any.whl

---
+++ packages/spec2nexus/spec2nexus-2021.1.7-py2.py3-none-any.whl/spec2nexus/dev_datafile.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Process specific data file(s)"""
+
+# TODO: fold into the unit test suite
+
+
+import os
+from spec2nexus import writer
+from spec2nexus import spec
+
+
+if __name__ == '__main__':
+    pwd = os.path.abspath(os.getcwd())
+    prefix = os.path.abspath(os.path.join(pwd, 'data', 'APS_spec_data'))
+    # file1 = prefix + '.dat'
+    hfile = prefix + '.hdf5'
+    # # lmn40.spe has two header sections, 2nd is just before "#S 8"
+    # prefix = os.path.abspath(os.path.join(pwd, 'data', 'lmn40'))
+    # file1 = prefix + '.spe'
+
+    # CdOsO has four header sections and two #S 1 scans
+    prefix = os.path.abspath(os.path.join(pwd, 'data', 'CdOsO'))
+    file1 = prefix
+
+    # writer interface has changed, must use new spec module to proceed
+    specfile = spec.SpecDataFile(file1)
+    writer = writer.Writer(specfile)
+    writer.save(hfile, sorted(specfile.getScanNumbers()))
---
+++ packages/spec2nexus/spec2nexus-2021.1.7-py2.py3-none-any.whl/spec2nexus/dev_extractSpecScan.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+"""developer test for extractSpecScan"""
+
+# TODO: fold into the unit test suite
+
+#-----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     prjemian@gmail.com
+# :copyright: (c) 2014-2019, Pete R. Jemian
+#
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+import sys
+from spec2nexus import extractSpecScan
+
+#args = 'data/APS_spec_data.dat -s 1 6   -c mr USAXS_PD I0 seconds'
+#args = 'data/33id_spec.dat     -s 1 6   -c H K L signal elastic I0 seconds'
+#args = 'data/CdOsO     -s 1 1.1 48   -c HerixE H K L T_control_LS340  T_sample_LS340 ICO-C  PIN-D  PIN-C Seconds'
+# args = 'data/02_03_setup.dat     -s 46  1-3   -c ar  ay  dy  Epoch  seconds  I0  USAXS_PD'
+# args = 'data/02_03_setup.dat     -s 47   -c mr seconds  I0  USAXS_PD'
+args = 'data/xpcs_plugin_sample.spec  -s 7   -c img_n  Epoch  ccdc'
+
+for _ in args.split():
+    sys.argv.append(_)
+
+sys.argv.append('-G')
+sys.argv.append('-V')
+sys.argv.append('-Q')
+sys.argv.append('-P')
+extractSpecScan.main()
