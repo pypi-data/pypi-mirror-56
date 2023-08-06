4fef8b84839b8d78171d616cbbfe944d401d967f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:42 +0000

    Add package voltools/0.2.2/voltools-0.2.2.tar.gz

---
+++ packages/voltools/voltools-0.2.2.tar.gz/voltools-0.2.2/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+# version fetch
+from voltools import __version__
+
+# readme fetch
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='voltools',
+    version=__version__,
+    description='CUDA-accelerated 3D affine transformations for NumPy and CuPy',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='the-lay',
+    author_email='ilja.gubin@gmail.com',
+    url='https://github.com/the-lay/voltools',
+    platforms=['any'],
+    install_requires=[
+        #'cupy>=7.0.0b4',
+        'numpy',
+        #'scikit-cuda',
+        'aenum'
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+    ]
+)
