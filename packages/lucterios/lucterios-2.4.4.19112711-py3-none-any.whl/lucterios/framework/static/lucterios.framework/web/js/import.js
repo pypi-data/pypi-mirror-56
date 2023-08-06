5ca766a40f069bfaade174f85a1db7394b81ad30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:12 +0000

    Add package lucterios/2.4.4.19112711/lucterios-2.4.4.19112711-py3-none-any.whl

---
+++ packages/lucterios/lucterios-2.4.4.19112711-py3-none-any.whl/lucterios/framework/static/lucterios.framework/web/js/import.js
@@ -0,0 +1,8 @@
+/*global head */
+
+head.js("lib/jquery.min.js", "lib/jquery-ui.min.js", "lib/jquery.ui.touch-punch.min.js", "lib/jquery.class.min.js", "lib/jquery-te-1.4.0.min.js", "lib/jquery.splitter-0.14.0.js",
+		"lib/jquery.contextMenu.min.js", "lib/jquery.cookie.js", "lib/deflate.min.js", "lib/inflate.min.js", "lib/zip.min.js",
+
+		"js/utils.js", "js/language.js", "js/gui.utils.js", "js/singleton.js", "js/transport.js", "js/observer_factory.js", "js/action.js",
+		"js/observer.js", "js/observer.acknowledge.js", "js/observer.authentification.js", "js/observer.exception.js", "js/observer.print.js",
+		"js/observer.dialogbox.js", "js/observer.custom.js", "js/observer.custom.comp.js", "js/observer.custom.grid.js", "js/observer.menu.js");
