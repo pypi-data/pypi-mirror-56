424df16d458e1617e9ed7d6e6271b95c3942fe35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:14 +0000

    Add package pipelinewise-tap-kafka/1.0.2/pipelinewise-tap-kafka-1.0.2.tar.gz

---
+++ packages/pipelinewise-tap-kafka/pipelinewise-tap-kafka-1.0.2.tar.gz/pipelinewise-tap-kafka-1.0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pipelinewise-tap-kafka',
+      version='1.0.2',
+      description='Singer.io tap for extracting data from Kafka topic - PipelineWise compatible',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=[
+          'License :: OSI Approved :: GNU Affero General Public License v3',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
+      install_requires=[
+          'kafka-python',
+          'singer-python==5.2.0',
+          'requests==2.20.0',
+	      'strict-rfc3339==0.7',
+	      'nose==1.3.7',
+          'jsonschema==2.6.0',
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-kafka=tap_kafka:main
+      ''',
+      packages=['tap_kafka']
+)
