8449654b96b5f30a81108f74c487f26a85fcf31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:56 +0000

    Add package google-ime-skk-py/0.0.2/google_ime_skk_py-0.0.2-py2.py3-none-any.whl

---
+++ packages/google-ime-skk-py/google_ime_skk_py-0.0.2-py2.py3-none-any.whl/google_ime_skk_py/__version__.py
@@ -0,0 +1,6 @@
+__title__ = 'google-ime-skk-py'
+__description__ = 'Google IME SKK server written in Python'
+__url__ = 'https://github.com/chaploud/google-ime-skk-py'
+__version__ = '0.0.2'
+__author__ = 'chaploud'
+__license__ = 'MIT'
