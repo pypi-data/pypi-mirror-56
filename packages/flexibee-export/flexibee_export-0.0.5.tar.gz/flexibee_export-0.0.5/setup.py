ce299d7aba0bda687cdfec73bac59637c840891a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:59 +0000

    Add package flexibee-export/0.0.5/flexibee_export-0.0.5.tar.gz

---
+++ packages/flexibee-export/flexibee_export-0.0.5.tar.gz/flexibee_export-0.0.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flexibee_export",
+    version="0.0.5",
+    author="Endevel",
+    author_email="info@endevel.cz",
+    description="FlexiBee xml export package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EndevelCZ/flexibee-export.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
