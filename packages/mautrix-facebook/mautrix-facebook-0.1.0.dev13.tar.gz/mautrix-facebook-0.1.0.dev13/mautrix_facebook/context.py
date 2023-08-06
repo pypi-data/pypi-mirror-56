dce1b2a7f9efb88ee336de1ac51ffec886785543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:28 +0000

    Add package mautrix-facebook/0.1.0.dev13/mautrix-facebook-0.1.0.dev13.tar.gz

---
+++ packages/mautrix-facebook/mautrix-facebook-0.1.0.dev13.tar.gz/mautrix-facebook-0.1.0.dev13/mautrix_facebook/context.py
@@ -0,0 +1,41 @@
+# mautrix-facebook - A Matrix-Facebook Messenger puppeting bridge
+# Copyright (C) 2019 Tulir Asokan
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from typing import Optional, Tuple, TYPE_CHECKING
+from asyncio import AbstractEventLoop
+
+from mautrix.appservice import AppService
+
+from .config import Config
+
+if TYPE_CHECKING:
+    from .matrix import MatrixHandler
+
+
+class Context:
+    az: AppService
+    config: Config
+    loop: AbstractEventLoop
+    mx: Optional['MatrixHandler']
+
+    def __init__(self, az: AppService, config: Config, loop: AbstractEventLoop) -> None:
+        self.az = az
+        self.config = config
+        self.loop = loop
+        self.mx = None
+
+    @property
+    def core(self) -> Tuple[AppService, Config, AbstractEventLoop]:
+        return self.az, self.config, self.loop
