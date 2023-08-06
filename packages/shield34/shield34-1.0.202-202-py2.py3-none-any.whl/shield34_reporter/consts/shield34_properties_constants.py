52e77f9c9226896a75220e5f4683fdbfb1cd9855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:48 +0000

    Add package shield34/1.0.202/shield34-1.0.202-202-py2.py3-none-any.whl

---
+++ packages/shield34/shield34-1.0.202-202-py2.py3-none-any.whl/shield34_reporter/consts/shield34_properties_constants.py
@@ -0,0 +1,5 @@
+class Shield34PropertiesConstants():
+    PROP_REPORTS_SECTION = "config"
+    PROP_REPORTS_API_KEY = "reports.api.key"
+    PROP_REPORTS_API_SECRET = "reports.api.secret"
+    PROP_REPORTS_API_BASE_URL = "reports.api.base-url"
