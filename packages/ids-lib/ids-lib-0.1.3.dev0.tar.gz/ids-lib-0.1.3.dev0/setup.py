e0e0f739458dbb902c4143b2fb48de03a0c3c2df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:04 +0000

    Add package ids-lib/0.1.3.dev0/ids-lib-0.1.3.dev0.tar.gz

---
+++ packages/ids-lib/ids-lib-0.1.3.dev0.tar.gz/ids-lib-0.1.3.dev0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="ids-lib",
+    version="0.1.3-dev0",
+    author="dnk0 <Dennis Kreußel>",
+    author_email="dnk0@protonmail.com",
+    description="Common functionality and preprocessing for intrusion detection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    install_requires=requirements,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
+
