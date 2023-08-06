a39cfa3aa801d508096536281552d905406f8541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:11 +0000

    Add package senti-news/0.0.16/senti-news-0.0.16.tar.gz

---
+++ packages/senti-news/senti-news-0.0.16.tar.gz/senti-news-0.0.16/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requirements = [
+    'sqlalchemy',
+    'cytoolz',
+    'numpy',
+    'keras',
+    'spacy',
+    'textblob',
+    'vaderSentiment',
+    'pandas',
+    'newsapi',
+    'python-dateutil',
+    'requests',
+    'bs4',
+    'scrapy',
+]
+
+setuptools.setup(
+    name="senti-news",
+    version="0.0.16",
+    author="Nicholas Broad",
+    author_email="nicholas@nmbroad.com",
+    description="News title sentiment analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nbroad1881/senti-news",
+    packages=setuptools.find_packages(where='src/'),
+    package_dir={'': 'src'},
+    requirements=install_requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
