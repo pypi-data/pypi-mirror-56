2678b8cf1f0b98f696d89a283a998679282ee1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:24 +0000

    Add package sentry-databricks/0.1/sentry-databricks-0.1.tar.gz

---
+++ packages/sentry-databricks/sentry-databricks-0.1.tar.gz/sentry-databricks-0.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sentry_databricks', 'sentry_databricks.daemon', 'sentry_databricks.notebook']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['sentry-sdk>=0.13.0,<0.14.0']
+
+setup_kwargs = {
+    'name': 'sentry-databricks',
+    'version': '0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Jiri Koutny',
+    'author_email': 'jiri.koutny@datasentics.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+}
+
+
+setup(**setup_kwargs)
